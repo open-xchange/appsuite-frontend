bca2d3b214e75d91ab90f2dda72197a374f39200
biggeleben <matthias.biggeleben@open-xchange.com>
2011-08-22 08:35:20 +0200

    Added LFO to file/main.js

--- ui/apps/io.ox/files/main.js
+++ ui/apps/io.ox/files/main.js
@@ -72,6 +72,7 @@
             api.getList(ids)
                 .done(cont);
         });
+        
         // go!
         api.defaultFolder().done(function (folder) {
             currentFolder = folder;
@@ -81,20 +82,34 @@
             });
             win.show();
         });
+        
+        // hi cisco!
+        // kleine Änderung von mir - das erkläre ich vor ort
+        // es gibt die LFO function - Latest Function Only :)
+        // die braucht man, wenn man schnell durch Grid läufts und Detais geladen werden.
+        // Sonst ist es Lotto, welche Response als letztes malt.
+        
+        // LFO callback
+        function drawDetail(data) {
+            var detail = base.draw(data);
+            right.idle().empty().append(detail);
+            right.parent().scrollTop(0);
+        }
 
         grid.selection.bind("change", function (selection) {
             if (selection.length === 1) {
                 // get file
                 right.busy(true);
-                api.get(selection[0].folder_id, selection[0].id)
-                .done(function (data) {
-                    // draw file
-                    right.empty().append(base.draw(data));
-                });
+                api.get(selection[0].folder_id, selection[0].id) // evil! :)
+                .done(ox.util.lfo(drawDetail)); // LFO!
             } else {
                 right.empty();
             }
         });
+        
+        // explicit keyboard support
+        win.bind("show", function () { grid.selection.keyboard(true); });
+        win.bind("hide", function () { grid.selection.keyboard(false); });
     });
     
     return {
