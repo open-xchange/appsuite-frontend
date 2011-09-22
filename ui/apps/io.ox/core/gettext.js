c5f1c83d04beb7ca59f41b3df805aad1e9a2e480
Viktor Pracht <viktor.pracht@open-xchange.com>
2011-09-22 13:51:43 +0200

    Fixed trailing whitespace

--- ui/apps/io.ox/core/gettext.js
+++ ui/apps/io.ox/core/gettext.js
@@ -2,12 +2,12 @@
  * All content on this website (including text, images, source
  * code and any other original works), unless otherwise noted,
  * is licensed under a Creative Commons License.
- * 
+ *
  * http://creativecommons.org/licenses/by-nc-sa/2.5/
- * 
+ *
  * Copyright (C) Open-Xchange Inc., 2011
- * Mail: info@open-xchange.com 
- * 
+ * Mail: info@open-xchange.com
+ *
  * @author Viktor Pracht <viktor.pracht@open-xchange.com>
  */
 
