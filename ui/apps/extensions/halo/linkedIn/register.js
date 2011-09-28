54e0fff10610dcc8b1e67dbf9cb54265c65b0576
Francisco Laguna <francisco.laguna@open-xchange.com>
2011-09-28 15:09:02 +0200

    minor changes

--- ui/apps/extensions/halo/linkedIn/register.js
+++ ui/apps/extensions/halo/linkedIn/register.js
@@ -1,4 +1,12 @@
 define("extensions/halo/linkedIn/register", ["io.ox/core/extensions"], function (ext) {
-    
-    // TODO
+    ext.point("io.ox/halo/contact:renderer").extend({
+        handles: function (type) {
+            return type === "com.openexchange.halo.linkedIn.fullProfile";
+        },
+        draw: function  ($node, providerName, liResponse) {
+            require(["extensions/halo/linkedIn/view-halo", "css!extensions/halo/linkedIn/style.css"], function (base) {
+                $node.append(base.draw(contacts[0]));
+            });
+        }
+    });
 });
