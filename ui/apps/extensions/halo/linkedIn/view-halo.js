e34007e578b23c1e9fbffd858f7d0555a30e97df
Francisco Laguna <francisco.laguna@open-xchange.com>
2011-10-06 07:12:59 +0200

    bl

--- ui/apps/extensions/halo/linkedIn/view-halo.js
+++ ui/apps/extensions/halo/linkedIn/view-halo.js
@@ -22,11 +22,33 @@ define("extensions/halo/linkedIn/view-halo", ["io.ox/core/lightbox", "css!extens
                 });
                 return false;
             }
+            function todo() {
+                alert("todo");
+                return false;
+            }
 
             $pictureNode.append($("<img/>").attr("src", liResponse.pictureUrl));
             $nameNode.append($("<div class='name' />").text(liResponse.firstName + " " + liResponse.lastName));
             $nameNode.append($("<div class='headline' />").text(liResponse.headline));
-            $nameNode.append($("<a href='#' />").text("Details").click(openDetails));
+            $nameNode.append($("<a href='#' />").text("Connect").click(todo)).addClass("actions");
+            $nameNode.append($("<a href='#' />").text("Write Message").click(todo)).addClass("actions");
+            
+            
+            if (liResponse.positions && liResponse.positions.values && liResponse.positions.values.length !== 0) {
+                _(liResponse.positions.values).each(function (position) {
+                    var $posNode = $("<ul/>").appendTo($relationNode);
+                    if (position.isCurrent) {
+                        var $item = $("<li/>").appendTo($posNode);
+                        if (position.title) {
+                            $("<span/>").appendTo($item).text(position.title);
+                            $("<span/>").text(" at ").appendTo($item);
+                        }
+                        if (position.company && position.company.name) {
+                            $("<span/>").text(position.company.name).appendTo($item);
+                        }
+                    }
+                });
+            }
 
             $node.click(openDetails); // We don't care, where the user clicks in the LinkedIn area, we'll always open the details. The link is added only to entice the user to click
             return $node;
