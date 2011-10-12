3723ce982063ece836859a17b206a81b36a52696
christoph.kopp <christoph.kopp@open-xchange.com>
2011-10-12 12:13:24 +0200

    just corrected a bit

--- ui/apps/io.ox/contacts/view-detail.js
+++ ui/apps/io.ox/contacts/view-detail.js
@@ -7,7 +7,7 @@
  *
  * Copyright (C) Open-Xchange Inc., 2006-2011 Mail: info@open-xchange.com
  *
- * @author Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
+ * @author Christoph Kopp <christoph.kopp@open-xchange.com>
  */
 
 define("io.ox/contacts/view-detail", [
