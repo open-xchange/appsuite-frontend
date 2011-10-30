651fa8e8178cb26327284cf7553ae98a3ad8f998
biggeleben <matthias.biggeleben@open-xchange.com>
2011-10-30 14:11:19 +0100

    Support for multiple RSS feeds

--- ui/apps/extensions/portal/rss/register.js
+++ ui/apps/extensions/portal/rss/register.js
@@ -16,50 +16,82 @@ define("io.ox/portal/rss/register",
     
     "use strict";
     
-    ext.point("io.ox/portal/widget").extend({
-        id: "rss",
+    var feeds = [];
+    
+    // SPIEGEL Online
+    feeds.push({
+        id: "rss-spiegel",
+        url: "http://www.spiegel.de/schlagzeilen/tops/index.rss",
         index: 400,
-        load: function () {
-            // get 'spiegel' RSS via Google Feed API
-            var rss = "http://www.spiegel.de/schlagzeilen/tops/index.rss",
-                url = "http://ajax.googleapis.com/ajax/services/feed/" +
-                        "load?v=1.0&num=4&callback=?&q=" + encodeURIComponent(rss);
-            return $.getJSON(url)
-                .pipe(function (data) {
-                    return data && data.responseData ? data.responseData.feed : {};
-                });
-        },
-        draw: function (feed) {
-            
-            var self = this;
-            
-            this.addClass("io-ox-portal-rss")
-                .append(
-                    $("<div/>").addClass("clear-title")
-                        .text(feed.title || "RSS")
-                );
-            
-            _(feed.entries).each(function (entry) {
-                self.append(
-                    $("<div>").addClass("rss-entry")
-                    .append(
-                        $("<div>").addClass("rss-title").text(entry.title || "")
+        cssClass: "spiegel"
+    });
+    
+    // Engadget
+    feeds.push({
+        id: "rss-engadget",
+        url: "http://www.engadget.com/rss.xml",
+        index: 500
+    });
+    
+    _(feeds).each(function (extension) {
+    
+        ext.point("io.ox/portal/widget").extend({
+            id: extension.id,
+            index: extension.index,
+            load: function () {
+                // get RSS feed via Google Feed API
+                var url = "http://ajax.googleapis.com/ajax/services/feed/" +
+                    "load?v=1.0&num=4&callback=?&q=" + encodeURIComponent(extension.url);
+                return $.getJSON(url)
+                    .pipe(function (data) {
+                        return data && data.responseData ? data.responseData.feed : {};
+                    });
+            },
+            draw: function (feed) {
+                
+                var self = this;
+                
+                this.addClass(
+                        "io-ox-portal-rss" + (extension.cssClass ? " " + extension.cssClass : "")
                     )
                     .append(
-                        $("<div>").addClass("rss-content").html(entry.content || "")
-                        .find("img").removeAttr("hspace vspace align")
-                        .end()
-                        .append($.txt(" "))
+                        $("<div/>").addClass("clear-title")
+                            .text(feed.title || "RSS")
+                    );
+                
+                _(feed.entries).each(function (entry) {
+                    self.append(
+                        $("<div>").addClass("rss-entry")
                         .append(
-                            $("<a>", { href: entry.link, target: "_blank"})
-                            .css("whiteSpace", "nowrap")
-                            .text("Read full article")
+                            $("<div>").addClass("rss-title").text(entry.title || "")
                         )
-                    )
-                );
-            });
-            
-            return $.Deferred().resolve();
-        }
+                        .append(
+                            $("<div>").addClass("rss-content").html(entry.content || "")
+                            .find("a")
+                                .attr("target", "_blank")
+                            .end()
+                            .find("img")
+                                .removeAttr("hspace vspace align height")
+                                .css("height", "auto")
+                            .end()
+                            .find("iframe")
+                                .attr("frameborder", "0")
+                                .attr("border", "0")
+                                .attr("width", "405")
+                                .attr("height", "230")
+                            .end()
+                            .append($.txt(" "))
+                            .append(
+                                $("<a>", { href: entry.link, target: "_blank"})
+                                .css("whiteSpace", "nowrap")
+                                .text("Read full article")
+                            )
+                        )
+                    );
+                });
+                
+                return $.Deferred().resolve();
+            }
+        });
     });
 });
