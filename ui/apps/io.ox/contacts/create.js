05489442086fc30b57a3f47bab721f2b908f0069
biggeleben <matthias.biggeleben@open-xchange.com>
2011-11-25 16:28:01 +0100

    Merging ...

--- ui/apps/io.ox/contacts/create.js
+++ ui/apps/io.ox/contacts/create.js
@@ -1,5 +1,4 @@
 /**
- *
  * All content on this website (including text, images, source
  * code and any other original works), unless otherwise noted,
  * is licensed under a Creative Commons License.
@@ -9,130 +8,130 @@
  * Copyright (C) Open-Xchange Inc., 2006-2011
  * Mail: info@open-xchange.com
  *
+ * @author Christoph Kopp <christoph.kopp@open-xchange.com>
  * @author Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
- *
  */
 
-define("io.ox/contacts/create",
-    ["io.ox/contacts/util", "io.ox/contacts/api",
-     "io.ox/core/tk/dialogs",
-     "io.ox/core/config",
-     "css!io.ox/contacts/style.css"
+define('io.ox/contacts/create',
+    ['io.ox/contacts/util',
+     'io.ox/contacts/api',
+     'io.ox/core/tk/dialogs',
+     'io.ox/core/config',
+     'css!io.ox/contacts/style.css'
     ], function (util, api, dialogs, config) {
 
-    "use strict";
+    'use strict';
 
-    function fieldHtml(label, id) {
-        return $('<div>').addClass('field').append($('<label>').text(label))
-        .append($('<input>', {'class': id, type: 'text'}));
+    function fieldHtml(label, name) {
+        return $('<div>').addClass('field')
+            .append($('<label>').text(label))
+            .append($('<input>', { type: 'text', name: name }));
     }
-    
+
     //assemble create form
-    var newCreatePane = function () {
-       
-        var pane = new dialogs.CreateDialog();
-        pane.getContentNode().addClass("create-contact");
-        pane.append(
-            $('<div>').addClass('block new_contact name')
-            .append(fieldHtml('first name', 'first_name'))
-            .append(fieldHtml('last name', 'last_name'))
-        );
+    var show = function () {
 
-        pane.append(
-            $('<div>').addClass('block new_contact company')
-            .append(fieldHtml('company', 'company'))
-            .append(fieldHtml('department', 'department'))
-            .append(fieldHtml('position', 'position'))
-            .append(fieldHtml('profession', 'profession'))
-        );
-        
-        pane.append(
-            $('<div>').addClass('block new_contact address')
-            .append(fieldHtml('street', 'street_business'))
-            .append(fieldHtml('postal code', 'postal_code_business'))
-            .append(fieldHtml('city', 'city_business'))
-        );
-        
-        pane.append(
-            $('<div>').addClass('block new_contact phone')
-            .append(fieldHtml('tel.', 'telephone_business1'))
-        );
-        
-        pane.append(
-            $('<div>').addClass('block new_contact image')
-            .append($('<form>').attr({
-            'accept-charset': 'UTF-8',
-            'enctype': 'multipart/form-data',
-            'id': 'contactUploadImage',
-            'method': 'POST',
-            'name': 'contactUploadImage',
-            'target': 'hiddenframePicture'
-        })
-            
-            .append($('<label>').text('contact image'))
-            .append($('<input>').attr({
-            'id': 'image1',
-            'name': 'file',
-            'type': 'file'
-        })
-            .append($('<iframe>').attr({
-            'name': 'hiddenframePicture',
-            'src': ox.base + '/apps/io.ox/contacts/newInfoItemHidden.html'
-        }).css('display', 'none')))
-        
-        ));
-        
-        pane.addButton("resolveNewContact", "Save");
-        pane.addButton("cancelNewContact", "Cancel");
+        var pane = new dialogs.CreateDialog(),
+            content = pane.getContentNode();
 
-        $(".content .block .field:nth-child(even)").addClass('even');
+        content
+            .addClass('create-contact')
+            .append(
+                $('<div>')
+                .addClass('block new_contact name')
+                .append(fieldHtml('first name', 'first_name'))
+                .append(fieldHtml('last name', 'last_name'))
+            )
+            .append(
+                $('<div>')
+                .addClass('block new_contact company')
+                .append(fieldHtml('company', 'company'))
+                .append(fieldHtml('department', 'department'))
+                .append(fieldHtml('position', 'position'))
+                .append(fieldHtml('profession', 'profession'))
+            )
+            .append(
+                $('<div>')
+                .addClass('block new_contact address')
+                .append(fieldHtml('street', 'street_business'))
+                .append(fieldHtml('postal code', 'postal_code_business'))
+                .append(fieldHtml('city', 'city_business'))
+            )
+            .append(
+                $('<div>')
+                .addClass('block new_contact phone')
+                .append(fieldHtml('tel.', 'telephone_business1'))
+            )
+            .append(
+                $('<div>')
+                .addClass('block new_contact image')
+                .append(
+                    $('<form>',
+                    {   method: 'POST',
+                        target: 'hiddenframePicture',
+                        enctype: 'multipart/form-data',
+                        'accept-charset': 'UTF-8'
+                    })
+                    .append(
+                        $('<label>').text('contact image')
+                    )
+                    .append(
+                        $('<input>', { name: 'image1', type: 'file' })
+                    )
+                    .append(
+                        $('<iframe>',
+                        {   name: 'hiddenframePicture',
+                            src: ox.base + '/apps/io.ox/contacts/newInfoItemHidden.html'
+                        })
+                        .css('display', 'none')
+                    )
+                )
+            );
 
-        var actions = {
+        content.find('.block .field:nth-child(even)').addClass('even');
 
-                resolveNewContact: function () {
-                    var fId = config.get("folder.contacts"),
-                    formdata = {},
-                    formdataString,
-                    image = $('form input#image1').val(),
-                    imagePur = document.getElementById("image1");
-                    
-//                  collect the data
-                    
-                    $(".content .new_contact input").each(function (index) {
-                        var value =  $(this).val();
-                        var id = $(this).attr('class');
-                        if (id !== undefined && value !== "") { // no need for filefield in jsonstring
-                            formdata[id] = value;
-                        }
-                    });
-                    
-                    if (image !== "") {
-                        formdata.folder_id = fId;
-                        formdata.display_name = util.createDisplayName(formdata);
-                        formdataString = JSON.stringify(formdata);
-                        api.createNewImage(formdataString, imagePur.files[0]);
-                    } else {
-                        if (!_.isEmpty(formdata)) {
-                            formdata.folder_id = fId;
-                            formdata.display_name = util.createDisplayName(formdata);
-                            api.create(formdata);
-                        }
-                    }
-                },
+        pane.addButton('cancel', 'Cancel');
+        pane.addButton('create', 'Save');
 
-                cancelNewContact: function () {
-                    pane.close();// TODO needs to close the dialog
+        var create = function () {
+
+            var fId = config.get('folder.contacts'),
+                data = {},
+                image = content.find('input[type=file][name=image1]').val();
+
+            // collect the data
+            content.find('input[type=text]').each(function () {
+                var node = $(this),
+                    name = node.attr('name'),
+                    value = $.trim(node.val());
+                if (value !== '') {
+                    data[name] = value;
                 }
-            };
+            });
+
+            if (image !== '') {
+                data.folder_id = fId;
+                data.display_name = util.createDisplayName(data);
+                api.createNewImage(JSON.stringify(data), image.get(0).files[0]);
+            } else {
+                if (!_.isEmpty(data)) {
+                    data.folder_id = fId;
+                    data.display_name = util.createDisplayName(data);
+                    api.create(data);
+                }
+            }
+        };
 
         pane.show().done(function (action) {
-            actions[action]();
-          // console.debug(actions);
+            if (action === 'create') {
+                create();
+            }
+            pane = content = create = null;
         });
     };
 
     return {
-        show: newCreatePane
+        show: show
     };
 
 });
