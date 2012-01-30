67aaa4494b6d47f613fbfef1c36e75f3e472b08f
Mario Scheliga <mario@sourcegarden.de>
2012-01-31 00:05:28 +0100

    kicked a oop.js and added _.makeExtendable - utility

--- ui/src/util.js
+++ ui/src/util.js
@@ -110,10 +110,9 @@
         }
     };
 
-    var Ctor = function () { };
-    var underscoreExt;
+
     // extend underscore utilities
-    _.mixin(underscoreExt = {
+    _.mixin({
 
         /**
          * Serialize object (key/value pairs) to fit into URLs (e.g. a=1&b=2&c=HelloWorld)
@@ -370,54 +369,57 @@
             return def;
         },
         // taken from backbone.js
-        inherits: function (parent, protoProps, staticProps) {
-            var child;
-
-            // The constructor function for the new subclass is either defined by you
-            // (the "constructor" property in your `extend` definition), or defaulted
-            // by us to simply call the parent's constructor.
-            if (protoProps && protoProps.hasOwnProperty('constructor')) {
-                child = protoProps.constructor;
-            } else {
-                child = function () {
-                    parent.apply(this, arguments);
-                };
-            }
-
-            // Inherit class (static) properties from parent.
-            _.extend(child, parent);
-
-            // Set the prototype chain to inherit from `parent`, without calling
-            // `parent`'s constructor function.
-            Ctor.prototype = parent.prototype;
-            child.prototype = new Ctor();
-
-            // Add prototype properties (instance properties) to the subclass,
-            // if supplied.
-            if (protoProps) {
-                _.extend(child.prototype, protoProps);
-            }
-
-            // Add static properties to the constructor function, if supplied.
-            if (staticProps) {
-                _.extend(child, staticProps);
-            }
-
-            // Correctly set child's `prototype.constructor`.
-            child.prototype.constructor = child;
-
-            // Set a convenience property in case the parent's prototype is needed later.
-            child.__super__ = parent.prototype;
-
-            return child;
-        },
         makeExtendable: function (parent) {
-            parent.extend = function (protoProps, classProps) {
-                var child = underscoreExt.inherits(this, protoProps, classProps);
-                child.extend = this.extend;
+            var Ctor = function () { };
+            var inherits = function (parent, protoProps, staticProps) {
+                var child;
+
+                // The constructor function for the new subclass is either defined by you
+                // (the "constructor" property in your `extend` definition), or defaulted
+                // by us to simply call the parent's constructor.
+                if (protoProps && protoProps.hasOwnProperty('constructor')) {
+                    child = protoProps.constructor;
+                } else {
+                    child = function () {
+                        parent.apply(this, arguments);
+                    };
+                }
+
+                // Inherit class (static) properties from parent.
+                _.extend(child, parent);
+
+                // Set the prototype chain to inherit from `parent`, without calling
+                // `parent`'s constructor function.
+                Ctor.prototype = parent.prototype;
+                child.prototype = new Ctor();
+
+                // Add prototype properties (instance properties) to the subclass,
+                // if supplied.
+                if (protoProps) {
+                    _.extend(child.prototype, protoProps);
+                }
+
+                // Add static properties to the constructor function, if supplied.
+                if (staticProps) {
+                    _.extend(child, staticProps);
+                }
+
+                // Correctly set child's `prototype.constructor`.
+                child.prototype.constructor = child;
+
+                // Set a convenience property in case the parent's prototype is needed later.
+                child.__super__ = parent.prototype;
+
                 return child;
             };
-            return parent;
+            return (function (parent) {
+                parent.extend = function (protoProps, classProps) {
+                    var child = inherits(this, protoProps, classProps);
+                    child.extend = this.extend;
+                    return child;
+                };
+                return parent;
+            } (parent));
         },
         // helper for benchmarking
         clock: (function () {
