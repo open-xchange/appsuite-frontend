139333b960cf256b493b81517fd77b13ebbf061d
Mario Scheliga <mario@sourcegarden.de>
2012-01-30 11:00:01 +0100

    just added contact model

---
+++ ui/apps/io.ox/contacts/model.js
@@ -0,0 +1,21 @@
+/**
+ * All content on this website (including text, images, source
+ * code and any other original works), unless otherwise noted,
+ * is licensed under a Creative Commons License.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * Copyright (C) Open-Xchange Inc., 2006-2012
+ * Mail: info@open-xchange.com
+ *
+ * @author Mario Scheliga <mario.scheliga@open-xchange.com>
+ */
+define('io.ox/contacts/model',
+      ['io.ox/core/tk/model'], function (model) {
+
+    'use strict';
+
+
+
+
+});
