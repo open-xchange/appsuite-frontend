79a4ae44e0337e9a1010b3d77970270f92793a0b
Francisco Laguna <fla@synapps.de>
2012-02-23 16:12:51 +0100

    rename bootstrap

--- ui/apps/twitterBootstrap/basics.js
+++
@@ -1,4 +0,0 @@
-define("twitterBootstrap/basics", ["apps/twitterBootstrap/js/bootstrap-transition.js", "less!twitterBootstrap/less/bootstrap.less"], function () {
-    "use strict";
-    return true;
-});
