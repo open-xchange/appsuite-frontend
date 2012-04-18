f1832c3718765aa0e380efc235925776be9500df
biggeleben <matthias.biggeleben@open-xchange.com>
2012-04-18 19:33:40 +0200

    Changed tinyMCE's icons to grayscale; bit more polishing

--- ui/apps/io.ox/core/tk/html-editor.js
+++ ui/apps/io.ox/core/tk/html-editor.js
@@ -368,11 +368,11 @@ define.async('io.ox/core/tk/html-editor', [], function () {
             },
 
             theme_advanced_buttons1:
-                'bold,italic,underline,|,' +
-                'undo,redo,|,' +
+                'bold,italic,underline,strikethrough,|,' +
                 'bullist,numlist,indent,outdent,|,' +
                 'justifyleft,justifycenter,justifyright,|,' +
-                'forecolor,backcolor,|,formatselect',
+                'forecolor,backcolor,|,formatselect,|,' +
+                'undo,redo,',
             theme_advanced_buttons2: '',
             theme_advanced_buttons3: '',
             theme_advanced_toolbar_location: 'top',
