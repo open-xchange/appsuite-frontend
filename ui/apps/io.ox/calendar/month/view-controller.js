1582cdef0ee108579ce11747b334e0f338842b6b
biggeleben <matthias.biggeleben@open-xchange.com>
2012-06-12 14:29:36 +0200

    In progress: Month view

--- ui/apps/io.ox/calendar/month/view-controller.js
+++ ui/apps/io.ox/calendar/month/view-controller.js
@@ -14,85 +14,89 @@ define('io.ox/calendar/month/view-controller',
     ['io.ox/calendar/util',
      'io.ox/calendar/api',
      'io.ox/calendar/month/view'
-     ], function (util, api, gt, view) {
+     ], function (util, api, view) {
 
     'use strict';
 
     return new ox.ui.WindowView('month-view', function (main) {
 
-        var drawTs = _.now(),
-            drawDate = new Date(drawTs),
-            list = util.getMonthScaffold(drawTs),
-            appointmentFilter = {},
-            drawMonth = drawDate.getUTCMonth();
+        main.addClass('month-view').append(
+            view.drawScaffold()
+        );
 
-        var calendarCheck = $('ol.calendar', main),
-            render = false,
-            monthView, prevMonth, thisMonth, nextMonth;
-
-        if (!calendarCheck || calendarCheck.length === 0) {
-            render = true;
-
-            monthView = $('<ol>', {"class": "calendar", "start": drawMonth});
-            prevMonth = $('<ol>', {"class": "lastmonth"});
-            thisMonth = $('<ol>', {"class": "thismonth"});
-            nextMonth = $('<ol>', {"class": "nextmonth"});
-
-            monthView.append($('<li>').append(prevMonth))
-                .append($('<li>').append(thisMonth))
-                .append($('<li>').append(nextMonth));
-        }
-
-        _(list).each(function (weeks) {
-            _(weeks).each(function (day) {
-                var actualList;
-
-                if (!appointmentFilter.start) {
-                    appointmentFilter.start = day.timestamp;
-                }
-                appointmentFilter.end = day.timestamp;
-
-                if (day.month < drawDate.getUTCMonth()) {
-                    actualList = prevMonth;
-                    if (prevMonth && !prevMonth.attr('start')) {
-                        prevMonth.attr('start', day.date);
-                    }
-                } else if (day.month > drawDate.getUTCMonth()) {
-                    actualList = nextMonth;
-                } else {
-                    actualList = thisMonth;
-                }
-
-                if (render) {
-                    var actualDay = $("<li>", {"class": "calendarday day" + day.year + '-' + day.month + '-' + day.date});
-                    actualDay.data('date', day);
-                    actualDay.text(day.date + '.' + (day.month + 1));
-
-                    actualList.append(actualDay);
-                }
-            });
-        });
-
-        // add the data to the calendar
-        api.getAll(appointmentFilter).done(function (appointments) {
-            _(appointments).each(function (appointment) {
-
-                var appointmentId = 'appointment_' + appointment.folder_id + '_' + appointment.id,
-                    appointmentDate = new Date(appointment.start_date),
-                    dateStr = appointmentDate.getUTCFullYear() + '-' + appointmentDate.getUTCMonth() + '-' + appointmentDate.getUTCDate(),
-                    liItem = $('.calendarday.day' + dateStr, main),
-                    appointmentCheck = $('.day' + dateStr + ' .' + appointmentId, main);
-
-                if (!appointmentCheck || appointmentCheck.length === 0) {
-                    liItem.append($('<p>', {'class': 'appointmentItem ' + appointmentId}).data('appointment', appointment).text(appointment.title));
-                }
-            });
-
-        });
-
-        if (render) {
-            main.append(monthView);
-            main.scrollable();
-        }
+//        var drawTs = _.now(),
+//            drawDate = new Date(drawTs),
+//            list = util.getMonthScaffold(drawTs),
+//            appointmentFilter = {},
+//            drawMonth = drawDate.getUTCMonth();
+//
+//        var calendarCheck = $('ol.calendar', main),
+//            render = false,
+//            monthView, prevMonth, thisMonth, nextMonth;
+//
+//        if (!calendarCheck || calendarCheck.length === 0) {
+//            render = true;
+//
+//            monthView = $('<ol>', {"class": "calendar", "start": drawMonth});
+//            prevMonth = $('<ol>', {"class": "lastmonth"});
+//            thisMonth = $('<ol>', {"class": "thismonth"});
+//            nextMonth = $('<ol>', {"class": "nextmonth"});
+//
+//            monthView.append($('<li>').append(prevMonth))
+//                .append($('<li>').append(thisMonth))
+//                .append($('<li>').append(nextMonth));
+//        }
+//
+//        _(list).each(function (weeks) {
+//            _(weeks).each(function (day) {
+//                var actualList;
+//
+//                if (!appointmentFilter.start) {
+//                    appointmentFilter.start = day.timestamp;
+//                }
+//                appointmentFilter.end = day.timestamp;
+//
+//                if (day.month < drawDate.getUTCMonth()) {
+//                    actualList = prevMonth;
+//                    if (prevMonth && !prevMonth.attr('start')) {
+//                        prevMonth.attr('start', day.date);
+//                    }
+//                } else if (day.month > drawDate.getUTCMonth()) {
+//                    actualList = nextMonth;
+//                } else {
+//                    actualList = thisMonth;
+//                }
+//
+//                if (render) {
+//                    var actualDay = $("<li>", {"class": "calendarday day" + day.year + '-' + day.month + '-' + day.date});
+//                    actualDay.data('date', day);
+//                    actualDay.text(day.date + '.' + (day.month + 1));
+//
+//                    actualList.append(actualDay);
+//                }
+//            });
+//        });
+//
+//        // add the data to the calendar
+//        api.getAll(appointmentFilter).done(function (appointments) {
+//            _(appointments).each(function (appointment) {
+//
+//                var appointmentId = 'appointment_' + appointment.folder_id + '_' + appointment.id,
+//                    appointmentDate = new Date(appointment.start_date),
+//                    dateStr = appointmentDate.getUTCFullYear() + '-' + appointmentDate.getUTCMonth() + '-' + appointmentDate.getUTCDate(),
+//                    liItem = $('.calendarday.day' + dateStr, main),
+//                    appointmentCheck = $('.day' + dateStr + ' .' + appointmentId, main);
+//
+//                if (!appointmentCheck || appointmentCheck.length === 0) {
+//                    liItem.append($('<p>', {'class': 'appointmentItem ' + appointmentId}).data('appointment', appointment).text(appointment.title));
+//                }
+//            });
+//
+//        });
+//
+//        if (render) {
+//            main.append(monthView);
+//            main.scrollable();
+//        }
     });
 });
