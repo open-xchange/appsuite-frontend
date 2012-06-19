4086963a77dd5747b8382b63b79df4e2023e0f26
Mario Scheliga <mario@sourcegarden.de>
2012-06-19 18:54:24 +0200

    just fixed small background issue with autocomplete in calendar/edit

--- ui/apps/io.ox/calendar/edit/view-addparticipants.js
+++ ui/apps/io.ox/calendar/edit/view-addparticipants.js
@@ -56,8 +56,8 @@ define('io.ox/calendar/edit/view-addparticipants',
                             var markup = pview.render().el;
 
                             // just hack a bit to make it work easely
-                            $(this).css({height: '39px'});
-                            $(markup).css({'list-style': 'none', 'margin-left': '0px'});
+                            $(this).css({height: '47px'});
+                            $(markup).css({'list-style': 'none', 'margin-left': '0px', 'background': 'none'});
                             $(markup).find('.person-link').removeClass('person-link');
 
                             $(markup).find('.remove').remove();
