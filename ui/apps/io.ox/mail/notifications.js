8b15ef812058cd47e12bfe193b843528b428ce16
Mario Scheliga <mario@sourcegarden.de>
2012-06-20 21:37:02 +0200

    just stash my work to go on with reworks on other USs

--- ui/apps/io.ox/mail/notifications.js
+++ ui/apps/io.ox/mail/notifications.js
@@ -20,18 +20,47 @@ define('io.ox/mail/notifications',
 
     function register() {
         var notifications = notificationService.get('io.ox/mail', NotificationView);
+        notifications.collection.shadow_collection = new Backbone.Collection([]);
+
+        window.mail_not = notifications;
 
         mailApi.on('new-mail', function (e, mails) {
-            mailApi.getList(_(mails).clone().splice(0, 10))
+            window.col = notifications.collection;
+
+            notifications.collection.shadow_collection.add(_(mails).clone());
+
+            console.log("size before splice", notifications.collection.shadow_collection.size());
+            var toFetch = _(notifications.collection.shadow_collection.models.splice(0, 3)).map(function (val) {
+                return val.toJSON();
+            });
+            console.log("size after splice", notifications.collection.shadow_collection.size());
+
+
+            console.log('TO FETCH', toFetch);
+            mailApi.getMailsWithOptions(toFetch, {unseen: 'true', view: 'text'})
+            //$.when.apply($, mailDFs)
                 .done(function (data) {
+                    /*var data = _(arguments).clone();*/
+                    console.log('FETCHED MAILS', data.reverse());
+                    var items = notifications.collection.toJSON();
+
                     _(data).each(function (mail) {
+                        console.log('on each', mail);
                         var f = mail.from || [['', '']];
-                        notifications.collection.unshift({
+                        items.unshift({
                             title: util.getDisplayName(f[0]),
                             subject: mail.subject,
+                            content: mail.attachments[0].content,
                             data: mail
                         });
+                        if (items.length > 3) {
+                            items.pop();
+                        }
                     });
+
+                    console.log('REST ITEMS', items);
+                    // just reset at once
+                    notifications.collection.reset(items);
                     console.log('fetched mails', arguments);
                 });
         });
