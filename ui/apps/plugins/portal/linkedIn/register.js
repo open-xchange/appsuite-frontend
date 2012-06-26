f3141d7e4dd39b009d9586bf635b6e4afb2730ba
Francisco Laguna <francisco.laguna@open-xchange.com>
2012-06-26 15:40:34 +0200

    use larger space

--- ui/apps/plugins/portal/linkedIn/register.js
+++ ui/apps/plugins/portal/linkedIn/register.js
@@ -113,9 +113,9 @@ define("plugins/portal/linkedin/register",
             
             if (message) {
                 $('<div class="linkedin-preview">').append(
-                    $('<div class="linkedin-name">').text(strings.shorten(message.from.person.firstName + " " + message.from.person.lastName, 27)),
-                    $('<div class="linkedin-subject">').text(strings.shorten(message.subject, 27)),
-                    $('<div class="linkedin-body">').text(strings.shorten(message.body, 60))
+                    $('<div class="linkedin-name">').text(strings.shorten(message.from.person.firstName + " " + message.from.person.lastName, 50)),
+                    $('<div class="linkedin-subject">').text(strings.shorten(message.subject, 50)),
+                    $('<div class="linkedin-body">').text(strings.shorten(message.body, 50))
                 ).appendTo(this);
                 
             }
