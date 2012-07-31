2f65adca5beeabe0ed119a3d964242e11188ff6e
Ingo Schmidt-Rosbiegal <ingo.schmidt-rosbiegal@open-xchange.com>
2012-07-31 11:04:28 +0200

    Fixing Chrome problem with empty positions in function isPositionInTable.

--- ui/apps/io.ox/office/editor/editor.js
+++ ui/apps/io.ox/office/editor/editor.js
@@ -654,11 +654,11 @@ define('io.ox/office/editor/editor', ['io.ox/core/event', 'io.ox/office/tk/utils
             }
 
             if (textNode.nodeType !== 3) {
-                dbgOutError("ERROR: Failed to determine text node from current node! NodeType must be 3, but it is: " + textNode.nodeType + "(" + textNode.nodeName + ")");
                 if (textNode.nodeName === 'BR') {
                     textNode = textNode.previousSibling;  // Special handling for <BR> in empty paragraphs.
                 }
                 if ((! textNode) || (textNode.nodeType !== 3)) {
+                    dbgOutError("ERROR: Failed to determine text node from current node! NodeType must be 3, but it is: " + textNode.nodeType + "(" + textNode.nodeName + ")");
                     return;
                 }
             }
@@ -2026,7 +2026,19 @@ define('io.ox/office/editor/editor', ['io.ox/core/event', 'io.ox/office/tk/utils
 
         this.isPositionInTable = function (position) {
             var positionInTable = false,
-                domPos = this.getDOMPosition(position || this.getSelection().endPaM.oxoPosition);
+                domPos = null;
+
+            if (position) {
+                domPos = this.getDOMPosition(position);
+            } else {
+                var selection = this.getSelection();
+                if (selection) {
+                    domPos = this.getDOMPosition(selection.endPaM.oxoPosition);
+                } else {
+                    return false;
+                }
+            }
+
             if (domPos) {
                 var node = domPos.node;
 
