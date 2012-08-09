86bd147384eaf8f7663d9910da4bb374a4ef0d40
tobias.prinz <tobias.prinz@open-xchange.com>
2012-08-09 11:36:49 +0200

    Portal RSS: First reviewable state

--- ui/src/serverconfig.js
+++ ui/src/serverconfig.js
@@ -29,8 +29,8 @@ define(function () {
             signin: [],
             core: ['halo'],
             halo: ['halo/contacts', 'halo/appointments', 'halo/linkedIn', 'halo/mail'],
-//            portal: ['appointments', 'linkedIn', 'mail', 'rss', 'facebook', 'twitter'],
-            portal: ['rss'],
+            portal: ['appointments', 'linkedIn', 'mail', 'rss', 'facebook', 'twitter'],
+//            portal: ['rss'],
             notifications: ['calendar', 'mail'],
             tests: ["io.ox/mail/write", "plugins/halo", "io.ox/contacts", "io.ox/contacts/edit", "io.ox/contacts/distrib",
                     "io.ox/core", "io.ox/files/tests/interface", "io.ox/files/tests/unit", "io.ox/settings/accounts/email", "io.ox/settings"]
