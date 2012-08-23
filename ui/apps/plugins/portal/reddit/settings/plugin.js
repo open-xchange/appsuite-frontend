88b7b2aed4ce2b6da766d2450c41fd57a4e94b2a
Markus Bode <markus.bode@open-xchange.com>
2012-08-23 11:31:58 +0200

    portal: reddit settings

--- ui/apps/plugins/portal/reddit/settings/plugin.js
+++ ui/apps/plugins/portal/reddit/settings/plugin.js
@@ -29,60 +29,17 @@ define('plugins/portal/reddit/settings/plugin',
             DELETE:     gt('Delete')
         },
 
-        PluginSettingsView = Backbone.View.extend({
-            initialize: function (options) {
-                this.template = doT.template(pluginSettingsTemplate);
-            },
-            render: function () {
-                var self = this;
-                self.$el.empty().append(self.template({
-                    strings: staticStrings
-                }));
-
-                return self;
-            },
-            events: {
-                'click [data-action="add"]': 'onAdd'
-//                'click [data-action="edit"]': 'onEdit',
-//                'click [data-action="delete"]': 'onDelete'
-            },
-
-            onAdd: function (args) {
-                console.log("onAdd");
-                var dialog = new dialogs.ModalDialog({
-                    easyOut: true
-                });
-
-                var $input = $('<input>').attr({type: 'text', id: 'add_subreddit', placeholder: 'r/'});
-
-                dialog.header($("<h4>").text(gt('Add an Subreddit')))
-                    .append($input)
-                    .addButton('add', 'Add')
-                    .addButton('cancel', 'Cancel')
-                    .show()
-                    .done(function (action) {
-                        if (action === 'add') {
-                            // TODO validate inputs
-                            // TODO hot or new?
-                            // TODO redraw
-                            subreddits.push({subreddit: $input.attr('value'), mode: 'hot'});
-                            settings.set('subreddits', subreddits);
-                            settings.save();
-                        }
-                    });
-            }
-        }),
-
         SubredditSelectView = Backbone.View.extend({
             _modelBinder: undefined,
             initialize: function (options) {
-                console.log(subredditSelectTemplate);
                 this.template = doT.template(subredditSelectTemplate);
                 this._modelBinder = new Backbone.ModelBinder();
             },
             render: function () {
                 var self = this;
+
                 self.$el.empty().append(self.template({
+                    subreddit: this.model.get('subreddit'),
                     strings: staticStrings
                 }));
 
@@ -100,18 +57,141 @@ define('plugins/portal/reddit/settings/plugin',
             }
         }),
 
+        PluginSettingsView = Backbone.View.extend({
+            initialize: function (options) {
+                this.template = doT.template(pluginSettingsTemplate);
+            },
+            render: function () {
+                this.$el.empty().append(this.template({
+                    strings: staticStrings
+                }));
+
+                var that = this;
+
+                function redraw() {
+                    var $listbox = that.$el.find('.listbox');
+                    var collection = new Backbone.Collection(subreddits);
+                    $listbox.empty();
+
+                    collection.each(function (item) {
+                        $listbox.append(new SubredditSelectView({ model: item }).render().el);
+                    });
+                }
+
+                redraw();
+
+                this.on('redraw', redraw);
+
+                return this;
+            },
+            events: {
+                'click [data-action="add"]': 'onAdd',
+//                'click [data-action="edit"]': 'onEdit',
+                'click [data-action="del"]': 'onDelete'
+            },
+
+            onAdd: function (args) {
+                var dialog = new dialogs.ModalDialog({
+                    easyOut: true,
+                    async: true
+                });
+
+                var $subreddit = $('<input>').attr({type: 'text', id: 'add_subreddit', placeholder: 'r/'});
+                var $mode = $('<select>')
+                    .append($('<option>').attr('value', 'hot').text(gt('hot')))
+                    .append($('<option>').attr('value', 'new').text(gt('new')));
+
+                var $error = $('<div>').addClass('alert alert-error').hide();
+
+                var that = this;
+
+                dialog.header($("<h4>").text(gt('Add an Subreddit')))
+                    .append($subreddit)
+                    .append($mode)
+                    .append($error)
+                    .addButton('add', 'Add', null, {classes: 'btn-primary'})
+                    .addButton('cancel', 'Cancel')
+                    .show();
+
+                dialog.on('add', function (e) {
+                    $error.hide();
+
+                    var subreddit = $.trim($subreddit.val()),
+                        deferred = $.Deferred();
+
+                    if (subreddit.length === 0) {
+                        $error.text(gt('Please enter a subreddit.'));
+                        deferred.reject();
+                    } else {
+                        $.ajax({
+                            url: 'http://www.reddit.com/r/' + subreddit + '/.json?jsonp=testcallback',
+                            type: 'HEAD',
+                            dataType: 'jsonp',
+                            jsonp: false,
+                            jsonpCallback: 'testcallback',
+                            success: function () {
+                                deferred.resolve();
+                            },
+                            error: function () {
+                                $error.text(gt('Unknown error while checking subreddit.'));
+                                deferred.reject();
+                            }
+                        });
+                    }
+
+                    deferred.done(function () {
+                        subreddits.push({subreddit: subreddit, mode: $mode.val()});
+                        settings.set('subreddits', subreddits);
+                        settings.save();
+                        that.trigger('redraw');
+                        dialog.close();
+                    });
+
+                    deferred.fail(function () {
+                        $error.show();
+                        dialog.idle();
+                    });
+                });
+            },
+            onDelete: function (args) {
+                console.log("onDelete");
+                var dialog = new dialogs.ModalDialog({
+                    easyOut: true
+                });
+
+                var subreddit = this.$el.find('[selected]').data('subreddit');
+
+                if (subreddit) {
+                    var that = this;
+
+                    dialog.header($("<h4>").text(gt('Delete an Subreddit')))
+                        .append($('<span>').text(gt('Do you really want to delete the following subreddit(s)?')))
+                        .append($('<ul>').append($('<li>').text(subreddit)))
+                        .addButton('delete', 'Delete', null, {classes: 'btn-primary'})
+                        .addButton('cancel', 'Cancel')
+                        .show()
+                        .done(function (action) {
+                            if (action === 'delete') {
+                                var newSubreddits = [];
+                                _.each(subreddits, function (sub) {
+                                    if (sub.subreddit !== subreddit) {
+                                        newSubreddits.push(sub);
+                                    }
+                                });
+
+                                subreddits = newSubreddits;
+                                settings.set('subreddits', subreddits);
+                                settings.save();
+                                that.trigger('redraw');
+                            }
+                            return false;
+                        });
+                }
+            }
+        }),
+
         renderSettings = function () {
-            var $node = $('<div>');
-            var collection = new Backbone.Collection(subreddits);
-
-            $node.append(new PluginSettingsView().render().el);
-            var $listbox = $node.find('.listbox');
-
-            collection.each(function (item) {
-                $listbox.append(new SubredditSelectView({ model: item }).render().el);
-            });
-
-            return $node;
+            return new PluginSettingsView().render().el;
         };
 
     return {
