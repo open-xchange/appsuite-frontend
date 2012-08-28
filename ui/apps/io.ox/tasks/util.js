3c45f59ba0f4aa11086825e73257c7e28607c880
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2012-08-28 16:41:07 +0200

    popup for reminder function improved

--- ui/apps/io.ox/tasks/util.js
+++ ui/apps/io.ox/tasks/util.js
@@ -10,59 +10,184 @@
  *
  * @author Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
  */
-define("io.ox/tasks/util", function () {
+define("io.ox/tasks/util", ['gettext!io.ox/tasks/util'], function (gt) {
     
     "use strict";
-    //order is important! Must be the same as in dropdownmenu
+    
     var lookupArray = [60000 * 5,           //five minutes
                        60000 * 15,          //fifteen minutes
                        60000 * 30,          //thirty minutes
-                       60000 * 60,          //one hour
-                       60000 * 60 * 3,      //three hours
-                       60000 * 60 * 6,      //six hours
-                       60000 * 60 * 24,     //one day
-                       60000 * 60 * 24 * 3, //three days
-                       60000 * 60 * 24 * 7];//one week
+                       60000 * 60],         //one hour]
+                       
+        lookupDaytimeStrings = ["this morning",
+                                "by noon",
+                                "this afternoon",
+                                "this evening",
+                                "tonight"],
+                                
+        lookupWeekdayStrings = ["Sunday",
+                                 "Monday",
+                                 "Tuesday",
+                                 "Wednesday",
+                                 "Thursday",
+                                 "Friday",
+                                 "Saturday"];
     
     var util = {
-            computePopupTime: function (time, selectorIndex)
+            computePopupTime: function (time, finderId)
             {
                 var endDate = time;
-                console.log(selectorIndex);
                 var offset = endDate.getTimezoneOffset() * -1 * 60000;
                 
-                if (selectorIndex < lookupArray.length)
+                switch (finderId)
+                {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                    endDate.setTime(endDate.getTime() + lookupArray[finderId]);
+                    break;
+                default:
+                    endDate.setTime(prepareTime(endDate));
+                    switch (finderId)
+                    {
+                    case "d0":
+                        endDate.setHours(6);
+                        break;
+                    case "d1":
+                        endDate.setHours(12);
+                        break;
+                    case "d2":
+                        endDate.setHours(15);
+                        break;
+                    case "d3":
+                        endDate.setHours(18);
+                        break;
+                    case "d4":
+                        endDate.setHours(22);
+                        break;
+                    default:
+                        endDate.setHours(6);
+                        switch (finderId)
                         {
-                    endDate.setTime(endDate.getTime() + lookupArray[selectorIndex]);
-                } else
-                    {
-                    endDate.setMilliseconds(0);
-                    endDate.setSeconds(0);
-                    endDate.setMinutes(0);
-                    
-                    switch (selectorIndex)
-                    {
-                    case 9:
-                        endDate.setTime(endDate.getTime() + 60000 * 60 * 24);
-                        break;
-                    case 10:
-                        var day = (((endDate.getDay() - 1) % 7) + 7) % 7;//workaroundjavascript modulo operator to stupid to handle negative numbers
-                        endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
-                        break;
-                    case 11:
-                        var day = (((endDate.getDay() - 5) % 7) + 7) % 7;//workaround again
-                        console.log(day);
-                        endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                        case "t":
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24);
+                            break;
+                        case "ww":
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * 7);
+                            break;
+                        case "w0":
+                            var day = endDate.getDay() % 7;
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                            break;
+                        case "w1":
+                            var day = (((endDate.getDay() - 1) % 7) + 7) % 7;//workaround: javascript modulo operator to stupid to handle negative numbers
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                            break;
+                        case "w2":
+                            var day = (((endDate.getDay() - 2) % 7) + 7) % 7;
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                            break;
+                        case "w3":
+                            var day = (((endDate.getDay() - 3) % 7) + 7) % 7;
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                            break;
+                        case "w4":
+                            var day = (((endDate.getDay() - 4) % 7) + 7) % 7;
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                            break;
+                        case "w5":
+                            var day = (((endDate.getDay() - 5) % 7) + 7) % 7;
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                            break;
+                        case "w6":
+                            var day = (((endDate.getDay() - 6) % 7) + 7) % 7;
+                            endDate.setTime(endDate.getTime() + 60000 * 60 * 24 * (7 - day));
+                            break;
+                        default:
+                            //cannot identify selector...set time now
+                            //maybe errormessage
+                            endDate = new Date();
+                            break;
+                        }
                         break;
                     }
-                    
-                    endDate.setHours(5);
+                    break;
                 }
                 
                 endDate.setTime(endDate.getTime() + offset);
                 
                 return endDate;
+            },
+    
+            buildDropdownMenu: function (time)
+            {
+                //normal times
+                var appendString = "<option finderId='0'>" + gt('in 5 minutes') + "</option>" +
+                "<option finderId='1'>" + gt('in 15 minutes') + "</option>" +
+                "<option finderId='2'>" + gt('in 30 minutes') + "</option>" +
+                "<option finderId='3'>" + gt('in one hour') + "</option>";
+                
+                // variable daytimes
+                var i = time.getHours();
+                
+                if (i < 6)
+                    {i = 0;
+                } else if (i < 12)
+                    {i = 1;
+                } else if (i < 15)
+                    {i = 2;
+                } else if (i < 18)
+                    {i = 3;
+                } else if (i < 22)
+                    {i = 4;
+                }
+                
+                var temp;
+                while (i < lookupDaytimeStrings.length)
+                    {
+                    temp = lookupDaytimeStrings[i];
+                    appendString = appendString + "<option finderId='d" + i + "'>" + gt(temp) + "</option>";
+                    i++;
+                }
+                
+                //weekdays
+                var circleIncomplete = true;
+                i = (time.getDay() + 2) % 7;
+                var startday = time.getDay();
+                
+                appendString = appendString + "<option finderId='t'>" + gt("tomorrow") + "</option>";
+                
+                while (circleIncomplete)
+                    {
+                    temp = lookupWeekdayStrings[i];
+                    appendString = appendString + "<option finderId='w" + i + "'>" + gt(temp) + "</option>";
+                    if (i < 6)
+                        {i++;
+                    } else
+                        {
+                        i = 0;
+                    }
+                    
+                    if (i === startday)
+                        {
+                        appendString = appendString + "<option finderId='ww'>" + gt("in one week") + "</option>";
+                        circleIncomplete = false;
+                    }
+                }
+                
+                return appendString;
             }
         };
+        
+    var prepareTime = function (time)
+    {
+        time.setMilliseconds(0);
+        time.setSeconds(0);
+        time.setMinutes(0);
+            
+        return time;
+    };
+        
     return util;
 });
