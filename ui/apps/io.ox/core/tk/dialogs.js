ac182c3909efc26cb7880bdcb97a03b45fe509e2
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2012-08-30 15:24:09 +0200

    Fixed linkedin picture sidepopups

--- ui/apps/io.ox/core/tk/dialogs.js
+++ ui/apps/io.ox/core/tk/dialogs.js
@@ -476,7 +476,7 @@ define("io.ox/core/tk/dialogs",
                 self.nodes.target.append((options.modal ? overlay : popup).css('visibility', 'hidden'));
 
                 // call custom handler
-                (handler || $.noop).call(self, pane.empty(), e);
+                (handler || $.noop).call(self, pane.empty(), e, my);
 
                 // set arrow top
                 var halfHeight = (my.outerHeight(true) / 2 >> 0),
