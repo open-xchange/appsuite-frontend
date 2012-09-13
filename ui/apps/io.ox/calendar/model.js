596e45e23519ac87e035acdcb1c7ac6daf5288ab
Francisco Laguna <francisco.laguna@open-xchange.com>
2012-09-13 09:50:19 +0200

    added super basic model

--- ui/apps/io.ox/calendar/model.js
+++ ui/apps/io.ox/calendar/model.js
@@ -10,17 +10,27 @@
  *
  * @author Francisco Laguna <francisco.laguna@open-xchange.com>
  */
-define("io.ox/calendar/model", ['io.ox/calendar/api', 'io.ox/backbone/modelFactory', 'io.ox/core/extensions'], function (api, ModelFactory, ext) {
+define("io.ox/calendar/model", ['io.ox/calendar/api', 'io.ox/backbone/modelFactory', 'io.ox/core/extensions', 'gettext!io.ox/calendar/calendar'], function (api, ModelFactory, ext, gt) {
+    
+    "use strict";
     
     var factory = new ModelFactory({
         ref: 'io.ox/calendar/model',
-        api: api
+        api: api,
+        model: {
+            // TODO: Add convenience methods for participant handling
+            // TODO: Add convenience methods for recurrence handling
+            // TODO: Add convenience methods for turning full day appointments into regular appointments and back
+        }
     });
     
     ext.point("io.ox/calendar/model/validation").extend({
         id: 'start-date-before-end-date',
         validate: function (attributes) {
-            
+            if (attributes.start_date && attributes.end_date && attributes.end_date < attributes.start_date) {
+                this.add('start_date', gt("The start date must be before the end date."));
+                this.add('end_date', gt("The start date must be before the end date."));
+            }
         }
     });
     
