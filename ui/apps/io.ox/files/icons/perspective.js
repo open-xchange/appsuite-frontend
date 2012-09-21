0857cf7efc6de49c2ab2eb72ab9fa32b8c7d8319
david.bauer <david.bauer@open-xchange.com>
2012-09-21 13:12:22 +0200

    US: User sees icon-based perspective in files

--- ui/apps/io.ox/files/icons/perspective.js
+++ ui/apps/io.ox/files/icons/perspective.js
@@ -20,8 +20,7 @@ define('io.ox/files/icons/perspective',
      'io.ox/core/extPatterns/shortcuts',
      'io.ox/core/commons',
      'gettext!io.ox/files/files',
-     'io.ox/core/config',
-     'less!io.ox/files/icons/style.css'
+     'io.ox/core/config'
      ], function (viewDetail, ext, dialogs, api, upload, dnd, shortcuts, commons, gt, config) {
 
     'use strict';
@@ -31,108 +30,152 @@ define('io.ox/files/icons/perspective',
     _.extend(perspective, {
         app:            null,
         dialog:         $(),
+        iconview:       $(),
+
+        drawIcon: function (file) {
+            var node = $('<div class="file-icon pull-left">').attr('data-obj-id', file.id);
+            var wrap = $('<div class="wrap">');
+            if (file.file_mimetype.match(/^image\/[gif|png|jpe?g|gmp]/i)) {
+                var src = api.getIcon(file);
+                node.append(wrap.append($('<img>', { 'data-original': src, alt: file.title }).addClass('img-polaroid').addClass('lazy')));
+            }
+            else
+            {
+                node.append(wrap.append(
+                    $('<img>', { src: ox.base + '/apps/themes/default/icons/file-generic.png', alt: file.title }).addClass('file-generic')
+                ));
+            }
+            node.append($('<div class="title">').text(
+                    file.title.replace(/^(.{10}).+(.{9})$/, "$1…$2")));
+            return node;
+        },
 
         draw: function (app) {
             var that = this;
-
+            var mode = false;
             var win = app.getWindow(),
-                iconview;
+                currentDetailViewTarget,
+                chunk,
+                $view = this.main;
+            var rows, cols;
+            var fileIconHeight = 175;
+            var fileIconWidth = 138;
+            var fileiconMargin = 30;
 
-            iconview = $('<div class="files-iconview">')
-                .on('click', '.file-icon', function (data) {
+            Array.prototype.chunk = function (chunkSize) {
+                var array = this;
+                return [].concat.apply([],
+                    array.map(function (elem, i) {
+                        return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];
+                    })
+                );
+            };
+
+            this.main.on('click', '.file-icon', function (data) {
+                var currentTarget = $(data.currentTarget).attr('data-obj-id');
+                if (currentDetailViewTarget !== currentTarget)
+                {
                     that.dialog.show(data, function (popup, e, target) {
-                        api.get({id: $(data.currentTarget).attr('data-obj-id')}).done(function (file) {
+                        currentDetailViewTarget = currentTarget;
+                        api.get({id: currentTarget}).done(function (file) {
                             var currentDetailView = viewDetail.draw(file);
-                            popup.append(currentDetailView.element)
-                                .on("dblclick", function (e) {
-                                    if (_(["a", "button", "input", "textarea"]).include(e.srcElement.tagName.toLowerCase())) {
-                                        return;
-                                    }
-                                    if (currentDetailView) {
-                                        currentDetailView.toggleEdit();
-                                    }
-                                });
+                            popup.append(currentDetailView.element);
                         });
                     });
-                })
-                .appendTo(this.main.empty());
-
-            var drawIcon, showIconview, drawIconview, drawFail;
-
-            drawIcon = function (file) {
-                var preview = $('<div class="file-icon pull-left">')
-                    .attr('data-obj-id', file.id);
-                var wrap = $('<div class="wrap">');
-                if (file.file_mimetype.match(/^image\/[gif|png|jpe?g|gmp]/i)) {
-                    var src = api.getIcon(file);
-                    preview.append(wrap.append($('<img>', { src: src, alt: file.title }).addClass('img-polaroid')));
                 }
                 else
                 {
-                    preview.append(wrap.append(
-                        //$('<span class="file-mimetype label">')
-                        //    .text(file.file_mimetype)
-                        $('<img>', { src: ox.base + '/apps/themes/default/icons/file-generic.png', alt: file.title }).addClass('file-generic')
-                    ));
+                    currentDetailViewTarget = null;
                 }
-                preview.append($('<div class="title">').text(
-                        file.title.replace(/^(.{26}).+(.{10})$/, "$1…$2")));
-                return preview;
-            };
+            });
 
-            showIconview = function (mode) {
-                iconview.busy(true);
+            var drawIcons, drawIconview, loadIcon, Icon;
+
+            loadIcon = function () {
+                var deferred = new $.Deferred();
                 if (!mode)
                 {
-                    api.getAll({ columns: "20,1,700,701,703,705,706", folder: app.folder.get() }, false)
-                        .done(_.lfo(drawIconview))
-                        .fail(_.lfo(drawFail));
+                    api.getAll({ folder: app.folder.get() })
+                        .done(function (ids) {
+                            chunk = 0;
+                            //var tmp = ids.chunk(rows * cols * 2);
+                            //api.getList(tmp[chunk])
+                            api.getList(ids)
+                            .done(deferred.resolve)
+                            .fail(deferred.reject);
+                        })
+                        .fail(_.lfo(deferred.reject));
                 }
                 else
                 {
                     api.search(win.search.query, {
-                        action: "search",
-                        columns: "20,1,700,701,703,705,706",
-                        sort: "700",
-                        order: "asc"
+                        action: "search"
                     })
-                        .done(_.lfo(drawIconview))
-                        .fail(_.lfo(drawFail));
+                    .done(function (ids) {
+                        api.getList(ids)
+                            .done(deferred.resolve)
+                            .fail(deferred.reject);
+                    })
+                    .fail(deferred.reject);
+
                 }
+                return deferred;
             };
 
-            drawIconview = function (data) {
-                iconview.idle().empty().append(
-                    _(data).each(function (file) {
-                        iconview.append(drawIcon(file));
-                    })
-                );
+            drawIcons = function () {
+                var deferred = new $.Deferred();
+                loadIcon().done(function (files) {
+                    _(files).each(function (file) {
+                        $view.append(that.drawIcon(file));
+                    });
+                    $view.idle().addClass('files-iconview-background');
+                    deferred.resolve();
+                });
+                return deferred;
             };
 
-            drawFail = function () {
-                iconview.idle().empty().append(
-                    $.fail("Oops, couldn't load file data.", function () {
-                        showIconview();
-                    })
-                );
+            var redraw = function () {
+                rows = Math.floor($view.height() / (fileIconHeight + fileiconMargin));
+                cols = Math.floor($view.width() / (fileIconWidth + fileiconMargin));
+                $view.empty().busy().removeClass('files-iconview-background');
+                drawIcons().done(function () {
+                    $(".file-icon > div > img").lazyload({
+                        container: $view
+                    });
+                });
             };
-            showIconview();
 
-            win.on('search', function () {
-                showIconview(true);
-            })
-            .on('cancel-search', function () {
-                showIconview();
+            redraw();
+
+            win.on('search cancel-search', function (e) {
+                mode = (e.type === 'search' ? true : false);
+                redraw();
+            });
+
+            // published?
+            app.folder.getData().done(function (data) {
+                win.nodes.title.find('.has-publications').remove();
+                if (data['com.openexchange.publish.publicationFlag']) {
+                    win.nodes.title.prepend(
+                        $('<img>', {
+                            src: ox.base + '/apps/themes/default/glyphicons_232_cloud_white.png',
+                            title: gt('This folder has publications'),
+                            alt: ''
+                        })
+                        .addClass('has-publications')
+                    );
+                }
             });
 
         },
         render: function (app) {
-            this.app = app;
+            this.main.addClass('files-iconview').empty();
+            var that = this;
             var win = app.getWindow();
-            app.on('folder:change', function () {
-                this.draw(app);
+            app.on('folder:change', function (e, id, folder) {
+                that.draw(app);
             });
-            this.dialog = new dialogs.SidePopup({ arrow: false});
+            this.dialog = new dialogs.SidePopup();
             this.draw(app);
         }
     });
