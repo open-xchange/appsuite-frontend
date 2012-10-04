ede5d54d6e90916579a1995f3e22bdc31e3696d4
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2012-10-04 17:32:42 +0200

    Added temp. twitter bootstrap fix (ios and dropdowns)

--- ui/apps/io.ox/core/bootstrap/basics.js
+++ ui/apps/io.ox/core/bootstrap/basics.js
@@ -9,4 +9,7 @@ define('io.ox/core/bootstrap/basics', function () {
 
     // fix bootstrap dropdown (close on click)
     $('body').off('click.dropdown touchstart.dropdown.data-api', '.dropdown');
+
+    // temporary fix (see https://github.com/twitter/bootstrap/issues/4550)
+    $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
 });
