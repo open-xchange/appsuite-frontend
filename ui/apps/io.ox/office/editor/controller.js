ff987a2940908b6f6e55a77c1fd75d22814a3762
Oliver Specht <oliver.specht@open-xchange.com>
2012-10-08 14:33:31 +0200

    paragraph fill color

--- ui/apps/io.ox/office/editor/controller.js
+++ ui/apps/io.ox/office/editor/controller.js
@@ -100,7 +100,11 @@ define('io.ox/office/editor/controller',
                     get: function (attributes) { return attributes.lineheight; },
                     set: function (lineHeight) { editor.setAttribute('paragraph', 'lineheight', lineHeight); }
                 },
-
+                'format/paragraph/fillcolor': {
+                    chain: 'chain/format/paragraph',
+                    get: function (attributes) { return attributes.parafill; },
+                    set: function (fill) { editor.setAttribute('paragraph', 'parafill', fill); }
+                },
                 // characters
 
                 'chain/format/character': {
