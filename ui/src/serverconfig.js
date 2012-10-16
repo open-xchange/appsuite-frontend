8bc90577b7e13240f9e7de66110370244e279c99
Francisco Laguna <francisco.laguna@open-xchange.com>
2012-10-17 01:06:03 +0200

    disable automatic inclusion of liberty plugin

--- ui/src/serverconfig.js
+++ ui/src/serverconfig.js
@@ -27,7 +27,7 @@ define(function () {
 
         plugins: {
             signin: [],
-            core: ['halo', 'liberty'],
+            core: ['halo'],
             halo: ['halo/contacts', 'halo/appointments', 'halo/linkedIn', 'halo/mail'],
             keychain: ["io.ox/mail/accounts/keychain", 'io.ox/oauth/keychain'],
             keychainSettings: ['io.ox/mail/accounts/settings', 'io.ox/oauth/settings'],
