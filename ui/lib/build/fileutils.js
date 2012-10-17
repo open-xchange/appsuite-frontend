475040749d6ddb5434b731edf7c8d4c0639cb385
Viktor Pracht <viktor.pracht@open-xchange.com>
2012-10-17 18:17:24 +0200

    Fixed line counting for //@include directives in files with CRLF linebreaks.

--- ui/lib/build/fileutils.js
+++ ui/lib/build/fileutils.js
@@ -457,7 +457,7 @@ exports.includeFilter = function (data) {
     var dest = this.task.name;
     exports.includes.set(dest, []);
     var self = this, line = 1;
-    return data.replace(/(\/\/@include\s+(.*?)(\S*)(;?))?$/gm,
+    return data.replace(/(\/\/@include\s+(.*?)(\S*)(;?))?\r?\n/g,
         function(m, include, prefix, name, semicolon) {
             if (!include) {
                 line++;
