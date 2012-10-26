a73635d2306eda8f59bf4de9fb85b9eff9e22fb3
Carsten Driesner <carsten.driesner@open-xchange.com>
2012-10-26 09:43:17 +0200

    fix isAutoColor should be true for a color object without type (default type)

--- ui/apps/io.ox/office/editor/format/color.js
+++ ui/apps/io.ox/office/editor/format/color.js
@@ -290,7 +290,7 @@ define('io.ox/office/editor/format/color', ['io.ox/office/tk/utils'], function (
      * true if color has type auto otherwise false
      */
     Color.isAutoColor = function (color) {
-        var type = Utils.getStringOption(color, 'type', 'none');
+        var type = Utils.getStringOption(color, 'type', 'auto');
         return (type === 'auto');
     };
 
