82d4084f948503af9e38dfd0a0ebcdea1a2a1b96
tobias.prinz <tobias.prinz@open-xchange.com>
2012-11-07 15:42:37 +0100

    Fixed bug 23359 for flickr, too

--- ui/apps/plugins/portal/flickr/settings/plugin.js
+++ ui/apps/plugins/portal/flickr/settings/plugin.js
@@ -14,11 +14,10 @@
 define('plugins/portal/flickr/settings/plugin',
        ['io.ox/core/extensions',
         'io.ox/core/tk/dialogs',
-        'text!plugins/portal/flickr/settings/tpl/stream.html',
-        'text!plugins/portal/flickr/settings/tpl/pluginsettings.html',
         'settings!plugins/portal/flickr',
-        'gettext!io.ox/portal'
-        ], function (ext, dialogs, streamSelectTemplate, pluginSettingsTemplate, settings, gt) {
+        'gettext!io.ox/portal',
+        'less!plugins/portal/flickr/style.css'
+        ], function (ext, dialogs, settings, gt) {
 
     'use strict';
 
@@ -61,21 +60,19 @@ define('plugins/portal/flickr/settings/plugin',
         StreamSelectView = Backbone.View.extend({
             _modelBinder: undefined,
             initialize: function (options) {
-                this.template = doT.template(streamSelectTemplate);
                 this._modelBinder = new Backbone.ModelBinder();
             },
             render: function () {
                 var self = this;
 
-                self.$el.empty().append(self.template({
-                    q: this.model.get('q'),
-                    method: this.model.get('method'),
-                    description: this.model.get('description'),
-                    strings: staticStrings
-                }));
-
-                // Used by jquery ui sortable
-                self.$el.attr('id', this.model.get('method') + '#' + this.model.get('q'));
+                self.$el.empty().append(
+                    $('<div class="io-ox-portal-flickr-setting">')
+                    .attr({'data-q': this.model.get('q'), 'data-method': this.model.get('method'), 'data-description': this.model.get('description')}).append(
+                        $('<span data-property="q">'),
+                        $('<i class="icon-edit setting-action">'),
+                        $('<i class="icon-remove setting-action">')
+                    )
+                );
 
                 var defaultBindings = Backbone.ModelBinder.createDefaultBindings(self.el, 'data-property');
                 self._modelBinder.bind(self.model, self.el, defaultBindings);
@@ -83,27 +80,170 @@ define('plugins/portal/flickr/settings/plugin',
                 return self;
             },
             events: {
-                'click .sortable-item': 'onSelect'
+                'click .icon-edit': 'onEdit',
+                'click .icon-remove': 'onDelete'
             },
-            onSelect: function () {
-                this.$el.parent().find('div[selected="selected"]').attr('selected', null);
-                this.$el.find('.sortable-item').attr('selected', 'selected');
+            onEdit: function (pEvent) {
+                var dialog = new dialogs.ModalDialog({
+                    easyOut: true,
+                    async: true
+                }),
+                    $myNode = $(pEvent.target).parent(),
+                    oldQ = $myNode.data('q'),
+                    oldMethod = $myNode.data('method'),
+                    oldDescription = $myNode.data('description');
+                console.log("onEdit", $myNode);
+                
+                if (oldQ && oldMethod) {
+                    oldQ = String(oldQ);
+                    var $q = $('<input>').attr({type: 'text', placeholder: gt('Search')}).val(oldQ);
+                    var $description = $('<input>').attr({type: 'text', placeholder: gt('Description')}).val(oldDescription);
+                    var $method = $('<select>')
+                    .append($('<option>').attr('value', 'flickr.photos.search').text(gt('flickr.photos.search')))
+                    .append($('<option>').attr('value', 'flickr.people.getPublicPhotos').text(gt('flickr.people.getPublicPhotos')))
+                    .val(oldMethod);
+                    var $error = $('<div>').addClass('alert alert-error').hide();
+
+                    var that = this;
+
+                    dialog.header($("<h4>").text(gt('Edit a stream')))
+                    .append($q)
+                    .append($description)
+                    .append($method)
+                    .append($error)
+                    .addButton('cancel', gt('Cancel'))
+                    .addButton('edit', gt('Edit'), null, {classes: 'btn-primary'})
+                    .show();
+
+                    dialog.on('edit', function (e) {
+                        $error.hide();
+
+                        var q = String($.trim($q.val())),
+                        method = $.trim($method.val()),
+                        description = $.trim($description.val()),
+                        deferred;
+
+                        if (method === 'flickr.people.getPublicPhotos') {
+                            deferred = getFlickrNsid(q, $error);
+                        } else {
+                            deferred = $.Deferred();
+                            deferred.resolve();
+                        }
+
+                        deferred.done(function (nsid) {
+                            if (q.length === 0) {
+                                $error.text(gt('Please enter a search-query.'));
+                                $error.show();
+                                dialog.idle();
+                            } else if (description.length === 0) {
+                                $error.text(gt('Please enter a description.'));
+                                $error.show();
+                                dialog.idle();
+                            } else {
+                                ext.point("io.ox/portal/widget").disable('flickr-' + oldQ.replace(/[^a-z0-9]/g, '_') + '-' + oldMethod.replace(/[^a-z0-9]/g, '_'));
+                                streams = removeStream(streams, oldQ, oldMethod);
+
+                                var newStream = {q: q, method: method, description: description};
+
+                                if (nsid) {
+                                    newStream.nsid = nsid;
+                                }
+
+                                streams.push(newStream);
+                                settings.set('streams', streams);
+                                settings.save();
+
+                                ext.point("io.ox/portal/widget").enable('flickr-' + q.replace(/[^a-z0-9]/g, '_') + '-' + method.replace(/[^a-z0-9]/g, '_'));
+
+                                require(['plugins/portal/flickr/register'], function (flickr) {
+                                    flickr.reload();
+                                    that.trigger('redraw');
+                                    ox.trigger("refresh^");
+                                    dialog.close();
+                                });
+                            }
+                        });
+
+                        deferred.fail(function () {
+                            $error.show();
+                            dialog.idle();
+                        });
+                    });
+                }
+            },
+            onDelete: function (pEvent) {
+                var dialog = new dialogs.ModalDialog({
+                        easyOut: true
+                    }),
+                     $myNode = $(pEvent.target).parent(),
+                     q = $myNode.data('q'),
+                     method = $myNode.data('method');
+                console.log("Bla", q, method);
+                if (q && method) {
+                    q = String(q);
+                    var that = this;
+                    console.log("Bla", q, method);
+                    
+                    dialog.header($("<h4>").text(gt('Delete a stream')))
+                        .append($('<span>').text(gt('Do you really want to delete the following stream?')))
+                        .append($('<ul>').append(
+                            $('<li>').text(q + " (" + method + ")")
+                        ))
+                    .addButton('cancel', gt('Cancel'))
+                    .addButton('delete', gt('Delete'), null, {classes: 'btn-primary'})
+                    .show()
+                    .done(function (action) {
+                        if (action === 'delete') {
+                            var newStreams = [];
+                            _.each(streams, function (sub) {
+                                if (sub.q !== q || (sub.q === q && sub.method !== method)) {
+                                    newStreams.push(sub);
+                                }
+                            });
+
+                            streams = removeStream(streams, q, method);
+                            settings.set('streams', streams);
+                            settings.save();
+
+                            $myNode.remove();
+                            var extId = 'flickr-' + q.replace(/[^a-z0-9]/g, '_') + '-' + method.replace(/[^a-z0-9]/g, '_');
+
+                            ext.point("io.ox/portal/widget").disable(extId);
+
+                            require(['plugins/portal/flickr/register'], function (flickr) {
+                                flickr.reload();
+                                ox.trigger("refresh^");
+                            });
+                        }
+                        return false;
+                    });
+                }
             }
         }),
 
         PluginSettingsView = Backbone.View.extend({
             initialize: function (options) {
-                this.template = doT.template(pluginSettingsTemplate);
             },
             render: function () {
-                this.$el.empty().append(this.template({
-                    strings: staticStrings
-                }));
+                this.$el.empty().append(
+                    $('<div>').append(
+                        $('<div class="section">').append(
+                            $('<legend class="sectiontitle">').text(staticStrings.STREAMS),
+                            $('<div class="settings-detail-pane">').append(
+                                $('<div class="io-ox-portal-flickr-settings">')
+                            ),
+                            $('<div class="sectioncontent">').append(
+                                $('<button class="btn" data-action="add" style="margin-right: 15px; ">').text(staticStrings.ADD)
+                            ),
+                            $('<div class="settings sectiondelimiter">')
+                        )
+                    )
+                );
 
                 var that = this;
 
                 function redraw() {
-                    var $listbox = that.$el.find('.listbox');
+                    var $listbox = that.$el.find('.io-ox-portal-flickr-settings');
                     var collection = new Backbone.Collection(streams);
                     $listbox.empty();
 
@@ -116,29 +256,6 @@ define('plugins/portal/flickr/settings/plugin',
                     } else {
                         $listbox.show();
                     }
-
-                    $listbox.sortable({
-                        update: function (event, ui) {
-                            var newStreams = [];
-
-                            _.each($(this).sortable('toArray'), function (value) {
-                                var i = value.indexOf('#');
-                                var method = value.substring(0, i),
-                                    q = value.substring(i + 1);
-
-                                var oldData = _.find(streams, function (stream) { return (stream.q === q && stream.method === method); });
-
-                                if (oldData) {
-                                    newStreams.push(oldData);
-                                }
-                            });
-                            streams = newStreams;
-                            settings.set('streams', streams);
-                            settings.save();
-
-                            ox.trigger("refresh^", [true]);
-                        }
-                    });
                 }
 
                 redraw();
@@ -148,9 +265,7 @@ define('plugins/portal/flickr/settings/plugin',
                 return this;
             },
             events: {
-                'click [data-action="add"]': 'onAdd',
-                'click [data-action="edit"]': 'onEdit',
-                'click [data-action="del"]': 'onDelete'
+                'click [data-action="add"]': 'onAdd'
             },
 
             onAdd: function (args) {
@@ -231,138 +346,6 @@ define('plugins/portal/flickr/settings/plugin',
                         dialog.idle();
                     });
                 });
-            },
-            onEdit: function (args) {
-                var dialog = new dialogs.ModalDialog({
-                    easyOut: true,
-                    async: true
-                });
-
-                var oldQ = this.$el.find('[selected]').data('q'),
-                    oldMethod = this.$el.find('[selected]').data('method'),
-                    oldDescription = this.$el.find('[selected]').data('description');
-
-                if (oldQ && oldMethod) {
-                    oldQ = String(oldQ);
-                    var $q = $('<input>').attr({type: 'text', placeholder: gt('Search')}).val(oldQ);
-                    var $description = $('<input>').attr({type: 'text', placeholder: gt('Description')}).val(oldDescription);
-                    var $method = $('<select>')
-                        .append($('<option>').attr('value', 'flickr.photos.search').text(gt('flickr.photos.search')))
-                        .append($('<option>').attr('value', 'flickr.people.getPublicPhotos').text(gt('flickr.people.getPublicPhotos')))
-                        .val(oldMethod);
-                    var $error = $('<div>').addClass('alert alert-error').hide();
-
-                    var that = this;
-
-                    dialog.header($("<h4>").text(gt('Edit a stream')))
-                        .append($q)
-                        .append($description)
-                        .append($method)
-                        .append($error)
-                        .addButton('cancel', gt('Cancel'))
-                        .addButton('edit', gt('Edit'), null, {classes: 'btn-primary'})
-                        .show();
-
-                    dialog.on('edit', function (e) {
-                        $error.hide();
-
-                        var q = String($.trim($q.val())),
-                            method = $.trim($method.val()),
-                            description = $.trim($description.val()),
-                            deferred;
-
-                        if (method === 'flickr.people.getPublicPhotos') {
-                            deferred = getFlickrNsid(q, $error);
-                        } else {
-                            deferred = $.Deferred();
-                            deferred.resolve();
-                        }
-
-                        deferred.done(function (nsid) {
-                            if (q.length === 0) {
-                                $error.text(gt('Please enter a search-query.'));
-                                $error.show();
-                                dialog.idle();
-                            } else if (description.length === 0) {
-                                $error.text(gt('Please enter a description.'));
-                                $error.show();
-                                dialog.idle();
-                            } else {
-                                ext.point("io.ox/portal/widget").disable('flickr-' + oldQ.replace(/[^a-z0-9]/g, '_') + '-' + oldMethod.replace(/[^a-z0-9]/g, '_'));
-                                streams = removeStream(streams, oldQ, oldMethod);
-
-                                var newStream = {q: q, method: method, description: description};
-
-                                if (nsid) {
-                                    newStream.nsid = nsid;
-                                }
-
-                                streams.push(newStream);
-                                settings.set('streams', streams);
-                                settings.save();
-
-                                ext.point("io.ox/portal/widget").enable('flickr-' + q.replace(/[^a-z0-9]/g, '_') + '-' + method.replace(/[^a-z0-9]/g, '_'));
-
-                                require(['plugins/portal/flickr/register'], function (flickr) {
-                                    flickr.reload();
-                                    that.trigger('redraw');
-                                    ox.trigger("refresh^");
-                                    dialog.close();
-                                });
-                            }
-                        });
-
-                        deferred.fail(function () {
-                            $error.show();
-                            dialog.idle();
-                        });
-                    });
-                }
-            },
-            onDelete: function (args) {
-                var dialog = new dialogs.ModalDialog({
-                    easyOut: true
-                });
-
-                var q = this.$el.find('[selected]').data('q'),
-                    method = this.$el.find('[selected]').data('method');
-
-                if (q && method) {
-                    q = String(q);
-                    var that = this;
-
-                    dialog.header($("<h4>").text(gt('Delete a stream')))
-                        .append($('<span>').text(gt('Do you really want to delete the following stream(s)?')))
-                        .append($('<ul>').append($('<li>').text(q + " (" + method + ")")))
-                        .addButton('cancel', gt('Cancel'))
-                        .addButton('delete', gt('Delete'), null, {classes: 'btn-primary'})
-                        .show()
-                        .done(function (action) {
-                            if (action === 'delete') {
-                                var newStreams = [];
-                                _.each(streams, function (sub) {
-                                    if (sub.q !== q || sub.q === q && sub.method !== method) {
-                                        newStreams.push(sub);
-                                    }
-                                });
-
-                                streams = removeStream(streams, q, method);
-                                settings.set('streams', streams);
-                                settings.save();
-
-                                var extId = 'flickr-' + q.replace(/[^a-z0-9]/g, '_') + '-' + method.replace(/[^a-z0-9]/g, '_');
-
-                                ext.point("io.ox/portal/widget").disable(extId);
-
-                                require(['plugins/portal/flickr/register'], function (flickr) {
-                                    flickr.reload();
-                                    that.trigger('redraw');
-                                    ox.trigger("refresh^");
-                                });
-                            }
-                            return false;
-                        });
-                }
             }
         }),
 
