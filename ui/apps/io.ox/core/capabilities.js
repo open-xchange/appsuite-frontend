b2af7cc3f160e60c117a71346ed9f1879b22e328
Francisco Laguna <francisco.laguna@open-xchange.com>
2012-11-08 12:11:00 +0100

    added capabilities client

---
+++ ui/apps/io.ox/core/capabilities.js
@@ -0,0 +1,84 @@
+/**
+ * All content on this website (including text, images, source
+ * code and any other original works), unless otherwise noted,
+ * is licensed under a Creative Commons License.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * Copyright (C) Open-Xchange Inc., 2006-2011
+ * Mail: info@open-xchange.com
+ *
+ * @author Francisco Laguna <francisco.laguna@open-xchange.com>
+ */
+
+/**
+* Usage:
+* if (capabilities.has('calendar withBackendSupport'))) { ... } else { ... }
+*/
+
+define.async('io.ox/core/capabilities', ['io.ox/core/http'], function (http) {
+	'use strict';
+
+	var def = new $.Deferred();
+
+	
+
+	var capabilities = {};
+
+	var capLookup = {
+		get: function (capName) {
+			return capabilities[capName];
+		},
+		has: function () {
+			var self = this;
+			var result = false;
+			_(arguments).each(function (capDef) {
+				var definition = capDef.split(/\s+/);
+				var name = definition[0];
+				var needsBackend = false;
+				if (definition[1] && (definition[1].toLowerCase() === 'withbackendsupport' || definition[1].toLowerCase() === 'backendsupport')) {
+					needsBackend = true;
+				}
+				var capability = self.get(name);
+				if (capability) {
+					if (needsBackend) {
+						result = capability.backendSupport;
+					} else {
+						result = true;
+					}
+				}
+			});
+
+			return result;
+		}
+	};
+
+	var dummyCapLookup = {
+		get: function (capName) {
+			return {id: capName, backendSupport: true};
+		},
+		has: function (capName) {
+			return !!this.get(capName.split(/\s+/))[0];
+		}
+	};
+
+
+	http.GET({
+		module: 'capabilities',
+		params: {
+			action: 'all'
+		}
+	}).done(function (data) {
+		_(data).each(function (entry) {
+			capabilities[entry.id] = entry;
+		});
+		def.resolve(capLookup);
+	}).fail(function (resp) {
+		console.error("Capabilities subsystem is disabled!");
+		def.resolve(dummyCapLookup);
+	});
+
+
+	return def;
+	
+});
