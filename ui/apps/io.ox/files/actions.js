3f4c3656839458c3f4d027eca6e51a66284a5aca
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2012-12-22 12:00:04 +0100

    Fixed broken file move

--- ui/apps/io.ox/files/actions.js
+++ ui/apps/io.ox/files/actions.js
@@ -317,7 +317,7 @@ define('io.ox/files/actions',
                     .addButton('cancel', gt('Cancel'));
                 dialog.getBody().css('height', '250px');
                 var item = _(list).first(),
-                    tree = new views.FolderList(dialog.getBody(), { type: type });
+                    tree = new views.FolderTree(dialog.getBody(), { type: type, rootFolderId: '9', skipRoot: true });
                 tree.paint();
                 dialog.show(function () {
                     tree.selection.set({ id: item.folder_id || item.folder });
