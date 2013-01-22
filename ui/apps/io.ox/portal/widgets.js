open-xchange/appsuite-frontend

commit 68c4656593a34c01c45581619683be2f2dc60bc1

found missing widgets (Bug 24476)

ui/apps/io.ox/portal/widgets.js
@@ -79,7 +79,7 @@ define('io.ox/portal/widgets',
         getAllTypes: function () {
             return _.chain(availablePlugins)
                 .map(function (id) {
-                    var type = id.replace(/^plugins\/portal\/(\w+)\/register$/, '$1').toLowerCase();
+                    var type = id.replace(/^plugins\/portal\/(\w+)\/register$/, '$1');
                     return ext.point('io.ox/portal/widget/' + type + '/settings').options();
                 })
                 .filter(function (obj) {

commit 0e6ddae518f38e78113e7ec0fd299a30e98d6979

always return the real names of the plugins
This fixes an undocumented regression introduced in
68c4656593a34c01c45581619683be2f2dc60bc1.

The regression was, that the user settings widget on the portal wasn’t
detected as a unique plugin.

ui/apps/io.ox/portal/widgets.js
@@ -95,7 +95,7 @@ define('io.ox/portal/widgets',
         getUsedTypes: function () {
             return collection.pluck('plugin')
                 .map(function (id) {
-                    return id.replace(/^plugins\/portal\/(\w+)\/register$/, '$1').toLowerCase();
+                    return id.replace(/^plugins\/portal\/(\w+)\/register$/, '$1');
                 });
         },
 