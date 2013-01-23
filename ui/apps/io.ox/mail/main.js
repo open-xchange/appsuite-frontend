open-xchange/appsuite-frontend

commit 9f2d31e5e95df102355161750399167c0596f8d0

Fixed O(n²) behavior

ui/apps/io.ox/mail/main.js
@@ -352,9 +352,9 @@ define("io.ox/mail/main",
 
         // customize selection
         grid.selection.unfold = function () {
-            return _(this.get()).inject(function (memo, o) {
-                return memo.concat(isInOpenThreadSummary(o) ? o : api.getThread(o));
-            }, []);
+            return [].concat(_(this.get()).map(function (o) {
+                return isInOpenThreadSummary(o) ? o : api.getThread(o);
+            }));
         };
 
         var showMail, drawMail, drawFail, drawThread;

commit a8cfe23f159aa3a6fa42f3dfa5fed70f48b9b974

fixup for 9f2d31e "Fixed O(n²) behavior"
The old version returned an array of arrays, but the selection should
return an array of mail objects. This fixes a regression introduced in
9f2d31e which was intended to fix O(n²) behaviour.

ui/apps/io.ox/mail/main.js
@@ -383,7 +383,7 @@ define("io.ox/mail/main",
 
         // customize selection
         grid.selection.unfold = function () {
-            return [].concat(_(this.get()).map(function (o) {
+            return _.flatten(_(this.get()).map(function (o) {
                 return isInOpenThreadSummary(o) ? o : api.getThread(o);
             }));
         };