7620056e52c9ed3b3fb151e996ee139673069555
Viktor Pracht <viktor.pracht@open-xchange.com>
2013-01-30 17:14:35 +0100

    Removed debugger statement.

--- ui/apps/io.ox/contacts/api.js
+++ ui/apps/io.ox/contacts/api.js
@@ -49,7 +49,6 @@ define('io.ox/contacts/api',
                 columns: '20,1,500,501,502,505,520,555,556,557,569,602,606,524,592',
                 sort: '609', // magic sort field - ignores asc/desc
                 getData: function (query, opt) {
-                    debugger;
                     return {
                         display_name: query + '*',
                         first_name: query + '*',
