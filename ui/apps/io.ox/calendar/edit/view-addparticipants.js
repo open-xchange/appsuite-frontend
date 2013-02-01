9ab4328acebe100643573e8e905cbdd0b0a65415
Frank Paczynski <frank.paczynski@open-xchange.com>
2013-02-01 16:56:19 +0100

    autocomplete: optimized doublette check (calendar/tasks)

--- ui/apps/io.ox/calendar/edit/view-addparticipants.js
+++ ui/apps/io.ox/calendar/edit/view-addparticipants.js
@@ -65,7 +65,7 @@ define('io.ox/calendar/edit/view-addparticipants',
                             if (obj.type === 1 || obj.type === 5) {
                                 obj.type = 5;
                                 hash[obj.email] = true;
-                                hash[5 + '|' + obj.id] = true;
+                                hash[obj.type + '|' + obj.id] = true;
                             } else {
                                 hash[obj.type + '|' + obj.id] = true;
                             }
@@ -86,7 +86,7 @@ define('io.ox/calendar/edit/view-addparticipants',
                                 type = 3;
                                 break;
                             }
-                            return !hash[type + '|' + recipient.data.id] && !hash[recipient.email];
+                            return !hash[type + '|' + recipient.data.id] && !hash[type + '|' + recipient.data.internal_userid || ''] && !hash[recipient.email];
                         });
 
                         return data;
