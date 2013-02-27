f91398995d933255a782740b105b037d88d1d586
Julian Bäume <julian.baeume@open-xchange.com>
2013-02-27 09:24:38 +0100

    add pubsub model
    add first skeleton of a pubsub model. It needs wiring to the API, so the
    collection and the model classes can be populated.

---
+++ ui/apps/io.ox/core/pubsub/model.js
@@ -0,0 +1,64 @@
+/**
+ * All content on this website (including text, images, source
+ * code and any other original works), unless otherwise noted,
+ * is licensed under a Creative Commons License.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * Copyright (C) Open-Xchange Inc., 2013
+ * Mail: info@open-xchange.com
+ *
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+
+define('io.ox/core/pubsub/model',
+    ['io.ox/core/extensions',
+     'io.ox/backbone/basicModel',
+     'io.ox/core/api/pubsub',
+     'gettext!io.ox/core/pubsub'
+    ],
+    function (ext, BasicModel, api, gt) {
+
+    'use strict';
+
+    var Publication = BasicModel.extend({
+            ref: 'io.ox/core/pubsub/publication/',
+            syncer: {
+                create: function (model) {
+                    console.log(model, arguments);
+                    return $.when().resolve(true);
+                }
+            }
+        }),
+        Subscription = BasicModel.extend({
+            ref: 'io.ox/core/pubsub/subscription/'
+        }),
+        PubSubItems = Backbone.Collection.extend({
+            sync: function (method, collection, options) {
+                if (method !== 'read') return;
+
+                return api.publications.getAll().then(function (res) {
+                    return api.publications.getList(res);
+                }).then(function (res) {
+                    _(res).each(function (obj) {
+                        collection.add(new Publication(obj));
+                    });
+                    return collection;
+                });
+            }
+        });
+
+    ext.point('io.ox/core/pubsub/publication/validation').extend({
+        validate: function (obj, errors) {
+            if (!obj.site) {
+                errors.add(gt('Publication must have a site.'));
+            }
+
+        }
+    });
+
+    return {
+        Publication: Publication,
+        PubSubItems: PubSubItems
+    };
+});
