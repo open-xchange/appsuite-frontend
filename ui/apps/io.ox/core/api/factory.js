a9aff91bf799f8a84093fb1cfbef7c0477365c53
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2013-03-05 13:50:43 +0100

    Removed ineffective appendColumns=true

--- ui/apps/io.ox/core/api/factory.js
+++ ui/apps/io.ox/core/api/factory.js
@@ -50,7 +50,7 @@ define("io.ox/core/api/factory",
             module: "",
             // for all, list, and get
             requests: {
-                all: { action: "all", appendColumns: true },
+                all: { action: "all" },
                 list: { action: "list" },
                 get: { action: "get" },
                 search: { action: "search" },
