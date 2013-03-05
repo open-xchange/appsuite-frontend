43ef17694167b2a2d6b1e506e95d5bfe1c24440c
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2013-03-05 15:05:18 +0100

    Working on free/busy view

--- ui/apps/io.ox/calendar/freebusy/controller.js
+++ ui/apps/io.ox/calendar/freebusy/controller.js
@@ -14,9 +14,10 @@ define('io.ox/calendar/freebusy/controller',
     ['io.ox/core/tk/dialogs',
      'io.ox/calendar/week/view',
      'io.ox/core/api/folder',
+     'io.ox/calendar/edit/view-addparticipants',
      'gettext!io.ox/calendar',
      'settings!io.ox/core',
-     'less!io.ox/calendar/freebusy/style.css'], function (dialogs, WeekView, folderAPI, gt, settings) {
+     'less!io.ox/calendar/freebusy/style.css'], function (dialogs, WeekView, folderAPI, AddParticipantsView, gt, settings) {
 
     'use strict';
 
@@ -27,13 +28,13 @@ define('io.ox/calendar/freebusy/controller',
             var self = this;
 
             // create container node
-            this.$el = $('<div class="abs free-busy-view">').on('dispose', function () {
+            this.$el = $('<div class="abs">').on('dispose', function () {
                 // clean up
-                self.weekview.remove();
+                self.weekView.remove();
             });
 
             this.postprocess = function () {
-                this.weekview
+                this.weekView
                     // hide show all checkbox
                     .showAll(false)
                     // scroll to proper time
@@ -52,21 +53,37 @@ define('io.ox/calendar/freebusy/controller',
             this.collection = new Backbone.Collection([]);
 
             // get new instance of weekview
-            this.weekview = new WeekView({
+            this.weekView = new WeekView({
                 collection: this.collection,
                 mode: 2, // 2 = week:workweek
                 appExtPoint: 'io.ox/calendar/week/view/appointment'
             });
 
-            this.weekview
+            this.weekView
                 // listen to refresh event
                 .on('onRefresh', this.refresh, this)
                 // listen to create event
                 .on('openCreateAppointment', this.create, this);
 
+            // construct auto-complete
+            this.autoCompleteControls = $('<div class="autocomplete-controls input-append pull-left">').append(
+                $('<input type="text" class="add-participant">'),
+                $('<button class="btn" type="button" data-action="add">').append($('<i class="icon-plus">'))
+            );
+
+            // get instance of AddParticipantsView
+            this.autocomplete = new AddParticipantsView({ el: this.autoCompleteControls })
+                .render({
+                    parentSelector: '.free-busy-view > .modal-footer',
+                    autoselect: true,
+                    contacts: true,
+                    resources: true,
+                    distributionlists: true
+                });
+
             this.$el.append(
                 $('<div class="abs participants-view">'),
-                this.weekview.render().$el.addClass('abs calendar-week-view')
+                this.weekView.render().$el.addClass('abs calendar-week-view')
             );
         },
 
@@ -77,18 +94,24 @@ define('io.ox/calendar/freebusy/controller',
                 freebusy;
 
             new dialogs.ModalDialog({ width: width, easyOut: false })
+                .addPrimaryButton('close', gt('Close'))
                 .build(function () {
-                    this.getHeader()
-                        .append(
+
+                    this.getPopup().addClass('free-busy-view');
+
+                    // get free/busy instance
+                    freebusy = new that.FreeBusy(options, this);
+
+                    this.getHeader().append(
                             $('<h4 id="dialog-title">').text(gt('Find free time'))
+                        );
+                    this.getFooter().prepend(
+                            freebusy.autoCompleteControls
                         );
                     this.getContentNode()
                         .css({ height: height + 'px', maxHeight: height + 'px' })
-                        .append(
-                            (freebusy = new that.FreeBusy(options, this)).$el
-                        );
+                        .append(freebusy.$el);
                 })
-                .addPrimaryButton('close', gt('Close'))
                 .busy()
                 .show(function (popup) {
                     var id = settings.get('folder/calendar');
@@ -96,7 +119,7 @@ define('io.ox/calendar/freebusy/controller',
                         // pass folder data over to view (needs this for permission checks)
                         // use fallback data on error
                         data = data.error ? { folder_id: 1, id: id, own_rights: 403710016 } : data;
-                        freebusy.weekview.folder(data);
+                        freebusy.weekView.folder(data);
                         // clean up
                         popup.idle();
                         freebusy.postprocess();
