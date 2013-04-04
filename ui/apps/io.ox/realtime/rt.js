c749e45d03a3815b5e056a078d2140f19b2190a9
Daniel Rentz <daniel.rentz@open-xchange.com>
2013-04-04 14:54:47 +0200

    RT: response may be null

--- ui/apps/io.ox/realtime/rt.js
+++ ui/apps/io.ox/realtime/rt.js
@@ -177,7 +177,7 @@ define.async('io.ox/realtime/rt', ['io.ox/core/extensions', "io.ox/core/event", 
                     api.trigger("receive", stanza);
                     api.trigger("receive:" + stanza.selector, stanza);
                 });
-            } else {
+            } else if (_.isObject(json)) { // json may be null
                 if (api.debug) {
                     console.log("<-", json);
                 }
