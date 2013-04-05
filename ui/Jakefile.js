04ae70a82c1d94b49c4ddd94f3a41b22c3bbb715
Viktor Pracht <viktor.pracht@open-xchange.com>
2013-04-05 13:25:08 +0200

    Fixed theme build for external apps

--- ui/Jakefile.js
+++ ui/Jakefile.js
@@ -471,27 +471,42 @@ utils.merge('manifests/' + pkgName + '.json',
         }
     });
 
-// themes
+// own themes
 
-_.each(utils.list('apps/themes/*/definitions.less'), function (defs) {
-    var dir = path.dirname(defs), theme = path.basename(dir);
-    function subDir() {
-        return path.join.apply(path,
-            [dir].concat(Array.prototype.slice.call(arguments)));
-    }
-    utils.concat(subDir('less/common.css'),
-        ['apps/themes/definitions.less', defs, 'apps/themes/style.less'],
+_.each(utils.list('apps/themes/*/definitions.less'), function(defs) {
+    var dir = path.dirname(defs);
+    utils.concat(path.join(dir, 'less/common.css'),
+        [utils.dest('apps/themes/definitions.less'), defs,
+         utils.dest('apps/themes/style.less')],
         { filter: less.compile });
-    utils.concat(subDir('less/style.css'),
-        ['apps/themes/definitions.less', defs, subDir('style.less')],
+    utils.concat(path.join(dir, 'less/style.css'),
+        [utils.dest('apps/themes/definitions.less'), defs,
+         path.join(dir, 'style.less')],
         { filter: less.compile });
     _.each(utils.list('apps', '**/*.less'), function (file) {
         if (file.slice(0, 7) === 'themes/') return;
-        utils.concat(subDir('less', file),
-            ['apps/themes/definitions.less', defs, path.join('apps', file)],
-            { filter: less.compile });
+        utils.concat(path.join(dir, 'less', file),
+            [utils.dest('apps/themes/definitions.less'), defs,
+             path.join('apps', file)],
+             { filter: less.compile });
     });
 });
+
+// foreign themes
+
+_.each(utils.list(utils.dest('apps/themes'), '*/definitions.less'),
+    function (defs) {
+        if (path.existsSync(path.join('apps/themes', defs))) return;
+        var dir = path.join('apps/themes', path.dirname(defs));
+        _.each(utils.list('apps', '**/*.less'), function (file) {
+            if (file.slice(0, 7) === 'themes/') return;
+            utils.concat(path.join(dir, 'less', file),
+                [utils.dest('apps/themes/definitions.less'),
+                 utils.dest(path.join('apps/themes', defs)),
+                 path.join('apps', file)],
+                 { filter: less.compile });
+        });
+    });
 
 // docs task
 
