88929156b5a539eedcdab3d2feb1f70a2ee9efc4
Christoph Hellweg <christoph.hellweg@open-xchange.com>
2013-04-15 08:42:43 +0200

    Fixed: Bug 25883 - Quoting of emails (maybe) not activated by default?

--- ui/apps/io.ox/mail/settings/defaults.js
+++ ui/apps/io.ox/mail/settings/defaults.js
@@ -21,7 +21,7 @@ define('io.ox/mail/settings/defaults', function () {
             'contactCollectOnMailAccess': false,
             'useFixedWidthFont': false,
             'appendVcard': false,
-            'appendMailTextOnReply': false,
+            'appendMailTextOnReply': true,
             'forwardMessageAs': 'Inline',
             'messageFormat': 'html',
             'lineWrapAfter': '',
