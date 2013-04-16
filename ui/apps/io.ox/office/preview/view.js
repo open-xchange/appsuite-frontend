d5c69e7a84b5d261e86d3d9018f84a4eeebdbbe8
Daniel Rentz <daniel.rentz@open-xchange.com>
2013-04-16 17:58:26 +0200

    merge conflict

--- ui/apps/io.ox/office/preview/view.js
+++ ui/apps/io.ox/office/preview/view.js
@@ -78,35 +78,6 @@ define('io.ox/office/preview/view',
          */
         function initHandler() {
 
-            self.addPane(new Pane(app, { position: 'top', classes: 'inline right', overlay: true, transparent: true, hoverEffect: true })
-                .addViewComponent(new ToolBox(app)
-                    .addGroup('app/quit', new Button({ icon: 'icon-remove', tooltip: gt('Close document') }))
-                )
-            );
-
-            self.addPane(new Pane(app, { position: 'bottom', classes: 'inline right', overlay: true, transparent: true, hoverEffect: true })
-                .addViewComponent(new ToolBox(app)
-                    .addGroup('pages/first',    new Button({ icon: 'docs-first-page',    tooltip: gt('Show first page') }))
-                    .addGroup('pages/previous', new Button({ icon: 'docs-previous-page', tooltip: gt('Show previous page') }))
-                    .addGroup('pages/current',  new Label({                              tooltip: gt('Current page and total page count') }))
-                    .addGroup('pages/next',     new Button({ icon: 'docs-next-page',     tooltip: gt('Show next page') }))
-                    .addGroup('pages/last',     new Button({ icon: 'docs-last-page',     tooltip: gt('Show last page') }))
-                    .addGap()
-                    .addGroup('zoom/dec',     new Button({ icon: 'docs-zoom-out', tooltip: gt('Zoom out') }))
-                    .addGroup('zoom/current', new Label({                         tooltip: gt('Current zoom factor') }))
-                    .addGroup('zoom/inc',     new Button({ icon: 'docs-zoom-in',  tooltip: gt('Zoom in') }))
-                )
-            );
-
-            // listen to specific scroll keys to switch to previous/next page
-            self.getAppPaneNode().on('keydown', keyHandler);
-        }
-
-        /**
-         * Initialization after construction.
-         */
-        function initHandler() {
-
             model = app.getModel();
             self.insertContentNode(pageNode);
 
