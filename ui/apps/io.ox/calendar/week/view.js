ab4fbf8f7347d4efc38a862af6498d667fd65016
tobias.prinz <tobias.prinz@open-xchange.com>
2013-04-24 15:41:31 +0200

    [Bug 26124] Errors were encountered while trying to overwrite '/opt/open-xchange/templates/cp_monthview_list.tmpl'
    Renamed templates to avoid clash with ox6 ones

--- ui/apps/io.ox/calendar/week/view.js
+++ ui/apps/io.ox/calendar/week/view.js
@@ -1533,9 +1533,9 @@ define('io.ox/calendar/week/view',
                 self = this,
                 folder = this.folder(this.folder()),
                 templates = {
-                    'day': {name: 'cp_dayview_table.tmpl'},
-                    'workweek': {name: 'cp_weekview_table.tmpl'},
-                    'week': {name: 'cp_weekview_table.tmpl'}
+                    'day': {name: 'cp_appsuite_dayview_table.tmpl'},
+                    'workweek': {name: 'cp_appsuite_weekview_table.tmpl'},
+                    'week': {name: 'cp_appsuite_weekview_table.tmpl'}
                 };
             var tmpl = templates[self.mode],
                 data = null;
