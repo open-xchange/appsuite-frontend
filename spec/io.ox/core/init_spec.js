570a122bb0ace164162b5531b284f21f8ad02bf5
Julian Bäume <julian.baeume@open-xchange.com>
2013-04-24 12:37:15 +0200

    add initial spec describing the dependencies
    single examples will fail if a dependency is not loaded.

---
+++ spec/io.ox/core/init_spec.js
@@ -0,0 +1,22 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2013 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+define([], function () {
+    describe('checking dependencies', function () {
+        it('loads underscore', function () {
+            expect(_).toBeDefined();
+        });
+
+        it('loads jQuery', function () {
+            expect($).toBeDefined();
+        });
+    });
+});
