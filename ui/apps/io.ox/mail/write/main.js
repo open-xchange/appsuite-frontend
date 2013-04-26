fc167a916d0da9e0eb6a31ab733281a89d19f97d
Julian Bäume <julian.baeume@open-xchange.com>
2013-04-26 14:57:27 +0200

    Fixed: Bug 26196 - "Save Draft" damages mail content
    A fix from Bug 24401 triggered this bug. This problem is solved by using
    the unescapeHTML code from ox6 gui which is will tested against our
    backend.

--- ui/apps/io.ox/mail/write/main.js
+++ ui/apps/io.ox/mail/write/main.js
@@ -563,9 +563,7 @@ define('io.ox/mail/write/main',
                 var attachments = data.attachments ? (_.isArray(data.attachments) ? data.attachments : data.attachments[mail.format] || []) : (undefined),
                     content = attachments && attachments.length ? (attachments[0].content || '') : '';
                 if (mail.format === 'text') {
-                    content = content.replace(/<br( \/)?>\n?/g, '\n');
-                    // backend sends html entities, these need to be transformed into plain text
-                    content = $('<div>').html(content).text();
+                    content = _.unescapeHTML(content.replace(/<br\s*\/?>/g, '\n'));
                 }
                 // image URL fix
                 if (editorMode === 'html') {
