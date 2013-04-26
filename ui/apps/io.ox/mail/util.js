5e7f6073d7d0f63cc104b495a0b9ae1601802e86
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2013-04-26 09:12:33 +0200

    Fixed: Bug 26195 - javascript error when viewing a e-mail with a from: header without address

--- ui/apps/io.ox/mail/util.js
+++ ui/apps/io.ox/mail/util.js
@@ -83,13 +83,11 @@ define('io.ox/mail/util',
             var recipient = $.trim(s), match, name, email;
             if ((match = recipient.match(rRecipient)) !== null) {
                 // case 1: display name plus email address
-                email = match[3].replace(rMailCleanup, '')
-                    .toLowerCase();
+                email = match[3].replace(rMailCleanup, '').toLowerCase();
                 name = match[1].replace(rDisplayNameCleanup, '');
             } else {
                 // case 2: assume plain email address
-                email = recipient.replace(rMailCleanup, '')
-                    .toLowerCase();
+                email = recipient.replace(rMailCleanup, '').toLowerCase();
                 name = email.split(/@/)[0];
             }
             return [name, email];
@@ -189,7 +187,7 @@ define('io.ox/mail/util',
             if (!pair) {
                 return '';
             }
-            var name = pair[0], email = pair[1].toLowerCase(),
+            var name = pair[0], email = String(pair[1] || '').toLowerCase(),
                 display_name = _.isString(name) ? name.replace(rDisplayNameCleanup, '') : '';
             return display_name || email;
         },
