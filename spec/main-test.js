70d65f7c662e5683221a704a35ef07b94ae4e500
Julian Bäume <julian.baeume@open-xchange.com>
2013-05-03 15:03:23 +0200

    add support for pending specs/expected fails
    this is not yet supported by jasmine, so this patch provides a hack to
    support it. This is needed, because shared examples might define
    something that should be implemented, but is not. This will help to
    identify inconsistent API.

--- spec/main-test.js
+++ spec/main-test.js
@@ -36,3 +36,37 @@ window.ox = {
     user_id: 0,
     windowState: 'foreground'
 };
+
+if (jasmine) {
+    /**
+     * Hack pending specs/expected fails
+     *
+     * It’s possible to provide an option parameter to the sharedExamples
+     * call with an attribute 'markedPending'. This must contain an object
+     * with attributes for each (full) spec name representing a truthy value.
+     *
+     * This method will then fail the spec if the test is marked pending and doesn’t fail.
+     * Or it will just skip it otherwise.
+     *
+     * Jasmine from master branch supports pending specs, so once we update, we can change
+     * this to native jasmine.
+     *
+     */
+    jasmine.Spec.prototype.handleExpectedFail = function (markedPending) {
+        if (!markedPending[this.getFullName()]) {
+            return;
+        }
+
+        if (this.results().passed()) {
+            console.error('expected to fail: ' + this.getFullName());
+            this.results_.totalCount++;
+            this.results_.failedCount++;
+            return;
+        }
+        this.results_.skipped = true;
+        this.results_.items_ = [];
+        this.results_.totalCount = 1;
+        this.results_.passedCount = 1;
+        this.results_.failedCount = 0;
+    };
+}
