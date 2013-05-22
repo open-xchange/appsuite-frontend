f889166534fd79dea5fc7dac517894797621f6fc
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2013-05-22 15:43:40 +0200

    US: Redesign of the 'text and html' switch when composing a message

--- ui/apps/io.ox/mail/write/view-main.js
+++ ui/apps/io.ox/mail/write/view-main.js
@@ -504,30 +504,25 @@ define("io.ox/mail/write/view-main",
 
             this.addLink('options', gt('More'));
 
-            var fnChangeFormat = function (e) {
-                e.preventDefault();
-                $(this).addClass('active').siblings().removeClass('active');
-                app.setFormat(e.data.format).done(function () {
-                    app.getEditor().focus();
-                });
-            };
-
             if (!Modernizr.touch) {
                 var format = settings.get('messageFormat', 'html');
-                this.addSection('format', gt('Text format'), true, false)
-                    .append(
-                        $('<div>').addClass('change-format').append(
-                            $('<a>', { href: '#' })
-                                .text(gt('Text'))
-                                .addClass(format === 'text' ? 'active' : '')
-                                .on('click', { format: 'text' }, fnChangeFormat),
-                            $.txt(_.noI18n(' \u00A0\u2013\u00A0 ')), // &ndash;
-                            $('<a>', { href: '#' })
-                                .text(gt('HTML'))
-                                .addClass(format === 'html' || format === 'alternative' ? 'active' : '')
-                                .on('click', { format: 'html' }, fnChangeFormat)
-                        )
-                    );
+                this.addSection('format', gt('Text format'), true, false).append(
+
+                    $('<div class="section-item">').append(
+                        createRadio('format', 'text', gt('Text')),
+                        createRadio('format', 'html', gt('HTML'), true)
+                    )
+                    .css({
+                        paddingTop: '1em',
+                        paddingBottom: '1em'
+                    })
+                    .on('change', 'input', function () {
+                        var radio = $(this), format = radio.val();
+                        app.setFormat(format).done(function () {
+                            app.getEditor().focus();
+                        });
+                    })
+                );
             }
 
             /*
