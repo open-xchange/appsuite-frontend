0d70fa20a78ab42abd15d511d401ec69dade0744
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2013-05-22 11:04:16 +0200

    US: Remove the QR-Code in the contact views by default

--- ui/apps/io.ox/contacts/view-detail.js
+++ ui/apps/io.ox/contacts/view-detail.js
@@ -416,32 +416,32 @@ define("io.ox/contacts/view-detail",
         }
     });
 
-    ext.point("io.ox/contacts/detail").extend({
-        index: 10000,
-        id: 'qr',
-        draw: function (baton) {
-            var data = baton.data;
-            if (Modernizr.canvas && !data.mark_as_distributionlist) {
-                addField("\u00A0", true, this, function (node) {
-                    node.append(
-                        $('<i class="icon-qrcode">'), $.txt(' '),
-                        $("<a>", { href: '#' })
-                        .text(gt('Show QR code'))
-                        .on("click", function (e) {
-                            e.preventDefault();
-                            node.empty().busy();
-                            require(["io.ox/contacts/view-qrcode"], function (qr) {
-                                var vc = qr.getVCard(data);
-                                node.idle().qrcode(vc);
-                                vc = node = qr = null;
-                            });
-                        })
-                    );
-                });
-                addField("", "\u00A0", this);
-            }
-        }
-    });
+    // ext.point("io.ox/contacts/detail").extend({
+    //     index: 10000,
+    //     id: 'qr',
+    //     draw: function (baton) {
+    //         var data = baton.data;
+    //         if (Modernizr.canvas && !data.mark_as_distributionlist) {
+    //             addField("\u00A0", true, this, function (node) {
+    //                 node.append(
+    //                     $('<i class="icon-qrcode">'), $.txt(' '),
+    //                     $("<a>", { href: '#' })
+    //                     .text(gt('Show QR code'))
+    //                     .on("click", function (e) {
+    //                         e.preventDefault();
+    //                         node.empty().busy();
+    //                         require(["io.ox/contacts/view-qrcode"], function (qr) {
+    //                             var vc = qr.getVCard(data);
+    //                             node.idle().qrcode(vc);
+    //                             vc = node = qr = null;
+    //                         });
+    //                     })
+    //                 );
+    //             });
+    //             addField("", "\u00A0", this);
+    //         }
+    //     }
+    // });
 
     ext.point("io.ox/contacts/detail").extend({
         index: 20000,
