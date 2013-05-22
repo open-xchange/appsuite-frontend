81a78544a08b39c2a9234dffdaa81fe38c85effb
Alexander Quast <alexander.quast@open-xchange.com>
2013-05-22 14:53:39 +0200

    fixed test for indexddb on Android devices
    All Android 4.x devices running a stock browser could not login
    to App Suite due to an old indexdbb implementation. This version
    of indexddb did not work with our indexddb-cache implementation. For all
    devices using the old spec we now force localstorage instead.

--- ui/apps/io.ox/core/cache/indexeddb.js
+++ ui/apps/io.ox/core/cache/indexeddb.js
@@ -23,7 +23,14 @@ define.async('io.ox/core/cache/indexeddb', ['io.ox/core/extensions'], function (
         opened,
         that;
 
-    if ((defunct = !(Modernizr.indexeddb && window.indexedDB))) {
+    // Android 4.1.x do have a indexddb implementation in their stock browser!
+    // Cool, but this implementation is based on old spec of indexddb. That's not so cool
+    // and will make the script down here break on each newer Android device.
+    // So we force localsstorage for Android 4.x devices even if modernizr tells us about
+    // indexddb support on theses devices
+
+    // we test for this by looking for the IDBVersionChangeEvent which these devices do not have
+    if ((defunct = !(window.IDBVersionChangeEvent !== undefined && Modernizr.indexeddb && window.indexedDB))) {
         return $.when();
     }
 
