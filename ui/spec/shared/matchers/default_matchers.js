9457e07e9ba1aa67d4608d5e98acf7f64236ea4f
Frank Paczynski <frank.paczynski@open-xchange.com>
2013-06-03 11:06:15 +0200

    jasmine: listed default and jasmin-sion matchers

--- ui/spec/shared/matchers/default_matchers.js
+++ ui/spec/shared/matchers/default_matchers.js
@@ -11,23 +11,55 @@
  */
 
 /*
-taken from jasmine wiki: https://github.com/pivotal/jasmine/wiki/Matchers
+    jasmine default matchers
+    ========================
+    https://github.com/pivotal/jasmine/wiki/Matchers
 
-expect(function(){fn();}).toThrow(e);
-expect(x).toBe(y);
-expect(x).toBeDefined();
-expect(x).toBeFalsy();
-expect(x).toBeGreaterThan(y);
-expect(x).toBeLessThan(y);
-expect(x).toBeNull();
-expect(x).toBeTruthy();
-expect(x).toBeUndefined();
-expect(x).toContain(y);
-expect(x).toEqual(y);
-expect(x).toMatch(pattern);
+    expect(function(){fn();}).toThrow(e);
+    expect(x).toBe(y);
+    expect(x).toBeDefined();
+    expect(x).toBeFalsy();
+    expect(x).toBeGreaterThan(y);
+    expect(x).toBeLessThan(y);
+    expect(x).toBeNull();
+    expect(x).toBeTruthy();
+    expect(x).toBeUndefined();
+    expect(x).toContain(y);
+    expect(x).toEqual(y);
+    expect(x).toMatch(pattern);
 
-hint: Every matcher's criteria can be inverted by prepending '.not'
+    hint: Every matcher's criteria can be inverted by prepending '.not'
 */
 
-//inspiration for other matchers can be found here:
-//https://github.com/JamieMason/Jasmine-Matchers
+/*
+    jasmin-sinon matchers
+    =====================
+    https://github.com/froots/jasmine-sinon
+
+    Sinon.JS property / method   jasmine-sinon matcher
+    ---------------------------- --------------------------
+    called                       toHaveBeenCalled
+    calledOnce                   toHaveBeenCalledOnce
+    calledTwice                  toHaveBeenCalledTwice
+    calledThrice                 toHaveBeenCalledThrice
+    calledBefore()               toHaveBeenCalledBefore()
+    calledAfter()                toHaveBeenCalledAfter()
+    calledOn()                   toHaveBeenCalledOn()
+    alwaysCalledOn()             toHaveBeenAlwaysCalledOn()
+    calledWith()                 toHaveBeenCalledWith()
+    alwaysCalledWith()           toHaveBeenAlwaysCalledWith()
+    calledWithExactly()          toHaveBeenCalledWithExactly()
+    alwaysCalledWithExactly()    toHaveBeenAlwaysCalledWithExactly()
+    calledWithMatch()            toHaveBeenCalledWithMatch()
+    alwaysCalledWithMatch()      toHaveBeenAlwaysCalledWithMatch()
+    returned()                   toHaveReturned()
+    alwaysReturned()             toHaveAlwaysReturned()
+    threw()                      toHaveThrown()
+    alwaysThrew()                toHaveAlwaysThrown()
+*/
+
+/*
+    inspiration for other matchers
+    ==============================
+    https://github.com/JamieMason/Jasmine-Matchers
+*/
