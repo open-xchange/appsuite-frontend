37936ff15882fbb8255eb9e686996fe482f958a3
David Bauer <david.bauer@open-xchange.com>
2013-06-05 14:58:58 +0200

    User can edit settings via keyboard - WIP
    Fixed actions / Default selection

--- ui/apps/io.ox/mail/settings/signatures/register.js
+++ ui/apps/io.ox/mail/settings/signatures/register.js
@@ -15,13 +15,15 @@ define('io.ox/mail/settings/signatures/register',
     ['io.ox/core/extensions',
      'gettext!io.ox/mail',
      'settings!io.ox/mail',
-     'less!io.ox/mail/settings/signatures/style.less'], function (ext, gt, settings) {
+     'io.ox/core/tk/dialogs',
+     'io.ox/core/api/snippets',
+     'less!io.ox/mail/settings/signatures/style.less'], function (ext, gt, settings, dialogs, snippets) {
 
     'use strict';
 
     function fnMigrateClassicSignatures() {
         var def = $.Deferred();
-        require(['io.ox/core/config', 'io.ox/core/api/snippets'], function (config, snippets) {
+        require(['io.ox/core/config'], function (config) {
             var classicSignatures = config.get('gui.mail.signatures');
 
             var deferreds = _(classicSignatures).map(function (classicSignature) {
@@ -65,216 +67,204 @@ define('io.ox/mail/settings/signatures/register',
             }
         }
 
-        require(['io.ox/core/tk/dialogs'], function (dialogs) {
-            var $name, $signature, $insertion, popup, $error;
+        var $name, $signature, $insertion, popup, $error;
 
-            popup = new dialogs.SidePopup({
-                modal: true
-            })
-            .show(evt, function ($pane) {
-                if (_.isString(signature.misc)) { signature.misc = JSON.parse(signature.misc); }
-                var $entirePopup = $pane.closest('.io-ox-sidepopup');
-                $('<form>').append(
-                    $('<div class="row-fluid">').append(
-                        $name = $('<input type="text" class="span12">').attr('placeholder', gt('Name'))
-                    ),
-                    $error = $('<div>').addClass('help-block error'),
-                    $('<div class="row-fluid">').append(
-                        $signature = $('<textarea class="span12" rows="10">').on('keydown', function (e) {
-                            if (e.which === 38 || e.which === 40) {//if arrowkey up and down are pressed the settingsmenu would move to the next item for exsample mails
-                                e.stopPropagation();//this must be prevented to allow the use of the arrowkeys properly in the textarea see bug:25114
-                            }
-                        })
-                    ),
-                    $('<div class="row-fluid">').append(
-                        $('<label>').text(gt('Signature insertion:')),
-                        $insertion = $('<select>').append(
-                            $('<option value="above">').text(gt('Above content')),
-                            $('<option value="below">').text(gt('Below content')).attr('selected', true)
-                        )
+        popup = new dialogs.SidePopup({
+            modal: true
+        })
+        .show(evt, function ($pane) {
+            if (_.isString(signature.misc)) { signature.misc = JSON.parse(signature.misc); }
+            var $entirePopup = $pane.closest('.io-ox-sidepopup');
+            $('<form>').append(
+                $('<div class="row-fluid">').append(
+                    $name = $('<input type="text" class="span12">').attr('placeholder', gt('Name'))
+                ),
+                $error = $('<div>').addClass('help-block error'),
+                $('<div class="row-fluid">').append(
+                    $signature = $('<textarea class="span12" rows="10">').on('keydown', function (e) {
+                        if (e.which === 38 || e.which === 40) {//if arrowkey up and down are pressed the settingsmenu would move to the next item for exsample mails
+                            e.stopPropagation();//this must be prevented to allow the use of the arrowkeys properly in the textarea see bug:25114
+                        }
+                    })
+                ),
+                $('<div class="row-fluid">').append(
+                    $('<label>').text(gt('Signature insertion:')),
+                    $insertion = $('<select>').append(
+                        $('<option value="above">').text(gt('Above content')),
+                        $('<option value="below">').text(gt('Below content')).attr('selected', true)
                     )
-                ).appendTo($pane);
+                )
+            ).appendTo($pane);
 
-                $name.val(signature.displayname);
-                $signature.val(signature.content);
-                if (_.isObject(signature.misc) && signature.misc.insertion) {
-                    $insertion.val(signature.misc.insertion);
+            $name.val(signature.displayname);
+            $signature.val(signature.content);
+            if (_.isObject(signature.misc) && signature.misc.insertion) {
+                $insertion.val(signature.misc.insertion);
+            }
+
+            _.defer(function () {
+                if (signature.displayname) {
+                    $signature.select();
+                } else {
+                    $name.select();
                 }
+            });
 
-                _.defer(function () {
-                    if (signature.displayname) {
-                        $signature.select();
-                    } else {
-                        $name.select();
-                    }
-                });
+            // Replace the buttons
+            var $buttonDiv = $entirePopup.find('.io-ox-sidepopup-close');
 
-                // Replace the buttons
-                var $buttonDiv = $entirePopup.find('.io-ox-sidepopup-close');
+            $buttonDiv.empty();
 
-                $buttonDiv.empty();
+            function busy() {
+                dialogs.busy($entirePopup);
+            }
 
-                function busy() {
-                    dialogs.busy($entirePopup);
-                }
+            function idle() {
+                dialogs.idle($entirePopup);
+            }
 
-                function idle() {
-                    dialogs.idle($entirePopup);
-                }
+            $name.on('change', function () {
+                validateField($name, $error);
+            });
 
-                $name.on('change', function () {
-                    validateField($name, $error);
-                });
+            $buttonDiv.append(
+                $('<button class="btn">').text(gt('Discard')).on('click', function () {
+                    popup.close();
+                    return false;
+                }),
+                $('<button class="btn btn-primary">').text(gt('Save')).on('click', function () {
+                    if ($name.val() !== '') {
+                        busy();
 
-                $buttonDiv.append(
-                    $('<button class="btn">').text(gt('Discard')).on('click', function () {
-                        popup.close();
-                        return false;
-                    }),
-                    $('<button class="btn btn-primary">').text(gt('Save')).on('click', function () {
-                        if ($name.val() !== '') {
-                            busy();
+                        if (signature.id && _.isString(signature.misc)) { signature.misc = JSON.parse(signature.misc); }
 
-                            if (signature.id && _.isString(signature.misc)) { signature.misc = JSON.parse(signature.misc); }
+                        var update = signature.id ? {} : {type: 'signature', module: 'io.ox/mail', displayname: '', content: '', misc: {insertion: 'below'}};
 
-                            var update = signature.id ? {} : {type: 'signature', module: 'io.ox/mail', displayname: '', content: '', misc: {insertion: 'below'}};
+                        if ($signature.val() !== signature.content) {
+                            update.content = $signature.val();
+                        }
 
-                            if ($signature.val() !== signature.content) {
-                                update.content = $signature.val();
-                            }
-
-                            if ($name.val() !== signature.displayname) {
-                                update.displayname = $name.val();
-                            }
-
-
-                            update.misc = { insertion: $insertion.val() };
-
-                            update.id = signature.id;
-
-                            require(['io.ox/core/api/snippets'], function (snippets) {
-                                var def = null;
-                                if (signature.id) {
-                                    def = snippets.update(update);
-                                } else {
-                                    def = snippets.create(update);
-                                }
-                                def.done(function (resp) {
-                                    idle();
-                                    popup.close();
-                                }).fail(require('io.ox/core/notifications').yell);
-                            });
-
-                            return false;
-                        } else {
-                            validateField($name, $error);
+                        if ($name.val() !== signature.displayname) {
+                            update.displayname = $name.val();
                         }
 
 
-                    })
-                );
-            });
+                        update.misc = { insertion: $insertion.val() };
 
+                        update.id = signature.id;
+
+                        var def = null;
+                        if (signature.id) {
+                            def = snippets.update(update);
+                        } else {
+                            def = snippets.create(update);
+                        }
+                        def.done(function (resp) {
+                            idle();
+                            popup.close();
+                        }).fail(require('io.ox/core/notifications').yell);
+
+                        return false;
+                    } else {
+                        validateField($name, $error);
+                    }
+
+
+                })
+            );
         });
-
     }
 
     function fnImportSignatures(evt, signatures) {
 
-
-        require(['io.ox/core/tk/dialogs', 'io.ox/core/api/snippets'], function (dialogs, snippets) {
-
-            var popup = new dialogs.SidePopup({
-                modal: true
-            })
-            .show(evt, function ($pane) {
-                $pane.addClass('io-ox-signature-import');
-                var $container;
-                var $entirePopup = $pane.closest('.io-ox-sidepopup');
+        var popup = new dialogs.SidePopup({
+            modal: true
+        })
+        .show(evt, function ($pane) {
+            $pane.addClass('io-ox-signature-import');
+            var $container;
+            var $entirePopup = $pane.closest('.io-ox-sidepopup');
 
 
-                function busy() {
-                    dialogs.busy($entirePopup);
-                }
+            function busy() {
+                dialogs.busy($entirePopup);
+            }
 
-                function idle() {
-                    dialogs.idle($entirePopup);
-                }
+            function idle() {
+                dialogs.idle($entirePopup);
+            }
 
-                $container = $('<table>').appendTo($pane);
-                _(signatures).each(function (classicSignature, index) {
+            $container = $('<table>').appendTo($pane);
+            _(signatures).each(function (classicSignature, index) {
 
-                    var $row = $('<tr>').addClass('sig-row').appendTo($container);
-                    var preview = (classicSignature.signature_text || '')
-                                    .replace(/\s\s+/g, ' ') // remove subsequent white-space
-                                    .replace(/(\W\W\W)\W+/g, '$1 '); // reduce special char sequences
+                var $row = $('<tr>').addClass('sig-row').appendTo($container);
+                var preview = (classicSignature.signature_text || '')
+                                .replace(/\s\s+/g, ' ') // remove subsequent white-space
+                                .replace(/(\W\W\W)\W+/g, '$1 '); // reduce special char sequences
 
-                    $row.append(
-                        $('<td>').css({width: '10%', textAlign: 'center'}).append(
-                            $('<input type="checkbox">').attr('data-index', index)
-                        ),
-                        $('<td>').css({width: '80%', padding: '10px'}).append(
-                            classicSignature.signature_name, $('<br>'),
-                            $('<div>').text(gt.noI18n(preview)).addClass('classic-sig-preview')
-                        )
-                    );
+                $row.append(
+                    $('<td>').css({width: '10%', textAlign: 'center'}).append(
+                        $('<input type="checkbox">').attr('data-index', index)
+                    ),
+                    $('<td>').css({width: '80%', padding: '10px'}).append(
+                        classicSignature.signature_name, $('<br>'),
+                        $('<div>').text(gt.noI18n(preview)).addClass('classic-sig-preview')
+                    )
+                );
 
-                    var specializedClick = false;
-                    $row.find(':checkbox').on('click', function (e) {
-                        specializedClick = true;
-                    });
-
-                    $row.on('click', function () {
-                        if (specializedClick) {
-                            specializedClick = false;
-                            return;
-                        }
-                        var $checkbox = $(this).find(':checkbox');
-                        $checkbox.attr('checked', !$checkbox.attr('checked'));
-                    });
+                var specializedClick = false;
+                $row.find(':checkbox').on('click', function (e) {
+                    specializedClick = true;
                 });
 
-                $pane.append($('<a href="#">').text(gt('Select all')).on('click', function () {
-                    $container.find(':checkbox').each(function () {
-                        if (!$(this).attr('checked')) {
-                            $(this).attr('checked', 'checked');
+                $row.on('click', function () {
+                    if (specializedClick) {
+                        specializedClick = false;
+                        return;
+                    }
+                    var $checkbox = $(this).find(':checkbox');
+                    $checkbox.attr('checked', !$checkbox.attr('checked'));
+                });
+            });
+
+            $pane.append($('<a href="#">').text(gt('Select all')).on('click', function () {
+                $container.find(':checkbox').each(function () {
+                    if (!$(this).attr('checked')) {
+                        $(this).attr('checked', 'checked');
+                    }
+                });
+                return false;
+            }));
+
+            $pane.append($('<br>'), $('<br>'), $('<br>'));
+
+            var $button = $('<button class="btn btn-primary">')
+                .text(gt('Import signatures')).appendTo($pane);
+
+            $button.on('click', function () {
+                busy();
+                var deferreds = [];
+                $container.find(':checked').each(function () {
+                    var index = $(this).data('index');
+                    var classicSignature = signatures[index];
+
+                    deferreds.push(
+                        snippets.create({
+
+                        type: 'signature',
+                        module: 'io.ox/mail',
+                        displayname: classicSignature.signature_name,
+                        content: classicSignature.signature_text,
+                        meta: {
+                            imported: classicSignature
                         }
-                    });
-                    return false;
-                }));
-
-                $pane.append($('<br>'), $('<br>'), $('<br>'));
-
-                var $button = $('<button class="btn btn-primary">')
-                    .text(gt('Import signatures')).appendTo($pane);
-
-                $button.on('click', function () {
-                    busy();
-                    var deferreds = [];
-                    $container.find(':checked').each(function () {
-                        var index = $(this).data('index');
-                        var classicSignature = signatures[index];
-
-                        deferreds.push(
-                            snippets.create({
-
-                            type: 'signature',
-                            module: 'io.ox/mail',
-                            displayname: classicSignature.signature_name,
-                            content: classicSignature.signature_text,
-                            meta: {
-                                imported: classicSignature
-                            }
-                        }).fail(require('io.ox/core/notifications').yell));
-                    });
-
-                    $.when.apply($, deferreds).always(function () { idle(); popup.close(); });
+                    }).fail(require('io.ox/core/notifications').yell));
                 });
 
+                $.when.apply($, deferreds).always(function () { idle(); popup.close(); });
             });
 
         });
-
     }
 
     ext.point('io.ox/mail/settings/detail').extend({
@@ -282,7 +272,7 @@ define('io.ox/mail/settings/signatures/register',
         index: 300,
         draw: function () {
             var $node = this;
-            require(['io.ox/core/api/snippets', 'io.ox/core/config'], function (snippets, config) {
+            require(['io.ox/core/config'], function (config) {
                 var $list, signatures;
 
                 function fnDrawAll() {
@@ -321,19 +311,19 @@ define('io.ox/mail/settings/signatures/register',
                     // List
                     $list = $('<ul class="settings-list">')
                     .on('click', 'a[data-action=edit]', function (e) {
-                        var id = $(this).parent().attr('data-id');
+                        var id = $(this).closest('li').attr('data-id');
                         fnEditSignature(e, signatures[id]);
                     })
                     .on('click', 'a[data-action=delete]', function (e) {
-                        var id = $(this).parent().attr('data-id');
+                        var id = $(this).closest('li').attr('data-id');
                         snippets.destroy(id).fail(require('io.ox/core/notifications').yell);
                     })
                     .on('click', 'a[data-action=default]', function (e) {
-                        var id = $(this).parent().attr('data-id');
+                        var id = $(this).closest('li').attr('data-id');
                         settings.set('defaultSignature', id).save();
                         $list.find('li').removeClass('default')
                             .find('a[data-action="default"]').text(gt('Set as default'));
-                        $(this).parent().addClass('default')
+                        $(this).closest('li').addClass('default')
                             .find('a[data-action="default"]').text(gt('(Default)'));
                     })
                     .appendTo($node);
