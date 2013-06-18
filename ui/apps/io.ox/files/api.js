10ebe25079540958871d9c8f0be9537081520720
David Bauer <david.bauer@open-xchange.com>
2013-06-18 11:51:39 +0200

    Fixed: Bug 27087 - All new notes called 'blob'

--- ui/apps/io.ox/files/api.js
+++ ui/apps/io.ox/files/api.js
@@ -323,7 +323,7 @@ define('io.ox/files/api',
 
             return http.UPLOAD({
                     module: 'files',
-                    params: { action: 'new' },
+                    params: { action: 'new', filename: options.filename },
                     data: formData,
                     fixPost: true
                 })
@@ -376,7 +376,7 @@ define('io.ox/files/api',
 
         return http.UPLOAD({
                 module: 'files',
-                params: { action: 'update', timestamp: _.now(), id: options.id },
+                params: { action: 'update', timestamp: _.now(), id: options.id, filename: options.filename },
                 data: formData,
                 fixPost: true // TODO: temp. backend fix
             })
