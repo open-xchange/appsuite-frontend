dfec38627de8c727d09025f31021c36845c35c74
Julian Bäume <julian.baeume@open-xchange.com>
2013-07-02 19:46:46 +0200

    add first specs for different emoji collections
    yay, even more emoji specs

---
+++ ui/spec/emoji/collections_spec.js
@@ -0,0 +1,69 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2013 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+define([
+    'moxiecode/tiny_mce/plugins/emoji/main',
+    'settings!io.ox/mail/emoji'
+], function (emoji, settings) {
+    "use strict";
+
+    describe('Emoji support', function () {
+        describe('with different collections', function () {
+            it('should have a default collection', function () {
+                settings.set({defaultCollection: 'unified'});
+                this.emoji = emoji.getInstance();
+
+                expect(this.emoji.getCollection()).toBe('unified');
+            });
+
+            it('should be possible to have a user defined collection', function () {
+                settings.set({userCollection: 'unified'});
+                this.emoji = emoji.getInstance();
+
+                expect(this.emoji.getCollection()).toBe('unified');
+            });
+
+            it('should parse the availableCollections setting', function () {
+                //FIXME: reload emoji module once reloading is possible
+                //run some initialisation code (run during module loading) again,
+                //to be able to change collections during run-time
+                function parseCollections() {
+                    var e = settings.get('availableCollections', '');
+                    return _(e.split(','))
+                        .chain()
+                        .map(function (collection) {
+                            return collection.trim();
+                        })
+                        .compact()
+                        .value();
+                }
+
+                function fakeInstance() {
+                    var i = emoji.getInstance();
+                    i.collections = parseCollections();
+                    return i;
+                }
+
+                settings.set({availableCollections: ''});
+
+                expect(fakeInstance().collections).toEqual([]);
+
+                settings.set({availableCollections: 'unified'});
+
+                expect(fakeInstance().collections).toEqual(['unified']);
+
+                settings.set({availableCollections: 'unified,bar'});
+
+                expect(fakeInstance().collections).toEqual(['unified', 'bar']);
+            });
+        });
+    });
+});
