04e913513bdb93f614bc90b9ef1c573b3699f010
Julian Bäume <julian.baeume@open-xchange.com>
2013-07-03 17:26:56 +0200

    add failing spec for require static files bug
    needs still to be fixed, though ;)

---
+++ ui/spec/lib/require_spec.js
@@ -0,0 +1,31 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2013 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+define(function () {
+    describe('require static files', function () {
+        afterEach(function () {
+            this.handleExpectedFail({'require static files should fetch static files via backend.': true});
+        });
+
+        it('should fetch static files via backend', function () {
+            var def = require(['apps/file/doesnt/exist.js']);
+
+            waitsFor(function () {
+                return def.state() !== 'pending';
+            }, 'loading fake file failed', 500);
+
+            def.fail(function (err) {
+                expect(err.requireType).toBe('define');
+                expect(err.message).toBe('Could not read \'file/doesnt/exist.js\'');
+            });
+        });
+    });
+});
