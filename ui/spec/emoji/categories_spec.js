b05ad22d9647e5d7b790998b292bc205d3cec156
Julian Bäume <julian@svg4all.de>
2013-07-04 14:13:06 +0200

    add more specs for emoji categories
    more code coverage === better code

--- ui/spec/emoji/categories_spec.js
+++ ui/spec/emoji/categories_spec.js
@@ -15,6 +15,8 @@ define([
 ], function (emoji, settings) {
     "use strict";
 
+    var pile_unicode = '\ud83d\udca9';
+
     describe('Emoji support', function () {
         describe('provides collections with categories', function () {
             beforeEach(function () {
@@ -30,6 +32,80 @@ define([
                 this.settingsSpy.restore();
             });
 
+            it('should contain some category names', function () {
+                var categories = _(this.emoji.getCategories()).pluck('name');
+
+                expect(categories).toContain('Nature');
+                expect(categories).toContain('People');
+                expect(categories).toContain('Symbols');
+                expect(categories).toContain('Objects');
+                expect(categories).toContain('Places');
+            });
+
+            it('should contain metadata for a category', function () {
+                var data = this.emoji.getCategories()[0];
+
+                expect(data.iconClass).toBeDefined();
+                expect(data.name).toBeDefined();
+                expect(data.title).toBeDefined();
+            });
+
+            it('should provide all icons for a specific category', function () {
+                var peopleIcons = this.emoji.iconsForCategory('People'),
+                    pile = _(peopleIcons).where({unicode: pile_unicode})[0];
+
+                expect(this.emoji.icons.length > peopleIcons.length).toBeTruthy();
+                expect(peopleIcons.length).toBe(167);
+                expect(pile).toBeDefined();
+                expect(pile.desc).toBe('pile of poo');
+            });
+
+            it('should have a default category', function () {
+                var defaultCategory = this.emoji.getDefaultCategory();
+
+                expect(defaultCategory).toBe('People');
+            });
+
+            it('should provide a method to test existance of a category', function () {
+                expect(this.emoji.hasCategory('People')).toBeTruthy();
+                expect(this.emoji.hasCategory('nonsense')).toBeFalsy();
+            });
+
+            describe('and meta category for recently used icons', function () {
+                it('should contain a recenty category', function () {
+                    expect(this.emoji.getRecently()).toBeDefined();
+                });
+
+                it('should be possible to reset recenty used icons', function () {
+                    //add item to recently used list
+                    this.emoji.recent(pile_unicode);
+                    expect(_(this.emoji.iconsForCategory('recently')).pluck('desc')).toContain('pile of poo');
+
+                    this.emoji.resetRecents();
+                    expect(this.emoji.iconsForCategory('recently').length).toBe(0);
+                });
+
+                it('should store a maximum of 40 recently used icons', function () {
+                    var emoji = this.emoji;
+                    _(this.emoji.icons)
+                        .chain()
+                        .first(35)
+                        .each(function (icon) {
+                            emoji.recent(icon.unicode);
+                        });
+
+                    expect(this.emoji.iconsForCategory('recently').length).toBe(35);
+
+                    _(this.emoji.icons)
+                        .chain()
+                        .first(50)
+                        .each(function (icon) {
+                            emoji.recent(icon.unicode);
+                        });
+
+                    expect(this.emoji.iconsForCategory('recently').length).toBe(40);
+                });
+            });
         });
     });
 });
