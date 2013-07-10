97324dc0547e7f2c200fd8f3e5f9d4503f14c635
Daniel Rentz <daniel.rentz@open-xchange.com>
2013-07-10 17:29:04 +0200

    OX Preview: add 'View' link for viewable documents

--- ui/apps/io.ox/office/preview/app/fileactions.js
+++ ui/apps/io.ox/office/preview/app/fileactions.js
@@ -11,27 +11,44 @@
  */
 
 define('io.ox/office/preview/app/fileactions',
-    ['io.ox/core/extPatterns/links',
-     'io.ox/office/framework/app/extensionregistry'
-    ], function (links, ExtensionRegistry) {
+    ['io.ox/core/extensions',
+     'io.ox/core/extPatterns/links',
+     'io.ox/office/framework/app/extensionregistry',
+     'gettext!io.ox/office/main'
+    ], function (ext, links, ExtensionRegistry, gt) {
 
     'use strict';
 
-    new links.Action('io.ox/files/actions/open', {
-
-        id: 'officepreview',
-
-        // we just need to be called before 'default'
-        before: 'default',
-
-        // pick items you want to take care of (actually this function is called by underscore's "filter")
+    new links.Action('io.ox/files/actions/office/view', {
+        requires: function (e) {
+            return e.collection.has('one', 'read') && ExtensionRegistry.isViewable(e.context.filename);
+        },
         filter: function (data) {
             return ExtensionRegistry.isViewable(data.filename);
         },
-
         action: function (baton) {
             ox.launch('io.ox/office/preview/main', { action: 'load', file: baton.data });
         }
     });
+
+    ext.point('io.ox/files/links/inline').extend({
+        id: 'disable_open',
+        index: 1,
+        prio: 'hi',
+        ref: 'io.ox/files/actions/open',
+        draw: function (baton) {
+            if (ExtensionRegistry.isViewable(baton.data.filename)) {
+                baton.disable('io.ox/files/links/inline', 'open');
+            }
+        }
+    });
+
+    ext.point('io.ox/files/links/inline').extend(new links.Link({
+        id: 'view',
+        index: 100,
+        prio: 'hi',
+        label: gt('View'),
+        ref: 'io.ox/files/actions/office/view'
+    }));
 
 });
