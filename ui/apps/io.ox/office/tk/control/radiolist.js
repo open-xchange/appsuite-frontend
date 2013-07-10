712d4c8eb7b6b9904f73fc7a3c2454375401acb8
Daniel Rentz <daniel.rentz@open-xchange.com>
2013-07-10 16:18:41 +0200

    Office/TK: set data-value attribute at root node of drop-down list elements, containing all selected item values

--- ui/apps/io.ox/office/tk/control/radiolist.js
+++ ui/apps/io.ox/office/tk/control/radiolist.js
@@ -163,9 +163,9 @@ define('io.ox/office/tk/control/radiolist',
             var // the target caption button
                 captionButton = self.getCaptionButton(),
                 // activate an option button
-                optionButton = Utils.selectOptionButton(self.getItems(), value, equality),
+                selectedButtons = Utils.selectOptionButton(self.getItems(), value, equality),
                 // the options used to create the list item button
-                buttonOptions = optionButton.data('options') || {},
+                buttonOptions = selectedButtons.data('options') || {},
                 // the options used to set the caption of the drop-down menu button
                 captionOptions = _.clone(options),
                 // whether the drop-down button will be highlighted
@@ -176,7 +176,7 @@ define('io.ox/office/tk/control/radiolist',
                 if (_.isFunction(updateHighlightHandler)) {
                     isHighlighted = updateHighlightHandler.call(self, value);
                 } else {
-                    isHighlighted = highlight && (optionButton.length > 0);
+                    isHighlighted = highlight && (selectedButtons.length > 0);
                 }
             }
             Utils.toggleButtons(self.getMenuButton().add(captionButton), isHighlighted);
@@ -191,9 +191,14 @@ define('io.ox/office/tk/control/radiolist',
                 Utils.setControlCaption(captionButton, captionOptions);
             }
 
+            // update the data-value attribute of the group from all selected items
+            self.getNode().attr('data-value', selectedButtons.map(function () {
+                return $(this).attr('data-value');
+            }).get().join(','));
+
             // call custom update handler
             if (_.isFunction(updateCaptionHandler)) {
-                updateCaptionHandler.call(self, captionButton, value, optionButton);
+                updateCaptionHandler.call(self, captionButton, value, selectedButtons);
             }
         }
 
