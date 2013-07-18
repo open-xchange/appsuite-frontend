455c3dda0fd13d391ad18beac5a33e6182dd062a
Francisco Laguna <francisco.laguna@open-xchange.com>
2013-07-18 12:41:11 +0200

    use unchanged document title as file name if present

--- ui/apps/io.ox/editor/main.js
+++ ui/apps/io.ox/editor/main.js
@@ -99,7 +99,7 @@ define('io.ox/editor/main',
 
         getFilename: function () {
             var title = this.getTitle(),
-                filename = String(title || this.getContent().substr(0, 20).split('.')[0] || 'unnamed');
+                filename = String(title || (this.getContent().substr(0, 20).split('.')[0] || 'unnamed')).toLowerCase();
             // has file extension?
             if (!/\.\w{1,4}$/.test(filename)) {
                 filename += '.txt';
