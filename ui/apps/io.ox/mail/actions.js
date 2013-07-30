adfb077ae893a4b5a3081463c36bc52e7d8a3eb2
David Bauer <david.bauer@open-xchange.com>
2013-07-30 09:50:46 +0200

    Fixed: Bug 27483 - [L3] Option to view slideshow does not display if attachments include non-image as well as image file

--- ui/apps/io.ox/mail/actions.js
+++ ui/apps/io.ox/mail/actions.js
@@ -521,7 +521,7 @@ define('io.ox/mail/actions',
         id: 'slideshow',
         requires: function (e) {
             return e.collection.has('multiple') && _(e.context).reduce(function (memo, obj) {
-                return (/\.(gif|bmp|tiff|jpe?g|gmp|png)$/i).test(obj.filename);
+                return memo || (/\.(gif|bmp|tiff|jpe?g|gmp|png)$/i).test(obj.filename);
             }, false);
         },
         multiple: function (list) {
