ef04638f0930f71d1b672656313904b97d6aaeb2
Francisco Laguna <francisco.laguna@open-xchange.com>
2013-07-30 23:30:44 +0900

    alternate implementation of config access. can only be enabled after paths.perfMap is updated everywhere.

--- ui/apps/io.ox/office/tk/config.js
+++ ui/apps/io.ox/office/tk/config.js
@@ -11,6 +11,46 @@
  * @author Daniel Rentz <daniel.rentz@open-xchange.com>
  */
 
+// define('io.ox/office/tk/config', ['settings!io.ox/office'], function (settings) {
+
+//     'use strict';
+//     var // the configuration items of the module 'com.open-xchange.documents'
+//         documentsConfig = settings.get("module");
+
+//     // static class Config ====================================================
+
+//     /**
+//      * Wraps the configuration of the module 'com.open-xchange.documents'.
+//      */
+//     var Config = {};
+
+    
+//      * Returns the complete set of configuration properties.
+//      *
+//      * @returns {Object}
+//      *  The complete set of configuration properties of the module
+//      *  'com.open-xchange.documents'.
+     
+//     Config.get = function () {
+//         return documentsConfig;
+//     };
+
+//     /**
+//      * Returns the value of the configuration property 'debugavailable'.
+//      *
+//      * @returns {Boolean}
+//      *  Whether debug mode is enabled.
+//      */
+//     Config.isDebug = function () {
+//         return !!documentsConfig.debugavailable;
+//     };
+
+//     // exports ================================================================
+
+//     return Config;
+
+// });
+
 define('io.ox/office/tk/config', ['io.ox/core/config'], function (CoreConfig) {
 
     'use strict';
