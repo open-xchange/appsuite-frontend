bd0a78f89953022ab0240ee6c756b5412dea8ce0
Julian Bäume <julian.baeume@open-xchange.com>
2013-07-31 17:52:50 +0200

    add examples for multiple caches
    in practice, there is more than one cache instance active. Those
    instances should work independent from each other.

--- ui/spec/io.ox/core/cacheStorage_spec.js
+++ ui/spec/io.ox/core/cacheStorage_spec.js
@@ -198,6 +198,129 @@ define(['io.ox/core/extensions',
             });
         });
 
+        describe('with multiple caches', function () {
+            beforeEach(function () {
+                this.cache1 = testStorage.getInstance('TEST1');
+                this.cache2 = testStorage.getInstance('TEST2');
+                this.cache3 = testStorage.getInstance('TEST3');
+            });
+
+            it('should initialize an empty cache', function () {
+                expect(this.cache1.keys()).toResolveWith([]);
+                expect(this.cache2.keys()).toResolveWith([]);
+                expect(this.cache3.keys()).toResolveWith([]);
+            });
+
+            describe('and test data', function () {
+                beforeEach(function () {
+                    var done = $.Deferred(),
+                        cache1 = this.cache1,
+                        cache2 = this.cache2,
+                        cache3 = this.cache3;
+
+                    cache1.set('testKey1', 'testValue')
+                    .then(function () {
+                        return cache2.set('testKey2', 'testValue');
+                    })
+                    .then(function () {
+                        return cache3.set('testKey3', 'testValue');
+                    })
+                    .then(function (result) {
+                        done.resolve(result);
+                    });
+
+                    waitsFor(function () {
+                        return done.state() === 'resolved';
+                    }, 'Store and fetch cache values', TIMEOUT);
+                });
+
+                it('should correctly store values in each cache', function () {
+                    var done = $.Deferred(),
+                        cache1 = this.cache1,
+                        cache2 = this.cache2,
+                        cache3 = this.cache3;
+
+                    cache1.keys().then(function (r1) {
+                        return { cache1: r1 };
+                    })
+                    .then(function (result) {
+                        return cache2.keys().then(function (r2) {
+                            return _.extend({ cache2: r2 }, result);
+                        });
+                    })
+                    .then(function (result) {
+                        return cache3.keys().then(function (r3) {
+                            return _.extend({ cache3: r3 }, result);
+                        });
+                    })
+                    .then(function (result) {
+                        done.resolve(result);
+                    });
+
+                    waitsFor(function () {
+                        return done.state() === 'resolved';
+                    }, 'Store and fetch cache values', TIMEOUT);
+
+                    runs(function () {
+                        expect(done).toResolveWith({
+                            cache1: ['testKey1'],
+                            cache2: ['testKey2'],
+                            cache3: ['testKey3']
+                        });
+                    });
+                });
+
+                it('should clear cache1 correctly', function () {
+                    var done,
+                        cache1 = this.cache1;
+
+                    done = cache1.clear().then(function () {
+                        return cache1.keys();
+                    });
+
+                    waitsFor(function () {
+                        return done.state() === 'resolved';
+                    }, 'Clear cache and get keys', TIMEOUT);
+
+                    runs(function () {
+                        expect(done).toResolveWith([]);
+                    });
+                });
+
+                it('should not affect other caches when clearing cache', function () {
+                    var done,
+                        cache1 = this.cache1,
+                        cache2 = this.cache2,
+                        cache3 = this.cache3;
+
+                    done = cache1.clear().then(function () {
+                        return cache1.keys().then(function (r1) {
+                            return { cache1: r1 };
+                        });
+                    }).then(function (result) {
+                        return cache2.keys().then(function (r2) {
+                            return _.extend({ cache2: r2 }, result);
+                        });
+                    }).then(function (result) {
+                        return cache3.keys().then(function (r3) {
+                            return _.extend({ cache3: r3 }, result);
+                        });
+                    });
+
+                    waitsFor(function () {
+                        return done.state() === 'resolved';
+                    }, 'Clear cache and get keys', TIMEOUT);
+
+                    runs(function () {
+                        expect(done).toResolveWith({
+                            cache1: [],
+                            cache2: ['testKey2'],
+                            cache3: ['testKey3']
+                        });
+                    });
+                });
+            });
+        });
     });
     });
 });
