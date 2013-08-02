c768cb83ffe5d3f3d213674b29628a39a30f27a1
Daniel Rentz <daniel.rentz@open-xchange.com>
2013-08-02 19:05:06 +0200

    OX Viewer: remove office code from io.ox/tasks/actions, simplify creation of viewer actions/links

--- ui/apps/io.ox/office/preview/app/fileactions.js
+++ ui/apps/io.ox/office/preview/app/fileactions.js
@@ -10,29 +10,29 @@
  * @author Carsten Driesner <carsten.driesner@open-xchange.com>
  */
 
-define('io.ox/office/preview/app/fileactions',
-    ['io.ox/core/extensions',
-     'io.ox/core/extPatterns/links',
-     'io.ox/office/preview/app/actionshelper',
-     'gettext!io.ox/office/main'
-    ], function (ext, links, ActionsHelper, gt) {
+define('io.ox/office/preview/app/fileactions', ['io.ox/office/preview/app/actionshelper'], function (ActionsHelper) {
 
     'use strict';
 
-    ActionsHelper.createViewerAction('io.ox/files/actions/office/view', function (baton) {
+    var // the identifier of the 'View' action
+        ACTION_ID = 'io.ox/files/actions/office/view';
+
+    // global initialization ==================================================
+
+    // create the action to view the file in OX Viewer
+    ActionsHelper.createViewerAction(ACTION_ID, function (baton) {
         // directly return the data object created by OX Files as file descriptor
         return baton.data;
     });
 
-    ext.point('io.ox/files/links/inline').extend(new links.Link({
-        id: 'office_view',
-        index: 100,
-        prio: 'hi',
-        label: gt('View'),
-        ref: 'io.ox/files/actions/office/view'
-    }));
+    // create a link in the attachment drop-down menu
+    ActionsHelper.createViewerLink('io.ox/files/links/inline', ACTION_ID, { prio: 'hi' });
 
     // disable default 'Open' action defined by OX Files
     ActionsHelper.disableActionForViewable('io.ox/files/actions/open');
+
+    // exports ================================================================
+
+    return;
 
 });
