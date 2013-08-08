3e21495ef1d2bb7c0f30c7a6f96e07aa6e1f4534
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2013-08-08 17:04:21 +0200

    Fixed: Bug 28079 - [L3] No reset option on changing Object permission to "no read permissions"

--- ui/apps/io.ox/core/api/folder.js
+++ ui/apps/io.ox/core/api/folder.js
@@ -362,9 +362,9 @@ define('io.ox/core/api/folder',
                                     // checks if folder is blacklisted
                                     return visible(folder);
                                 },
-                                canRead = function (obj) {
+                                canReadOrIsAdmin = function (obj) {
                                     // read permission?
-                                    return api.can('read', obj);
+                                    return api.can('read', obj) || (perm(obj.own_rights, 28) === 1);
                                 },
                                 addToCache = function (obj) {
                                     // add to folder cache
@@ -379,7 +379,7 @@ define('io.ox/core/api/folder',
                                 folders = _.chain(data[id])
                                     .map(makeObject)
                                     .filter(blacklisted)
-                                    .filter(canRead)
+                                    .filter(canReadOrIsAdmin)
                                     .map(addToCache) // since each doesn't chain
                                     .value();
                                 // empty?
