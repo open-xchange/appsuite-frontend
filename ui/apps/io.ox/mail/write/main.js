baccbc24f95e355e2465cab039e2f811a965e42f
David Bauer <david.bauer@open-xchange.com>
2013-08-29 16:31:32 +0200

    US: The grey screen that shows while dragging...

--- ui/apps/io.ox/mail/write/main.js
+++ ui/apps/io.ox/mail/write/main.js
@@ -296,7 +296,7 @@ define('io.ox/mail/write/main',
             );
 
             if (_.browser.IE === undefined || _.browser.IE > 9) {
-                var dropZone = upload.dnd.createDropZone({'type': 'single'});
+                var dropZone = upload.dnd.createDropZone({'type': 'single', actions: [{id: 'mailAttachment', label: gt('Drop the file anywhere to add attachment')}]});
                 dropZone.on('drop', function (e, file) {
                     view.form.find('input[type=file]').last()
                         .prop('file', file)
