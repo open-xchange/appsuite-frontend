6e0afb0360e739b4b4f1100ce933b53eb1491c23
Carsten Driesner <carsten.driesner@open-xchange.com>
2013-08-30 13:25:14 +0200

    Check for timing problem: #28529#

--- ui/apps/io.ox/office/framework/app/baseapplication.js
+++ ui/apps/io.ox/office/framework/app/baseapplication.js
@@ -1488,19 +1488,26 @@ define('io.ox/office/framework/app/baseapplication',
                 // propagate changed file to the files API
                 return FilesAPI.propagate('change', file).then(function () {
 
-                    return self.getFilterModuleUrl({
-                            action: 'getdocument',
-                            documentformat: format || 'native',
-                            filename: self.getFullFileName(),
-                            mimetype: file.file_mimetype || '',
-                            version: 0, // always use the latest version
-                            nocache: _.uniqueId(), // needed to trick the browser cache (is not evaluated by the backend)
-                            source: file.source,// document source: file|mail|task
-                            folder: file.folder_id,
-                            id: file.id,
-                            module: file.module,
-                            attachment: file.attached
-                        });
+                    var def = $.Deferred();
+
+                    self.executeDelayed(function () {
+
+                        def.resolve(self.getFilterModuleUrl({
+                                action: 'getdocument',
+                                documentformat: format || 'native',
+                                filename: self.getFullFileName(),
+                                mimetype: file.file_mimetype || '',
+                                version: 0, // always use the latest version
+                                nocache: _.uniqueId(), // needed to trick the browser cache (is not evaluated by the backend)
+                                source: file.source,// document source: file|mail|task
+                                folder: file.folder_id,
+                                id: file.id,
+                                module: file.module,
+                                attachment: file.attached
+                            }));
+                    }, { delay: 5000 });
+
+                    return def;
                 });
             }
 
