5d6bef7378595c2a38b2f2faa8d4f6a436fddf59
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2013-09-06 16:01:44 +0200

    Fixed: Bug 28681 - Malformed email handled incorrectly

--- ui/apps/io.ox/mail/view-detail.js
+++ ui/apps/io.ox/mail/view-detail.js
@@ -1046,8 +1046,12 @@ define('io.ox/mail/view-detail',
         id: 'fromlist',
         draw: function (baton) {
             var data = baton.data, list = util.serializeList(data, 'from'), node;
-            this.append($('<div class="from list">').append(list.removeAttr('style')));
-            if (ox.ui.App.get('io.ox/mail').length) {
+            this.append(
+                $('<div class="from list">').append(
+                    baton.data.from ? list.removeAttr('style') : $.txt('\u00A0')
+                )
+            );
+            if (baton.data.from && ox.ui.App.get('io.ox/mail').length) {
                 node = list.last();
                 node.after(
                     $('<i class="icon-search">').on('click', node.data('person'), searchSender)
