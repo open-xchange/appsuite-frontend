ebb243d2f3a16d8aef1f685a0075405e517bf3ad
Christoph Hellweg <christoph.hellweg@open-xchange.com>
2013-09-10 11:31:43 +0200

    Fixed: Bug 28751 - Order of buttons (accept/decline) different

--- ui/apps/io.ox/calendar/invitations/register.js
+++ ui/apps/io.ox/calendar/invitations/register.js
@@ -71,7 +71,7 @@ define('io.ox/calendar/invitations/register',
         3: gt("You have tentatively accepted the appointment")
     };
 
-    var priority = ['update', 'ignore', 'decline', 'tentative', 'accept', 'declinecounter', 'accept_and_replace', 'accept_and_ignore_conflicts', 'accept_party_crasher', 'create', 'delete'];
+    var priority = ['update', 'ignore', 'accept', 'tentative', 'decline', 'declinecounter', 'accept_and_replace', 'accept_and_ignore_conflicts', 'accept_party_crasher', 'create', 'delete'];
 
 
     function discoverIMipAttachment(baton) {
