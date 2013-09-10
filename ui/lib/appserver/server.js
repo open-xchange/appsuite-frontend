83320ca6afd2a6a51e24882de1ed1244cfe347b4
Julian Bäume <julian.baeume@open-xchange.com>
2013-09-10 15:30:24 +0200

    move some methods into commons module
    will be used around the newly created middleware modules

--- ui/lib/appserver/server.js
+++ ui/lib/appserver/server.js
@@ -12,18 +12,14 @@
  * @author Julian Bäume <julian.baeume@open-xchange.com>
  */
 
-function normalizePath(str) {
-    if (str.slice(-1) !== '/') str += '/';
-
-    return str;
-}
-
 function create(options) {
     var fs = require('fs');
     var http = require('http');
     var https = require('https');
     var url = require('url');
     var path = require('path');
+    var escape = require('./common').escape;
+    var normalizePath = require('./common').normalizePath;
 
     var verbose = (options.verbose || []).reduce(function (opt, val) {
         if (val === 'all') {
@@ -49,24 +45,6 @@ function create(options) {
         var protocol = server.protocol === 'https:' ? https : http;
         appsLoadPath = urlPath + appsLoadPath.slice(1);
         manifestsPath = urlPath + manifestsPath.slice(1);
-    }
-
-    var escapes = {
-        '\x00': '\\x00', '\x01': '\\x01', '\x02': '\\x02', '\x03': '\\x03',
-        '\x04': '\\x04', '\x05': '\\x05', '\x06': '\\x06', '\x07': '\\x07',
-        '\b': '\\b', '\t': '\\t', '\n': '\\n', '\v': '\\v', '\f': '\\f',
-        '\r': '\\r', '\x0e': '\\x0e', '\x0f': '\\x0f', '\x10': '\\x10',
-        '\x11': '\\x11', '\x12': '\\x12', '\x13': '\\x13', '\x14': '\\x14',
-        '\x15': '\\x15', '\x16': '\\x16', '\x17': '\\x17', '\x18': '\\x18',
-        '\x19': '\\x19', '\x1a': '\\x1a', '\x1b': '\\x1b', '\x1c': '\\x1c',
-        '\x1d': '\\x1d', '\x1e': '\\x1e', '\x1f': '\\x1f', "'": "\\'",
-        '\\': '\\\\', '\u2028': '\\u2028', '\u2029': '\\u2029'
-    };
-
-    function escape(s) {
-        return s.replace(/[\x00-\x1f'\\\u2028\u2029]/g, function(c) {
-            return escapes[c];
-        });
     }
 
     function httpDate(d) {
