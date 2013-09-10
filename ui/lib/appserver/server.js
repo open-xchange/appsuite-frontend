fa4ef0a4bd8e2c3db5ea95ce8a07165b390361f9
Julian Bäume <julian.baeume@open-xchange.com>
2013-09-10 15:38:08 +0200

    factor out appsload related handler into module
    this is the most complicated module and might still need some more
    cleanup. But for now, this should be fine

--- ui/lib/appserver/server.js
+++ ui/lib/appserver/server.js
@@ -21,8 +21,9 @@ function create(options) {
     var escape = require('./common').escape;
     var normalizePath = require('./common').normalizePath;
     var connect = require('connect');
+    var appsLoadMiddleware = require('./middleware/appsload');
 
-    var verbose = (options.verbose || []).reduce(function (opt, val) {
+    var verbose = options.verbose = (options.verbose || []).reduce(function (opt, val) {
         if (val === 'all') {
             opt.local = opt.remote = opt.proxy = true;
         } else {
@@ -30,34 +31,25 @@ function create(options) {
         }
             return opt;
     }, {});
-    var prefixes = options.prefixes || [];
-    var manifests = options.manifests || [];
+    var prefixes;
+    var manifests;
+    var urlPath;
 
-    var tzModule = 'io.ox/core/date/tz/zoneinfo/';
-    var tzPath = [normalizePath(options.zoneinfo || '/usr/share/zoneinfo/')];
+    prefixes = options.prefixes = options.prefixes || [];
+    manifests = options.manifests = options.manifests || [];
 
-    var appsLoadPath = '/api/apps/load/';
+    urlPath = options.urlPath = normalizePath(options.path || '/appsuite');
     var manifestsPath = '/api/apps/manifests';
-    var urlPath = normalizePath(options.path || '/appsuite');
 
     if (options.server) {
         options.server = normalizePath(options.server);
         var server = url.parse(options.server);
         var protocol = server.protocol === 'https:' ? https : http;
-        appsLoadPath = urlPath + appsLoadPath.slice(1);
         manifestsPath = urlPath + manifestsPath.slice(1);
     }
 
     var handler = connect()
-        .use(function (request, response, next) {
-            var URL = url.parse(request.url, true);
-            if ((request.method === 'GET') &&
-                (URL.pathname.slice(0, appsLoadPath.length) === appsLoadPath)) {
-
-                return load(request, response);
-            }
-            return next();
-        })
+        .use(appsLoadMiddleware.create(options))
         .use(function (request, response, next) {
             var URL = url.parse(request.url, true);
             if ((request.method === 'GET') &&
@@ -78,138 +70,6 @@ function create(options) {
 
     http.createServer(handler)
         .listen(options.port || 8337);
-
-    function load(request, response) {
-
-        // parse request URL
-        var list = url.parse(request.url).pathname.split(',');
-        var version = list.shift();
-        version = version.slice(version.lastIndexOf('v='));
-
-        // find local files, request unknown files from server
-        var files = [], remoteCounter = 0;
-        for (var i in list) {
-            var m = /^(?:\/(text|raw);)?([\w\/+-]+(?:\.[\w\/+-]+)*)$/.exec(list[i]);
-            if (!m) {
-                files.push(invalid(list[i]));
-                continue;
-            }
-            if (m[2].slice(0, tzModule.length) === tzModule) {
-                var paths = tzPath;
-                var name = m[2].slice(tzModule.length);
-            } else {
-                var paths = prefixes;
-                var name = m[2];
-            }
-            for (var j = 0; j < paths.length; j++) {
-                var filename = path.join(paths[j], name);
-                if (path.existsSync(filename) && fs.statSync(filename).isFile()) {
-                    break;
-                }
-            }
-            files.push(j >= paths.length ? remote(filename, list[i], m[2]) :
-                       !m[1]             ? module(filename, list[i]) :
-                       m[1] === 'raw'    ? raw(filename, list[i]) :
-                                           text(filename, list[i]));
-        }
-        if (!remoteCounter) complete();
-
-        // invalid module name
-        function invalid(fullName) {
-            return function () {
-                console.log('Invalid module name: ' + fullName);
-                response.write("console.log('Invalid module name: \"" +
-                               escape(fullName) + "\"');\n");
-            };
-        }
-
-        // remote file
-        function remote(filename, fullName, name) {
-            if (!options.server) {
-                return function() {
-                    console.log('Could not read', filename);
-                    response.write(
-                        "define('" + escape(fullName) + "', function () {\n" +
-                        "  if (ox.debug) console.log(\"Could not read '" +
-                            escape(name) + "'\");\n" +
-                        "  throw new Error(\"Could not read '" +
-                            escape(name) + "'\");\n" +
-                        "});\n");
-                };
-            }
-            remoteCounter++;
-            var chunks = [];
-            var URL = url.resolve(options.server,
-                                  'api/apps/load/' + version + ',' + fullName);
-            protocol.get(url.parse(URL), ok).on('error', error);
-            return function () {
-                if (verbose.remote) console.log(URL);
-                for (var i = 0; i < chunks.length; i++) response.write(chunks[i]);
-            };
-            function ok(res) {
-                if (res.statusCode === 200) {
-                    res.on('data', chunk).on('end', end);
-                } else {
-                    res.resume();
-                    console.log('HTTP error ' + res.statusCode + ' for ' + URL);
-                    chunk("console.log('HTTP error " + res.statusCode + "');\n");
-                    end();
-                }
-            }
-            function error(e) {
-                console.log('Server error for ' + URL + ' : ' + e.message);
-                chunk("console.log('Server error: " + e.message + "');\n");
-                end();
-            }
-            function chunk(data) { chunks.push(data); }
-            function end() { if (!--remoteCounter) complete(); }
-        }
-
-        // normal RequireJS module
-        function module(filename, fullName) {
-            return function () {
-                if (verbose.local) console.log(filename);
-                response.write(fs.readFileSync(filename) + "\n/*:oxsep:*/\n");
-            };
-        }
-
-        // raw data as string (e.g. timezones)
-        function raw(filename, fullName) {
-            return function () {
-                if (verbose.local) console.log(filename);
-                var data = fs.readFileSync(filename), s = [];
-                for (var j = 0; j < data.length; j++) s.push(data[j]);
-                s = String.fromCharCode.apply(String, s);
-                response.write("define('" + escape(fullName) + "','" + escape(s) +
-                    "');\n/*:oxsep:*/\n");
-            };
-        }
-
-        // text file as a string (e.g. CSS)
-        function text(filename, fullName) {
-            return function () {
-                if (verbose.local) console.log(filename);
-                var s = fs.readFileSync(filename, 'utf8');
-                response.write("define('" + escape(fullName) + "','" + escape(s) +
-                    "');\n/*:oxsep:*/\n");
-            };
-        }
-
-        // send reply
-        function complete() {
-
-            // set headers
-            response.setHeader('Content-Type', 'text/javascript;charset=UTF-8');
-            response.setHeader('Expires', '0');
-
-            // send data
-            for (var i = 0; i < files.length; i++) files[i]();
-
-            // all done
-            response.end();
-            if (verbose.local || verbose.remote) console.log();
-        }
-    }
 
     function lock() {
         var counter = 1, cb;
