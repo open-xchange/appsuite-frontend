c32d1c062d7c542e47efcc792638195617e1dd89
Frank Paczynski <frank.paczynski@open-xchange.com>
2013-09-20 09:01:30 +0200

    specs: no functional changes - just keep jshint happy

--- ui/spec/io.ox/contacts/util_spec.js
+++ ui/spec/io.ox/contacts/util_spec.js
@@ -10,7 +10,7 @@
  * @author Christoph Kopp <christoph.kopp@open-xchange.com>
  */
 
-    define(["io.ox/contacts/util"], function (util) {
+define(["io.ox/contacts/util"], function (util) {
 
     "use strict";
 
@@ -31,48 +31,49 @@
         testPersonHttps = {
         image1_url: "https://www.test.de/ajax/image/contact/picture?folder=11179&id=510778&timestamp=1379590562489",
     };
-        describe("Contact util", function () {
 
-            it ('should return a proper image path ', function () {
-                expect(util.getImage(testPerson)).toEqual(ox.apiRoot + '/image/contact/picture?folder=11179&id=510778&timestamp=1379590562489');
-                expect(util.getImage(testPersonWOPic)).toEqual(ox.base + '/apps/themes/default/dummypicture.png');
-                expect(util.getImage(testDistList)).toEqual(ox.base + '/apps/themes/default/dummypicture_group.png');
-                expect(util.getImage(testPersonHttps)).toEqual(ox.apiRoot + '/image/contact/picture?folder=11179&id=510778&timestamp=1379590562489');
-            });
+    describe("Contact util", function () {
 
-            it ('should return a prepared full contact name for sorting purpose ', function () {
-                expect(util.getSortName(testPerson)).toEqual('tester, georg');
-            });
+        it('should return a proper image path ', function () {
+            expect(util.getImage(testPerson)).toEqual(ox.apiRoot + '/image/contact/picture?folder=11179&id=510778&timestamp=1379590562489');
+            expect(util.getImage(testPersonWOPic)).toEqual(ox.base + '/apps/themes/default/dummypicture.png');
+            expect(util.getImage(testDistList)).toEqual(ox.base + '/apps/themes/default/dummypicture_group.png');
+            expect(util.getImage(testPersonHttps)).toEqual(ox.apiRoot + '/image/contact/picture?folder=11179&id=510778&timestamp=1379590562489');
+        });
 
-            it ('should return a prepared full contact name for sorting purpose ', function () {
-                expect(util.getSortName(testPerson)).toEqual('tester, georg');
-            });
+        it('should return a prepared full contact name for sorting purpose ', function () {
+            expect(util.getSortName(testPerson)).toEqual('tester, georg');
+        });
+
+        it('should return a prepared full contact name for sorting purpose ', function () {
+            expect(util.getSortName(testPerson)).toEqual('tester, georg');
+        });
 
 //            it ('should return a string combined from titel, last name and first menu ', function () {
 //                expect(util.getFullNameFormat(testPerson)).toEqual('tester, georg');
 //            });
 
-            it ('should return the prepared full name ', function () {
-                expect(util.getFullName(testPerson)).toEqual('Tester, Georg');
-            });
+        it('should return the prepared full name ', function () {
+            expect(util.getFullName(testPerson)).toEqual('Tester, Georg');
+        });
 
-            it ('should return the display name if available otherwise combine first and last name ', function () {
-                expect(util.getDisplayName(testPerson)).toEqual('Dr. Tester, Georg');
-                expect(util.getDisplayName(testPersonWOPic)).toEqual('Tester, Georg');
-            });
+        it('should return the display name if available otherwise combine first and last name ', function () {
+            expect(util.getDisplayName(testPerson)).toEqual('Dr. Tester, Georg');
+            expect(util.getDisplayName(testPersonWOPic)).toEqual('Tester, Georg');
+        });
 
 //            getMailFullNameFormat
 
-            it ('should return the display name if available otherwise combine first and last name ', function () {
-                expect(util.getMailFullName(testPerson)).toEqual('Georg Tester');
-            });
+        it('should return the display name if available otherwise combine first and last name ', function () {
+            expect(util.getMailFullName(testPerson)).toEqual('Georg Tester');
+        });
 
 //            getMailFormat
 
-            it ('should return the first available mail address ', function () {
-                expect(util.getMail(testPerson)).toEqual('georg@tester.com');
-            });
-
+        it('should return the first available mail address ', function () {
+            expect(util.getMail(testPerson)).toEqual('georg@tester.com');
         });
 
     });
+
+});
