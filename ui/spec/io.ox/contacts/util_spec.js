c45f4f91e1b5f1838c2b330188be4af8ed667984
Frank Paczynski <frank.paczynski@open-xchange.com>
2013-09-24 08:10:28 +0200

    specs: keep jshint happy

--- ui/spec/io.ox/contacts/util_spec.js
+++ ui/spec/io.ox/contacts/util_spec.js
@@ -48,16 +48,16 @@ define(["io.ox/contacts/util"], function (util) {
             expect(util.getImage(testPersonHttps)).toEqual(ox.apiRoot + '/image/contact/picture?folder=11179&id=510778&timestamp=1379590562489');
         });
 
-        it ('should return a prepared full contact name for sorting purpose ', function () {
+        it('should return a prepared full contact name for sorting purpose ', function () {
             expect(util.getSortName(testPerson)).toEqual('tester, georg');
             expect(util.getSortName({})).toEqual('');
         });
 
-        it ('should return a object containing the format ', function () {
+        it('should return a object containing the format ', function () {
             expect(util.getFullNameFormat(testPerson)).toEqual({ format : '%2$s, %1$s', params : [ 'Georg', 'Tester' ] });
         });
 
-        it ('should return the prepared full name ', function () {
+        it('should return the prepared full name ', function () {
             expect(util.getFullName(testPerson)).toEqual('Tester, Georg');
             expect(util.getFullName({})).toEqual('');
         });
@@ -67,7 +67,7 @@ define(["io.ox/contacts/util"], function (util) {
             expect(util.getDisplayName(testPersonWOPic)).toEqual('Tester, Georg');
         });
 
-        it ('should return a object containing the format ', function () {
+        it('should return a object containing the format ', function () {
             expect(util.getMailFullNameFormat(testPerson)).toEqual({ format : '%1$s %2$s', params : [ 'Georg', 'Tester' ] });
         });
 
@@ -75,25 +75,25 @@ define(["io.ox/contacts/util"], function (util) {
             expect(util.getMailFullName(testPerson)).toEqual('Georg Tester');
         });
 
-        it ('should return a object containing the format ', function () {
+        it('should return a object containing the format ', function () {
             expect(util.getMailFormat(testPerson)).toEqual({ format : '%1$s', params : [ 'georg1@tester.com' ] });
         });
 
-        it ('should return the first available mail address ', function () {
+        it('should return the first available mail address ', function () {
             expect(util.getMail(testPerson)).toEqual('georg1@tester.com');
         });
 
-        it ('should return a descriptiv string for the contact', function () {
+        it('should return a descriptiv string for the contact', function () {
             expect(util.getDescription(testPerson)).toEqual('conpany, department, position, city_business, city_home');
         });
 
-        it ('should return a combined string of position and company', function () {
+        it('should return a combined string of position and company', function () {
             expect(util.getJob(testPerson)).toEqual('position, conpany');
         });
 
 //            nameSort is not used any more
 
-        it ('should return the mailfield ID of a selected E-Mail', function () {
+        it('should return the mailfield ID of a selected E-Mail', function () {
             expect(util.calcMailField(testPerson, testPerson.email2)).toEqual(2);
             expect(util.calcMailField(testPerson, testPerson.email1)).toEqual(1);
             expect(util.calcMailField(testPerson, testPerson.email3)).toEqual(3);
