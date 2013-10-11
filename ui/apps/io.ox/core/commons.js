3d2729784dbdc379d974369ab0719907a1897ad3
Christoph Hellweg <christoph.hellweg@open-xchange.com>
2013-10-11 11:51:41 +0200

    a11y add element count to vgrid

--- ui/apps/io.ox/core/commons.js
+++ ui/apps/io.ox/core/commons.js
@@ -285,7 +285,7 @@ define('io.ox/core/commons',
 
                     var total = countGridData ? grid.getIds().length : data.total,
                         node = grid.getToolbar().find('[data-folder-id="' + folder_id + '"]');
-
+                    grid.getContainer().attr('aria-setsize', total);
                     node.find('.folder-name').text(data.title);
 
                     if (total > 0) {
