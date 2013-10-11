4d34bf4ef2446401e056bfb7ee795f1bbb316d46
christoph.kopp <christoph.kopp@open-xchange.com>
2013-10-11 10:05:21 +0200

    polishing for tabclose

--- ui/apps/io.ox/core/main.js
+++ ui/apps/io.ox/core/main.js
@@ -487,15 +487,17 @@ define('io.ox/core/main',
             });
 
             if (model.get('closable')) {
+                if (first) {
+                    launcher.find('a').after(quitApp);
+                }
+            }
+
+            if (model.get('userContent')) {
                 var cls = model.get('userContentClass') || '',
                     icon = model.get('userContentIcon') || '';
                 launcher.addClass('user-content').addClass(cls).children().first().prepend($('<span>').append(
                     $('<i class="' + icon + '">'))
                 );
-                if (first) {
-                    launcher.find('a').addClass('special').after(quitApp);
-                }
-
             }
         }
 
