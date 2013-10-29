dafc1c3b6d3cf6a721adf2b781c8bfb71fb6fc03
Julian Bäume <julian.baeume@open-xchange.com>
2013-10-29 12:17:42 +0100

    add gettext module for io.ox/core/emoji/view
    gt has not been used, until now, so the cherry-pick (last commit) was
    not enough to fix bug 29545

--- ui/apps/io.ox/core/emoji/view.js
+++ ui/apps/io.ox/core/emoji/view.js
@@ -11,7 +11,10 @@
  * @author Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
  */
 
-define('io.ox/core/emoji/view', ['moxiecode/tiny_mce/plugins/emoji/main'], function (emoji) {
+define('io.ox/core/emoji/view', [
+    'io.ox/emoji/main',
+    'gettext!io.ox/mail/emoji'
+], function (emoji, gt) {
 
     'use strict';
 
