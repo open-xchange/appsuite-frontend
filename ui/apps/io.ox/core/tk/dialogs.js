4b591bb5b223b9bd1c3b769010cb2f438bb08786
christoph.kopp <christoph.kopp@open-xchange.com>
2013-11-19 18:33:09 +0100

    Chore: Restore last focus when closing side-popups

--- ui/apps/io.ox/core/tk/dialogs.js
+++ ui/apps/io.ox/core/tk/dialogs.js
@@ -520,6 +520,7 @@ define('io.ox/core/tk/dialogs',
             closeByEvent, //for example: The view within this SidePopup closes itself
             previousProp,
             timer = null,
+            lastFocus = $(),
 
             overlay,
 
@@ -643,6 +644,10 @@ define('io.ox/core/tk/dialogs',
                         popup.detach();
                     }
                     pane.empty();
+
+                    lastFocus = lastFocus.closest(':visible');
+                    lastFocus.focus();
+
                     self.trigger('close');
                 }, 100);
             }
@@ -673,6 +678,9 @@ define('io.ox/core/tk/dialogs',
         open = function (e, handler) {
             // get proper elements
             var my = $(this), zIndex, sidepopup;
+
+            lastFocus = $(document.activeElement);
+
             self.nodes = {
                 closest: target || my.parents('.io-ox-sidepopup-pane, .window-content, .window-container-center, .io-ox-dialog-popup, .notifications-overlay').first(),
                 click: my.parents('.io-ox-sidepopup-pane, .window-body, .window-container-center, .io-ox-dialog-popup, .notifications-overlay').first(),
