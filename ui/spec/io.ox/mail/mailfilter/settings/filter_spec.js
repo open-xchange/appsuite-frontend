5888acb324bbbf1c5f16d2e3adb16e962a661af2
christoph.kopp <christoph.kopp@open-xchange.com>
2013-11-22 16:28:42 +0100

    some more mailfilter filter.js tests

--- ui/spec/io.ox/mail/mailfilter/settings/filter_spec.js
+++ ui/spec/io.ox/mail/mailfilter/settings/filter_spec.js
@@ -15,7 +15,7 @@ define(['io.ox/mail/mailfilter/settings/filter', 'gettext!io.ox/mail'], function
 
     'use strict';
 
-    var resultWithFilter = {timestamp: 1378223251586, data: {
+    var resultWithFilter = {timestamp: 1378223251586, data: [{
             'position': 0,
             'id': 0,
             'flags': [],
@@ -31,7 +31,40 @@ define(['io.ox/mail/mailfilter/settings/filter', 'gettext!io.ox/mail'], function
             }],
             'rulename': 'New rule',
             'active': true
-        }},
+        },
+        {
+            'position': 1,
+            'id': 1,
+            'flags': ['vacation'],
+            'test': {'id': 'true'},
+            'actioncmds': [{
+                'id': 'vacation',
+                'text': 'text',
+                'days': '7',
+                'subject': 'subject',
+                'addresses': ['tester@open-xchange.com']
+            }],
+            'rulename': 'Abwesenheitsnotiz',
+            'active': false
+        }, {
+            'position': 2,
+            'id': 2,
+            'flags': [],
+            'test': {
+                'headers': ['To', 'Cc'],
+                'id': 'header',
+                'values': [''],
+                'comparison': 'matches'
+            },
+            'actioncmds': [{
+                'id': 'addflags',
+                'flags': ['$cl_1']
+            }],
+            'rulename': 'New rule 2',
+            'active': false
+        }]
+    },
+
         resultWithoutFilter = { data: [] };
 
     describe('Mailfilter filter with rules', function () {
@@ -57,11 +90,15 @@ define(['io.ox/mail/mailfilter/settings/filter', 'gettext!io.ox/mail'], function
             expect($container.find('h1').length).toBe(1);
             expect($container.find('.btn-primary[data-action="add"]').length).toBe(1);
             expect($container.find('li[data-id="0"]').length).toBe(1);
-            expect($container.find('li a.drag-handle').length).toBe(1);
-            expect($container.find('li .list-title').length).toBe(1);
-            expect($container.find('li [data-action="edit"]').length).toBe(1);
-            expect($container.find('li [data-action="toggle"]').length).toBe(1);
-            expect($container.find('li [data-action="delete"]').length).toBe(1);
+            expect($container.find('li[data-id="0"]').hasClass('editable')).toBe(true);
+            expect($container.find('li[data-id="1"]').length).toBe(1);
+            expect($container.find('li[data-id="1"]').hasClass('editable')).toBe(false);
+            expect($container.find('li[data-id="2"]').hasClass('disabled')).toBe(true);
+            expect($container.find('li a.drag-handle').length).toBe(3);
+            expect($container.find('li .list-title').length).toBe(3);
+            expect($container.find('li [data-action="edit"]').length).toBe(2);
+            expect($container.find('li [data-action="toggle"]').length).toBe(2);
+            expect($container.find('li [data-action="delete"]').length).toBe(2);
 
         });
 
@@ -91,6 +128,21 @@ define(['io.ox/mail/mailfilter/settings/filter', 'gettext!io.ox/mail'], function
             expect($container.find('.btn-primary[data-action="add"]').length).toBe(1);
             expect($container.find('h1').length).toBe(1);
             expect($container.find('ol div').text()).toBe(gt('There is no rule defined'));
+
+        });
+
+        it('should trigger the create new rule dialog', function () {
+            var $container = $('<div>'),
+                addButton,
+                $popup;
+            filters.editMailfilter($container);
+            this.server.respond();
+
+            addButton = $container.find('.btn-primary[data-action="add"]');
+            addButton.trigger('click');
+
+            $popup = $('body').find('h4:contains("' + gt('Create new rule') + '")').closest('.io-ox-dialog-popup');
+            expect($popup.length).toBe(1);
 
         });
 
