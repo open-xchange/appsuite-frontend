dc6e4d8a51a4d1a7553fccaeb81e9c3072257d82
christoph.kopp <christoph.kopp@open-xchange.com>
2013-11-27 10:03:34 +0100

    specs: autoforward filter.js tests

---
+++ ui/spec/io.ox/mail/autoforward/settings/filter_spec.js
@@ -0,0 +1,68 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * © 2013 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Christoph Kopp <christoph.kopp@open-xchange.com>
+ */
+
+define(['io.ox/mail/autoforward/settings/filter', 'gettext!io.ox/mail'], function (filter, gt) {
+
+    'use strict';
+
+    var resultWithFlag = { timestamp: 1378223251586,
+        'data': [{
+            'position': 1,
+            'id': 1,
+            'flags': ['autoforward'],
+            'test': {
+                'headers': ['To'],
+                'id': 'header',
+                'values': ['tester@open-xchange.com'],
+                'comparison': 'contains'
+            },
+            'actioncmds': [{
+                'to': 'tester@open-xchange.com',
+                'id': 'redirect'
+            },
+            {
+                'id': 'keep'
+            }],
+            'rulename': 'autoforward',
+            'active': false
+        }]
+    },
+    multiValues = {};
+    
+    describe('Vacationnotice', function () {
+
+        beforeEach(function () {
+            this.server = ox.fakeServer.create();
+            this.server.respondWith('GET', /api\/mailfilter\?action=list&flag=autoforward/, function (xhr) {
+                xhr.respond(200, { 'Content-Type': 'text/javascript;charset=UTF-8'}, JSON.stringify(resultWithFlag));
+            });
+            $('body', document).append(this.node = $('<div id="autoforwardtestNode">'));
+
+        });
+
+        afterEach(function () {
+            $('#autoforwardtestNode', document).remove();
+            this.server.restore();
+        });
+
+        it('should draw the form', function () {
+            filter.editAutoForward(this.node, multiValues, 'tester@open-xchange.com');
+            this.server.respond();
+            expect(this.node.find('input[name="forwardmail"]').length).toBe(1);
+            expect(this.node.find('input[type="checkbox"]').length).toBe(1);
+
+        });
+
+    });
+
+});
