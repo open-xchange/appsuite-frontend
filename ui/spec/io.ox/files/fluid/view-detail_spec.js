9559ccb5474a886931b2e775c3a99ea3000d07c5
Frank Paczynski <frank.paczynski@open-xchange.com>
2013-11-27 10:22:59 +0100

    specs: file detail view / actions
    move fake data to fixture
    fix indentation

--- ui/spec/io.ox/files/fluid/view-detail_spec.js
+++ ui/spec/io.ox/files/fluid/view-detail_spec.js
@@ -11,8 +11,11 @@
  * @author Frank Paczynski <frank.paczynski@open-xchange.com>
  */
 define(['io.ox/files/fluid/view-detail',
-	'io.ox/files/api',
-	'io.ox/core/extensions'], function (view, api, ext) {
+    'io.ox/files/api',
+    'io.ox/core/extensions',
+    'fixture!io.ox/files/file.json',
+    'fixture!io.ox/files/file-versions.json'
+    ], function (view, api, ext, file, fileversions) {
 
     //reset tracker (lock/unlock states)
     afterEach(function () {
@@ -21,28 +24,7 @@ define(['io.ox/files/fluid/view-detail',
     });
 
     describe('files detail view', function () {
-        var baton = ext.Baton.ensure({
-                data: {
-		    'color_label': 0,
-		    'created_by': 395,
-		    'creation_date': 1384934347734,
-		    'current_version': true,
-		    'file_md5sum': '04b384525949d824f954368319558196',
-		    'file_mimetype': 'application/octet-stream',
-		    'file_size': 498,
-		    'filename': '2013-01-11-intro-to-aria-roles-states-and-properties.txt',
-		    'folder_id': '0815',
-		    'id': '4711',
-		    'last_modified': 1384938624162,
-		    'last_modified_utc': 1384938624162,
-		    'locked_until': 0,
-		    'modified_by': 9999,
-		    'number_of_versions': 3,
-		    'sequence_number': 1384938624162,
-		    'title': '2013-01-11-intro-to-aria-roles-states-and-properties.txt',
-		    'version': '3'
-                }
-            }),
+	var baton = ext.Baton.ensure({data: file}),
             app = {
                 getName: function () {
                     return 'testapp';
@@ -50,16 +32,16 @@ define(['io.ox/files/fluid/view-detail',
                 folder : {
                     set: '0815'
                 }
-	    },
-	    //use different id for each mod
-	    modified = function (data) {
-		var bat = $.extend(true, {}, baton, {data: data});
-		_.each(data, function (value, key) {
-		    if (!value) delete bat.data[key];
-		});
-		api.tracker.updateFile(bat.data);
-		return view.draw(bat, app);
-            };
+	},
+	//use different id for each mod
+	modified = function (data) {
+	    var bat = $.extend(true, {}, baton, {data: data});
+	    _.each(data, function (value, key) {
+		if (!value) delete bat.data[key];
+	    });
+	    api.tracker.updateFile(bat.data);
+	    return view.draw(bat, app);
+	};
 
         describe('needs a baton that', function () {
             it('has to be defined', function () {
@@ -86,7 +68,7 @@ define(['io.ox/files/fluid/view-detail',
             describe('with action menu that', function () {
 		describe('always', function () {
 		    it('has some actions', function () {
-			var actions = node.find('ul.io-ox-inline-links');
+		    var actions = node.find('ul.io-ox-inline-links');
 			expect(actions.children().length).toBeGreaterThan(0);
 		    });
 		    it('has a show internal link action if filename is defined', function () {
@@ -96,7 +78,7 @@ define(['io.ox/files/fluid/view-detail',
 			//TODO: stub read grants
 			//expect(node.find('[data-action="copy"]').length).toBeTruthy();
 		    });
-                });
+		});
 		describe('(if', function () {
 		    describe('filename or file_size are defined)', function () {
 			var mod = modified({filename: undefined, file_size: undefined});
@@ -173,86 +155,14 @@ define(['io.ox/files/fluid/view-detail',
 	    });
 	    describe('that contain a table with information about versions', function () {
 		sinon.stub(api.caches.versions, 'get', function () {
-		    return $.Deferred().resolve(
-			[
-			    {
-				"id": "130943",
-				"created_by": 395,
-				"modified_by": 395,
-				"creation_date": 1384934347734,
-				"last_modified": 1384934347734,
-				"folder_id": "14048",
-				"meta": null,
-				"categories": null,
-				"color_label": 0,
-				"title": "favicon.ico",
-				"url": null,
-				"filename": "favicon.ico",
-				"file_mimetype": "image/vnd.microsoft.icon",
-				"file_size": 318,
-				"version": "1",
-				"description": null,
-				"locked_until": 0,
-				"file_md5sum": "836e4e0a52845a3ce8153edc23c0b2ca",
-				"version_comment": null,
-				"current_version": false,
-				"number_of_versions": 0
-			    },
-			    {
-				"id": "130943",
-				"created_by": 395,
-				"modified_by": 395,
-				"creation_date": 1384934652737,
-				"last_modified": 1384934652688,
-				"folder_id": "14048",
-				"meta": null,
-				"categories": null,
-				"color_label": 0,
-				"title": "IE9_-_Win7_simple_click.jpeg",
-				"url": null,
-				"filename": "IE9_-_Win7_simple_click.jpeg",
-				"file_mimetype": "image/jpeg",
-				"file_size": 96077,
-				"version": "2",
-				"description": null,
-				"locked_until": 0,
-				"file_md5sum": "bf01a8218d73d7c9d067b9396518574d",
-				"version_comment": null,
-				"current_version": true,
-				"number_of_versions": 0
-			    },
-			    {
-				"id": "130943",
-				"created_by": 395,
-				"modified_by": 395,
-				"creation_date": 1384938624316,
-				"last_modified": 1384938624162,
-				"folder_id": "14048",
-				"meta": null,
-				"categories": null,
-				"color_label": 0,
-				"title": "2013-01-11-intro-to-aria-roles-states-and-properties.md",
-				"url": null,
-				"filename": "2013-01-11-intro-to-aria-roles-states-and-properties.md",
-				"file_mimetype": "application/octet-stream",
-				"file_size": 498,
-				"version": "3",
-				"description": null,
-				"locked_until": 0,
-				"file_md5sum": "04b384525949d824f954368319558196",
-				"version_comment": null,
-				"current_version": false,
-				"number_of_versions": 0
-			    }
-			]
-		    );
+		    return $.Deferred().resolve(fileversions);
 		});
 		var mod = modified({id: 4717, number_of_versions: 3});
 		it('when versions exist', function () {
 		    var mod = modified({id: 4716, number_of_versions: 0});
 		    expect(node.find('table.versiontable').length).toBeTruthy();
 		    expect(mod.find('table.versiontable').length).toBeFalsy();
-		});
+                });
 		it('that can be collapsed', function () {
 		    expect(mod.find('.versiontable').attr('style')).toBeFalsy();
 		    mod.find('[data-action="history"]').trigger('click');
