09b5ba37974bc4130036cd2d5f2b1faf371b1ecc
Frank Paczynski <frank.paczynski@open-xchange.com>
2013-11-29 13:21:09 +0100

    specs: always use beforeEach/afterEach

--- ui/spec/io.ox/files/fluid/view-detail_spec.js
+++ ui/spec/io.ox/files/fluid/view-detail_spec.js
@@ -17,11 +17,6 @@ define(['io.ox/files/fluid/view-detail',
     'fixture!io.ox/files/file-versions.json'
     ], function (view, api, ext, file, fileversions) {
 
-    //reset tracker (lock/unlock states)
-    afterEach(function () {
-        api.tracker.clear();
-        api.caches.versions.get.reset();
-    });
 
     describe('files detail view', function () {
         var baton = ext.Baton.ensure({
@@ -47,6 +42,13 @@ define(['io.ox/files/fluid/view-detail',
                 return view.draw(bat, app);
             };
 
+        //reset tracker (lock/unlock states)
+        afterEach(function () {
+            api.tracker.clear();
+            if (api.caches.versions.get.reset)
+                api.caches.versions.get.reset();
+        });
+
         describe('needs a baton that', function () {
             it('has to be defined', function () {
                 var node = view.draw();
@@ -60,23 +62,25 @@ define(['io.ox/files/fluid/view-detail',
             });
         });
         describe('creates a DOM structure', function () {
-            var node = view.draw(baton);
+            beforeEach(function () {
+                this.node = view.draw(baton);
+            })
             describe('with a container that', function () {
                 it('has class file-details', function () {
-                    expect(node.hasClass('file-details')).toBeTruthy;
+                    expect(this.node.hasClass('file-details')).toBeTruthy;
                 });
                 it('use folder-id and file id as data-cid', function () {
-                    expect(node.attr('data-cid')).toEqual(baton.data.folder_id + '.' + baton.data.id);
+                    expect(this.node.attr('data-cid')).toEqual(baton.data.folder_id + '.' + baton.data.id);
                 });
             });
             describe('with action menu that', function () {
                 describe('always', function () {
                     it('has some actions', function () {
-                        var actions = node.find('ul.io-ox-inline-links');
+                        var actions = this.node.find('ul.io-ox-inline-links');
                         expect(actions.children().length).toBeGreaterThan(0);
                     });
                     it('has a show internal link action if filename is defined', function () {
-                        expect(node.find('[data-action="showlink"]').length).toBeTruthy();
+                        expect(this.node.find('[data-action="showlink"]').length).toBeTruthy();
                     });
                     it('has a copy action', function () {
                         //TODO: stub read grants
@@ -85,136 +89,157 @@ define(['io.ox/files/fluid/view-detail',
                 });
                 describe('(if', function () {
                     describe('filename or file_size are defined)', function () {
-                        var mod = modified({
-                            filename: undefined,
-                            file_size: undefined
+                        beforeEach(function () {
+                            this.mod = modified({
+                                filename: undefined,
+                                file_size: undefined
+                            });
                         });
                         it('has a open action', function () {
-                            expect(node.find('[data-action="open"]').length).toBeTruthy();
-                            expect(mod.find('[data-action="open"]').length).toBeFalsy();
+                            expect(this.node.find('[data-action="open"]').length).toBeTruthy();
+                            expect(this.mod.find('[data-action="open"]').length).toBeFalsy();
                         });
                         it('has a download action', function () {
-                            expect(node.find('[data-action="download"]').length).toBeTruthy();
-                            expect(mod.find('[data-action="download"]').length).toBeFalsy();
+                            expect(this.node.find('[data-action="download"]').length).toBeTruthy();
+                            expect(this.mod.find('[data-action="download"]').length).toBeFalsy();
                         });
                     });
                     describe('filename is defined)', function () {
-                        var mod = modified({
-                            filename: undefined
+                        beforeEach(function () {
+                            this.mod = modified({
+                                filename: undefined
+                            });
                         });
                         it('has a publish action if filename is defined', function () {
-                            expect(node.find('[data-action="publish"]').length).toBeTruthy();
-                            expect(mod.find('[data-action="publish"]').length).toBeFalsy();
+                            expect(this.node.find('[data-action="publish"]').length).toBeTruthy();
+                            expect(this.mod.find('[data-action="publish"]').length).toBeFalsy();
                         });
                     });
                     describe('filetype is supported)', function () {
-                        var mod = modified({
-                            filename: 'something.jpg'
+                        beforeEach(function () {
+                            this.mod = modified({
+                                filename: 'something.jpg'
+                            });
                         });
+
                         it('has a edit action ', function () {
-                            expect(node.find('[data-action="editor"]').length).toBeTruthy();
-                            expect(mod.find('[data-action="editor"]').length).toBeFalsy();
+                            expect(this.node.find('[data-action="editor"]').length).toBeTruthy();
+                            expect(this.mod.find('[data-action="editor"]').length).toBeFalsy();
                         });
                     });
                     describe('file is not locked)', function () {
-                        var mod = modified({
-                            id: 4712,
-                            modified_by: ox.user_id,
-                            locked_until: _.now() + (604800000 / 2)
+                        beforeEach(function () {
+                            this.mod = modified({
+                                id: 4712,
+                                modified_by: ox.user_id,
+                                locked_until: _.now() + (604800000 / 2)
+                            });
                         });
+
                         it('has a rename action', function () {
-                            expect(node.find('[data-action="rename"]').length).toBeTruthy();
+                            expect(this.node.find('[data-action="rename"]').length).toBeTruthy();
                         });
                         it('has a edit-description action', function () {
-                            expect(node.find('[data-action="edit-description"]').length).toBeTruthy();
+                            expect(this.node.find('[data-action="edit-description"]').length).toBeTruthy();
                         });
                         it('has a move action', function () {
                             //TODO: stub read/delete grants
-                            //expect(node.find('[data-action="move"]').length).toBeTruthy();
-                            expect(mod.find('[data-action="move"]').length).toBeFalsy();
+                            //expect(this.node.find('[data-action="move"]').length).toBeTruthy();
+                            expect(this.mod.find('[data-action="move"]').length).toBeFalsy();
                         });
                     });
                     describe('file is locked by myself)', function () {
-                        var mod = modified({
-                            id: 4713,
-                            modified_by: ox.user_id,
-                            locked_until: _.now() + (604800000 / 2)
+                        beforeEach(function () {
+                            this.mod = modified({
+                                id: 4713,
+                                modified_by: ox.user_id,
+                                locked_until: _.now() + (604800000 / 2)
+                            });
                         });
                         it('has a rename action', function () {
-                            expect(mod.find('[data-action="rename"]').length).toBeTruthy();
+                            expect(this.mod.find('[data-action="rename"]').length).toBeTruthy();
                         });
                         it('has a unlock action', function () {
-                            expect(node.find('[data-action="unlock"]').length).toBeFalsy();
-                            expect(mod.find('[data-action="unlock"]').length).toBeTruthy();
+                            expect(this.node.find('[data-action="unlock"]').length).toBeFalsy();
+                            expect(this.mod.find('[data-action="unlock"]').length).toBeTruthy();
                         });
                     });
                     describe('file is locked by another user)', function () {
-                        var mod = modified({
-                            id: 4714,
-                            locked_until: _.now() + (604800000 / 2)
+                        beforeEach(function () {
+                            this.mod = modified({
+                                id: 4714,
+                                locked_until: _.now() + (604800000 / 2)
+                            });
                         });
+
                         it('has a rename action', function () {
-                            expect(mod.find('[data-action="rename"]').length).toBeFalsy();
+                            expect(this.mod.find('[data-action="rename"]').length).toBeFalsy();
                         });
                         it('has a edit description action', function () {
-                            expect(mod.find('[data-action="edit-description"]').length).toBeFalsy();
+                            expect(this.mod.find('[data-action="edit-description"]').length).toBeFalsy();
                         });
                         it('has a unlock action', function () {
-                            expect(mod.find('[data-action="unlock"]').length).toBeFalsy();
+                            expect(this.mod.find('[data-action="unlock"]').length).toBeFalsy();
                         });
                     });
                     describe('file is unlocked)', function () {
-                        var mod = modified({
-                            id: 4715,
-                            locked_until: _.now() + (604800000 / 2)
+                        beforeEach(function () {
+                            this.mod = modified({
+                                id: 4715,
+                                locked_until: _.now() + (604800000 / 2)
+                            });
                         });
                         it('has a lock action', function () {
-                            expect(node.find('[data-action="lock"]').length).toBeTruthy();
-                            expect(mod.find('[data-action="lock"]').length).toBeFalsy();
+                            expect(this.node.find('[data-action="lock"]').length).toBeTruthy();
+                            expect(this.mod.find('[data-action="lock"]').length).toBeFalsy();
                         });
                         it('has a edit-description action if file isn not locked', function () {
-                            expect(node.find('[data-action="edit-description"]').length).toBeTruthy();
+                            expect(this.node.find('[data-action="edit-description"]').length).toBeTruthy();
                         });
                     });
                 });
             });
             describe('that contain a table with information about versions', function () {
-                sinon.stub(api.caches.versions, 'get', function () {
-                    return $.Deferred().resolve(fileversions);
-                });
-                var mod = modified({
-                    id: 4717,
-                    number_of_versions: 3
+                beforeEach(function () {
+                    if (api.caches.versions.get.restore)
+                        api.caches.versions.get.restore();
+                    sinon.stub(api.caches.versions, 'get', function () {
+                        return $.Deferred().resolve(fileversions);
+                    });
+                    this.mod = modified({
+                        id: 4717,
+                        number_of_versions: 3
+                    });
                 });
                 it('when versions exist', function () {
                     var mod = modified({
                         id: 4716,
                         number_of_versions: 0
                     });
-                    expect(node.find('table.versiontable').length).toBeTruthy();
+                    expect(this.node.find('table.versiontable').length).toBeTruthy();
                     expect(mod.find('table.versiontable').length).toBeFalsy();
                 });
                 it('that can be collapsed', function () {
-                    expect(mod.find('.versiontable').attr('style')).toBeFalsy();
-                    mod.find('[data-action="history"]').trigger('click');
-                    expect(mod.find('.versiontable').attr('style').trim()).toBe('display: table;');
+                    expect(this.mod.find('.versiontable').attr('style')).toBeFalsy();
+                    this.mod.find('[data-action="history"]').trigger('click');
+                    expect(this.mod.find('.versiontable').attr('style').trim()).toBe('display: table;');
                 });
                 it('that is initially collapsed', function () {
                     //TODO: initially hidden via css class; refactor
                 });
                 it('that shows all versions', function () {
-                    expect(mod.find('.versiontable').find('tbody>tr').length).toBe(3);
+                    expect(this.mod.find('.versiontable').find('tbody>tr').length).toBe(3);
                 });
                 it('that highlights current version by adding class "info"', function () {
-                    expect(mod.find('.versiontable').find('.info .versionLabel').text()).toBe('2');
+                    expect(this.mod.find('.versiontable').find('.info .versionLabel').text()).toBe('2');
                 });
             });
             it('that shows additional info about the folder', function () {
-                expect(node.find('ul.breadcrumb').length).toBe(1);
+                expect(this.node.find('ul.breadcrumb').length).toBe(1);
             });
             it('that allows uploading a new file version', function () {
-                expect(node.find('.fileupload').length).toBe(1);
-                expect(node.find('.fileupload.btn-file').hasClass('disabled')).toBeFalsy();
+                expect(this.node.find('.fileupload').length).toBe(1);
+                expect(this.node.find('.fileupload.btn-file').hasClass('disabled')).toBeFalsy();
             });
         });
     });
