be56c49815c36ff2a182d27ec6756c67ffc39680
David Bauer <david.bauer@open-xchange.com>
2014-01-14 11:50:29 +0100

    Added grunt-modernizr and modernizr task to Gruntfile

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -201,6 +201,56 @@ module.exports = function (grunt) {
                 ]
             }
         },
+        modernizr: {
+
+            // [REQUIRED] Path to the build you're using for development.
+            "devFile" : "node_modules/grunt-modernizr/lib/modernizr-dev.js",
+
+            // [REQUIRED] Path to save out the built file.
+            "outputFile" : "lib/modernizr.js",
+
+            // Based on default settings on http://modernizr.com/download/
+            "extra" : {
+                "shiv" : true,
+                "printshiv" : false,
+                "load" : true,
+                "mq" : false,
+                "cssclasses" : true
+            },
+
+            // Based on default settings on http://modernizr.com/download/
+            "extensibility" : {
+                "addtest" : false,
+                "prefixed" : false,
+                "teststyles" : false,
+                "testprops" : false,
+                "testallprops" : false,
+                "hasevents" : false,
+                "prefixes" : false,
+                "domprefixes" : false
+            },
+
+            // By default, source is uglified before saving
+            "uglify" : true,
+
+            // Define any tests you want to implicitly include.
+            "tests" : [],
+
+            // By default, this task will crawl your project for references to Modernizr tests.
+            // Set to false to disable.
+            "parseFiles" : true,
+
+            // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
+            // You can override this by defining a "files" array below.
+            "files" : ['apps/**/*.js', 'apps/**/*.less'],
+
+            // When parseFiles = true, matchCommunityTests = true will attempt to
+            // match user-contributed tests.
+            "matchCommunityTests" : false,
+
+            // Have custom Modernizr tests? Add paths to their location here.
+            "customTests" : []
+        },
         concat: {
             static: {
                 files: [
@@ -397,6 +447,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks("grunt-modernizr");
     grunt.loadNpmTasks('grunt-devtools');
 
     grunt.registerTask('manifests', ['newer:jsonlint:manifests', 'concat:manifests']);
