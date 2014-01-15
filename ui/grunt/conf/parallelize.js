da3a7fdee7f1338aeec313c44e3d8ac96d713b17
David Bauer <david.bauer@open-xchange.com>
2014-01-15 22:38:59 +0100

    Added parallelize.js configuration to enable previous commit

---
+++ ui/grunt/conf/parallelize.js
@@ -0,0 +1,19 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2014 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author David Bauer <david.bauer@open-xchange.com>
+ */
+
+'use strict';
+
+module.exports = {
+    jshint: {
+        all: 4
+    }
+};
