4f6fb9a358de40ccf0f0b46d0d54b4ff52144ba7
David Bauer <david.bauer@open-xchange.com>
2014-01-24 11:01:30 +0100

    Added tmp dir to clean task

--- ui/grunt/tasks/clean.js
+++ ui/grunt/tasks/clean.js
@@ -15,7 +15,7 @@
 
 module.exports = function (grunt) {
 
-    grunt.config('clean', ['build/', 'node_modules/grunt-newer/.cache']);
+    grunt.config('clean', ['build/', 'node_modules/grunt-newer/.cache', 'tmp/']);
 
     grunt.loadNpmTasks('grunt-contrib-clean');
 };
