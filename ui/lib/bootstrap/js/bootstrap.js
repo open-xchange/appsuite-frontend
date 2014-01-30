e320b9c2a948f1c9b5bdd75a9d1e702c1c786568
David Bauer <david.bauer@open-xchange.com>
2014-01-30 10:45:38 +0100

    Revert "Fixed: Bug 30796 - Long-click on function suggestion list does nothing"
    This reverts commit f6a311e99dfaeb7e9beca40ec07bf590568f0748.

--- ui/lib/bootstrap/js/bootstrap.js
+++ ui/lib/bootstrap/js/bootstrap.js
@@ -2016,7 +2016,7 @@ $(document)
 
   , blur: function (e) {
       var that = this
-      //setTimeout(function () { that.hide() }, 150) //bug fix 30796
+      setTimeout(function () { that.hide() }, 150)
     }
 
   , click: function (e) {
