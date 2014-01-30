6b1c8ae809b5c850b43f7327fdef68591ef10107
Marko Benigar <marko.benigar@open-xchange.com>
2014-01-30 12:05:42 +0100

    Fixed: Bug 30796 - addition #2

--- ui/lib/bootstrap/js/bootstrap.js
+++ ui/lib/bootstrap/js/bootstrap.js
@@ -1793,11 +1793,16 @@ $(document)
     this.$menu = $(this.options.menu)
     this.shown = false
     this.listen()
+    this.hadMouse = false //bug fix 30796
   }
 
   Typeahead.prototype = {
 
     constructor: Typeahead
+
+  , mouseUpDown: function(e) { //bug fix 30796
+        this.hadMouse = true;  //bug fix 30796
+    }
 
   , select: function () {
       var val = this.$menu.find('.active').attr('data-value')
@@ -1939,6 +1944,7 @@ $(document)
       this.$menu
         .on('click', $.proxy(this.click, this))
         .on('mouseenter', 'li', $.proxy(this.mouseenter, this))
+        .on('mouseup mousedown', 'li', $.proxy(this.mouseUpDown, this)) //bug fix 30796
     }
 
   , eventSupported: function(eventName) {
@@ -2016,7 +2022,9 @@ $(document)
 
   , blur: function (e) {
       var that = this
-      setTimeout(function () { that.hide() }, 150)
+      var timing = this.hadMouse ? 20000 : 150;        //bug fix 30796
+      this.hadMouse = false;                           //bug fix 30796
+      setTimeout(function () { that.hide() }, timing)  //bug fix 30796
     }
 
   , click: function (e) {
