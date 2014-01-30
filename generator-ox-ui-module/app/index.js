a252b2bbda06d911824c123785d1e9ca8efdea81
Julian Bäume <julian@svg4all.de>
2014-01-30 23:52:49 +0100

    some work on ui-module generator
    * workaround a possible bug in generator-generator: copy actually behaves like template
        * use bulkCopy, instead of copy
    * install watch.js

--- generator-ox-ui-module/app/index.js
+++ generator-ox-ui-module/app/index.js
@@ -43,20 +43,23 @@ OxUiModuleGenerator.prototype.app = function app() {
   this.template('_package.json', 'package.json');
   this.template('_Gruntfile.js', 'Gruntfile.js');
 
-  this.copy('grunt/clean.js', 'grunt/tasks/clean.js');
-  this.copy('grunt/newer.js', 'grunt/tasks/newer.js');
-  this.copy('grunt/concat.js', 'grunt/tasks/concat.js');
-  this.copy('grunt/jshint.js', 'grunt/tasks/jshint.js');
-  this.copy('grunt/parallelize.js', 'grunt/tasks/parallelize.js');
-  this.copy('grunt/jsonlint.js', 'grunt/tasks/jsonlint.js');
-  this.copy('grunt/less.js', 'grunt/tasks/less.js');
-  this.copy('grunt/i18n.js', 'grunt/tasks/i18n.js');
-  this.copy('grunt/i18n_module.js.tpl', 'grunt/templates/i18n_module.js.tpl');
-  this.copy('grunt/uglify.js', 'grunt/tasks/uglify.js');
-  this.copy('gitignore', '.gitignore');
-  this.copy('lessrc', '.lessrc');
+  this.template('grunt/_i18n.js', 'grunt/tasks/i18n.js');
+  this.bulkCopy('grunt/i18n_module.js.tpl', 'grunt/templates/i18n_module.js.tpl');
+  this.bulkCopy('grunt/clean.js', 'grunt/tasks/clean.js');
+  this.bulkCopy('grunt/concat.js', 'grunt/tasks/concat.js');
+  this.bulkCopy('grunt/jshint.js', 'grunt/tasks/jshint.js');
+  this.bulkCopy('grunt/jsonlint.js', 'grunt/tasks/jsonlint.js');
+  this.bulkCopy('grunt/less.js', 'grunt/tasks/less.js');
+  this.bulkCopy('grunt/local.js', 'grunt/tasks/local.js');
+  this.bulkCopy('grunt/newer.js', 'grunt/tasks/newer.js');
+  this.bulkCopy('grunt/parallelize.js', 'grunt/tasks/parallelize.js');
+  this.bulkCopy('grunt/pkg.js', 'grunt/tasks/pkg.js');
+  this.bulkCopy('grunt/uglify.js', 'grunt/tasks/uglify.js');
+  this.bulkCopy('grunt/watch.js', 'grunt/tasks/watch.js');
+  this.bulkCopy('gitignore', '.gitignore');
+  this.bulkCopy('lessrc', '.lessrc');
 };
 
 OxUiModuleGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('jshintrc', '.jshintrc');
+  this.bulkCopy('jshintrc', '.jshintrc');
 };
