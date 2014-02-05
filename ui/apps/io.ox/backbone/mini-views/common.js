75d92b86b5f1490ce24b1e1939e4dc1448369593
christoph.kopp <christoph.kopp@open-xchange.com>
2014-02-05 12:54:53 +0100

    added radio and select to miniviews

--- ui/apps/io.ox/backbone/mini-views/common.js
+++ ui/apps/io.ox/backbone/mini-views/common.js
@@ -90,10 +90,73 @@ define('io.ox/backbone/mini-views/common', ['io.ox/backbone/mini-views/abstract'
         }
     });
 
+    //
+    // <input type="radio">
+    //
+
+    var RadioView = AbstractView.extend({
+        tagName: 'div',
+        className: 'controls',
+        events: { 'change': 'onChange' },
+        onChange: function () {
+            this.model.set(this.name, this.$el.find('[name="' + this.name + '"]:checked').val());
+        },
+        initialize: function (options) {
+            this.name = options.name;
+            this.listenTo(this.model, 'change:' + this.name, this.update);
+        },
+        update: function () {
+            var self = this;
+            _.each(self.$el.find('[name="' + self.name + '"]'), function (option) {
+                if (self.model.get(self.name) === option.value) $(option).prop('checked', true);
+            });
+        },
+        render: function () {
+            var self = this;
+            this.$el.append(_.map(this.options.list, function (option) {
+                return $('<label>').addClass('radio').text(option.label).append(
+                    $('<input type="radio" name="' + self.name + '">').val(option.value).attr({ tabindex: self.options.tabindex || 1 })
+                );
+            }));
+            this.update();
+            return this;
+        }
+    });
+
+    //
+    // <select>
+    //
+
+    var SelectView = AbstractView.extend({
+            tagName: 'select',
+            className: 'input-xlarge',
+            events: { 'change': 'onChange' },
+            onChange: function () {
+                this.model.set(this.name, this.$el.val());
+            },
+            initialize: function (options) {
+                this.name = options.name;
+                this.listenTo(this.model, 'change:' + this.name, this.update);
+            },
+            update: function () {
+                this.$el.val(this.model.get(this.name));
+            },
+            render: function () {
+                this.$el.attr({ id: this.name, name: this.name, tabindex: this.options.tabindex || 1 });
+                this.$el.append(_.map(this.options.list, function (option) {
+                    return $('<option>').attr({ value: option.value}).text(option.label);
+                }));
+                this.update();
+                return this;
+            }
+        });
+
     return {
         AbstractView: AbstractView,
         InputView: InputView,
         TextView: TextView,
-        CheckboxView: CheckboxView
+        CheckboxView: CheckboxView,
+        RadioView: RadioView,
+        SelectView: SelectView
     };
 });
