34299f06b389d23aca41224638b092b2fef9b638
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2014-02-06 14:17:23 +0100

    US 61280648: Show password strength in App Suite UI

--- ui/apps/plugins/portal/userSettings/register.js
+++ ui/apps/plugins/portal/userSettings/register.js
@@ -15,8 +15,10 @@
 define('plugins/portal/userSettings/register',
     ['io.ox/core/extensions',
      'io.ox/core/main',
-     'gettext!io.ox/core'
-    ], function (ext, main, gt) {
+     'gettext!io.ox/core',
+     'settings!io.ox/core',
+     'less!plugins/portal/userSettings/style.less'
+    ], function (ext, main, gt, settings) {
 
     'use strict';
 
@@ -66,7 +68,30 @@ define('plugins/portal/userSettings/register',
 
         require(['io.ox/core/tk/dialogs', 'io.ox/core/http', 'io.ox/core/notifications'], function (dialogs, http, notifications) {
 
-            var oldPass, newPass, newPass2;
+            var oldPass, newPass, newPass2, strengthBar, strengthLabel, strengthBarWrapper, passwordHintContainer,
+                minLength = settings.get('password/minLength', 4),
+                maxLength = settings.get('password/maxLength', 0),
+                pwStrengths = [
+                    {label: gt('Passwordstrength: Too short'), color: 'bar-darkgrey', barLength: '20%',
+                              //#. %1$s is the minimum passwordlength
+                              //#, c-format
+                        hint: gt('Minimum password length is %1$d.', minLength)},//darkgrey
+                    {label: gt('Passwordstrength: Wrong length'), color: 'bar-darkgrey', barLength: '20%',
+                              //#. %1$s is the minimum passwordlength
+                              //#. %2$s is the maximum passwordlength
+                              //#, c-format
+                        hint: gt('Password length must be between %1$d and %2$d characters.', minLength, maxLength)},//darkgrey
+                    {label: gt('Passwordstrength: Very weak'), color: 'bar-darkgrey', barLength: '20%',
+                     hint: gt('Your password is more secure if it also contains capital letters, numbers, and special characters like $, _, %')},//darkgrey
+                    {label: gt('Passwordstrength: Weak'), color: 'bar-danger', barLength: '40%',
+                     hint: gt('Your password is more secure if it also contains capital letters, numbers, and special characters like $, _, %')},//red
+                    {label: gt('Passwordstrength: Good'), color: 'bar-success', barLength: '60%',
+                     hint: gt('Your password is more secure if it also contains capital letters, numbers, and special characters like $, _, %')},//green
+                    {label: gt('Passwordstrength: Strong'), color: 'bar-info', barLength: '80%'},//blue
+                    {label: gt('Passwordstrength: Very strong'), color: 'bar-epic', barLength: '100%'},//purple
+                    {label: gt('Passwordstrength: Lengendary!'), color: 'bar-legendary', barLength: '100%'},//Orange
+                ];
+
             new dialogs.ModalDialog({ async: true, width: 400 })
             .header($('<h4>').text(gt('Change password')))
             .build(function () {
@@ -74,9 +99,13 @@ define('plugins/portal/userSettings/register',
                     $('<label>').text(gt('Your current password')),
                     oldPass = $('<input type="password" class="input-large current-password">'),
                     $('<label>').text(gt('New password')),
-                    newPass = $('<input type="password" class="input-large new-password">'),
+                    newPass = $('<input type="password" class="input-large new-password">').on('keyup', updateStrength),
                     $('<label>').text(gt('Repeat new password')),
-                     newPass2 = $('<input type="password" class="input-large repeat-new-password">'),
+                    newPass2 = $('<input type="password" class="input-large repeat-new-password">'),
+                    strengthLabel = $('<label class="password-strength-label">').hide(),//hide till new pw is inserted
+                    strengthBarWrapper = $('<div class="progress">').append(
+                        strengthBar = $('<div class="bar password-strength-bar">')).hide(),//hide till new pw is inserted
+                    passwordHintContainer = $('<div class=password-hint-container>').hide(),
                     $('<div class="alert alert-block alert-info">')
                     .css('margin', '14px 0px')
                     .text(
@@ -121,6 +150,44 @@ define('plugins/portal/userSettings/register',
             .show(function () {
                 oldPass.focus();
             });
+                           
+            function strengthtest(pw) {//returns the strength of a password
+                //length test
+                if (pw.length >= minLength && (pw.length <= maxLength || maxLength === 0)) {// between min and max length
+                    // lower case; +1 upper case +1; numbers +1; special chars +1
+                    var score = 2 + /[a-z]/.test(pw) + /[A-Z]/.test(pw) + /[0-9]/.test(pw) + (new RegExp('[^a-z0-9]', 'i')).test(pw),
+                        // (>6)-->2, (6-7)-->4,(8-9)-->5, (>10)-->6 or more
+                        maxScore = 2 + Math.floor((Math.max(0, pw.length - 4) / 2)) + (pw.length >= 6 ? 1 : 0);
+                    //legendary test for truly awesome passwords
+                    if (score === 6 && pw.length > 99) {
+                        score = 7;//really legendary
+                    }
+                    return pwStrengths[Math.min(score, maxScore)];
+                } else if (maxLength === 0) {
+                    return pwStrengths[0];// to short with no maxlength
+                } else {
+                    return pwStrengths[1];// to short or to long
+                }
+            }
+
+            function updateStrength() {//tests the password and draws the bar
+                if (newPass.val() === '') {
+                    strengthLabel.hide();
+                    strengthBarWrapper.hide();
+                    passwordHintContainer.text('').hide();
+                } else {
+                    var result = strengthtest(newPass.val());
+                    strengthLabel.text(result.label).show();
+                    strengthBar.removeClass('bar-darkgrey bar-danger bar-success bar-info bar-epic bar-legendary')//cleanup
+                        .addClass(result.color).css('width', result.barLength);
+                    strengthBarWrapper.show();
+                    if (result.hint) {
+                        passwordHintContainer.text(result.hint).show();
+                    } else {
+                        passwordHintContainer.text('').hide();
+                    }
+                }
+            }
         });
     }
 
