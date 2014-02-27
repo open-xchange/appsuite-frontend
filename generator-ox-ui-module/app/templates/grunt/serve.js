c0be70ec29dc27b39a3a368d35d3354e419b4c91
David Bauer <david.bauer@open-xchange.com>
2014-02-27 12:54:17 +0100

    Ignore default connect middlewares for now #2

--- generator-ox-ui-module/app/templates/grunt/serve.js
+++ generator-ox-ui-module/app/templates/grunt/serve.js
@@ -7,26 +7,29 @@ module.exports = function (grunt) {
         server: {
             options: {
                 port: 8337,
-                 base: ['build/'],
-                 livereload: true,
-                 middleware: function (connect, options, middlewares) {
-                     var config = grunt.config().local.appserver;
-                     if (config.server === '') {
-                         grunt.log.error('Server not specified in grunt/local.conf.json');
-                         grunt.log.writeln('Hint: If this is a new setup you may want to copy the file grunt/local.conf.defaults.json to grunt/local.conf.json and change its values according to your setup.');
-                         grunt.fail.fatal('Please adjust your local.conf.json');
-                     }
+                base: ['build/'],
+                livereload: true,
+                middleware: function (connect, options, middlewares) {
+                    var config = grunt.config().local.appserver;
+                    if (config.server === '') {
+                        grunt.log.error('Server not specified in grunt/local.conf.json');
+                        grunt.log.writeln('Hint: If this is a new setup you may want to copy the file grunt/local.conf.defaults.json to grunt/local.conf.json and change its values according to your setup.');
+                        grunt.fail.fatal('Please adjust your local.conf.json');
+                    }
 
-                     config.prefixes = (config.prefixes || []).concat([options.base, options.base + '/apps/']);
-                     config.manifests = (config.manifests || []).concat(options.base + '/manifests/');
-                     config = appserver.tools.unifyOptions(config);
+                    config.prefixes = (config.prefixes || []).concat([options.base, options.base + '/apps/']);
+                    config.manifests = (config.manifests || []).concat(options.base + '/manifests/');
+                    config = appserver.tools.unifyOptions(config);
 
-                     middlewares.push(appserver.middleware.appsload(config));
-                     middlewares.push(appserver.middleware.manifests(config));
-                     middlewares.push(appserver.middleware.localfiles(config));
-                     middlewares.push(appserver.middleware.proxy(config));
-                     return middlewares;
-                 }
+                    // Todo: Take care of middlewares by connect
+                    middlewares = [];
+
+                    middlewares.push(appserver.middleware.appsload(config));
+                    middlewares.push(appserver.middleware.manifests(config));
+                    middlewares.push(appserver.middleware.localfiles(config));
+                    middlewares.push(appserver.middleware.proxy(config));
+                    return middlewares;
+                }
             }
         }
     });
