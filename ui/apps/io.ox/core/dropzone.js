6fe3bd9fcb4a3ee0a4986b156c2397b0cb591ade
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2014-03-07 15:32:00 +0100

    Finished dropzone; refactored import EML; works now.

--- ui/apps/io.ox/core/dropzone.js
+++ ui/apps/io.ox/core/dropzone.js
@@ -46,13 +46,8 @@ define('io.ox/core/dropzone', [], function () {
             case 'drop':
                 this.stop(e);
                 this.hide();
-                this.onDrop(e);
                 return false;
             }
-        },
-
-        onDrop: function () {
-            this.trigger('drop');
         },
 
         stop: function (e) {
@@ -60,16 +55,28 @@ define('io.ox/core/dropzone', [], function () {
             e.stopPropagation();
         },
 
-        show: function () {
+        show: function (e) {
+            // show dropzone
+            if (!this.accepts(e)) return;
             this.visible = true;
             this.$el.show();
             this.trigger('show');
         },
 
         hide: function () {
+            // hide dropzone
             this.visible = false;
-            this.$el.hide();
+            this.$el.hide().removeClass('dragover');
             this.trigger('hide');
+        },
+
+        drop: function (e) {
+            // final event when a file was dropped over the dropzone
+            var files = _(e.originalEvent.dataTransfer.files).toArray();
+            // filter valid files
+            files = _(files).filter(this.filter, this);
+            // call proper event
+            this.trigger(files.length > 0 ? 'drop' : 'invalid', files, e);
         },
 
         initialize: function (options) {
@@ -81,13 +88,49 @@ define('io.ox/core/dropzone', [], function () {
             this.$el.on('dispose', function (e) { this.dispose(e); }.bind(this));
         },
 
+        isFile: function (e) {
+            // adopted from: http://stackoverflow.com/questions/6848043/how-do-i-detect-a-file-is-being-dragged-rather-than-a-draggable-element-on-my-pa
+            var dt = e.originalEvent.dataTransfer;
+            if (_.browser.Firefox) return e.type === 'dragleave' ? dt.dropEffect === 'none' : dt.dropEffect !== 'none';
+            return _(dt.types).contains('Files') || _(dt.types).contains('application/x-moz-file');
+        },
+
+        accepts: function (e) {
+            // we need this function to make sure the user drags
+            // a file and not just selected text, for example.
+            return this.isFile(e);
+        },
+
+        filter: function (file) {
+            // apply regex to filter valid files
+            var filter = this.options.filter, name = file.name;
+            return !_.isRegExp(filter) || filter.test(name);
+        },
+
         render: function () {
+
             if (this.options.text) {
                 this.$el.append(
                     $('<caption>').text(this.options.text)
                 );
             }
             this.$el.hide();
+
+            this.$el.on({
+                // highlight dropzone
+                'dragover': function (e) {
+                    $(this).addClass('dragover');
+                    e.originalEvent.dataTransfer.dropEffect = 'copy';
+                },
+                // remove highlight
+                'dragleave': function () {
+                    $(this).removeClass('dragover');
+                },
+                // while we can ignore document's drop event, we need this one
+                // to detect that a file was dropped over the dropzone
+                'drop': this.drop.bind(this)
+            });
+
             return this;
         },
 
