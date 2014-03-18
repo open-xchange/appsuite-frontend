22497e5ada1f1ae96ed82945b310e19c958afd97
David Bauer <david.bauer@open-xchange.com>
2014-03-18 16:58:27 +0100

    Fixed emoji spec / Added alt tag to tests

--- ui/spec/io.ox/emoji/basic_spec.js
+++ ui/spec/io.ox/emoji/basic_spec.js
@@ -51,7 +51,7 @@ define([
                     text = emoji.unifiedToImageTag('On Tuesday "Some Body" <somebody@example.com> wrote: \u2600'),
                     test = 'On Tuesday "Some Body" <somebody@example.com> wrote: ' +
                            '<img src="apps/themes/login/1x1.gif" class="emoji emoji-unified emoji2600" ' +
-                           'data-emoji-unicode="\u2600">';
+                           'data-emoji-unicode="\u2600" alt="\u2600">';
 
                 expect($(imgTag).attr('data-emoji-unicode')).toBe('\u2600');
                 //leave text between <> as is!
@@ -63,8 +63,8 @@ define([
                     settings.set('defaultCollection', 'unified');
                     this.imgTag = emoji.unifiedToImageTag('\u2600 \u2601');
                     this.expected = '<img src="apps/themes/login/1x1.gif" class="emoji emoji-unified emoji2600" ' +
-                                    'data-emoji-unicode="\u2600"> <img src="apps/themes/login/1x1.gif" ' +
-                                    'class="emoji emoji-unified emoji2601" data-emoji-unicode="\u2601">';
+                                    'data-emoji-unicode="\u2600" alt="\u2600"> <img src="apps/themes/login/1x1.gif" ' +
+                                    'class="emoji emoji-unified emoji2601" data-emoji-unicode="\u2601" alt="\u2601">';
                 });
 
                 it('with default collection', function () {
