d3f975506def1e14b1a001819e3ad828cbefdc9a
David Bauer <david.bauer@open-xchange.com>
2014-03-21 15:47:16 +0100

    Grunt/Less: Added support for multiple themes

--- ui/grunt/tasks/less.js
+++ ui/grunt/tasks/less.js
@@ -14,41 +14,73 @@
 'use strict';
 
 module.exports = function (grunt) {
-
-    grunt.config('less', {
-
-        default: {
-            options: { lessrc: '.lessrc' },
+    var wrench = require('wrench');
+    wrench.readdirSyncRecursive('apps/themes').filter(function (file) {
+        return file.match(/\/definitions.less$/);
+    }).forEach(function (file) {
+        var themeName = file.replace(/\/definitions.less$/, '');
+        var theme = {};
+        theme[themeName] = {
+            options: {
+                compress: true,
+                cleancss: true,
+                ieCompat: false,
+                syncImport: true,
+                strictMath: false,
+                strictUnits: false,
+                relativeUrls: false,
+                paths: ['apps/themes', 'bower_components/bootstrap/less', 'bower_components/font-awesome/less'],
+                imports: {
+                    reference: [
+                        'variables.less',
+                        'mixins.less'
+                    ],
+                    less: [
+                        'definitions.less',
+                        themeName + '/definitions.less'
+                    ]
+                }
+            },
             files: [
                 {
                     src: ['apps/themes/style.less'],
                     expand: true,
                     rename: function (dest) { return dest; },
-                    dest: 'build/apps/themes/default/common.css'
+                    dest: 'build/apps/themes/' + themeName + '/common.css'
                 },
                 {
                     src: [
                         'bower_components/bootstrap/less/bootstrap.less',
                         'bower_components/bootstrap-datepicker/less/datepicker3.less',
                         'bower_components/font-awesome/less/font-awesome.less',
-                        'apps/themes/default/style.less'
+                        'apps/themes/' + themeName + '/style.less'
                     ],
                     expand: true,
                     rename: function (dest) { return dest; },
-                    dest: 'build/apps/themes/default/style.css'
-                },
-                {
-                    src: ['bower_components/bootstrap/less/bootstrap.less'],
-                    expand: true,
-                    rename: function (dest) { return dest; },
-                    dest: 'build/apps/io.ox/core/bootstrap/css/bootstrap.min.css'
+                    dest: 'build/apps/themes/' + themeName + '/style.css'
                 },
                 {
                     src: ['**/*.less', '!themes/**/*.less', '!themes/*.less'],
                     expand: true,
                     ext: '.css',
                     cwd: 'apps/',
-                    dest: 'build/apps/themes/default/'
+                    dest: 'build/apps/themes/' + themeName + '/'
+                }
+            ]
+        };
+        grunt.config.extend('less', theme);
+    });
+
+    grunt.config.extend('less', {
+
+        bootstrap: {
+            options: { lessrc: '.lessrc' },
+            files: [
+                {
+                    src: ['bower_components/bootstrap/less/bootstrap.less'],
+                    expand: true,
+                    rename: function (dest) { return dest; },
+                    dest: 'build/apps/io.ox/core/bootstrap/css/bootstrap.min.css'
                 }
             ]
         }
