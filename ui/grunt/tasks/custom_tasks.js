5cdba0864a59fdf581d3137bb36dfda5d91177d8
Julian Bäume <julian.baeume@open-xchange.com>
2014-03-25 17:14:59 +0100

    really work around problem with assemble-less
    assemble-less seems to overwrite compress task in some way. this is a
    work around (compress task will be triggered manually, for now)

--- ui/grunt/tasks/custom_tasks.js
+++ ui/grunt/tasks/custom_tasks.js
@@ -26,7 +26,7 @@ module.exports = function (grunt) {
     // steps to build the ui (ready for development)
     grunt.registerTask('build', ['bower', 'lint', 'copy_build', 'assemble_build', 'newer:concat', 'newer:less', 'compile_po']);
     // create a package ready version of the ui (aka what jenkins does)
-    grunt.registerTask('dist', ['clean', 'build', 'uglify', 'copy_dist', 'assemble:dist', 'create_i18n_properties', 'compress:source']);
+    grunt.registerTask('dist', ['clean', 'build', 'uglify', 'copy_dist', 'assemble:dist', 'create_i18n_properties']);
     // default task
     grunt.registerTask('default', ['checkDependencies', 'build', 'force_update']);
 };
