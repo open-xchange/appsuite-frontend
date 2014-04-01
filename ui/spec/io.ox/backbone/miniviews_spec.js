713cf9cace12a05846eff7ed4d2bdcd18790c674
Julian Bäume <julian.baeume@open-xchange.com>
2014-04-01 17:45:39 +0200

    port mini-view specs to chai assertions
    plain port with a few minor changes, if it made sense

--- ui/spec/io.ox/backbone/miniviews_spec.js
+++ ui/spec/io.ox/backbone/miniviews_spec.js
@@ -28,35 +28,35 @@ define(['io.ox/backbone/mini-views/common', 'io.ox/backbone/mini-views/date'], f
             });
 
             it('has an "initialize" function', function () {
-                expect(this.view.initialize).toBeFunction();
+                expect(this.view.initialize).to.be.a('function');
             });
 
             it('has a "dispose" function', function () {
-                expect(this.view.dispose).toBeFunction();
+                expect(this.view.dispose).to.be.a('function');
             });
 
             it('has a "valid" function', function () {
-                expect(this.view.valid).toBeFunction();
+                expect(this.view.valid).to.be.a('function');
             });
 
             it('has an "invalid" function', function () {
-                expect(this.view.invalid).toBeFunction();
+                expect(this.view.invalid).to.be.a('function');
             });
 
             it('has a model', function () {
-                expect(this.view.model).toBeDefined();
+                expect(this.view.model).to.be.defined;
             });
 
             it('references itself via data("view")', function () {
-                expect(this.view.$el.data('view')).toBe(this.view);
+                expect(this.view.$el.data('view')).to.equal(this.view);
             });
 
             it('cleans up after being removed from the DOM', function () {
-                spyOn(this.view, 'dispose').andCallThrough();
+                var spy = sinon.spy(this.view, 'dispose');
                 $('body').append(this.view.$el);
                 this.view.$el.remove();
-                expect(this.view.dispose).toHaveBeenCalled();
-                expect(this.view.model).toBe(null);
+                expect(spy).to.have.been.called;
+                expect(this.view.model).to.be.null;
             });
         });
 
@@ -73,56 +73,56 @@ define(['io.ox/backbone/mini-views/common', 'io.ox/backbone/mini-views/date'], f
             });
 
             it('is an input field', function () {
-                expect(this.view.$el.prop('tagName')).toBe('INPUT');
-                expect(this.view.$el.attr('type')).toBe('text');
+                expect(this.view.$el.prop('tagName')).to.equal('INPUT');
+                expect(this.view.$el.attr('type')).to.equal('text');
             });
 
             it('has a setup function', function () {
-                expect(this.view.setup).toBeFunction();
+                expect(this.view.setup).to.be.a('function');
             });
 
             it('has an update function', function () {
-                expect(this.view.update).toBeFunction();
+                expect(this.view.update).to.be.a('function');
             });
 
             it('has a render function', function () {
-                expect(this.view.render).toBeFunction();
+                expect(this.view.render).to.be.a('function');
             });
 
             it('has a render function that returns "this"', function () {
                 var result = this.view.render();
-                expect(result).toBe(this.view);
+                expect(result).to.equal(this.view);
             });
 
             it('has a render function that calls update', function () {
-                spyOn(this.view, 'update').andCallThrough();
+                var spy = sinon.spy(this.view, 'update');
                 this.view.render();
-                expect(this.view.update).toHaveBeenCalled();
+                expect(spy).to.have.been.called;
             });
 
             it('should render a name attribute', function () {
                 this.view.render();
-                expect(this.view.$el.attr('name')).toBe('test');
+                expect(this.view.$el.attr('name')).to.equal('test');
             });
 
             it('should have a default tabindex of 1', function () {
                 this.view.render();
-                expect(this.view.$el.attr('tabindex')).toBe('1');
+                expect(this.view.$el.attr('tabindex')).to.equal('1');
             });
 
             it('should be empty', function () {
-                expect(this.view.$el.val()).toBe('');
-                expect(this.model.get('test')).toBe('');
+                expect(this.view.$el.val()).to.be.empty;
+                expect(this.model.get('test')).to.be.empty;
             });
 
             it('reflects model changes', function () {
                 this.model.set('test', '1337');
-                expect(this.view.$el.val()).toBe('1337');
+                expect(this.view.$el.val()).to.equal('1337');
             });
 
             it('updates the model', function () {
                 this.view.$el.val('Hello World').trigger('change');
-                expect(this.model.get('test')).toBe('Hello World');
+                expect(this.model.get('test')).to.equal('Hello World');
             });
         });
 
@@ -139,17 +139,17 @@ define(['io.ox/backbone/mini-views/common', 'io.ox/backbone/mini-views/date'], f
             });
 
             it('is an input field', function () {
-                expect(this.view.$el.prop('tagName')).toBe('TEXTAREA');
+                expect(this.view.$el.prop('tagName')).to.equal('TEXTAREA');
             });
 
             it('reflects model changes', function () {
                 this.model.set('test', 'Lorem Ipsum');
-                expect(this.view.$el.val()).toBe('Lorem Ipsum');
+                expect(this.view.$el.val()).to.equal('Lorem Ipsum');
             });
 
             it('updates the model', function () {
                 this.view.$el.val('Lorem Ipsum').trigger('change');
-                expect(this.model.get('test')).toBe('Lorem Ipsum');
+                expect(this.model.get('test')).to.equal('Lorem Ipsum');
             });
         });
 
@@ -166,18 +166,18 @@ define(['io.ox/backbone/mini-views/common', 'io.ox/backbone/mini-views/date'], f
             });
 
             it('is an input field', function () {
-                expect(this.view.$el.prop('tagName')).toBe('INPUT');
-                expect(this.view.$el.attr('type')).toBe('checkbox');
+                expect(this.view.$el.prop('tagName')).to.equal('INPUT');
+                expect(this.view.$el.attr('type')).to.equal('checkbox');
             });
 
             it('reflects model changes', function () {
                 this.model.set('test', true);
-                expect(this.view.$el.prop('checked')).toBe(true);
+                expect(this.view.$el.prop('checked')).to.be.true;
             });
 
             it('updates the model', function () {
                 this.view.$el.prop('checked', true).trigger('change');
-                expect(this.model.get('test')).toBe(true);
+                expect(this.model.get('test')).to.be.true;
             });
         });
 
@@ -197,69 +197,69 @@ define(['io.ox/backbone/mini-views/common', 'io.ox/backbone/mini-views/date'], f
             });
 
             it('is a <div> tag with three <select> contols', function () {
-                expect(this.view.$el.prop('tagName')).toBe('DIV');
-                expect(this.view.$el.children().length).toBe(3);
-                expect(this.view.$el.find('div > select').length).toBe(3);
+                expect(this.view.$el.prop('tagName')).to.equal('DIV');
+                expect(this.view.$el.children().length).to.equal(3);
+                expect(this.view.$el.find('div > select').length).to.equal(3);
             });
 
             it('contains 0001 as fallback year', function () {
-                expect(this.view.$el.find('.year').children().first().attr('value')).toBe('0001');
+                expect(this.view.$el.find('.year').children().first().attr('value')).to.equal('0001');
             });
 
             it('contains an empty option for month', function () {
-                expect(this.view.$el.find('.month').children().eq(0).attr('value')).toBe('');
+                expect(this.view.$el.find('.month').children().eq(0).attr('value')).to.be.empty;
             });
 
             it('lists month as one-digit numbers starting with 0', function () {
-                expect(this.view.$el.find('.month').children().eq(1).attr('value')).toBe('0');
+                expect(this.view.$el.find('.month').children().eq(1).attr('value')).to.equal('0');
             });
 
             it('contains an empty option for dates', function () {
-                expect(this.view.$el.find('.date').children().eq(0).attr('value')).toBe('');
+                expect(this.view.$el.find('.date').children().eq(0).attr('value')).to.be.empty;
             });
 
             it('lists dates as one-digit numbers starting with 1', function () {
-                expect(this.view.$el.find('.date').children().eq(1).attr('value')).toBe('1');
+                expect(this.view.$el.find('.date').children().eq(1).attr('value')).to.equal('1');
             });
 
             it('reflects model state', function () {
-                expect(this.view.$el.find('.date').val()).toBe(String(this.date.getUTCDate()));
-                expect(this.view.$el.find('.month').val()).toBe(String(this.date.getUTCMonth()));
-                expect(this.view.$el.find('.year').val()).toBe(String(this.date.getUTCFullYear()));
+                expect(this.view.$el.find('.date').val()).to.equal(String(this.date.getUTCDate()));
+                expect(this.view.$el.find('.month').val()).to.equal(String(this.date.getUTCMonth()));
+                expect(this.view.$el.find('.year').val()).to.equal(String(this.date.getUTCFullYear()));
             });
 
             it('updates the model', function () {
                 this.view.$el.find('.year').val('1978').trigger('change');
                 this.view.$el.find('.month').val('0').trigger('change');
                 this.view.$el.find('.date').val('29').trigger('change');
-                expect(this.model.get('test')).toBe(Date.UTC(1978, 0, 29));
+                expect(this.model.get('test')).to.equal(Date.UTC(1978, 0, 29));
             });
 
             it('handles non-existent days correctly', function () {
                 // start end of January
                 this.model.set('test', date.DateView.utc(2013, 0, 31).getTime());
-                expect(this.view.value()).toBe('2013-01-31');
+                expect(this.view.value()).to.equal('2013-01-31');
                 // jump to February
                 this.view.$el.find('.month').val('1').trigger('change');
-                expect(this.view.value()).toBe('2013-02-28');
-                expect(this.model.get('test')).toBe(1362009600000);
+                expect(this.view.value()).to.equal('2013-02-28');
+                expect(this.model.get('test')).to.equal(1362009600000);
             });
 
             it('updates the model (without a year)', function () {
                 this.view.$el.find('.year').val('0001').trigger('change');
                 this.view.$el.find('.month').val('0').trigger('change');
                 this.view.$el.find('.date').val('29').trigger('change');
-                expect(this.model.get('test')).toBe(-62133177600000); // 0001-01-29
+                expect(this.model.get('test')).to.equal(-62133177600000); // 0001-01-29
             });
 
             it('handles non-existent days correctly (without a year)', function () {
                 // start end of January
                 this.model.set('test', date.DateView.utc(1, 0, 31).getTime());
-                expect(this.view.value()).toBe('0001-01-31');
+                expect(this.view.value()).to.equal('0001-01-31');
                 // jump to February
                 this.view.$el.find('.month').val('1').trigger('change');
-                expect(this.view.value()).toBe('0001-02-28');
-                expect(this.model.get('test')).toBe(-62130585600000); // 0001-02-28Error while detecting browser, using fallback
+                expect(this.view.value()).to.equal('0001-02-28');
+                expect(this.model.get('test')).to.equal(-62130585600000); // 0001-02-28Error while detecting browser, using fallback
             });
         });
     });
