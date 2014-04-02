50376f97f74120ed399e0fbc234ae890338eb6b1
Julian Bäume <julian.baeume@open-xchange.com>
2014-04-02 18:07:59 +0200

    add custom copy tasks for help packages
    it's now possible to install each part of the package individually

---
+++ help-drive/grunt/tasks/custom/copy.js
@@ -0,0 +1,77 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.config.extend('copy', {
+        help: {
+            files: [
+                {
+                    src: ['help-drive/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    dest: 'build/'
+                }
+            ]
+        },
+        dist_help_common: {
+            files: [
+                {
+                    src: ['help-drive/**/*' '!help-drive/l10n/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    cwd: 'build/',
+                    dest: 'dist/<%= pkg.name %>-<%= pkg.version %>'
+                }
+            ]
+        }
+    });
+
+    grunt.registerTask('copy_build', [
+        'newer:copy:apps',
+        'newer:copy:themes',
+        'newer:copy:help'
+    ]);
+
+    // add dist l10n copy tasks
+
+    grunt.file.expand({
+        cwd: 'help-drive/l10n',
+        filter: 'isDirectory'
+    }, '*').forEach(function (Lang) {
+        var lang = Lang.toLowerCase().replace(/_/g, '-'),
+            config = {};
+
+        config['dist_help_' + Lang] = {
+            files: [
+                {
+                    src: ['help-drive/l10n/' + Lang + '/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    cwd: 'build/',
+                    dest: 'dist/<%= pkg.name %>-<%= pkg.version %>'
+                }
+            ]
+        };
+        config['local_install_' + Lang] = {
+            files: [
+                {
+                    src: ['help-drive/l10n/' + Lang + '/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    cwd: 'dist/<%= pkg.name %>-<%= pkg.version %>',
+                    dest: grunt.option('dest')
+                }
+            ]
+        };
+
+        grunt.config.extend('copy', config);
+        grunt.registerTask('install:' + Lang, 'install language directory into a custom location', function () {
+            if (!grunt.option('dest')) {
+                grunt.fail.fatal('Need --dest option to be set');
+            }
+            grunt.task.run('copy:local_install_' + Lang);
+        });
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+};
---
+++ help/grunt/tasks/custom/copy.js
@@ -0,0 +1,76 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.config.extend('copy', {
+        help: {
+            files: [
+                {
+                    src: ['help/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    dest: 'build/'
+                }
+            ]
+        },
+        dist_help: {
+            files: [
+                {
+                    src: ['help/**/*', '!help/l10n/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    cwd: 'build/',
+                    dest: 'dist/<%= pkg.name %>-<%= pkg.version %>'
+                }
+            ]
+        }
+    });
+
+    grunt.registerTask('copy_build', [
+        'newer:copy:apps',
+        'newer:copy:themes',
+        'newer:copy:help'
+    ]);
+
+    // add dist l10n copy tasks
+
+    grunt.file.expand({
+        cwd: 'help/l10n',
+        filter: 'isDirectory'
+    }, '*').forEach(function (Lang) {
+        var lang = Lang.toLowerCase().replace(/_/g, '-'),
+            config = {};
+
+        config['dist_help_' + Lang] = {
+            files: [
+                {
+                    src: ['help/l10n/' + Lang + '/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    cwd: 'build/',
+                    dest: 'dist/<%= pkg.name %>-<%= pkg.version %>'
+                }
+            ]
+        };
+        config['local_install_' + Lang] = {
+            files: [
+                {
+                    src: ['help/l10n/' + Lang + '/**/*'],
+                    expand: true,
+                    filter: 'isFile',
+                    cwd: 'dist/<%= pkg.name %>-<%= pkg.version %>',
+                    dest: grunt.option('dest')
+                }
+            ]
+        };
+
+        grunt.config.extend('copy', config);
+        grunt.registerTask('install:' + Lang, 'install language directory into a custom location', function () {
+            if (!grunt.option('dest')) {
+                grunt.fail.fatal('Need --dest option to be set');
+            }
+            grunt.task.run('copy:local_install_' + Lang);
+        });
+    });
+    grunt.loadNpmTasks('grunt-contrib-copy');
+};
