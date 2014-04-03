690fe7a07219a624c0aa125793ebef94b5dd816e
Julian Bäume <julian.baeume@open-xchange.com>
2014-04-03 13:27:29 +0200

    port collectionloader api to chai and mocha

--- ui/spec/io.ox/core/api/collection_spec.js
+++ ui/spec/io.ox/core/api/collection_spec.js
@@ -15,13 +15,6 @@ define(['io.ox/core/api/collection-loader'], function (CollectionLoader) {
 
     'use strict';
 
-    // jasmin helper
-    function Done() {
-        var f = function () { return !!f.value; };
-        f.yep = function () { f.value = true; };
-        return f;
-    }
-
     function fetch(params) {
         return $.Deferred().resolve(
             params.limit === '0,3' ? [{ id: 10 }, { id: 20 }, { id: 30 }] : [{ id: 40 }, { id: 50 }, { id: 60 }]
@@ -34,7 +27,7 @@ define(['io.ox/core/api/collection-loader'], function (CollectionLoader) {
         );
     }
 
-    describe.skip('Collection loader', function () {
+    describe('Collection loader', function () {
 
         beforeEach(function () {
             this.loader = new CollectionLoader({ LIMIT: 3 });
@@ -44,87 +37,83 @@ define(['io.ox/core/api/collection-loader'], function (CollectionLoader) {
         describe('cid()', function () {
 
             it('is a function', function () {
-                expect(this.loader.cid).toBeFunction();
+                expect(this.loader.cid).to.be.a('function');
             });
 
             it('handles missing parameters correctly', function () {
-                expect(this.loader.cid()).toBe('default');
+                expect(this.loader.cid()).to.equal('default');
             });
 
             it('handles empty object correctly', function () {
-                expect(this.loader.cid({})).toBe('default');
+                expect(this.loader.cid({})).to.equal('default');
             });
 
             it('returns correct composite ID', function () {
-                expect(this.loader.cid({ a: 1, b: 2 })).toBe('a=1&b=2');
+                expect(this.loader.cid({ a: 1, b: 2 })).to.equal('a=1&b=2');
             });
         });
 
         describe('addIndex()', function () {
 
             it('is a function', function () {
-                expect(this.loader.addIndex).toBeFunction();
+                expect(this.loader.addIndex).to.be.a('function');
             });
 
             it('injects index property', function () {
                 var data = [{ a: 10 }];
                 this.loader.addIndex(0, {}, data);
-                expect(data).toEqual([{ a: 10, index: 0 }]);
+                expect(data).to.deep.equal([{ a: 10, index: 0 }]);
             });
 
             it('calls each()', function () {
                 var data = [{ a: 10 }];
                 this.loader.each = function (obj) { obj.test = true; };
                 this.loader.addIndex(0, {}, data);
-                expect(data).toEqual([{ a: 10, index: 0, test: true }]);
+                expect(data).to.deep.equal([{ a: 10, index: 0, test: true }]);
             });
         });
 
         describe('Instance', function () {
 
             it('returns a collection', function () {
-                expect(this.loader.getDefaultCollection() instanceof Backbone.Collection).toBe(true);
-                expect(this.loader.getCollection() instanceof Backbone.Collection).toBe(true);
+                expect(this.loader.getDefaultCollection()).to.be.an.instanceof(Backbone.Collection);
+                expect(this.loader.getCollection()).to.be.an.instanceof(Backbone.Collection);
             });
 
             it('has a load method that returns a deferred object', function () {
                 var def = this.loader.load();
-                expect(def.promise).toBeFunction();
-                expect(def.done).toBeFunction();
+                expect(def.promise).to.be.a('function');
+                expect(def.done).to.be.a('function');
             });
 
-            it('has a load method that loads initial data', function () {
-                var done = new Done();
-                waitsFor(done);
+            it('has a load method that loads initial data', function (done) {
                 this.loader.load().done(function (collection) {
-                    expect(collection instanceof Backbone.Collection).toBe(true);
-                    expect(collection.pluck('id')).toEqual([10, 20, 30]);
-                    expect(collection.pluck('index')).toEqual([0, 1, 2]);
-                    done.yep();
+                    expect(collection).to.be.an.instanceof(Backbone.Collection);
+                    expect(collection.pluck('id')).to.deep.equal([10, 20, 30]);
+                    expect(collection.pluck('index')).to.deep.equal([0, 1, 2]);
+                    done();
                 });
             });
 
-            it('has a paginate method that loads more data', function () {
-                var done = new Done(), loader = this.loader;
-                waitsFor(done);
+            it('has a paginate method that loads more data', function (done) {
+                var loader = this.loader;
                 this.loader.load().done(function () {
                     loader.paginate().done(function (collection) {
-                        expect(collection.pluck('id')).toEqual([10, 20, 30, 40, 50, 60]);
-                        expect(collection.pluck('index')).toEqual([0, 1, 2, 3, 4, 5]);
-                        done.yep();
+                        expect(collection.pluck('id')).to.deep.equal([10, 20, 30, 40, 50, 60]);
+                        expect(collection.pluck('index')).to.deep.equal([0, 1, 2, 3, 4, 5]);
+                        done();
                     });
                 });
             });
 
-            it('has a reload method that reloads data', function () {
-                var done = new Done(), loader = this.loader;
-                waitsFor(done);
+            it('has a reload method that reloads data', function (done) {
+                var loader = this.loader;
                 this.loader.load().done(function () {
                     loader.fetch = fetchAlternative;
                     loader.reload().done(function (collection) {
-                        expect(collection.pluck('id')).toEqual([70, 20, 40, 50, 80]);
-                        expect(collection.pluck('index')).toEqual([0, 1, 2, 3, 4]);
-                        done.yep();
+                        expect(collection.pluck('id')).to.deep.equal([70, 20, 40, 50, 80]);
+                        expect(collection.pluck('index')).to.deep.equal([0, 1, 2, 3, 4]);
+                        done();
                     });
                 });
             });
