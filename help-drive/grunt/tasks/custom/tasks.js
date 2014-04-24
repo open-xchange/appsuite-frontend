5865905d95e4d641a8951ed747b2e110445466c5
Julian Bäume <julian.baeume@open-xchange.com>
2014-04-24 18:57:04 +0200

    optimize packaging dependencies for help packages
    switch to optionalDependencies for deps used for packaging
    and add shrinkwrap file

---
+++ help-drive/grunt/tasks/custom/tasks.js
@@ -0,0 +1,24 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2013 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author David Bauer <david.bauer@open-xchange.com>
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    //Override the default tasks
+
+    // steps to build the ui (ready for development)
+    grunt.registerTask('build', ['copy_build']);
+    // create a package ready version of the ui (aka what jenkins does)
+    grunt.registerTask('dist', ['clean', 'build', 'copy_dist']);
+};
