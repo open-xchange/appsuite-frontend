bc49b550fc09131c0bce1e64ec9e4e14369777bb
christoph.kopp <christoph.kopp@open-xchange.com>
2014-04-25 09:58:46 +0200

    Fixed: Bug 31659 - [L3] edit draft email open new tab instead of reusing already opened one

--- ui/apps/io.ox/mail/write/main.js
+++ ui/apps/io.ox/mail/write/main.js
@@ -101,7 +101,9 @@ define('io.ox/mail/write/main',
         timer = function () {
             // only auto-save if something changed (see Bug #26927)
             if (app.dirty()) {
-                app.saveDraft();
+                app.saveDraft().done(function (data) {
+                    app.refId = data.id;
+                });
             } else {
                 delay();
             }
