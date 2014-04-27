6f5d262195b2fefb2540d3cdaa4413ebba3ff4e2
tobias.prinz <tobias.prinz@open-xchange.com>
2014-04-27 16:19:19 +0200

    Merge of the XING features into the release branch.
    Development done.
    Reviewed and polished accordingly.
    
    Squashed commit of the following:
    
    commit b31c7277b3e4176ae10e9b48381bedff4e01d4a5
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Sun Apr 27 16:13:35 2014 +0200
    
        Third round of polishing
    
    commit c4e5c208eed6d6d73410f46d245837b082002ded
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Fri Apr 25 16:55:58 2014 +0200
    
        another round of polishing after more feedback
        showing company info in the halo now
        external links use target=_blank
        popup for creating a xing account fixed
    
    commit 507ea396d944f8395320733c984b71fe3160c4ff
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Wed Apr 23 16:35:56 2014 +0200
    
        Fixed a check that used only & instead of &&
        Removed test data
        Fixed some CSS
    
    commit 17099bc8f372654ff34157f92ac57e224e31c32e
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Wed Apr 23 16:21:41 2014 +0200
    
        Polishing, round #1
    
    commit 549e92c68880dcac860e97da568e1e2cc8c18edb
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Tue Apr 22 14:17:04 2014 +0200
    
        Nice looking list of related contacts for XING
    
    commit f2cea293e3ca75adfd46db8a5c31326bb47db5ca
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Wed Apr 16 18:07:00 2014 +0200
    
        First draft of HALO view
    
    commit 71c562fb23316dde7a5a2bb1a570d88c151f48fc
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Wed Apr 16 10:13:28 2014 +0200
    
        Working blindly since the servers are down in Olpe
    
    commit d9723d5f136c323cbee8843cd7820fc7a78fe1a5
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Sat Apr 12 11:21:17 2014 +0200
    
        bla
    
    commit 3fe4559ca59e63a3669681d676b4a32f4f31b240
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Fri Apr 11 18:50:46 2014 +0200
    
        added custom setup method to chose between creating a XING account and just authenticating one.
    
    commit c4b937be7f8c62e1a7c080c938929a5cdcdcc1be
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Thu Apr 3 13:37:51 2014 +0200
    
        Developing the XING integration
    
    commit 43d2063181ed49fe7f50f1a3f5ce0a3932d33146
    Author: tobias.prinz <tobias.prinz@open-xchange.com>
    Date:   Tue Mar 25 14:39:06 2014 +0100
    
        Implemented the API naively.

---
+++ ui/apps/plugins/portal/xing/activities.js
@@ -0,0 +1,133 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * © 2014 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Tobias Prinz <tobias.prinz@open-xchange.com>
+ *
+ * Content: Activities that XING recognizes and shows in the activity
+ * feed of a user.
+ */
+
+define('plugins/portal/xing/activities',
+    ['io.ox/core/extensions',
+     'io.ox/xing/api',
+     'io.ox/core/notifications',
+     'io.ox/core/extPatterns/links',
+     'gettext!plugins/portal',
+     'less!plugins/portal/xing/xing'
+    ], function (ext, api, notifications, links, gt) {
+
+    'use strict';
+
+    var linkXingContact, makeName;
+
+    linkXingContact = function (contact) {
+        var contactNode = $('<a>')
+            .attr({href: 'https://www.xing.com/profile/' + contact.page_name, target: '_blank'})
+            .addClass('external xing');
+        if (contact.photo_urls) {
+            var ps = contact.photo_urls,
+                photoUrl = ps.maxi_thumb || ps.medium_thumb || ps.mini_thumb || ps.thumb || ps.large;
+
+            $('<img>').attr({href: photoUrl, 'class': 'photo'}).appendTo(contactNode);
+        }
+        contactNode.append($.txt(makeName(contact)));
+        return contactNode;
+    };
+
+    makeName = function (actor) {
+        return actor.type === 'user' ? actor.display_name : actor.name;
+    };
+
+
+
+
+
+    ext.point('io.ox/portal/widget/xing/activityhandler').extend({
+        id: 'link',
+        accepts: function (activity) {
+            return activity.verb === 'bookmark';
+        },
+        handle: function (activityObj, creator) {
+            return $('<div class="xing activityObj">').append(
+                $('<div class="actionDesc">').text(gt('%1$s recommends this link:', makeName(creator))),
+                $('<div class="actionContent">').append(
+                    $('<a>').attr({href: activityObj.url}),
+                    $('<div class="title">').text(activityObj.title),
+                    $('<div class="description">').text(activityObj.description)
+                )
+            );
+
+        }
+    });
+
+    ext.point('io.ox/portal/widget/xing/activityhandler').extend({
+        id: 'singleStatusPost',
+        accepts: function (activity) {
+            return activity.verb === 'post' &&
+                activity.objects.length === 1 &&
+                activity.objects[0].type === 'bookmark';
+        },
+        handle: function (activity) {
+            var linkActivity = activity.objects[0];
+            return $('<div class="xing activityObj">').append(
+                $('<div class="actionDesc">').text(gt('%1$s posted a link:', makeName(linkActivity.creator))),
+                $('<div class="actionContent">').text(linkActivity.url)
+            );
+        }
+    });
+
+    ext.point('io.ox/portal/widget/xing/activityhandler').extend({
+        id: 'singleStatusPost',
+        accepts: function (activity) {
+            return activity.verb === 'post' &&
+                activity.objects.length === 1 &&
+                activity.objects[0].type === 'status';
+        },
+        handle: function (activity) {
+            var statusActivity = activity.objects[0];
+
+            return $('<div class="xing activityObj">').append(
+                $('<div class="actionDesc">').text(gt('%1$s changed their status:', makeName(statusActivity.creator))),
+                $('<div class="actionContent">').text(statusActivity.content)
+            );
+        }
+    });
+
+    ext.point('io.ox/portal/widget/xing/activityhandler').extend({
+        id: 'friend',
+        accepts: function (activity) {
+            return activity.verb === 'make-friend';
+        },
+        handle: function (activity) {
+            var newContacts = [],
+                actors = activity.actors,
+                creator = activity.objects[0];
+
+            if (actors.length === 1) {
+                return $('<div class="xing activityObj">').append(
+                    $('<div class="actionDesc">').text(gt('%1$s has a new contact:', makeName(creator))),
+                    $('<div class="actionContent">').append(
+                        linkXingContact(actors[0])
+                    )
+                );
+            }
+
+            _(actors).each(function (actor) {
+                newContacts.push(linkXingContact(actor));
+                newContacts.push($.txt(', '));
+            });
+            newContacts.pop();
+            return $('<div class="xing activityObj">').append(
+                $('<div class="actionDesc">').text(gt('%1$s has new contacts:', makeName(creator))),
+                $('<div class="actionContent">').append(newContacts)
+            );
+        }
+    });
+});
