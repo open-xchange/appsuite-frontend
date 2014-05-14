09b66f39a2ceccbeb27c16b6979f6a65939dd623
Christoph Hellweg <christoph.hellweg@open-xchange.com>
2014-05-14 15:35:22 +0200

    Fixed: Bug 32300 - Single contact does not get updated after external change
    (cherry picked from commit dff1e9c26ba687c27e86f836ecd609405d4f3833)
    
    Conflicts:
    	ui/apps/io.ox/contacts/api.js
    	ui/apps/io.ox/core/api/factory.js

--- ui/apps/io.ox/contacts/api.js
+++ ui/apps/io.ox/contacts/api.js
@@ -226,7 +226,7 @@ define('io.ox/contacts/api',
                 }
                 return response;
             },
-            list: function (data) {
+            listPost: function (data) {
                 _(data).each(function (obj) {
                     // remove from cache if get cache is outdated
                     api.caches.get.dedust(obj, 'last_modified');
@@ -242,9 +242,11 @@ define('io.ox/contacts/api',
     * @returns defered
     */
     function clearUserApiCache(data) {
-        return $.when(userApi.caches.get.remove({ id: data.user_id }),
-                      userApi.caches.all.clear(),
-                      userApi.caches.list.remove({ id: data.user_id }));
+        return $.when(
+            userApi.caches.get.remove({ id: data.user_id }),
+            userApi.caches.all.clear(),
+            userApi.caches.list.remove({ id: data.user_id })
+        );
     }
 
     /**
