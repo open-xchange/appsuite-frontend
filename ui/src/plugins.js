1bffec8f77ab64e7eadffe9a486aeef24b9bf075
Alexander Quast <alexander.quast@open-xchange.com>
2014-05-15 10:24:06 +0200

    Minor fixes on static file cache

--- ui/src/plugins.js
+++ ui/src/plugins.js
@@ -312,19 +312,19 @@
             } else if (modulename.slice(0, 7) === 'static/') {
                 fileCache.retrieve(modulename).then(
                     function hit(contents) {
-                        if (ox.debug) console.log('Cache HIT for static file: ', modulename);
+                        if (_.url.hash('debug-filecache')) console.log('Cache HIT for static file: ', modulename);
                         runCode(modulename, contents);
                         context.completeLoad(modulename);
                     },
                     function miss() {
                         url = ox.base + '/' + url;
-                        // special käse
+                        // get the file via ajax as text, run it and store it later
                         $.ajax({
                             url: url,
                             type: 'get',
                             contentType: 'text'
                         }).done(function (sourceText) {
-                            if (ox.debug) console.log('Cache MISS for static file: ', modulename);
+                            if (_.url.hash('debug-filecache')) console.log('Cache MISS for static file: ', modulename);
                             runCode(modulename, sourceText);
                             fileCache.cache(modulename, sourceText);
                             context.completeLoad(modulename);
