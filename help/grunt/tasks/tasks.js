78a05bac9a4cdbdde6e8c7b02bafd00cc04b8d59
Julian Bäume <julian.baeume@open-xchange.com>
2014-06-03 11:22:58 +0200

    add check dependencies tasks (npm + bower) to dist
    this has been missing, running just grunt dist did not create bootstrap css.

--- help/grunt/tasks/tasks.js
+++ help/grunt/tasks/tasks.js
@@ -20,5 +20,5 @@ module.exports = function (grunt) {
     // steps to build the ui (ready for development)
     grunt.registerTask('build', ['copy_build', 'less']);
     // create a package ready version of the ui (aka what jenkins does)
-    grunt.registerTask('dist', ['clean', 'build', 'copy_dist']);
+    grunt.registerTask('dist', ['clean', 'checkDependencies:build', 'bower', 'build', 'copy_dist']);
 };
