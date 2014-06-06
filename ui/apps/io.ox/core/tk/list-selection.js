3eec64200e9b1085054b0975f0dc49929e7b501a
Alexander Quast <alexander.quast@open-xchange.com>
2014-06-06 10:52:50 +0200

    Removed typo

--- ui/apps/io.ox/core/tk/list-selection.js
+++ ui/apps/io.ox/core/tk/list-selection.js
@@ -1,4 +1,4 @@
-+/**
+/**
  * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
  * LICENSE. This work is protected by copyright and/or other applicable
  * law. Any use of the work other than as authorized under this license
