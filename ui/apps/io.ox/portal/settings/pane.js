18bbd4090207ba993976cef704a2532c96427e99
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2014-06-12 14:36:57 +0200

    additional commit for Bug 33035 - [L3] - Cannot disable quota plugin

--- ui/apps/io.ox/portal/settings/pane.js
+++ ui/apps/io.ox/portal/settings/pane.js
@@ -404,7 +404,9 @@ define('io.ox/portal/settings/pane',
                     this.sort({ silent: true });
                     list.empty();
                     this.each(function (model) {
-                        list.append(createView(model).render().el);
+                        if (model.get('protectedWidget') !== true || model.get('enabled') !== false) {
+                            list.append(createView(model).render().el);
+                        }
                     });
                 });
         }
