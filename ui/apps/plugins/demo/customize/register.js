b7fc76f451c5642084439450b93fc64cb59647d9
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2014-06-20 07:40:52 +0200

    Polished "customize" tool (added selection & link color)

--- ui/apps/plugins/demo/customize/register.js
+++ ui/apps/plugins/demo/customize/register.js
@@ -15,6 +15,10 @@ define('plugins/demo/customize/register', [], function () {
 
     'use strict';
 
+    //
+    // This is quick & dirty code. Don't adopt this style for productive use.
+    //
+
     // add model dialog
     $('body').append(
         '<div class="modal" id="customize-dialog">' +
@@ -22,17 +26,20 @@ define('plugins/demo/customize/register', [], function () {
         '    <div class="modal-content">' +
         '      <div class="modal-header">' +
         '        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-        '        <h5 class="modal-title">Customize</h4>' +
+        '        <h5 class="modal-title">Customize</h5>' +
         '      </div>' +
         '      <div class="modal-body">' +
         '        <form>' +
-        '          <div class="form-group"><label>Color</label><br><input type="color"></div>' +
+        '          <div class="form-group"><label>Tob bar color</label><br><input type="color" value="#3774A8" class="color-topbar"></div>' +
+        '          <div class="form-group"><label>Selection color</label><br><input type="color" value="#428BCA" class="color-selection"></div>' +
+        '          <div class="form-group"><label>Link color</label><br><input type="color" value="#428BCA" class="color-link"></div>' +
         '          <div class="form-group"><label>Logo</label><br><input type="file"></div>' +
         '        </form>' +
         '      </div>' +
         '    </div>' +
         '  </div>' +
-        '</div>'
+        '</div>' +
+        '<style id="customize-css"></style>'
     );
 
     // show modal dialog
@@ -40,10 +47,35 @@ define('plugins/demo/customize/register', [], function () {
         $('#customize-dialog').modal({ backdrop: false, keyboard: true, show: true });
     });
 
-    // on change color
-    $('#customize-dialog input[type="color"]').on('change', function () {
+    // on change color - topbar
+    $('#customize-dialog .color-topbar').on('change', function () {
         var color = $(this).val();
         $('#io-ox-topbar').css('backgroundColor', color);
+    });
+
+    var colors = { selection: '#428BCA', link: '#428BCA' };
+
+    function updateStylesheet() {
+        $('#customize-css').text(
+            '.list-view.visible-selection.has-focus .list-item.selected,\n' +
+            '.vgrid .vgrid-scrollpane > div:focus .vgrid-cell.selected,\n' +
+            '.foldertree-sidepanel .foldertree-container .io-ox-foldertree .folder:focus.selected {\n' +
+            '  background-color: ' + colors.selection + ';\n' +
+            '}\n' +
+            'a { color: ' + colors.link + '; }'
+        );
+    }
+
+    // on change color - selection
+    $('#customize-dialog .color-selection').on('change', function () {
+        colors.selection = $(this).val();
+        updateStylesheet();
+    });
+
+    // on change color - link
+    $('#customize-dialog .color-link').on('change', function () {
+        colors.link = $(this).val();
+        updateStylesheet();
     });
 
     // on select file
@@ -65,9 +97,11 @@ define('plugins/demo/customize/register', [], function () {
                 backgroundImage: 'url(' + url + ')',
                 backgroundSize: 'contain',
                 height: 40,
-                marginTop: 0,
+                margin: '0 10px',
                 width: Math.floor(img.width * ratio)
             });
+            // clear
+            file = reader = img = url = null;
         };
         reader.readAsDataURL(file);
     });
