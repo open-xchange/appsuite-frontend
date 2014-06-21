7ee700981b6ffbf089baafe4a3d47d73f24bcb84
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2014-06-21 13:50:39 +0200

    2nd iteration "customize": Added bannerDark and reset logo; little refactoring

--- ui/apps/io.ox/mail/common-extensions.js
+++ ui/apps/io.ox/mail/common-extensions.js
@@ -473,7 +473,7 @@ define('io.ox/mail/common-extensions',
                 }
 
                 this.append(
-                    $('<div class="alert alert-info external-images">').append(
+                    $('<div class="notification-item external-images">').append(
                         $('<button type="button" class="btn btn-primary btn-sm" tabindex="1">').text(gt('Show images')),
                         $('<div class="comment">').text(gt('External images have been blocked to protect you against potential spam!'))
                     )
