eae5f0c16ed128a6a0f12faf831030fc99e78f2b
David Bauer <david.bauer@open-xchange.com>
2014-07-24 13:58:41 +0200

    Changed grunt-jscs-checker to grunt-jscs and updated to latest version

--- ui/grunt/tasks/jscs.js
+++ ui/grunt/tasks/jscs.js
@@ -16,7 +16,7 @@
 module.exports = function (grunt) {
 
     try {
-        require('grunt-jscs-checker/tasks/jscs');
+        require('grunt-jscs/tasks/jscs');
     } catch (e) {
         grunt.verbose.warn('Skipping jscs optional tasks');
         return;
