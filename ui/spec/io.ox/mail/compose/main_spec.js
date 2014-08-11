1cf254fbfbb9f02d3e48a803697d6e066baa5e8b
Julian Bäume <julian.baeume@open-xchange.com>
2014-08-11 16:40:04 +0200

    add first few tests for new mail compose
    testing the app, first

---
+++ ui/spec/io.ox/mail/compose/main_spec.js
@@ -0,0 +1,62 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * © 2014 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+define(['io.ox/mail/compose/main'], function (compose) {
+    'use strict';
+
+    describe.only('Mail Compose', function () {
+
+        describe('main app', function () {
+            var app;
+            beforeEach(function (done) {
+                app = compose.getApp();
+                app.launch().then(done);
+            });
+            afterEach(function (done) {
+                app.quit().then(done);
+            });
+
+            it('should open up a new mail compose window', function () {
+                app.compose({ folder_id: 'default0/INBOX' });
+                expect(app.get('state')).to.equal('running');
+                expect(app.view.$el.is(':visible'), 'view element is visible').to.be.true;
+            });
+
+            it('should update the url hash', function () {
+                app.compose({ folder_id: 'default0/INBOX' });
+                expect(_.url.hash('app')).to.equal('io.ox/mail/compose:compose');
+            });
+
+            it('should provide an edit window', function () {
+                app.edit({ folder_id: 'default0/INBOX' });
+                expect(app.get('state')).to.equal('running');
+                expect(app.view.$el.is(':visible'), 'view element is visible').to.be.true;
+                expect(_.url.hash('app')).to.equal('io.ox/mail/compose:compose');
+            });
+
+            xit('should provide a reply window', function (done) {
+                this.server.respondWith('PUT', /api\/mail\?action=reply/, function (xhr) {
+                    xhr.respond(200, 'content-type:text/javascript;', JSON.stringify({
+                        data: {}
+                    }));
+                });
+                app.reply({ folder: 'default0/INBOX' }).done(function () {
+                    expect(app.get('state')).to.equal('running');
+                    expect(_.url.hash('app')).to.equal('io.ox/mail/compose:reply');
+                    expect(app.view.$el.is(':visible'), 'view element is visible').to.be.true;
+                    done();
+                });
+            });
+        });
+
+    });
+});
