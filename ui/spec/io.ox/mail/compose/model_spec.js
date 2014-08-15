3b936caf28fa6a1420e301e972bfcdef1e1ee8b1
Julian Bäume <julian.baeume@open-xchange.com>
2014-08-15 16:49:16 +0200

    more mail compose specs

--- ui/spec/io.ox/mail/compose/model_spec.js
+++ ui/spec/io.ox/mail/compose/model_spec.js
@@ -50,6 +50,34 @@ define(['io.ox/mail/compose/model'], function (MailModel) {
                 });
 
             });
+
+            describe('different content types', function () {
+                it('switching text -> html', function () {
+                    var model = new MailModel({
+                        editorMode: 'text'
+                    });
+                    model.setContent('This is some plain text\n\n with line breaks and stuff.');
+                    expect(model.get('attachments').at(0).get('content_type')).to.equal('text/plain');
+                    //TODO: bad API? 'text' is not a content type. text/html is, or rename method to 'editorMode' or so
+                    model.setMailContentType('html');
+                    expect(model.getContent()).to.equal('This is some plain text\n\n with line breaks and stuff.');
+                    expect(model.get('attachments').at(0).get('content')).to.equal('This is some plain text\n\n with line breaks and stuff.');
+                    expect(model.get('attachments').at(0).get('content_type')).to.equal('text/html');
+                });
+                it('switching html -> text', function () {
+                    var model = new MailModel({
+                        editorMode: 'html'
+                    });
+                    model.setContent('This is some <i>html</i> <b>text</b><br /><br> with line breaks and stuff.');
+                    expect(model.get('attachments').at(0).get('content_type')).to.equal('text/html');
+                    //TODO: bad API? 'text' is not a content type. text/plain is, or rename method to 'editorMode' or so
+                    model.setMailContentType('text');
+                    //FIXME: should not this be text/plain?
+                    expect(model.get('attachments').at(0).get('content_type')).to.equal('text/text');
+                    expect(model.get('attachments').at(0).get('content')).to.equal('This is some <i>html</i> <b>text</b><br /><br> with line breaks and stuff.');
+                    expect(model.getContent()).to.equal('This is some <i>html</i> <b>text</b><br /><br> with line breaks and stuff.');
+                });
+            });
         });
     });
 });
