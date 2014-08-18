d79d97c9cf9378c5f723d667aa822d554a30cd21
David Bauer <david.bauer@open-xchange.com>
2014-08-18 13:35:34 +0200

    Fixed: Bug 34067 - [L3] mail compose and reply does not work when language set to chinese (never-ending loading UI)

--- ui/apps/io.ox/core/tk/html-editor.js
+++ ui/apps/io.ox/core/tk/html-editor.js
@@ -384,20 +384,12 @@ define.async('io.ox/core/tk/html-editor',
         lookup_lang = ox.language,
         tinymce_langpacks = ['ar', 'ar_SA', 'az', 'be', 'bg_BG', 'bn_BD', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'de_AT', 'dv', 'el', 'en_CA', 'en_GB', 'es', 'et', 'eu', 'fa', 'fi', 'fo', 'fr_FR', 'gd', 'gl', 'he_IL', 'hr', 'hu_HU', 'hy', 'id', 'is_IS', 'it', 'ja', 'ka_GE', 'kk', 'km_KH', 'ko_KR', 'lb', 'lt', 'lv', 'ml', 'ml_IN', 'mn_MN', 'nb_NO', 'nl', 'pl', 'pt_BR', 'pt_PT', 'ro', 'ru', 'si_LK', 'sk', 'sl_SI', 'sr', 'sv_SE', 'ta', 'ta_IN', 'tg', 'th_TH', 'tr_TR', 'tt', 'ug', 'uk', 'uk_UA', 'vi', 'vi_VN', 'zh_CN', 'zh_TW'],
 
-        tinymce_special = {
-            zh_CN: 'zh-cn',
-            zh_TW: 'zh-tw'
-        };
-
-        if (_.has(tinymce_special, lookup_lang)) {
-            return tinymce_special[lookup_lang];
-        }
-
-        tinymce_lang = _.indexOf(tinymce_langpacks, lookup_lang.substr(0, 2), true);
+        tinymce_lang = _.indexOf(tinymce_langpacks, lookup_lang, true);
         if (tinymce_lang > -1) {
             return tinymce_langpacks[tinymce_lang];
         } else {
-            return 'en';
+            tinymce_lang = _.indexOf(tinymce_langpacks, lookup_lang.substr(0, 2), true);
+            return (tinymce_lang > -1) ? tinymce_langpacks[tinymce_lang] : 'en';
         }
     }
 
