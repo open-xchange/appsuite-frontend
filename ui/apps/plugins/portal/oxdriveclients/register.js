15174a047f6b01b6958963302cadf834ec48dd2e
Alexander Quast <alexander.quast@open-xchange.com>
2014-08-27 15:02:13 +0200

    Finished story: Links to OX Drive app stores from download area in UI (brandable), 66939084

---
+++ ui/apps/plugins/portal/oxdriveclients/register.js
@@ -0,0 +1,138 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * © 2012 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Alexander Quast <alexander.quast@open-xchange.com>
+ */
+
+define('plugins/portal/oxdriveclients/register',
+    ['io.ox/core/extensions',
+    'gettext!plugins/portal',
+    'io.ox/core/capabilities',
+    'settings!plugins/portal/oxdriveclients',
+    'less!plugins/portal/oxdriveclients/style'
+    ], function (ext, gt, capabilities, settings) {
+
+    'use strict';
+
+    function getPlatform() {
+        var isAndroid = _.device('android'),
+            isIOS = _.device('ios'),
+            isMac = _.device('macos'),
+            isWindows = _.device('windows');
+
+        if (isWindows) return 'Windows';
+        if (isAndroid) return 'Android';
+        if (isIOS) return 'iOS';
+        if (isMac) return 'Mac OS';
+        // .# Will be used as part of another string, forming a sentence like "Download OX Drive for your platform". Platform is meant as operating system like Windows or Mac OS
+        return gt('your platform');
+    }
+
+    function getAllOtherPlatforms() {
+        var isAndroid = _.device('android'),
+            isIOS = _.device('ios'),
+            isMac = _.device('macos'),
+            isWindows = _.device('windows');
+
+        if (isWindows) return ['Android', 'iOS', 'Mac OS'];
+        if (isAndroid) return  ['Windows', 'iOS', 'Mac OS'];
+        if (isIOS) return ['Mac OS', 'Windows', 'Android'];
+        if (isMac) return ['iOS', 'Android', 'Windows'];
+    }
+
+    function getShopLinkWithImage(platform, url) {
+
+        var lang = ox.language.split('_')[0],
+            // languages we have custom shop icons for
+            langs = ['de', 'en', 'es', 'fr', 'it', 'nl'],
+            imagePath = ox.abs + ox.root + '/apps/plugins/portal/oxdriveclients/img/',
+            platform = platform.toLowerCase();
+        // fallback
+        if (_.indexOf(langs, lang) === -1) lang = 'en';
+
+        if (platform.match(/android|ios|mac os/)) {
+            if (platform === 'mac os') platform = 'mac_os';
+            var $img = $('<div class="oxdrive-shop-image ' + platform +'">')
+                .css('background-image', 'url(' + imagePath + lang + '_'  + platform + '.png)');
+
+            return $('<a class="shoplink">').attr({
+                        href: url,
+                        target: '_blank',
+                    }).append($img);
+        } else if (platform === 'windows' && capabilities.has('oxupdater')) {
+            return  [$('<i class="fa fa-download">'),
+                    $('<a class="shoplink">').attr({
+                        href: ox.apiRoot + url + '?session=' + ox.session,
+                        target: '_blank'
+                    }).text(gt('Download installation file (for Windows)'))
+                    ];
+        } else {
+            return $();
+        }
+    }
+
+    ext.point('io.ox/portal/widget/oxdriveclients').extend({
+
+        //.# Product name will be inserted to adevertise the product. I.e. "Get OX Drive"
+        title: gt.format(gt('Get %s'), settings.get('productName')),
+
+        load: function (baton) {
+            var def = $.Deferred();
+            // .# String will include a product name and a platform forming a sentence like "Download OX Drive for Windows now."
+            baton.message = gt.format(gt('Download %s for %s now'), settings.get('productName'), getPlatform());
+            baton.teaser = gt.format(gt('The %s client lets you store and share your photos, files, documents and videos, anytime,' +
+                        'anywhere. Access any file you save to OX Drive from all your computers, iPhone, iPad or from within OX App Suite itself.'), settings.get('productName'));
+            baton.link = settings.get('linkTo/' + getPlatform());
+
+            return def.resolve();
+        },
+
+        preview: function (baton) {
+            var platform = getPlatform(),
+                link = getShopLinkWithImage(platform, settings.get('linkTo/' + platform));
+            this.append($('<ul class="oxdrive content pointer list-unstyled">').append(
+                $('<li class="first">').append($('<div class="appicon">').css('background-image', settings.get('appIconAsBase64'))),
+                $('<li class="message">').append($('<h4>').text(baton.message)),
+                $('<li class="teaser">').text(baton.teaser),
+                $('<li class="link">').append(link))
+            );
+        },
+
+        draw: function (baton) {
+            var ul, platform = getPlatform();
+            this.append(
+                ul = $('<ul class="oxdrive content pointer list-unstyled">').append(
+                    $('<li class="first">').append(
+                        $('<div class="appicon">').css('background-image', settings.get('appIconAsBase64'))
+                    ),
+                    $('<li class="message">').append(
+                        $('<h3>').text(baton.message)
+                    ),
+                    $('<li class="teaser">').text(baton.teaser),
+                    $('<li class="link">').append(
+                        getShopLinkWithImage(platform, settings.get('linkTo/' + platform))
+                    )
+                )
+            );
+            // all other platforms
+            ul.append($('<li>').append($('<h3>').text(gt('OX Drive is also available for other platforms:'))));
+
+            _.each(getAllOtherPlatforms(), function (os) {
+                ul.append($('<li class="link">').append(getShopLinkWithImage(os, settings.get('linkTo/' + os))));
+            });
+        }
+    });
+
+    ext.point('io.ox/portal/widget/oxdriveclients/settings').extend({
+        title: gt.format(gt('Get %s'), settings.get('productName')),
+        type: 'oxdriveclients',
+        editable: false
+    });
+});
