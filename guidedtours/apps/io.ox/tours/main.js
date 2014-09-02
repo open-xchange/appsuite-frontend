c895c88269b276470b39e87de085a438f1d0d0ad
Harald Petry <harald.petry@open-xchange.com>
2014-09-02 14:46:05 +0200

    Fixes for bug34125 Guided tour for mail app cannot be proceeded once the sorting is explained
    - Fixed the placement of some tour steps

--- guidedtours/apps/io.ox/tours/main.js
+++ guidedtours/apps/io.ox/tours/main.js
@@ -296,7 +296,9 @@ define('io.ox/tours/main',
                 placement: 'bottom', /* Check target */
                 target: function () { return $('.dropdown.grid-options.toolbar-item.pull-right:visible')[0]; },
                 content: gt('To sort the E-Mails, click on Sort by. Select a sort criteria.'),
-                xOffset: -15
+                xOffset: -230, // see bug #34010
+                arrowOffset: 230,
+                width: 260
             },
             {
                 title: gt('Selecting a view'),
@@ -304,14 +306,14 @@ define('io.ox/tours/main',
                 target: function () { return $('.classic-toolbar .pull-right:visible')[0]; },
                 content: gt('To choose between the different views. click on View in the toolbar. Select a menu entry in the layout.'),
                 xOffset: -15
-            }, 
+            },
             {
                 title: gt('Opening an E-Mail in a separate window'),
-                placement: 'right',
+                placement: 'top', // see bug #34125
                 target: function () { return $('.list-view:visible')[0]; },
                 content: gt('If double-clicking on an E-Mail in the list, the E-Mail is opened in a separate window.'),
-                xOffset: -15
-            }, 
+                yOffset: 110  // see bug #34125
+            },
             {
                 title: gt('Reading E-Mail conversations'),
                 placement: 'left',
@@ -706,7 +708,7 @@ define('io.ox/tours/main',
             },
             {
                 title: gt('Editing multiple tasks'),
-                placement: 'right',
+                placement: 'bottom', // preventive fixed along with bug #34125
                 target: function () { return $('.vgrid-scrollpane-container:visible')[0]; },
                 //target: function () { return $('abs.vgrid-scrollpane:visible')[0]; },
                 content: gt('To edit multiple tasks at once, enable the checkboxes at the left side of the tasks. If the checkboxes are not displayed, click on View > Checkboxes on the right side of the toolbar.'),
