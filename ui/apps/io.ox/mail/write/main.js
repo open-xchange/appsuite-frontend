76e544aef95c470408c912d6980a308bb69bdec2
David Bauer <david.bauer@open-xchange.com>
2014-09-04 16:36:47 +0200

    Fixed: Bug 34254 - Draft mails are not removed when sending
    (cherry picked from commit bd70d2731bdfa28751f002ee2dcdf9af7d779748)

--- ui/apps/io.ox/mail/write/main.js
+++ ui/apps/io.ox/mail/write/main.js
@@ -104,6 +104,9 @@ define('io.ox/mail/write/main',
             if (app.dirty()) {
                 app.autoSaveDraft().done(function (data) {
                     app.refId = data;
+                    app.setSendType(mailAPI.SENDTYPE.DRAFT);
+                    app.setMsgRef(data);
+                    app.dirty(false);
                 });
             } else {
                 delay();
