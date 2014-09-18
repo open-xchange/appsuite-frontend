open-xchange/appsuite-frontend

commit af0c81bc9a5afe65b044f9e974b1bde7f585a15a

move getFieldLabel function to contacts util

ui/apps/io.ox/contacts/util.js
@@ -15,8 +15,9 @@ define('io.ox/contacts/util',
     ['io.ox/core/util',
      'settings!io.ox/contacts',
      'gettext!io.ox/contacts',
-     'io.ox/core/date'
-    ], function (util, settings, gt, date) {
+     'io.ox/core/date',
+     'io.ox/contacts/model'
+    ], function (util, settings, gt, date, model) {
 
     'use strict';
 
@@ -58,6 +59,15 @@ define('io.ox/contacts/util',
     }
 
     var that = {
+
+        /**
+         * fieldname to fieldlabel
+         * @param  {string} field
+         * @return {string} label
+         */
+        getFieldLabel: function (field) {
+            return model.fields[field] || '';
+        },
 
         // variant of getFullName without title, all lowercase
         getSortName: function (obj) {

ui/apps/io.ox/mail/compose/view.js
@@ -201,28 +201,6 @@ define('io.ox/mail/compose/view',
     // disable attachmentList by default
     ext.point(POINT + '/attachments').disable('attachmentList');
 
-    /**
-     * mapping for getFieldLabel()
-     * @type {object}
-     */
-    var mapping = {
-        telephone_business1: gt('Phone (business)'),
-        telephone_business2: gt('Phone (business)'),
-        telephone_home1: gt('Phone (private)'),
-        telephone_home2: gt('Phone (private)'),
-        cellular_telephone1: gt('Mobile'),
-        cellular_telephone2: gt('Mobile')
-    };
-
-    /**
-     * fieldname to fieldlabel
-     * @param  {string} field
-     * @return {string} label
-     */
-    function getFieldLabel(field) {
-        return mapping[field] || '';
-    }
-
     /*
      * extension point for contact picture
      */
@@ -267,8 +245,8 @@ define('io.ox/mail/compose/view',
                 this.append(
                     $('<div class="ellipsis email">').append(
                         $.txt(baton.data.email + (baton.data.phone || '') + ' '),
-                        getFieldLabel(baton.data.field) !== '' ?
-                            $('<span style="color: #888;">').text('(' + getFieldLabel(baton.data.field) + ')') : []
+                        contactsUtil.getFieldLabel(baton.data.field) !== '' ?
+                            $('<span style="color: #888;">').text('(' + contactsUtil.getFieldLabel(baton.data.field) + ')') : []
                     )
                 );
             } else {

commit 0d5fe585e4d5a4d77dc628674e20cf7b5294dd3d

Rework "move getFieldLabel function to contacts util"
Commit af0c81bc9a5afe65b044f9e974b1bde7f585a15a introduced a lot of
problems (due to circular dependencies in util class). This is now
handled better.

Conflicts:
	ui/apps/io.ox/contacts/util.js

ui/apps/io.ox/contacts/util.js
@@ -15,9 +15,8 @@ define('io.ox/contacts/util', [
     'io.ox/core/util',
     'settings!io.ox/contacts',
     'gettext!io.ox/contacts',
-    'io.ox/core/date',
-    'io.ox/contacts/model'
-], function (util, settings, gt, date, model) {
+    'io.ox/core/date'
+], function (util, settings, gt, date) {
 
     'use strict';
 
@@ -59,15 +58,6 @@ define('io.ox/contacts/util', [
     }
 
     var that = {
-
-        /**
-         * fieldname to fieldlabel
-         * @param  {string} field
-         * @return {string} label
-         */
-        getFieldLabel: function (field) {
-            return model.fields[field] || '';
-        },
 
         // variant of getFullName without title, all lowercase
         getSortName: function (obj) {

ui/apps/io.ox/mail/compose/view.js
@@ -20,6 +20,7 @@ define('io.ox/mail/compose/view', [
     'io.ox/mail/util',
     'io.ox/contacts/api',
     'io.ox/contacts/util',
+    'io.ox/contacts/model',
     'settings!io.ox/mail',
     'settings!io.ox/core',
     'io.ox/core/notifications',
@@ -27,7 +28,7 @@ define('io.ox/mail/compose/view', [
     'gettext!io.ox/mail',
     'less!io.ox/mail/style',
     'less!io.ox/mail/compose/style'
-], function (extensions, MailModel, Dropdown, ext, mailAPI, mailUtil, contactsAPI, contactsUtil, settings, coreSettings, notifications, snippetAPI, gt) {
+], function (extensions, MailModel, Dropdown, ext, mailAPI, mailUtil, contactsAPI, contactsUtil, ContactModel, settings, coreSettings, notifications, snippetAPI, gt) {
 
     'use strict';
 
@@ -245,8 +246,8 @@ define('io.ox/mail/compose/view', [
                 this.append(
                     $('<div class="ellipsis email">').append(
                         $.txt(baton.data.email + (baton.data.phone || '') + ' '),
-                        contactsUtil.getFieldLabel(baton.data.field) !== '' ?
-                            $('<span style="color: #888;">').text('(' + contactsUtil.getFieldLabel(baton.data.field) + ')') : []
+                        ContactModel.fields[baton.data.field] ?
+                            $('<span style="color: #888;">').text('(' + ContactModel.fields[baton.data.field] + ')') : []
                     )
                 );
             } else {