3ccfc19da8422f14a227dd74b352dc834cc21f10
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2014-10-07 13:56:02 +0200

    Fixed linkedin's recent activities for non-plus case

--- ui/apps/plugins/portal/linkedIn/register.js
+++ ui/apps/plugins/portal/linkedIn/register.js
@@ -92,6 +92,20 @@ define('plugins/portal/linkedIn/register',
         return _(list).some(filterActivity);
     }
 
+    function renderActivities(node, list) {
+
+        if (!_.isArray(list)) return;
+        if (!hasActivities(list)) return;
+
+        node.append(
+            $('<h2 class="linkedin-activities-header">').text(gt('Recent activities'))
+        );
+
+        _(list).each(function (activity) {
+            ext.point('io.ox/plugins/portal/linkedIn/updates/renderer').invoke('draw', node, activity);
+        });
+    }
+
     ext.point('io.ox/plugins/portal/linkedIn/updates/renderer').extend({
         id: 'CONN',
         draw: function (activity) {
@@ -303,15 +317,7 @@ define('plugins/portal/linkedIn/register',
                     params: { action: 'updates' }
                 })
                 .done(function (activities) {
-                    var list = activities.values;
-                    if (hasActivities(list)) {
-                        node.append(
-                            $('<h2 class="linkedin-activities-header">').text(gt('Recent activities'))
-                        );
-                        _(list).each(function (activity) {
-                            ext.point('io.ox/plugins/portal/linkedIn/updates/renderer').invoke('draw', node, activity);
-                        });
-                    }
+                    renderActivities(node, activities.values);
                 });
 
                 this.append(node);
@@ -334,16 +340,7 @@ define('plugins/portal/linkedIn/register',
                     });
                 });
 
-                if (_.isArray(baton.data) && baton.data.length > 0) {
-
-                    node.append(
-                        $('<h2 class="linkedin-activities-header">').text(gt('Recent activities'))
-                    );
-
-                    _(baton.data).each(function (activity) {
-                        ext.point('portal/linkedIn/updates/renderer').invoke('draw', node, activity);
-                    });
-                }
+                renderActivities(node, baton.data);
 
                 this.append(node);
             }
