94da690725f4e9b2ca6ec3330a3dfc8dcf4b198c
christoph.kopp <christoph.kopp@open-xchange.com>
2014-10-13 16:19:23 +0200

    removed unused code

--- ui/apps/io.ox/mail/actions.js
+++ ui/apps/io.ox/mail/actions.js
@@ -35,15 +35,6 @@ define('io.ox/mail/actions', [
 
     // actions
 
-    new Action('io.ox/mail/actions/unselect', {
-        requires: function (e) {
-            return e.collection.has('toplevel', 'multiple') && !e.baton.isThread;
-        },
-        multiple: function (list, baton) {
-            if (baton.grid) baton.grid.selection.clear();
-        }
-    });
-
     new Action('io.ox/mail/actions/compose', {
         id: 'compose',
         requires: function () {
@@ -470,14 +461,6 @@ define('io.ox/mail/actions', [
     // inline links
 
     var INDEX = 0;
-
-    // ext.point('io.ox/mail/links/inline').extend(new links.Link({
-    //     index: 10, // should be first
-    //     prio: 'hi',
-    //     id: 'unselect',
-    //     label: gt('Unselect all'),
-    //     ref: 'io.ox/mail/actions/unselect'
-    // }));
 
     ext.point('io.ox/mail/links/inline').extend(new links.Link({
         index: INDEX += 100,
