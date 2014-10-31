65cd17fdf2a7d81f37c46e71405f02d951d1506d
Christoph Hellweg <christoph.hellweg@open-xchange.com>
2014-10-31 11:46:04 +0100

    Fix exif reader
    (cherry picked from commit 13cf1e34e3d66bcc22d8a00d48ec481b93f22457)

--- ui/apps/io.ox/contacts/widgets/exif.js
+++ ui/apps/io.ox/contacts/widgets/exif.js
@@ -157,25 +157,15 @@ define('io.ox/contacts/widgets/exif', function () {
                 return vals;
             }
             break;
-        case 5:
-            // rational = two long values, first is numerator, second is denominator
-            if (numValues === 1) {
-                numerator = file.getLongAt(valueOffset, bigEnd);
-                denominator = file.getLongAt(valueOffset + 4, bigEnd);
-                val = Number(numerator / denominator);
-                val.numerator = numerator;
-                val.denominator = denominator;
-                return val;
+        case 5: // rational = two long values, first is numerator, second is denominator
+            if (iNumValues == 1) {
+                return oFile.getLongAt(iValueOffset, bBigEnd) / oFile.getLongAt(iValueOffset+4, bBigEnd);
             } else {
-                vals = [];
-                for (n = 0; n < numValues; n++) {
-                    numerator = file.getLongAt(valueOffset + 8 * n, bigEnd);
-                    denominator = file.getLongAt(valueOffset + 4 + 8 * n, bigEnd);
-                    vals[n] = Number(numerator / denominator);
-                    vals[n].numerator = numerator;
-                    vals[n].denominator = denominator;
+                var aVals = [];
+                for (var n=0;n<iNumValues;n++) {
+                    aVals[n] = oFile.getLongAt(iValueOffset + 8*n, bBigEnd) / oFile.getLongAt(iValueOffset+4 + 8*n, bBigEnd);
                 }
-                return vals;
+                return aVals;
             }
             break;
         case 9:
