415511d0750ac1154dcfefde5d23ecd9521f2a69
Richard Petersen <richard.petersen@open-xchange.com>
2014-11-05 08:08:20 +0100

    Fixed specs for uploading files.
    The xhr object can now simulate some progress to test for correct time estimation.

--- ui/spec/io.ox/files/upload/main_spec.js
+++ ui/spec/io.ox/files/upload/main_spec.js
@@ -25,18 +25,34 @@ define([
 
         describe('time estimation', function () {
             var clock,
-                file = { file: { name: 'some name', size: 1000000 } };
+                file = { file: { name: 'some name', size: 1000000 } },
+                xhr,
+                requests;
+
+            function uploadProgress(obj) {
+                requests[0].uploadProgress(obj);
+            }
 
             beforeEach(function () {
                 //use fake timers to manipulate progress
                 clock = sinon.useFakeTimers();
 
+                //clear requests
+                requests = [];
+
                 this.server.autoRespond = false;
+
+                //setup fake html request
+                xhr = sinon.useFakeXMLHttpRequest();
+                xhr.onCreate = function (xhr) {
+                    requests.push(xhr);
+                };
 
                 //appends a file to the queue
                 upload.changed(file, 0, [file]);
             });
             afterEach(function () {
+                xhr.restore();
                 this.server.autoRespond = true;
                 clock.restore();
                 upload.stop();
@@ -48,88 +64,99 @@ define([
                 var request = upload.progress(file, 0, [file]);
 
                 expect(request).to.be.an('object');
-                expect(request).to.have.property('notify');
                 expect(request.state()).to.equal('pending');
             });
             it('calculates weeks', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 1, total: 1000000 });
                 clock.tick(1000);
+                uploadProgress({ loaded: 1, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s weeks', 2));
             });
             it('calculates 1 week', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 1, total: 1000000 });
                 clock.tick(800);
+                uploadProgress({ loaded: 1, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s week', 1));
             });
             it('calculates days', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 2, total: 1000000 });
                 clock.tick(1100);
+                uploadProgress({ loaded: 2, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s days', 6));
             });
             it('calculates 1 day', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 5, total: 1000000 });
                 clock.tick(450);
+                uploadProgress({ loaded: 5, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s day', 1));
             });
             it('calculates hours', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 20, total: 1000000 });
                 clock.tick(450);
+                uploadProgress({ loaded: 20, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s hours', 6));
             });
             it('calculates 1 hour', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 30, total: 1000000 });
                 clock.tick(120);
+                uploadProgress({ loaded: 30, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s hour', 1));
             });
             it('calculates minutes', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 83, total: 1000000 });
                 clock.tick(100);
+                uploadProgress({ loaded: 83, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s minutes', 20));
             });
             it('calculates 1 minute', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 350, total: 1000000 });
                 clock.tick(25);
+                uploadProgress({ loaded: 350, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s minute', 1));
             });
             it('calculates seconds', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 350, total: 1000000 });
                 clock.tick(20);
+                uploadProgress({ loaded: 350, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s seconds', 57));
             });
             it('calculates 1 second', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 1000, total: 1000000 });
                 clock.tick(1);
+                uploadProgress({ loaded: 1000, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s second', 1));
             });
             it('calculates progress', function () {
-                var request = upload.progress(file, 0, [file]);
+                upload.progress(file, 0, [file]);
 
-                request.notify({ loaded: 350, total: 1000000 });
                 clock.tick(20);
+                uploadProgress({ loaded: 350, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s seconds', 57));
 
-                request.notify({ loaded: 500000, total: 1000000 });
                 clock.tick(28000);
+                uploadProgress({ loaded: 500000, total: 1000000 });
+
                 expect(upload.getEstimatedTime()).to.equal(gt('%1$s seconds', 28));
             });
         });
