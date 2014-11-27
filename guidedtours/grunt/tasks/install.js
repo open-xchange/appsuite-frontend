f5d74a08ca99e7d1be01385f5ff0982246f70a73
Julian Bäume <julian.baeume@open-xchange.com>
2014-11-27 09:18:18 +0100

    port guidedtours to latest grunt config
    just a plain update

--- guidedtours/grunt/tasks/install.js
+++ guidedtours/grunt/tasks/install.js
@@ -2,15 +2,17 @@
 
 module.exports = function (grunt) {
 
-   grunt.config.extend('copy', {
-       local_install_static: {
-            files: [{
-                expand: true,
-                src: [],
-                cwd: 'dist/',
-                filter: 'isFile',
-                dest: grunt.option('htdoc')
-            }]
-        }
-    });
+   grunt.config.merge({
+       copy: {
+           local_install_static: {
+               files: [{
+                   expand: true,
+                   src: [],
+                   cwd: 'dist/',
+                   filter: 'isFile',
+                   dest: grunt.option('htdoc')
+               }]
+           }
+       }
+  });
 };
