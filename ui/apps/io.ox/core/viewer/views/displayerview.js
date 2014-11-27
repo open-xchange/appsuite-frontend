ab1551679e178c9b984d61c74454af93a6d0318c
Edy Haryono <edy.haryono@open-xchange.com>
2014-11-27 17:56:56 +0100

    initial viewer carousel with dummy slides

--- ui/apps/io.ox/core/viewer/views/displayerview.js
+++ ui/apps/io.ox/core/viewer/views/displayerview.js
@@ -20,7 +20,7 @@ define('io.ox/core/viewer/views/displayerview', function () {
      */
     var DisplayerView = Backbone.View.extend({
 
-        className: 'io-ox-viewer-displayer',
+        className: 'viewer-displayer',
 
         events: {
 
@@ -32,7 +32,26 @@ define('io.ox/core/viewer/views/displayerview', function () {
         },
 
         render: function () {
-            //console.warn('DisplayerView.render()');
+            // console.warn('DisplayerView.render()');
+            // init carousel with  dummy slides.
+            var carouselRoot = $('<div id="viewer-carousel" class="carousel">'),
+                carouselInner = $('<div class="carousel-inner">'),
+                slide1 = $('<div class="item active"><img src="http://localhost:8337/appsuite/api/files?action=document&folder=135&id=135/657&delivery=view&scaleType=contain&content_type=image/jpeg">'),
+                slide2 = $('<div class="item"><img src="http://localhost:8337/appsuite/api/files?action=document&folder=135&id=135/658&delivery=view&scaleType=contain&content_type=image/jpeg">'),
+                slide3 = $('<div class="item"><img src="http://localhost:8337/appsuite/api/files?action=document&folder=135&id=135/659&delivery=view&scaleType=contain&content_type=image/jpeg">'),
+                slide4 = $('<div class="item"><img src="http://localhost:8337/appsuite/api/files?action=document&folder=135&id=135/660&delivery=view&scaleType=contain&content_type=image/jpeg">'),
+                slide5 = $('<div class="item"><img src="http://localhost:8337/appsuite/api/files?action=document&folder=135&id=135/661&delivery=view&scaleType=contain&content_type=image/jpeg">');
+
+            var prevSlide = $('<a class="left carousel-control" href="#viewer-carousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>'),
+                nextSlide = $('<a class="right carousel-control" href="#viewer-carousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>');
+
+            carouselInner.append(slide1, slide2, slide3, slide4, slide5);
+            carouselRoot.append(carouselInner, prevSlide, nextSlide);
+
+            carouselRoot.carousel();
+
+            this.$el.append(carouselRoot);
+
             return this;
         },
 
