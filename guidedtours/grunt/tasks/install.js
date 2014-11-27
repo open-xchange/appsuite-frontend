0118cef5ffc5471cf94f95b5574e8a83c4eeb05f
Julian Bäume <julian.baeume@open-xchange.com>
2014-11-27 14:29:49 +0100

    cleanup guidedtours grunt configuration
    should fix the build and is more easy to read/use.

--- guidedtours/grunt/tasks/install.js
+++ guidedtours/grunt/tasks/install.js
@@ -1,18 +1,6 @@
 'use strict';
 
 module.exports = function (grunt) {
-
-   grunt.config.merge({
-       copy: {
-           local_install_static: {
-               files: [{
-                   expand: true,
-                   src: [],
-                   cwd: 'dist/',
-                   filter: 'isFile',
-                   dest: grunt.option('htdoc')
-               }]
-           }
-       }
-  });
+    //no static files
+    grunt.registerTask('install:static', []);
 };
