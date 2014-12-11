6ee5c29e694772a16cc3caa1a046546ba97da58e
Edy Haryono <edy.haryono@open-xchange.com>
2014-12-11 18:24:47 +0100

    initial viewer image lazy loading

--- ui/apps/io.ox/core/viewer/views/displayerview.js
+++ ui/apps/io.ox/core/viewer/views/displayerview.js
@@ -33,51 +33,67 @@ define('io.ox/core/viewer/views/displayerview', [
         initialize: function () {
             //console.warn('DisplayerView.initialize()');
             this.$el.on('dispose', this.dispose.bind(this));
-
-            this.listenTo(EventDispatcher, 'viewer:displayeditem:change', function (/*data*/) {
-                //console.warn('SidebarbarView viewer:displayeditem:change', data);
-            });
-
         },
 
         render: function () {
             //console.warn('DisplayerView.render()');
-
-            // init carousel with  dummy slides.
             var carouselRoot = $('<div id="viewer-carousel" class="carousel">'),
                 carouselInner = $('<div class="carousel-inner">'),
                 prevSlide = $('<a class="left carousel-control" href="#viewer-carousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>'),
-                nextSlide = $('<a class="right carousel-control" href="#viewer-carousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>');
+                nextSlide = $('<a class="right carousel-control" href="#viewer-carousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>'),
+                slideToLoad = 0;
 
-            function createSlide (model) {
-
+            function createSlide (model, modelIndex) {
                 var slide = $('<div>').addClass('item'),
-                    image = $('<img>'),
+                    image = $('<img>').addClass('viewer-carousel-image'),
                     previewUrl = model && model.getPreviewUrl(),
                     filename = model && model.get('filename') || '';
-
                 if (previewUrl) {
-                    image.attr({ src: _.unescapeHTML(previewUrl), alt: filename });
-                    image.css({ maxHeight: window.innerHeight - carouselRoot.offset().top });
+                    image.attr({ 'data-src': _.unescapeHTML(previewUrl), alt: filename })
+                        .css({ maxHeight: window.innerHeight - carouselRoot.offset().top });
                     slide.append(image);
                 }
-
+                slide.attr('data-slide', modelIndex);
                 return slide;
             }
 
-            this.collection.each(function (model) {
-                carouselInner.append(createSlide(model));
+            function loadSlide (slideIndex) {
+                //console.warn('DisplayerView.render.loadSlide()', slideIndex);
+                if (Math.abs(slideIndex) >= slidesList.length) { return; }
+                var slide = slidesList.eq(slideIndex),
+                    imageToLoad = slide.find('img');
+                slide.busy();
+                imageToLoad.attr('src', imageToLoad.attr('data-src'));
+                imageToLoad[0].onload = function () {
+                    slide.idle();
+                    imageToLoad.show();
+                };
+            }
+
+            // create slides from file collection and append them to the carousel
+            this.collection.each(function (model, modelIndex) {
+                carouselInner.append(createSlide(model, modelIndex));
             });
 
-            // set first item active, to be change when we handle selected drive items
-            carouselInner.find('div.item').first().addClass('active');
+            // set first item active, load first item and its neighbors initially
+            // TODO load selected file from OX Drive/Mail
+            var slidesList = carouselInner.children();
+            slidesList.first().addClass('active');
+            loadSlide(slideToLoad);
+            loadSlide(slideToLoad + 1);
+            loadSlide(slideToLoad - 1);
 
-            carouselRoot.append(carouselInner, prevSlide, nextSlide);
+            // init the carousel
+            carouselRoot.append(carouselInner, prevSlide, nextSlide)
+                .carousel()
+                .on('slid.bs.carousel', function (event) {
+                    var activeSlideIndex = $(event.relatedTarget).data('slide'),
+                        preloadOffset = event.direction === 'left' ? 1 : -1;
+                    loadSlide(activeSlideIndex + preloadOffset);
+                });
 
-            carouselRoot.carousel();
-
+            // append carousel to view
             this.$el.append(carouselRoot);
-
             return this;
         },
 
