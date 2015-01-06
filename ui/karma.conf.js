3fdee0edf224225494ab0e3b74d9c8fc199dd0a5
Edy Haryono <edy.haryono@open-xchange.com>
2015-01-06 18:22:34 +0100

    run only viewer unit tests

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -13,7 +13,8 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'spec/main-test.js',
+        //'spec/main-test.js',
+        'spec/viewer-test.js', // test only viewer component temporarily
         {pattern: 'spec/**/*_spec.js', included: false },
         {pattern: 'spec/fixtures/**/*.*', included: false, served: true },
         {pattern: 'spec/shared/**/*.js' }
