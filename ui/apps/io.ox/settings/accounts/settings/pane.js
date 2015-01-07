8638a89a783f5b98a6bcc74e4e837830665f1d85
Richard Petersen <richard.petersen@open-xchange.com>
2015-01-07 14:59:55 +0100

    Fixed: Bug 35986 - Social media account does not appear in settings
    Social media accounts are now added correctly.

--- ui/apps/io.ox/settings/accounts/settings/pane.js
+++ ui/apps/io.ox/settings/accounts/settings/pane.js
@@ -226,7 +226,6 @@ define('io.ox/settings/accounts/settings/pane',
                         this.collection.bind('remove', this.render);
                     },
                     render: function () {
-
                         var self = this, $dropDown;
 
                         self.$el.empty().append(drawPane);
@@ -286,10 +285,15 @@ define('io.ox/settings/accounts/settings/pane',
 
             redraw();
 
+            function onChange(e, list) {
+                if (!list || list.length === 0 || list[0].id !== 'io.ox/settings/accounts') {
+                    api.off('refresh.all refresh.list', redraw);
+                    data.grid.selection.off('change', onChange);
+                }
+            }
+
             api.on('refresh.all refresh.list', redraw);
-            data.grid.selection.on('change', function () {
-                api.off('refresh.all refresh.list', redraw);
-            });
+            data.grid.selection.on('change', onChange);
         },
         save: function () {
             // TODO
