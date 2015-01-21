4a6851bff511ece1aac56771391b355457780a93
Alexander Quast <alexander.quast@open-xchange.com>
2015-01-21 09:57:15 +0100

    Added setClient to session to change client string

--- ui/apps/io.ox/core/session.js
+++ ui/apps/io.ox/core/session.js
@@ -262,7 +262,9 @@ define('io.ox/core/session', [
                 return $.Deferred().resolve();
             }
         },
-
+        setClient: function (client) {
+            if (client) CLIENT = client;
+        },
         client: function () {
             return CLIENT;
         },
