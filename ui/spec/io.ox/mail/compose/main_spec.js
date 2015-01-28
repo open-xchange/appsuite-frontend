f983df5ea1539ba871ec9869a96cc928d5e29f2c
Julian Bäume <julian.baeume@open-xchange.com>
2015-01-28 18:31:48 +0100

    be more DRY
    use beforeEach to setup the mail application to test

--- ui/spec/io.ox/mail/compose/main_spec.js
+++ ui/spec/io.ox/mail/compose/main_spec.js
@@ -65,79 +65,59 @@ define(['io.ox/mail/compose/main', 'waitsFor'], function (compose, waitsFor) {
             describe('model change events', function () {
                 var address = 'testuser@open-xchange.com',
                     addressEntry = [[address, address]];
+
+                beforeEach(function () {
+                    return app.compose({ folder_id: 'default0/INBOX' });
+                });
                 it('should add recipients to "to" tokenfield', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('to', addressEntry);
-                        expect(app.view.$el.find('.tokenfield.to .token').attr('title')).to.equal(address);
-                    });
+                    app.view.model.set('to', addressEntry);
+                    expect(app.view.$el.find('.tokenfield.to .token').attr('title')).to.equal(address);
                 });
                 it('should add recipients to "cc" tokenfield', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('cc', addressEntry);
-                        expect(app.view.$el.find('.tokenfield.cc .token').attr('title')).to.equal(address);
-                    });
+                    app.view.model.set('cc', addressEntry);
+                    expect(app.view.$el.find('.tokenfield.cc .token').attr('title')).to.equal(address);
                 });
                 it('should add recipients to "bcc" tokenfield', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('bcc', addressEntry);
-                        expect(app.view.$el.find('.tokenfield.bcc .token').attr('title')).to.equal(address);
-                    });
+                    app.view.model.set('bcc', addressEntry);
+                    expect(app.view.$el.find('.tokenfield.bcc .token').attr('title')).to.equal(address);
                 });
                 it('should add text to subject', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('subject', 'Test subject');
-                        expect(app.view.$el.find('.subject input').val()).to.equal('Test subject');
-                    });
+                    app.view.model.set('subject', 'Test subject');
+                    expect(app.view.$el.find('.subject input').val()).to.equal('Test subject');
                 });
                 it('should set priority to high', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('priority', 0);
-                        expect(app.view.$el.find('a[data-name="priority"][data-value="0"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
-                    });
+                    app.view.model.set('priority', 0);
+                    expect(app.view.$el.find('a[data-name="priority"][data-value="0"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
                 });
                 it('should set priority to normal', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('priority', 3);
-                        expect(app.view.$el.find('a[data-name="priority"][data-value="3"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
-                    });
+                    app.view.model.set('priority', 3);
+                    expect(app.view.$el.find('a[data-name="priority"][data-value="3"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
                 });
                 it('should set priority to low', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('priority', 5);
-                        expect(app.view.$el.find('a[data-name="priority"][data-value="5"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
-                    });
+                    app.view.model.set('priority', 5);
+                    expect(app.view.$el.find('a[data-name="priority"][data-value="5"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
                 });
                 it('should set request read receipt', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('disp_notification_to', true);
-                        expect(app.view.$el.find('a[data-name="disp_notification_to"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
-                    });
+                    app.view.model.set('disp_notification_to', true);
+                    expect(app.view.$el.find('a[data-name="disp_notification_to"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
                 });
                 it('should set attach vcard', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('vcard', 1);
-                        expect(app.view.$el.find('a[data-name="vcard"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
-                    });
+                    app.view.model.set('vcard', 1);
+                    expect(app.view.$el.find('a[data-name="vcard"] i').hasClass('fa-check'), ' option is checked in menu options').to.be.true;
                 });
                 it('should set attach vcard', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('vcard', 0);
-                        expect(app.view.$el.find('a[data-name="vcard"] i').hasClass('fa-check'), ' option is unchecked in menu options').to.be.false;
-                    });
+                    app.view.model.set('vcard', 0);
+                    expect(app.view.$el.find('a[data-name="vcard"] i').hasClass('fa-check'), ' option is unchecked in menu options').to.be.false;
                 });
                 it('should change editor mode from text to html', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).done(function () {
-                        app.view.model.set('editorMode', 'html');
-                        expect(app.view.$el.find('.editable-toolbar').is(':visible'), 'tinymce toolbar element is visible').to.be.true;
-                        expect(app.view.$el.find('.editable.mce-content-body').is(':visible'), 'tinymce contenteditable editor element is visible').to.be.true;
-                    });
+                    app.view.model.set('editorMode', 'html');
+                    expect(app.view.$el.find('.editable-toolbar').is(':visible'), 'tinymce toolbar element is visible').to.be.true;
+                    expect(app.view.$el.find('.editable.mce-content-body').is(':visible'), 'tinymce contenteditable editor element is visible').to.be.true;
                 });
                 it('should change editor mode from html to text', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).then(function () {
-                        app.view.model.set('editorMode', 'text');
-                        return waitsFor(function () {
-                            return app.view.$el.find('textarea.plain-text').is(':visible') === true;
-                        });
+                    app.view.model.set('editorMode', 'text');
+                    return waitsFor(function () {
+                        return app.view.$el.find('textarea.plain-text').is(':visible') === true;
                     }).then(function () {
                         expect(app.view.$el.find('textarea.plain-text').is(':visible'), 'plain text editor is visible').to.be.true;
                         expect(app.view.$el.find('.editable-toolbar').is(':visible'), 'tinymce toolbar element is visible').to.be.false;
@@ -145,11 +125,9 @@ define(['io.ox/mail/compose/main', 'waitsFor'], function (compose, waitsFor) {
                     });
                 });
                 it('should change editor mode from text to html', function () {
-                    return app.compose({ folder_id: 'default0/INBOX' }).then(function () {
-                        app.view.model.set('editorMode', 'html');
-                        return waitsFor(function () {
-                            return app.view.$el.find('.editable.mce-content-body').is(':visible') === true;
-                        });
+                    app.view.model.set('editorMode', 'html');
+                    return waitsFor(function () {
+                        return app.view.$el.find('.editable.mce-content-body').is(':visible') === true;
                     }).then(function () {
                         expect(app.view.$el.find('textarea.plain-text').is(':visible'), 'plain text editor is visible').to.be.false;
                         expect(app.view.$el.find('.editable-toolbar').is(':visible'), 'tinymce toolbar element is visible').to.be.true;
