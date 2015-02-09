917b5281980f4773ac60a2809626487bb98d5e7a
Frank Paczynski <frank.paczynski@open-xchange.com>
2015-02-09 15:34:34 +0100

    a11y/fileUploadWidget: focus upload button instead of save button

--- ui/apps/io.ox/core/tk/attachments.js
+++ ui/apps/io.ox/core/tk/attachments.js
@@ -601,7 +601,7 @@ define('io.ox/core/tk/attachments',
             gguid = _.uniqueId('form-control-label-'),
             label = $('<label>').attr('for', gguid).addClass('sr-only').text(options.buttontext),
             input = $('<input name="file" type="file" class="file-input">').prop({ multiple: options.multi }).attr({ id: gguid, tabindex: options.tabindex }),
-            uploadButton = $('<span class="btn btn-default btn-file" role="button">').append($.txt(options.buttontext)).append(label, input),
+            uploadButton = $('<span class="btn btn-default btn-file" role="button" tabindex="1">').append($.txt(options.buttontext)).append(label, input),
             driveButton = $('<button type="button" class="btn btn-default" data-action="addinternal">').attr({ tabindex: options.tabindex }).text(gt('Add from Drive'));
 
         input.on('focus', function () {
