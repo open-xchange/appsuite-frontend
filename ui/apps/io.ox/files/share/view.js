b3cd52a67264c76b18a12b9148833b22fea4665d
Christoph Hellweg <christoph.hellweg@open-xchange.com>
2015-02-20 14:34:03 +0100

    add notifications to sharing view

--- ui/apps/io.ox/files/share/view.js
+++ ui/apps/io.ox/files/share/view.js
@@ -20,9 +20,10 @@ define('io.ox/files/share/view', [
     'io.ox/contacts/util',
     'io.ox/core/tk/tokenfield',
     'io.ox/core/date',
+    'io.ox/core/yell',
     'gettext!io.ox/files',
     'less!io.ox/files/share/style'
-], function (ext, ShareModel, miniViews, Dropdown, contactsAPI, contactsUtil, Tokenfield, date, gt) {
+], function (ext, ShareModel, miniViews, Dropdown, contactsAPI, contactsUtil, Tokenfield, date, yell, gt) {
 
     'use strict';
 
@@ -369,7 +370,7 @@ define('io.ox/files/share/view', [
                 if (val === 'link' && model.get('link') === '' ) {
                     model.save().then(function (url) {
                         model.set('link', url);
-                    });
+                    }, yell);
                 }
             });
 
@@ -388,7 +389,7 @@ define('io.ox/files/share/view', [
         },
 
         share: function () {
-            this.model.save();
+            this.model.save().then(yell.done, yell);
         },
 
         cancel: function () {
