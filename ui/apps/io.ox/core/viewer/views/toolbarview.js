d7cdb81b29d5c09f399ddc591b5e7b636fc535c2
Edy Haryono <edy.haryono@open-xchange.com>
2015-04-07 13:42:56 +0200

    rename file per single click in viewer toolbar

--- ui/apps/io.ox/core/viewer/views/toolbarview.js
+++ ui/apps/io.ox/core/viewer/views/toolbarview.js
@@ -293,7 +293,6 @@ define('io.ox/core/viewer/views/toolbarview', [
             'click a.viewer-toolbar-close': 'onClose',
             'click a.viewer-toolbar-togglesidebar': 'onToggleSidebar',
             'click a.viewer-toolbar-filename': 'onRename',
-            'dblclick a.viewer-toolbar-filename': 'onRename',
             'keydown a.viewer-toolbar-filename': 'onRename'
         },
 
@@ -332,8 +331,7 @@ define('io.ox/core/viewer/views/toolbarview', [
          */
         onRename: function (event) {
             //console.warn('TooölbarView.onRename()', event);
-            if (this.model.isDriveFile() && (event.which === 32 || event.which === 13 || event.type === 'dblclick' ||
-                (_.device('smartphone || tablet') && event.type === 'click'))) {
+            if (this.model.isDriveFile() && (event.which === 32 || event.which === 13 || event.type === 'click')) {
                 event.preventDefault();
                 ActionsPattern.invoke('io.ox/files/actions/rename', null, { data: this.model.get('origData').toJSON() });
             }
