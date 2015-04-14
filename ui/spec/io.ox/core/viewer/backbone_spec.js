ebd7690cbf3d7b9f2458ca5475aa767a4c91b4f2
Mario Schröder <mario.schroeder@open-xchange.com>
2015-04-14 17:49:55 +0200

    Viewer: adopted test to new drive models.

--- ui/spec/io.ox/core/viewer/backbone_spec.js
+++ ui/spec/io.ox/core/viewer/backbone_spec.js
@@ -12,15 +12,20 @@
  */
 
 define([
-    'io.ox/core/viewer/backbone'
-], function (backbone) {
+    'io.ox/core/viewer/backbone',
+    'io.ox/core/files/api',
+    'io.ox/core/moment'
+], function (backbone, FilesAPI, moment) {
 
     'use strict';
 
-    var Model = backbone.Model,
+    var Model = FilesAPI.Model,
         Collection = backbone.Collection;
 
     describe('OX Viewer', function () {
+        
+        moment.locale('de');
+        moment.tz.setDefault('Europe/Berlin');
 
         var driveFile = {
                 id: '124/374',
@@ -93,26 +98,11 @@ define([
 
                 it('should be initialized with defaults', function () {
                     var model = new Model();
-                    expect(model.get('origData')).to.equal(null);
-                    expect(model.get('source')).to.equal(null);
-                    expect(model.get('filename')).to.equal('');
-                    expect(model.get('size')).to.equal(0);
-                    expect(model.get('version')).to.equal(null);
-                    expect(model.get('contentType')).to.equal(null);
-                    expect(model.get('id')).to.equal(null);
-                    expect(model.get('folderId')).to.equal(null);
-                    expect(model.get('meta')).to.deep.equal({});
-                    expect(model.get('lastModified')).to.equal(null);
-                    expect(model.get('module')).to.equal(null);
-                    expect(model.get('versions')).to.equal(null);
-                    expect(model.get('description')).to.equal(null);
-                    expect(model.get('numberOfVersions')).to.equal(0);
+                    expect(model.get('source')).to.equal('drive');
 
                     expect(model.isMailAttachment()).to.be['false'];
                     expect(model.isPIMAttachment()).to.be['false'];
-                    expect(model.isDriveFile()).to.be['false'];
-
-                    expect(model.getPreviewUrl()).to.equal(null);
+                    expect(model.isDriveFile()).to.be['true'];
                 });
             });
 
@@ -127,21 +117,18 @@ define([
                     var model = new Model(driveFile, { parse: true });
                     expect(model.get('source')).to.equal('drive');
                     expect(model.get('filename')).to.equal('cola.jpg');
-                    expect(model.get('size')).to.equal(106120);
+                    expect(model.get('file_size')).to.equal(106120);
                     expect(model.get('version')).to.equal('1');
-                    expect(model.get('contentType')).to.equal('image/jpeg');
+                    expect(model.get('content_type')).to.equal('image/jpeg');
                     expect(model.get('id')).to.equal('124/374');
-                    expect(model.get('folderId')).to.equal('124');
+                    expect(model.get('folder_id')).to.equal('124');
                     expect(model.get('meta')).to.deep.equal({});
-                    expect(model.get('lastModified')).to.equal(1402646241319);
-                    expect(model.get('module')).to.equal(null);
-                    expect(model.get('numberOfVersions')).to.equal(1);
+                    expect(model.get('last_modified')).to.equal(1402646241319);
+                    expect(model.get('number_of_versions')).to.equal(1);
 
                     expect(model.isMailAttachment()).to.be['false'];
                     expect(model.isPIMAttachment()).to.be['false'];
                     expect(model.isDriveFile()).to.be['true'];
-
-                    //expect(model.getPreviewUrl()).to.be.a('string');
                 });
             });
 
@@ -156,21 +143,14 @@ define([
                     var model = new Model(mailAttachment, { parse: true });
                     expect(model.get('source')).to.equal('mail');
                     expect(model.get('filename')).to.equal('cola.jpg');
-                    expect(model.get('size')).to.equal(145218);
-                    expect(model.get('version')).to.equal(null);
-                    expect(model.get('contentType')).to.equal('image/jpeg');
+                    expect(model.get('file_size')).to.equal(145218);
+                    expect(model.get('file_mimetype')).to.equal('image/jpeg');
                     expect(model.get('id')).to.equal('2');
-                    expect(model.get('folderId')).to.equal('default0/INBOX');
-                    expect(model.get('meta')).to.deep.equal({});
-                    expect(model.get('lastModified')).to.equal(null);
-                    expect(model.get('module')).to.equal(null);
-                    expect(model.get('numberOfVersions')).to.equal(0);
+                    expect(model.get('folder_id')).to.equal('default0/INBOX');
 
                     expect(model.isMailAttachment()).to.be['true'];
                     expect(model.isPIMAttachment()).to.be['false'];
                     expect(model.isDriveFile()).to.be['false'];
-
-                    //expect(model.getPreviewUrl()).to.be.a('string');
                 });
             });
 
@@ -185,21 +165,15 @@ define([
                     var model = new Model(pimAttachment, { parse: true });
                     expect(model.get('source')).to.equal('pim');
                     expect(model.get('filename')).to.equal('Happy-Minion-Icon.png');
-                    expect(model.get('size')).to.equal(24670);
-                    expect(model.get('version')).to.equal(null);
-                    expect(model.get('contentType')).to.equal('image/png');
+                    expect(model.get('file_size')).to.equal(24670);
+                    expect(model.get('file_mimetype')).to.equal('image/png');
                     expect(model.get('id')).to.equal(3);
-                    expect(model.get('folderId')).to.equal(187);
-                    expect(model.get('meta')).to.deep.equal({});
-                    expect(model.get('lastModified')).to.equal(null);
+                    expect(model.get('folder_id')).to.equal(187);
                     expect(model.get('module')).to.equal(4);
-                    expect(model.get('numberOfVersions')).to.equal(0);
 
                     expect(model.isMailAttachment()).to.be['false'];
                     expect(model.isPIMAttachment()).to.be['true'];
                     expect(model.isDriveFile()).to.be['false'];
-
-                    //expect(model.getPreviewUrl()).to.be.a('string');
                 });
             });
 
