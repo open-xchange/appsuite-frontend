81d1b5f3ebbe21020cf7f4043a0c5cf6fb06df82
Frank Paczynski <frank.paczynski@open-xchange.com>
2015-04-14 13:32:08 +0200

    US 91601058: find: show folder names in search result (mail)

--- ui/apps/io.ox/mail/detail/view.js
+++ ui/apps/io.ox/mail/detail/view.js
@@ -154,6 +154,12 @@ define('io.ox/mail/detail/view', [
         }
     });
 
+    ext.point('io.ox/mail/detail').extend({
+        id: 'breadcrumb',
+        index: INDEX += 100,
+        draw: extensions.breadcrumb
+    });
+
     var INDEX_notifications = 0;
 
     ext.point('io.ox/mail/detail/notifications').extend({
@@ -421,6 +427,10 @@ define('io.ox/mail/detail/view', [
                     baton.disable(point, extension);
                 }
             });
+            // show breadcrumb for search results only
+            if (this.app.listView.loader.mode !== 'search') {
+                baton.disable('io.ox/mail/detail', 'breadcrumb');
+            }
 
             this.$el.attr({
                 'data-cid': this.model.cid,
