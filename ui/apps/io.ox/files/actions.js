7b2b1aa1a83515de8bc1d2710ae139e55b37c4b0
Stefan Eckert <stefan.eckert@open-xchange.com>
2015-04-20 17:05:42 +0200

    added .csv to plain-text-group

--- ui/apps/io.ox/files/actions.js
+++ ui/apps/io.ox/files/actions.js
@@ -72,7 +72,7 @@ define('io.ox/files/actions', [
             requires: function (e) {
                 return util.conditionChain(
                     e.collection.has('one'),
-                    (/\.(txt|js|css|md|tmpl|html?)$/i).test(e.context.filename),
+                    (/\.(csv|txt|js|css|md|tmpl|html?)$/i).test(e.context.filename),
                     (e.baton.openedBy !== 'io.ox/mail/compose'),
                     util.isFolderType('!trash', e.baton)
                 );
