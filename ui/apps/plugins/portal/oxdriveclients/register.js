af66c2806fcbb6651b3e13cfd441b0f67cf356cb
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2015-04-21 18:41:45 +0200

    Add support for custom OX Drive client for Windows (that run without updater)

--- ui/apps/plugins/portal/oxdriveclients/register.js
+++ ui/apps/plugins/portal/oxdriveclients/register.js
@@ -68,9 +68,19 @@ define('plugins/portal/oxdriveclients/register', [
                 href: url,
                 target: '_blank'
             }).append($img, $('<span class="sr-only">').text(gt.format(gt('Download the %s client for %s'), settings.get('productName'), platform)));
+        } else if (platform === 'windows' && settings.get('standaloneWindowsClient') === true) {
+            return [
+                $('<i class="fa fa-download">'),
+                $.txt(' '),
+                $('<a class="shoplink">').attr({
+                    href: ox.apiRoot + url + '?session=' + ox.session,
+                    target: '_blank'
+                }).text(gt.format(gt('Download %s'), settings.get('productName')))
+            ];
         } else if (platform === 'windows' && capabilities.has('oxupdater')) {
             return [
                 $('<i class="fa fa-download">'),
+                $.txt(' '),
                 $('<a class="shoplink">').attr({
                     href: ox.apiRoot + url + '?session=' + ox.session,
                     target: '_blank'
