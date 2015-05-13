57b8c600a44efed9b94e10eeece5b2df11ad4d82
Julian Bäume <julian.baeume@open-xchange.com>
2015-05-13 12:26:05 +0200

    fix calendar settings specs
    a string changed and cleanup happened to early. Need to defer execution
    a bit to allow all painting to have happened.

--- ui/spec/io.ox/calendar/settings/pane_spec.js
+++ ui/spec/io.ox/calendar/settings/pane_spec.js
@@ -25,7 +25,7 @@ define([
         });
 
         afterEach(function () {
-            $('#calendarsettingsNode', document).remove();
+            this.node.remove();
         });
 
         it('should draw the form', function () {
@@ -60,8 +60,12 @@ define([
             this.node.find('input[name="notifyAcceptedDeclinedAsParticipant"]').parent().text().should.be.equal(gt('Receive notification as appointment participant when other participants accept or decline'));
 
             this.node.find('input[name="deleteInvitationMailAfterAction"]').should.have.length(1);
-            this.node.find('input[name="deleteInvitationMailAfterAction"]').parent().text().should.be.equal(gt('Automatically delete notifications after having accepted or declined'));
+            this.node.find('input[name="deleteInvitationMailAfterAction"]').parent().text().should.be.equal(gt('Automatically delete incoming notifications after the appointment has been accepted or declined'));
 
+            //wait for deferred painting to happen
+            var def = new $.Deferred();
+            _.defer(def.resolve);
+            return def;
         });
 
     });
