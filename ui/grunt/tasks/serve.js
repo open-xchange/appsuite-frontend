9625cf12edcd18023d2dde96fc97a71a157dc99e
Julian Bäume <julian.baeume@open-xchange.com>
2015-06-11 13:21:46 +0200

    do not test if undefined exists as a file
    breaks testserver if no custom ssl is setup (which is kind of the normal
    case)

--- ui/grunt/tasks/serve.js
+++ ui/grunt/tasks/serve.js
@@ -3,9 +3,9 @@ module.exports = function (grunt) {
     var cert = grunt.config('local.appserver.cert');
     var ca = grunt.config('local.appserver.ca');
 
-    if (grunt.file.exists(key)) key = grunt.file.read(key);
-    if (grunt.file.exists(cert)) cert = grunt.file.read(cert);
-    if (grunt.file.exists(ca)) ca = grunt.file.read(ca);
+    if (key && grunt.file.exists(key)) key = grunt.file.read(key);
+    if (cert && grunt.file.exists(cert)) cert = grunt.file.read(cert);
+    if (ca && grunt.file.exists(ca)) ca = grunt.file.read(ca);
     grunt.config.set('connect.server.options.key', key || grunt.file.read('ssl/host.key').toString());
     grunt.config.set('connect.server.options.cert', cert || grunt.file.read('ssl/host.crt').toString());
     grunt.config.set('connect.server.options.ca', ca || grunt.file.read('ssl/rootCA.crt').toString());
