8039bd96a45c3aab13f5cca3b8885db53c2c5d8a
Frank Paczynski <frank.paczynski@open-xchange.com>
2015-06-29 13:32:43 +0200

    Revert "Fixed: Bug 38879 - Issues with threading and mail results"
    This reverts commit a401e7515eac783014fdd84e750e4362bb57635f.

--- ui/apps/io.ox/mail/main.js
+++ ui/apps/io.ox/mail/main.js
@@ -1098,15 +1098,6 @@ define('io.ox/mail/main',
                                 }
                             });
 
-                        // invalidate collection state when thread setting changes
-                        var onChangeThread = function () {
-                            if (app.listView.loader.mode === 'search') {
-                                api.collectionLoader.collection.expired = true;
-                            } else {
-                                collectionLoader.collection.expired = true;
-                            }
-                        };
-
                         var register = function () {
                             var view = win.facetedsearch.view,
                                 // remember original setCollection
@@ -1118,9 +1109,6 @@ define('io.ox/mail/main',
                                 view.listenTo(collection, 'add reset remove', view.trigger.bind(view, 'query:resultready', collection));
                                 return setCollection.apply(this, arguments);
                             };
-                            // register listener to expire collection when thread setting is changed
-                            app.props.off('change:thread', onChangeThread);
-                            app.props.on('change:thread', onChangeThread);
                         };
                         // events
                         win.facetedsearch.ready
