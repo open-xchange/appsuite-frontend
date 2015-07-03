862132300fd026105c0809ec681d28e70fd2b640
Frank Paczynski <frank.paczynski@open-xchange.com>
2015-07-03 07:43:02 +0200

    Chore 92603530: remove autocomplete dependency from mobile version
    - prefer 'all' option as default for contact facet

--- ui/apps/io.ox/search/apiproxy.js
+++ ui/apps/io.ox/search/apiproxy.js
@@ -39,6 +39,20 @@ define('io.ox/search/apiproxy',[
         });
 
         POINT.extend({
+            id: 'contact-all-option',
+            index: 100,
+            customize: function (baton) {
+                baton.data = _.each(baton.data, function (facet) {
+                    if (['contacts', 'contact', 'participant', 'task_participants'].indexOf(facet.id) < 0) return;
+                    // use 'all' option als default (in contrast to 'from' or 'to')
+                    _.each(facet.values, function (value) {
+                        value.options.reverse();
+                    });
+                });
+            }
+        });
+
+        POINT.extend({
             id: 'folder',
             index: 350,
             customize: function (baton) {
