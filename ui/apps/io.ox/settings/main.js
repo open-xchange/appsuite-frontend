cd57017560fce5ec0d9aa30b7785d2fcf17e26f6
Viktor Pracht <viktor.pracht@open-xchange.com>
2015-07-06 17:19:37 +0200

    Fixed: Bug 39792 - Wrong tab order in the UI
    Several small fixes of tabindex attributes.
    
    (cherry picked from commit 9e71f7c57defa6323634a0c477095e03ebfed0be)

--- ui/apps/io.ox/settings/main.js
+++ ui/apps/io.ox/settings/main.js
@@ -156,7 +156,6 @@ define('io.ox/settings/main',
         });
 
         right = vsplit.right.addClass('default-content-padding settings-detail-pane f6-target').attr({
-            'tabindex': 1,
             'aria-describedby': 'currentsettingtitle',
             //needed or mac voice over reads the whole settings pane when an input element is focused
             'role': 'main'
