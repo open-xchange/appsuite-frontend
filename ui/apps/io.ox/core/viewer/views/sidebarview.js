3a8626724d7fbbbe3a2509a085cfec4b8d63b555
Edy Haryono <edy.haryono@open-xchange.com>
2015-07-06 16:44:35 +0200

    document viewer: update wording

--- ui/apps/io.ox/core/viewer/views/sidebarview.js
+++ ui/apps/io.ox/core/viewer/views/sidebarview.js
@@ -81,10 +81,10 @@ define('io.ox/core/viewer/views/sidebarview', [
         initTabNavigation: function () {
             // build tab navigation and its panes
             var tabsList = $('<ul class="viewer-sidebar-tabs">'),
-                detailTabLink = $('<a class="tablink" data-tab-id="detail" tabindex="1">').text(gt('Detail')),
+                detailTabLink = $('<a class="tablink" data-tab-id="detail" tabindex="1">').text(gt('Details')),
                 detailTab = $('<li class="viewer-sidebar-detailtab">').append(detailTabLink),
                 detailPane = $('<div class="viewer-sidebar-pane detail-pane" data-tab-id="detail">'),
-                thumbnailTabLink = $('<a class="tablink selected"  data-tab-id="thumbnail" tabindex="1">').text(gt('Thumbnail')),
+                thumbnailTabLink = $('<a class="tablink selected"  data-tab-id="thumbnail" tabindex="1">').text(gt('Thumbnails')),
                 thumbnailTab = $('<li class="viewer-sidebar-thumbnailtab">').append(thumbnailTabLink),
                 thumbnailPane = $('<div class="viewer-sidebar-pane thumbnail-pane" data-tab-id="thumbnail">');
             tabsList.append(thumbnailTab, detailTab).hide();
