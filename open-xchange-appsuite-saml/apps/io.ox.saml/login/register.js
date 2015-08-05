06991a0b5e29a9967bac67c6ae94ada3ff5c3ab1
Steffen Templin <steffen.templin@open-xchange.com>
2015-08-05 17:49:58 +0200

    Avoid endless loops when SAML login is active
    (cherry picked from commit 087e386d6f382b48898a1bb5c5a201a5b47c23de)

--- open-xchange-appsuite-saml/apps/io.ox.saml/login/register.js
+++ open-xchange-appsuite-saml/apps/io.ox.saml/login/register.js
@@ -1,13 +1,27 @@
 define.async('io.ox.saml/login/register', ['io.ox/core/extensions', 'io.ox.saml/handlers'], function (ext) {
-	var def = $.Deferred();
-    if (ox.serverConfig.samlLogin) {
-    	$.get(ox.apiRoot + '/saml/init?flow=login').done(function (data) {
-			var baton = new ext.Baton({data: data});
-		    ext.point('io.ox.saml/login').invoke('handle', baton, baton);
-		}).fail(def.reject);
-		return def; // Never resolve, so the login page doesn't get drawn.    	
+    var def = $.Deferred();
+    if (ox.serverConfig.samlLogin && noSessionSet()) {
+        $.get(ox.apiRoot + '/saml/init?flow=login').done(function (data) {
+            var baton = new ext.Baton({data: data});
+            ext.point('io.ox.saml/login').invoke('handle', baton, baton);
+        }).fail(def.reject);
+        return def; // Never resolve, so the login page doesn't get drawn.
     } else {
-    	def.resolve({});
+        def.resolve({});
     }
     return $.when();
+
+    function noSessionSet() {
+        var hashData = _.url.hash();
+        if (hashData.session) {
+            return false;
+        }
+
+        var ref = hashData.ref;
+        if (ref && _.deserialize(ref).session) {
+            return false;
+        }
+
+        return true;
+    }
 });
