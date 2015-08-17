d2d6f63e3c1b265b5f08b39fe17afaa13b7ed083
Julian Bäume <julian.baeume@open-xchange.com>
2015-08-17 10:49:39 +0200

    port mail tours to WTF
    Fixes Bug 40230 - Guided tours crash with JS error

--- guidedtours/apps/io.ox/tours/mail.js
+++ guidedtours/apps/io.ox/tours/mail.js
@@ -13,151 +13,114 @@
  */
 
 define('io.ox/tours/mail', [
-    'io.ox/core/extensions',
+    'io.ox/core/tk/wizard',
     'io.ox/core/notifications',
     'io.ox/tours/utils',
     'gettext!io.ox/tours'
-], function (ext, notifications, utils, gt) {
+], function (Tour, notifications, utils, gt) {
 
     'use strict';
 
-    /* Tour: e-mail */
-    ext.point('io.ox/tours/extensions').extend({
+    var composeApp;
+
+    Tour.registry.add({
         id: 'default/io.ox/mail',
         app: 'io.ox/mail',
-        priority: 1,
-        tour: {
-            id: 'E-Mail',
-            steps: [{
-                title: gt('Composing a new E-Mail'),
-                placement: 'right',
-                target: function () { return $('[data-ref="io.ox/mail/actions/compose"]:visible')[0]; },
-                content: gt('To compose a new E-Mail, click on Compose in the toolbar.'),
-                arrowOffset: 1,
-                yOffset: -5,
-                multipage: true,
-                onNext: function () {
-                    if ($('.launcher[data-app-name="io.ox/mail/write"]').length === 0) {
-                        utils.switchToApp('io.ox/mail/write/main', function () {
-                            window.hopscotch.nextStep();
-                            window.hopscotch.prevStep();
-                        });
-                    } else {
-                        $('.launcher[data-app-name="io.ox/mail/write"]').first().click();
-                        window.hopscotch.nextStep();
-                        window.hopscotch.prevStep();
-                    }
-                }
-            },
-            {
-                title: gt('Entering the recipient\'s name'),
-                placement: 'right',
-                target: function () { return $('#writer_field_to:visible')[0]; },
-                content: gt('Enter the recipient\'s name on the top left side. As soon as you typed the first letters, suggestions from the address books are displayed. To accept a recipient suggestion, click on it.'),
-                arrowOffset: 1,
-                yOffset: -5
-            },
-            {
-                title: gt('Further functions'),
-                placement: 'right',
-                target: function () { return $('.section-link:visible')[0]; },
-                content: gt('Below the recipient you will find further functions, e.g. for sending copies to other recipients or for adding attachments.')
-            },
-            {
-                title: gt('Entering the subject'),
-                placement: 'bottom',
-                target: function () { return $('.subject-wrapper:visible')[0]; },
-                content: gt('Enter the subject on the right side of the recipient.')
-            },
-            {
-                title: gt('Entering the E-Mail text'),
-                placement: 'top',
-                target: function () { return $('.editor-outer-container:visible')[0]; },
-                content: (_.device('desktop') ? gt('Enter the E-Mail text below the subject. If the text format was set to HTMl in the options, you can format the E-Mail text. To do so select a text part and then click an icon in the formatting bar.')
-                    : gt('Enter the E-Mail text below the subject.')),
-                yOffset: 110,
-                arrowOffset: 'center'
-            },
-            {
-                title: gt('Sending the E-Mail'),
-                placement: 'left',
-                target: function () { return $('.btn[data-action="send"]:visible')[0]; },
-                content: gt('To send the E-Mail, click on Send on the upper right side.'),
-                multipage: true,
-                onNext: function () {
-                    if ($('input[name="subject"]').val() === '[Guided tours] Example e-mail') {
-                        $('.btn[data-action="discard"]:visible').click();
-                    }
-                    utils.switchToApp('io.ox/mail/main', function () {
-                        window.hopscotch.nextStep();
-                        window.hopscotch.prevStep();
-                    });
-                },
-                arrowOffset: 1,
-                yOffset: -5
-            },
-            {
-                title: gt('Sorting your E-Mails'),
-                placement: 'bottom', /* Check target */
-                target: function () { return $('.dropdown.grid-options.toolbar-item.pull-right:visible')[0]; },
-                content: gt('To sort the E-Mails, click on Sort by. Select a sort criteria.'),
-                xOffset: -230, // see bug #34010
-                arrowOffset: 230,
-                width: 260
-            },
-            {
-                title: gt('Selecting a view'),
-                placement: 'left',
-                target: function () { return $('.classic-toolbar .pull-right:visible')[0]; },
-                content: gt('To choose between the different views. click on View in the toolbar. Select a menu entry in the layout.'),
-                xOffset: -15
-            },
-            {
-                title: gt('Opening an E-Mail in a separate window'),
-                placement: 'top', // see bug #34125
-                target: function () { return $('.list-view:visible')[0]; },
-                content: gt('If double-clicking on an E-Mail in the list, the E-Mail is opened in a separate window.'),
-                yOffset: 110  // see bug #34125
-            },
-            {
-                title: gt('Reading E-Mail conversations'),
-                placement: 'left',
-                target: function () {
-                    var visibleMails = $('.selectable.mail').slice(0, 5),
-                        threadedMails = visibleMails.find('.thread-size:visible');
-                    if (threadedMails.length > 0) {
-                        return threadedMails[0];
-                    }
-                    return null;
-                },
-                content: gt('To open or close an E-Mail in a conversation, click on a free area in the header.'),
-                arrowOffset: 1,
-                yOffset: -10
-            },
-            {
-                title: gt('Halo view'),
-                placement: (_.device('desktop') ? 'right' : 'bottom'),
-                target: function () { return $('.person-link.person-from:visible')[0]; },
-                content: gt('To receive information about the sender or other recipients, open the Halo view by clicking on a name.'),
-                arrowOffset: 1,
-                yOffset: -10
-            },
-            {
-                title: gt('Editing multiple E-Mails'),
-                placement: 'bottom',
-                target: function () { return $('[data-ref="io.ox/mail/listview"]:visible')[0]; },
-                //target: function () { return $('.list-view:visible')[0]; },
-                content: gt('In order to edit multiple E-Mails at once, enable the checkboxes on the left side of the E-Mails. If the checkboxes are not displayed, click on View > Checkboxes on the right side of the toolbar.'),
-                xOffset: -15
-            },
-            {
-                title: gt('Opening the E-Mail settings'),
-                placement: 'left',
-                target: function () { return $('.launcher .fa-cog:visible')[0]; },
-                content: gt('To open the E-Mail settings, click the System menu icon on the upper right side of the menu bar. Select Settings. Click on E-Mail on the left side. To display all settings, enable Advanced settings in the bottom left side'),
-                arrowOffset: 1,
-                yOffset: -5
-            }]
-        }
+        priority: 1
+    }, function () {
+        new Tour()
+        .step()
+            .title(gt('Composing a new E-Mail'))
+            .content(gt('To compose a new E-Mail, click on Compose in the toolbar.'))
+            .hotspot('[data-ref="io.ox/mail/actions/compose"]:visible')
+            .on('next', function () {
+                ox.registry.call('mail-compose', 'compose').then(function (result) {
+                    composeApp = result.app;
+                });
+                ox.once('mail:compose:ready', function () {
+                    //HACK: add ready class, to have some class to waitFor
+                    $('.io-ox-mail-compose-window').addClass('ready');
+                });
+            })
+            .end()
+        .step()
+            .title(gt('Entering the recipient\'s name'))
+            .content(gt('Enter the recipient\'s name on the top left side. As soon as you typed the first letters, suggestions from the address books are displayed. To accept a recipient suggestion, click on it.'))
+            .waitFor('.io-ox-mail-compose-window.ready')
+            .hotspot('[data-extension-id=to] .tokenfield.to')
+            .on('ready', function () {
+                //clean up
+                //HACK: add ready class, to have some class to waitFor
+                $('.io-ox-mail-compose-window').removeClass('ready');
+            })
+            .end()
+        .step()
+            .title(gt('Entering the subject'))
+            .content(gt('Enter the subject on the right side of the recipient.'))
+            .hotspot('[data-extension-id=subject] input')
+            .end()
+        .step()
+            .title(gt('Further functions'))
+            .content(gt('Below the recipient you will find further functions, e.g. for sending copies to other recipients or for adding attachments.'))
+            .spotlight('[data-extension-id=composetoolbar]')
+            .end()
+        .step()
+            .title(gt('Entering the E-Mail text'))
+            .content(_.device('desktop') ? gt('Enter the E-Mail text below the subject. If the text format was set to HTMl in the options, you can format the E-Mail text. To do so select a text part and then click an icon in the formatting bar.') : gt('Enter the E-Mail text below the subject.'))
+            .referTo('.io-ox-mail-compose-container')
+            .spotlight('.mail-compose-contenteditable-fields, .io-ox-mail-compose.container textarea')
+            .end()
+        .step()
+            .title(gt('Sending the E-Mail'))
+            .content(gt('To send the E-Mail, click on Send on the upper right side.'))
+            .hotspot('.io-ox-mail-compose-window button[data-action=send]')
+            .end()
+        .step()
+            .title(gt('Sorting your E-Mails'))
+            .content(gt('To sort the E-Mails, click on Sort by. Select a sort criteria.'))
+            .waitFor('.io-ox-mail-window')
+            .spotlight('.list-view-control > .toolbar > .dropdown')
+            .navigateTo('io.ox/mail/main')
+            .end()
+        .step()
+            .title(gt('Selecting a view'))
+            .content(gt('To choose between the different views. click on View in the toolbar. Select a menu entry in the layout.'))
+            .spotlight('.classic-toolbar > li[data-dropdown=view]')
+            .end()
+        .step()
+            .title(gt('Opening an E-Mail in a separate window'))
+            .content(gt('If double-clicking on an E-Mail in the list, the E-Mail is opened in a separate window.'))
+            .spotlight('.list-view.mail-item > li:first')
+            .end()
+        .step()
+            .title(gt('Reading E-Mail conversations'))
+            .content(gt('To open or close an E-Mail in a conversation, click on a free area in the header.'))
+            .hotspot('.thread-view .detail-view-header')
+            .end()
+        .step()
+            .title(gt('Halo view'))
+            .content(gt('To receive information about the sender or other recipients, open the Halo view by clicking on a name.'))
+            .hotspot('.halo-link')
+            .end()
+        .step()
+            .title(gt('Editing multiple E-Mails'))
+            .content(gt('In order to edit multiple E-Mails at once, enable the checkboxes on the left side of the E-Mails. If the checkboxes are not displayed, click on View > Checkboxes on the right side of the toolbar.'))
+            .hotspot('.classic-toolbar > li[data-dropdown=view]')
+            .referTo('.classic-toolbar > li[data-dropdown=view]')
+            .end()
+        .step()
+            .title(gt('Opening the E-Mail settings'))
+            .content(gt('To open the E-Mail settings, click the System menu icon on the upper right side of the menu bar. Select Settings. Click on E-Mail on the left side. To display all settings, enable Advanced settings in the bottom left side'))
+            .referTo('#io-ox-topbar-dropdown-icon')
+            .hotspot('#io-ox-topbar-dropdown-icon')
+            .end()
+        .on('stop', function () {
+            if (composeApp) {
+                //prevent app from asking about changed content
+                composeApp.model.dirty(false);
+                composeApp.quit();
+            }
+        })
+        .start();
     });
 });
