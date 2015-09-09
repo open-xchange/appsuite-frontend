4058fd95c61e16693a97cfc7cd1f9d2aeee6f452
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2015-09-09 14:26:02 +0200

    Always set language cookie on login

--- ui/apps/io.ox/core/session.js
+++ ui/apps/io.ox/core/session.js
@@ -44,6 +44,7 @@ define('io.ox/core/session', [
         if ('context_id' in data) ox.context_id = data.context_id || 0;
         // if the user has set the language on the login page, use this language instead of server settings lang
         ox.language = language || check(data.locale) || check(getBrowserLanguage()) || 'en_US';
+        _.setCookie('language', ox.language);
         manifests.reset();
         $('html').attr('lang', ox.language.split('_')[0]);
         // should not hide store() request here; made debugging hard
