54ff3699b64100eca2231a15ae42073a9f86f326
Alexander Quast <alexander.quast@open-xchange.com>
2015-09-10 14:51:44 +0200

    Fixed Bug 41063 - [OnePlus/Android 5.0.2] Drive - Refresh sign stays forever
    Disabled PTR by option in listview

--- ui/apps/io.ox/files/main.js
+++ ui/apps/io.ox/files/main.js
@@ -273,7 +273,7 @@ define('io.ox/files/main', [
          * Setup list view
          */
         'list-view': function (app) {
-            app.listView = new FileListView({ app: app, draggable: true, ignoreFocus: true, noSwipe: true, noPullToRefrsh: true });
+            app.listView = new FileListView({ app: app, draggable: true, ignoreFocus: true, noSwipe: true, noPullToRefresh: true });
             app.listView.model.set({ folder: app.folder.get(), sort: app.props.get('sort'), order: app.props.get('order') });
             // for debugging
             window.list = app.listView;
