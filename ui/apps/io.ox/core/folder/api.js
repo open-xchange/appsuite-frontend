d0c349500a3fe2ccf12705c1d1b24fa35490f76d
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2015-09-14 13:37:55 +0200

    Fixed: Bug 41166 - Not possible to create public contact folders

--- ui/apps/io.ox/core/folder/api.js
+++ ui/apps/io.ox/core/folder/api.js
@@ -895,8 +895,8 @@ define('io.ox/core/folder/api', [
                 subscribed: 1,
                 title: gt('New Folder')
             }, options);
-            // don't inherit permissions for flat folders
-            if (isFlat(options.module)) {
+            // don't inherit permissions for private flat folders
+            if (isFlat(options.module) && !(parent.id === '2' || util.is('public', parent))) {
                 // empty array doesn't work; we heve to copy the admins
                 options.permissions = _(parent.permissions).filter(function (item) {
                     return !!(item.bits & 268435456);
