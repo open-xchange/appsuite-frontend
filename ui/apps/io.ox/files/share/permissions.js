fea4fd119987d2f21311a3211621f90c54e568ea
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2015-09-30 16:26:44 +0200

    Fixed wrong folder permissions for guests from addressbook (1 instead of 257)

--- ui/apps/io.ox/files/share/permissions.js
+++ ui/apps/io.ox/files/share/permissions.js
@@ -846,7 +846,7 @@
                             var isInternal = member.get('type') === 2 || member.get('type') === 1,
                                 isGuest = member.get('type') === 5,
                                 obj = {
-                                    bits: isInternal && objModel.isFolder() ? 4227332 : 1, // Author : Viewer
+                                    bits: isInternal ? 4227332 : objModel.isFolder() ? 257 : 1, // Author : (Viewer for folders: Viewer for files)
                                     group: member.get('type') === 2,
                                     type: member.get('type') === 2 ? 'group' : 'user',
                                     new: true
