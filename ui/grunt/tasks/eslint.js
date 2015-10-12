ccbc7124eacd6ecd8fa1862665046a5bf4180ffc
David Bauer <david.bauer@open-xchange.com>
2015-10-12 17:15:00 +0200

    Added grunt-eslint and intitial rules

---
+++ ui/grunt/tasks/eslint.js
@@ -0,0 +1,24 @@
+/**
+* This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+* LICENSE. This work is protected by copyright and/or other applicable
+* law. Any use of the work other than as authorized under this license
+* or copyright law is prohibited.
+*
+* http://creativecommons.org/licenses/by-nc-sa/2.5/
+* © 2015 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+*
+* @author David Bauer <david.bauer@open-xchange.com>
+*/
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.config.merge({
+        eslint: {
+            target: ['apps/io.ox/**/*.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-eslint');
+};
