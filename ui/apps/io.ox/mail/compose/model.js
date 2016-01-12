ca55abb8c8cdebbf32964022ac6effdce6389538
David Bauer <david.bauer@open-xchange.com>
2016-01-12 12:19:06 +0100

    Fixed: Bug 43226 - [L3] auto save draft fails on some HTML Mails

--- ui/apps/io.ox/mail/compose/model.js
+++ ui/apps/io.ox/mail/compose/model.js
@@ -231,6 +231,8 @@ define.async('io.ox/mail/compose/model', [
                     attachment.set('content', attachment.get('content').replace(/<img[^>]*src=\\?"data:[^>]*>/gi, ''));
                 }
             });
+            // Get flat attachments
+            mail.attachments = this.attributes.attachments.toJSON();
             return {
                 description: gt('Mail') + ': ' + (mail.subject || gt('No subject')),
                 point: mail
