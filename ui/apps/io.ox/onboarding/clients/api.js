163e1b45599a8d29650f9034e2b519efe4764a3b
Frank Paczynski <frank.paczynski@open-xchange.com>
2016-01-14 07:56:47 +0100

    onboarding: temporary fix for backend issues

--- ui/apps/io.ox/onboarding/clients/api.js
+++ ui/apps/io.ox/onboarding/clients/api.js
@@ -23,6 +23,18 @@ define('io.ox/onboarding/clients/api', [
             return http.GET({
                 module: 'onboarding',
                 params: { action: 'config' }
+            }).then(function (data) {
+                // TODO: remove when backend is ready
+                // fix wrong identifiers
+                _.each(data.matching, function (match) {
+                    if (match.id.indexOf('mailappinstall') > -1) match.actions = ['link/mailappinstall'];
+                    if (match.id.indexOf('driveappinstall') > -1) match.actions = ['link/driveappinstall'];
+                    if (match.id.indexOf('mailmanual') > -1) match.actions = ['display/mailmanual'];
+                });
+                _.each(_.where(data.devices, { id: 'apple.mac' }), function (device) {
+                    device.scenarios = _.without(device.scenarios, 'apple.mac/mailappinstall', 'apple.mac/driveappinstall');
+                });
+                return data;
             });
         },
 
