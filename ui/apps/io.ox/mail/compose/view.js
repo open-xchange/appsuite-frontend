b566183995ce69533c1a65901e690913fc2faf65
Alexander Quast <alexander.quast@open-xchange.com>
2016-01-26 15:43:12 +0100

    Fixed broken check for attachments in mailFetch function
    (cherry picked from commit f594e46967fe80a79e89ef37a6d054cd1ec9d149)

--- ui/apps/io.ox/mail/compose/view.js
+++ ui/apps/io.ox/mail/compose/view.js
@@ -380,7 +380,11 @@ define('io.ox/mail/compose/view', [
 
             var self = this,
                 mode = obj.mode,
-                attachmentMailInfo = obj.attachment ? obj.attachments[1].mail : undefined;
+                attachmentMailInfo;
+
+            if (obj.attachment && obj.attachments) {
+                attachmentMailInfo = obj.attachments[1] ? obj.attachments[1].mail : undefined;
+            }
 
             delete obj.mode;
 
