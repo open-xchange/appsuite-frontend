28dc3cff29036fb871365de100e2f2e2082cf67d
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2016-01-27 11:12:10 +0100

    Additional Commit for Bug 43573 - [L3] This mail has no content in case Content-Type:text/html; name="emailtext.html" is used instead of Content-Type:text/plain; charset=ISO-8859-1 (cherry picked from commit 4e997607660d8c0c6e50d72d9423c4c11f941853)

--- ui/apps/io.ox/mail/api.js
+++ ui/apps/io.ox/mail/api.js
@@ -497,10 +497,10 @@ define('io.ox/mail/api',
         return get.call(api, obj, options && options.cache).done(function (data) {
             //don't save raw data in our models. We only want preformated content there
             if (!obj.view || (obj.view && obj.view !== 'raw')) {
-                // sanitize content Types
-                _(data.attachments).map(function (attachment) {
-                    attachment.content_type = attachment.content_type.replace(/^text\/plain.*/, 'text/plain').replace(/^text\/html.*/, 'text/html');
-                    return attachment;
+                // sanitize content Types (we want lowercase 'text/plain' or 'text/html')
+                // split by ; because this field might contain further unwanted data
+                _(data.attachments).each(function (attachment) {
+                    attachment.content_type = String(attachment.content_type).toLowerCase().split(';')[0];
                 });
                 // either update or add model
                 if (model) {
