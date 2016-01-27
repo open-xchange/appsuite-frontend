a55c34bc3c505baf946beb31d22c6cdb5321a4da
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2016-01-27 11:51:34 +0100

    Fixed mail display issues (lazyload; display: table)

--- ui/apps/io.ox/mail/detail/content.js
+++ ui/apps/io.ox/mail/detail/content.js
@@ -401,19 +401,20 @@ define('io.ox/mail/detail/content', [
         }
     });
 
-    ext.point('io.ox/mail/detail/content').extend({
-        id: 'lazyload-images',
-        index: 1200,
-        process: function () {
-            $(this).find('img[src!=""]').each(function () {
-                var img = $(this);
-                img.attr({
-                    'data-original': img.attr('src'),
-                    'src': '//:0'
-                }).addClass('lazyload');
-            });
-        }
-    });
+    // commented out DUE TO BUGS! (27.01.2016)
+    // ext.point('io.ox/mail/detail/content').extend({
+    //     id: 'lazyload-images',
+    //     index: 1200,
+    //     process: function () {
+    //         $(this).find('img[src!=""]').each(function () {
+    //             var img = $(this);
+    //             img.attr({
+    //                 'data-original': img.attr('src'),
+    //                 'src': '//:0'
+    //             }).addClass('lazyload');
+    //         });
+    //     }
+    // });
 
     function isBlockquoteToggle(elem) {
         return $(elem).parent().hasClass('blockquote-toggle');
