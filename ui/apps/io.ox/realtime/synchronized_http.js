5541fc625ee2c94cb834e744ef1ea10ca0bb0b0e
Francisco Laguna <francisco.laguna@open-xchange.com>
2016-02-04 11:38:50 +0100

    Bug #43690 - make sure PUTs of stanzas are completed before sending queries

---
+++ ui/apps/io.ox/realtime/synchronized_http.js
@@ -0,0 +1,50 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * © 2016 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ *
+ * @author Francisco Laguna <francisco.laguna@open-xchange.com>
+ */
+define('io.ox/realtime/synchronized_http', ['io.ox/core/http'], function (http) {
+    'use strict';
+
+    // Ensures PUTs to be performed one after the other
+    var synchronizedHTTP = {
+        transmitting: false,
+        buffer: [],
+        PUT: function (options) {
+            var def = $.Deferred();
+            this.buffer.push({
+                options: options,
+                deferred: def
+            });
+
+            this.tick();
+
+            return def;
+        },
+
+        tick: function () {
+            var self = this;
+            if (this.transmitting) {
+                return;
+            }
+            if (this.buffer.length === 0) {
+                return;
+            }
+            this.transmitting = true;
+            var nextRequest = this.buffer.shift();
+            http.PUT(nextRequest.options).always(function () {
+                self.transmitting = false;
+                self.tick();
+            }).done(nextRequest.deferred.resolve).fail(nextRequest.deferred.reject);
+        }
+    };
+
+    return synchronizedHTTP;
+});
