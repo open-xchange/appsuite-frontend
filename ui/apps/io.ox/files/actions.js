fea4f7b08c4d42fb6282e59507e2fb0396a49413
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2016-02-11 15:59:24 +0100

    Fixed: Bug 42232 - File shared for review (read write) can't be edited

--- ui/apps/io.ox/files/actions.js
+++ ui/apps/io.ox/files/actions.js
@@ -73,7 +73,7 @@ define('io.ox/files/actions', [
         new Action('io.ox/files/actions/editor', {
             requires: function (e) {
                 return util.conditionChain(
-                    e.collection.has('one'),
+                    e.collection.has('one', 'modify'),
                     !util.hasStatus('lockedByOthers', e),
                     (/\.(csv|txt|js|css|md|tmpl|html?)$/i).test(e.context.filename),
                     (e.baton.openedBy !== 'io.ox/mail/compose'),
