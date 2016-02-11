fed291531ab2d348c23b760b2497f4977971a9bf
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2016-02-11 11:07:30 +0100

    Fixed Bug 43856 - "Add storage account" link in Drive is not working

--- ui/apps/io.ox/files/actions/add-storage-account.js
+++ ui/apps/io.ox/files/actions/add-storage-account.js
@@ -77,7 +77,7 @@ define('io.ox/files/actions/add-storage-account', [
 
         // consider metrics
         if (metrics.isEnabled()) {
-            this.delegate('.toolbar-item', 'mousedown', function (e) {
+            node.delegate('.storage-account-item', 'mousedown', function (e) {
                 metrics.trackEvent({
                     app: 'drive',
                     target: 'folder/account/add',
