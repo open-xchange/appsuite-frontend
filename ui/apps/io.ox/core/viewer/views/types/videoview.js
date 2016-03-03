616d3782a2a8e8c1a9b8a426bc44b6cce55746d0
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2016-03-03 13:10:30 +0100

    Fixed: Bug 44258 - After movie playing for a while it stops telling "Ihr browser unterstützt das Video Format dieser Datei nicht"

--- ui/apps/io.ox/core/viewer/views/types/videoview.js
+++ ui/apps/io.ox/core/viewer/views/types/videoview.js
@@ -79,7 +79,9 @@ define('io.ox/core/viewer/views/types/videoview', [
             this.$el.idle().find('.viewer-displayer-video').addClass('player-hidden');
             this.$el.find('div.viewer-displayer-notification').remove();
             this.$el.append(
-                this.createNotificationNode(gt('Your browser does not support the video format of this file.'))
+                this.createNotificationNode(
+                    gt('Error while playing the video. Either your browser does not support the format or you have connection problems.')
+                )
             );
         },
 
