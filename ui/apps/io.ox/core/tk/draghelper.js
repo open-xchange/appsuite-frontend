dfa282d80a502ef5124a064e9bf387eec816482a
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2016-03-11 12:34:01 +0100

    Updated copyright notice

--- ui/apps/io.ox/core/tk/draghelper.js
+++ ui/apps/io.ox/core/tk/draghelper.js
@@ -6,11 +6,11 @@
  *
  * http://creativecommons.org/licenses/by-nc-sa/2.5/
  *
- * Copyright (C) 2004-2014 Open-Xchange, Inc.
- * Mail: info@open-xchange.com
+ * © 2016 OX Software GmbH, Germany. info@open-xchange.com
  *
  * @author Viktor Pracht <viktor.pracht@open-xchange.com>
  */
+
 define('io.ox/core/tk/draghelper', ['io.ox/core/extensions'], function (ext) {
 
     'use strict';
