2138525904860eb106930250ab1ab8969fe83fbd
David Bauer <david.bauer@open-xchange.com>
2016-03-21 16:43:15 +0100

    Fixed copyright headers

--- ui/apps/io.ox/core/api/autocomplete.js
+++ ui/apps/io.ox/core/api/autocomplete.js
@@ -6,7 +6,7 @@
  *
  * http://creativecommons.org/licenses/by-nc-sa/2.5/
  *
- * © 2011 Open-Xchange Inc., Tarrytown, NY, USA. info@open-xchange.com
+ * © 2016 OX Software GmbH, Germany. info@open-xchange.com
  *
  * @author Mario Scheliga <mario.scheliga@open-xchange.com>
  */
