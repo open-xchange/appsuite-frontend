5d3ca196a0b5c23706a267bf24ce5ad706edf564
Francisco Laguna <francisco.laguna@open-xchange.com>
2016-04-18 11:49:38 +0200

    hide login screen

--- open-xchange-appsuite-saml/apps/io.ox.saml/login/register.js
+++ open-xchange-appsuite-saml/apps/io.ox.saml/login/register.js
@@ -1,5 +1,31 @@
-define.async('io.ox.saml/login/register', ['io.ox/core/extensions', 'io.ox.saml/handlers'], function (ext) {
-    var def = $.Deferred();
+define('io.ox.saml/login/register', ['io.ox/core/extensions', 'io.ox.saml/handlers'], function (ext) {
+    var loginFormHandled = false;
+
+    function hideLogin() {
+        if ($('#io-ox-login-container').length > 0) {
+            loginFormHandled = true;
+            if (ox.serverConfig.samlLoginErrorPage === true) {
+                $('body').addClass('down');
+                $('#io-ox-login-container').empty().append(
+                    $('<div class="alert alert-info">').append(
+                        $('<div><b>Connection error</b></div> The service is not available right now. <a href="#">Retry</a>')
+                    )
+                    .on('click', function (e) { e.preventDefault(); location.reload(); })
+                );
+                $('#background-loader').fadeOut(250);
+                console.warn('Server is down.');
+            } else {
+                $('#io-ox-login-screen').hide();
+                setInterval(function () {
+                    $('#background-loader').show();
+                }, 100);
+            }
+        }
+        if (!loginFormHandled) {
+            setTimeout(hideLogin, 100);
+        }
+    }
+
     if (ox.serverConfig.samlLogin && noSessionSet()) {
         if (!ox.busy) {
              ox.busy = function (block) {
@@ -8,7 +34,7 @@ define.async('io.ox.saml/login/register', ['io.ox/core/extensions', 'io.ox.saml/
                     .show()
                     .addClass('secure' + (block ? ' block' : ''));
             };
-        }      
+        }
         $.get(ox.apiRoot + '/saml/init?flow=login').done(function (data) {
             var baton = new ext.Baton({data: data});
             ext.point('io.ox.saml/login').invoke('handle', baton, baton);
@@ -18,25 +44,13 @@ define.async('io.ox.saml/login/register', ['io.ox/core/extensions', 'io.ox.saml/
                     '#&' + _.serialize({ language: ox.language, statusCode: jqXHR.status || 'undefined', statusText: textStatus, error: errorThrown }));
             }
         });
-        if (ox.serverConfig.samlLoginErrorPage === true) {
-            $('body').addClass('down');
-            $('#io-ox-login-container').empty().append(
-                $('<div class="alert alert-info">').append(
-                    $('<div><b>Connection error</b></div> The service is not available right now. <a href="#">Retry</a>')
-                )
-                .on('click', function (e) { e.preventDefault(); location.reload(); })
-            );
-            $('#background-loader').fadeOut(250);
-            console.warn('Server is down.');
-            return def;
-        } else {
-            ox.busy(true);
-            return def.resolve();
-        }
-    } else {
-        def.resolve({});
+
+
+
+        hideLogin();
+
     }
-    return $.when();
+    return {};
 
     function noSessionSet() {
         var hashData = _.url.hash();
