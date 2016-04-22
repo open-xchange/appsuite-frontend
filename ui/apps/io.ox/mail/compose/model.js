bfc365096f7c19c6e73fc03057bdf8fd9b22d5c1
David Bauer <david.bauer@open-xchange.com>
2016-04-22 08:02:27 +0200

    Fixed: Bug 45132 - [L3] New lines added when editing Drafts

--- ui/apps/io.ox/mail/compose/model.js
+++ ui/apps/io.ox/mail/compose/model.js
@@ -189,6 +189,9 @@ define.async('io.ox/mail/compose/model', [
             // image URL fix
             if (mode === 'html') {
                 content = content.replace(/(<img[^>]+src=")\/ajax/g, '$1' + ox.apiRoot);
+
+                // Remove wrapping div
+                content = content.replace(/^<div\sid="ox-\S+">/, '').replace(/<\/div>$/, '');
             }
 
             // convert different emoji encodings to unified
