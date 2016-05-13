open-xchange/appsuite-frontend

commit f55add31858d50b66e05c0f371b27f79bd935725

Fixed Bug 45798 - [L3] Quick Reply not correctly translated

ui/apps/io.ox/mail/inplace-reply.js
@@ -172,6 +172,9 @@ define('io.ox/mail/inplace-reply', [
             // remember cid
             this.cid = options.cid;
 
+            // store numberOfRecipients to label the button correctly
+            this.numberOfRecipients = options.numberOfRecipients;
+
             this.$send = $();
             this.$textarea = $('<textarea class="inplace-editor form-control" tabindex="1">');
 
@@ -193,7 +196,8 @@ define('io.ox/mail/inplace-reply', [
                 $('<div class="form-group">').append(
                     this.$send = $('<button class="btn btn-primary btn-sm disabled" data-action="send" tabindex="1">')
                         .prop('disabled', true)
-                        .text(gt('Reply to all')),
+                        //#. used in Quick reply function to reply to one or more persons.
+                        .text(gt.ngettext('Reply', 'Reply to all', this.numberOfRecipients || 2)),
                     $.txt(' '),
                     $('<button class="btn btn-default btn-sm" data-action="cancel" tabindex="1">')
                         .text(gt('Cancel'))

ui/apps/io.ox/mail/actions.js
@@ -73,12 +73,14 @@ define('io.ox/mail/actions', [
             var cid = _.cid(baton.data),
                 // needs baton view
                 view = baton.view,
+                // reply to all, so count, to, from, cc and bcc and subtract 1 (you don't sent the mail to yourself)
+                numberOfRecipients = _.union(baton.data.to, baton.data.from, baton.data.cc, baton.data.bcc).length - 1,
                 // hide inline link
                 link = view.$('[data-ref="io.ox/mail/actions/inplace-reply"]').hide();
 
             require(['io.ox/mail/inplace-reply'], function (InplaceReplyView) {
                 view.$('section.body').before(
-                    new InplaceReplyView({ tagName: 'section', cid: cid })
+                    new InplaceReplyView({ tagName: 'section', cid: cid, numberOfRecipients: numberOfRecipients })
                     .on('send', function (cid) {
                         view.$el.closest('.thread-view-control').data('open', cid);
                     })

commit b9c579695acdf50cdc525fb5520c7a8ffbc87e0a

Fixup for Bug 45798 - [L3] Quick Reply not correctly translated
Fixes commit f55add31858d50b66e05c0f371b27f79bd935725, which broke the
build of language files.

Technically this is not a plural decision at all, the string to be used
just happens to depend on the number of recipients. Its just, that the
action is named slightly different if only one participant is involved.
There should be no difference in any language for more than one
participant. If there really is such a use-case, npgettext must be used
and a context added, because "Reply" as a msgid is already present from
other parts of the software and can therefore not be used a second time.

ui/apps/io.ox/mail/inplace-reply.js
@@ -184,6 +184,15 @@ define('io.ox/mail/inplace-reply', [
         },
 
         render: function () {
+            var replyText;
+            // do _not_ use gt.ngettext here, see Bug 45798
+            if (this.numberOfRecipients || 2) {
+              //#. Used as a verb to reply to one recipient
+              replyText = gt('Reply');
+            } else {
+              //#. Used as a verb to reply to all recipients
+              replyText = gt('Reply to all');
+            }
 
             this.$el.append(
                 // editor
@@ -196,8 +205,7 @@ define('io.ox/mail/inplace-reply', [
                 $('<div class="form-group">').append(
                     this.$send = $('<button class="btn btn-primary btn-sm disabled" data-action="send" tabindex="1">')
                         .prop('disabled', true)
-                        //#. used in Quick reply function to reply to one or more persons.
-                        .text(gt.ngettext('Reply', 'Reply to all', this.numberOfRecipients || 2)),
+                        .text(replyText),
                     $.txt(' '),
                     $('<button class="btn btn-default btn-sm" data-action="cancel" tabindex="1">')
                         .text(gt('Cancel'))