73bc2a3e7842c77e473daa27ef30da63bcf956f8
Christoph Kopp <christoph.kopp@open-xchange.com>
2016-06-23 14:10:32 +0200

    additional fix for bug 46754 - File-Quota bar does not update on addition/deletion of files

--- ui/apps/io.ox/files/main.js
+++ ui/apps/io.ox/files/main.js
@@ -212,7 +212,7 @@ define('io.ox/files/main', [
                 quota.getQuota(true);
             });
 
-            api.on('add:file', function () {
+            api.on('add:file remove:file', function () {
                 quota.getQuota(true);
             });
 
