open-xchange/appsuite-frontend

commit 71558fd05cf5ce9cfab4face161b0c27f4e703cc

Fixed: Bug 46857 - Not able to move email to another mail-tab.
Root cause:
- dragging a unread mail triggers a redraw of the tab nodes  (unread counter change)
- drop handler is registers on specific node right when the drag starts
Solution: add option to use delegate for event binding
Resolved state: Fix
Version: 7.8.2
Git repository: wd/frontend/web
Git branch: develop
Package name: open-xchange-appsuite

ui/apps/io.ox/core/tk/list-dnd.js
@@ -40,6 +40,7 @@ define('io.ox/core/tk/list-dnd', [
         options = _.extend({
             container: $(),
             data: null,
+            delegate: false,
             draggable: false,
             dragMessage: defaultMessage,
             dragType: '',
@@ -256,7 +257,13 @@ define('io.ox/core/tk/list-dnd', [
             if (collection.isResolved() && !collection.has('delete')) return;
             // bind events
             $('.dropzone').each(function () {
-                var node = $(this), selector = node.attr('data-dropzones');
+                var node = $(this),
+                    selector = node.attr('data-dropzones'),
+                    delegate = node.attr('data-delegate');
+                // pitfall: re-render after bind
+                if (delegate && selector) {
+                    return node.on('mouseup.dnd', selector, drop);
+                }
                 (selector ? node.find(selector) : node).on('mouseup.dnd', drop);
             });
             $(document)
@@ -268,6 +275,11 @@ define('io.ox/core/tk/list-dnd', [
         // draggable?
         if (options.draggable) {
             container.on('mousedown.dnd', options.selectable, start);
+        }
+
+        // use delegate for drop binding?
+        if (options.delegate) {
+            container.attr('data-delegate', true);
         }
 
         // dropzone?

ui/apps/io.ox/mail/categories/main.js
@@ -89,7 +89,7 @@ define('io.ox/mail/categories/main', [
         refresh: function () {
             var def = $.Deferred(),
                 self = this;
-            // defer to ensure mail multiple requests first
+            // defer to ensure mail requests multiple first
             _.defer(function () {
                 api.get().then(function (data) {
                     data = _.map(data, function (value, key) {
@@ -140,7 +140,7 @@ define('io.ox/mail/categories/main', [
                 container: this.$el.closest('.window-container')
             };
             // dnd
-            dnd.enable({ draggable: true, container: this.$el, selection: this.selection, dropzone: true, dropzoneSelector: '.category' });
+            dnd.enable({ draggable: true, container: this.ui.list, selection: this.selection, delegate: true, dropzone: true, dropzoneSelector: '.category' });
             // register listeners
             this.register();
         },
@@ -516,7 +516,7 @@ define('io.ox/mail/categories/main', [
         }, 500, { leading: false })
     };
 
-    // hint: settings at io.ox/mail/settings/pane
+    // hint: settings in io.ox/mail/settings/pane
     return module;
 
 });

commit 4f9e3dcca6b8eb4f98058a2d64287cb4b8dd1c67

fixes broken list dnd
- add off for delegate option
- broken with 71558fd05cf5ce9cfab4face161b0c27f4e703cc

ui/apps/io.ox/core/tk/list-dnd.js
@@ -202,7 +202,12 @@ define('io.ox/core/tk/list-dnd', [
             // unbind handlers
             $(document).off('mousemove.dnd mouseup.dnd mouseover.dnd mouseout.dnd keyup.dnd');
             $('.dropzone').each(function () {
-                var node = $(this), selector = node.attr('data-dropzones');
+                var node = $(this),
+                    selector = node.attr('data-dropzones'),
+                    delegate = node.attr('data-delegate');
+                if (delegate && selector) {
+                    return node.off('mouseup.dnd', selector);
+                }
                 (selector ? node.find(selector) : node).off('mouseup.dnd');
             });
             $('.dnd-over').removeClass('dnd-over');