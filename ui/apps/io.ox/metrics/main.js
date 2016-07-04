c5ec1afc4ce8a832e88e0f5f8973ecc99495b4bf
Frank Paczynski <frank.paczynski@open-xchange.com>
2016-07-04 10:13:06 +0200

    metrics: introduce context adapter
    - a queue that holds latest 10 events
    - idea: add this data to user feedbacks
    - remove storage part from console adapter

--- ui/apps/io.ox/metrics/main.js
+++ ui/apps/io.ox/metrics/main.js
@@ -20,13 +20,13 @@ define('io.ox/metrics/main', [
     'io.ox/metrics/extensions',
     'io.ox/metrics/adapters/default',
     'io.ox/metrics/adapters/analytics',
-    'io.ox/metrics/adapters/console'
+    'io.ox/metrics/adapters/console',
+    'io.ox/metrics/adapters/context'
 ], function (ext, settings, util, caps, http) {
 
     'use strict';
 
     var point = ext.point('io.ox/metrics/adapter'),
-        // TODO: salt?
         userhash = util.getUserHash(),
         metrics;
 
