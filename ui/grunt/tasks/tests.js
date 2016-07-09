3b00c15af86c35d53d2f27b232cfe86d6ae8ece0
Julian Bäume <julian.baeume@open-xchange.com>
2016-07-09 11:59:53 +0200

    disable most tests by moving them into a "disabled" folder
    we want to basically start all over, again

--- ui/grunt/tasks/tests.js
+++ ui/grunt/tasks/tests.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function (grunt) {
+    grunt.config.set('copy.specs', {
+        files: [{
+            expand: true,
+            src: ['spec/**/*', '!spec/disabled/**/*'],
+            dest: 'build/'
+        }]
+    });
     grunt.config.set('karma.continuous.reporters', ['spec']);
 };
