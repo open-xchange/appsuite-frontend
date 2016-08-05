437b665390370886078253de77936742cc560378
David Bauer <david.bauer@open-xchange.com>
2016-08-05 11:41:33 +0200

    Fixed: Bug 47790 - window.opener function is not used while opening links but target="_blank" is #2

--- ui/apps/io.ox/mail/detail/content.js
+++ ui/apps/io.ox/mail/detail/content.js
@@ -346,9 +346,8 @@ define('io.ox/mail/detail/content', [
                     // fix ID, i.e. replace the DOT (old notation) by a SLASH (new notation, 7.8.0)
                     if (/^\d+\./.test(data.id)) data.id = data.id.replace(/\./, '/');
                     link.addClass(data.className).data(data);
-                }
-                // mailto
-                if (href.indexOf('mailto') > -1) {
+                } else if (href.indexOf('mailto') > -1) {
+                    // mailto:
                     link.addClass('mailto-link').attr('target', '_blank');
                     text = link.text();
                     if (text.search(/^mailto:/) > -1) {
@@ -358,8 +357,10 @@ define('io.ox/mail/detail/content', [
                         text = text.split(/\?/, 2)[0];
                         link.text(text);
                     }
+                } else if (link.attr('href')) {
+                    // other links
+                    link.attr('rel', 'noopener');
                 }
-                if (link.attr('href')) link.attr('rel', 'noopener');
             });
         }
     });
