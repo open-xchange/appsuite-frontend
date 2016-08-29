34cac8ccd98e772989cf557b0a9210805cb4b5d7
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2016-08-29 14:07:16 +0200

    Datepicker: Working on keyboard support and a11y

--- ui/apps/io.ox/backbone/views/datepicker.js
+++ ui/apps/io.ox/backbone/views/datepicker.js
@@ -31,10 +31,9 @@ define('io.ox/backbone/views/datepicker', [
         events: {
             'click .btn-prev, .btn-next': 'onNavigate',
             'click .btn-today': 'onToday',
-            'click .date': 'onChangeDate',
             'click .switch-mode': 'onSwitchMode',
             'keydown': 'onKeydown',
-            'keydown .grid': 'onCursor'
+            'keydown .grid': 'onGridKeydown'
         },
 
         // we use the constructor here not to collide with initialize()
@@ -79,12 +78,15 @@ define('io.ox/backbone/views/datepicker', [
             var headerId = _.uniqueId('header');
             this.$el.attr({ 'aria-labelledby': headerId, 'role': 'region', 'tabindex': 0 }).append(
                 $('<div class="navigation">').append(
-                    $('<a href="#" role="button" class="control btn-prev pull-left"><i class="fa fa-chevron-left"></i></a>'),
-                    $('<span role="header">').attr('id', headerId),
-                    $('<a href="#" role="button" class="control btn-next pull-right"><i class="fa fa-chevron-right"></i></a>')
+                    $('<button role="button" class="btn-prev pull-left"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>'),
+                    $('<span role="header" aria-live="assertive" aria-atomic="true">').attr('id', headerId),
+                    $('<button role="button" class="btn-next pull-right"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>')
                 ),
-                $('<table class="grid" role="grid" tabindex="0">'),
-                $('<a href="#" role="button" class="control btn-today">').text(gt('Today'))
+                this.$grid = $('<table class="grid" role="grid" tabindex="0">')
+                    .attr('aria-label', gt('Use cursor keys to navigate, press enter to select a date')),
+                $('<button role="button" class="btn-today">')
+                    .attr('aria-label', 'Go to today')
+                    .text(gt('Today'))
             );
         },
 
@@ -93,7 +95,20 @@ define('io.ox/backbone/views/datepicker', [
         },
 
         renderGrid: function () {
-            this.$('.grid').empty().append(_(arguments).toArray());
+            this.$grid.empty().append(_(arguments).toArray());
+        },
+
+        setActiveDescedant: function (id) {
+            if (!id) {
+                var selected = this.$grid.find('[aria-selected="true"]');
+                id = (selected.length ? selected : this.$grid.find('td:first').attr('aria-selected', true)).attr('id');
+            }
+            this.$grid.attr('aria-activedescendant', id);
+        },
+
+        setNavigationLabels: function (prev, next) {
+            this.$('.btn-prev').attr('aria-label', prev);
+            this.$('.btn-next').attr('aria-label', next);
         },
 
         renderMonth: function () {
@@ -102,65 +117,95 @@ define('io.ox/backbone/views/datepicker', [
                 m = this.dates.display.clone();
 
             this.renderHeader(
-                $('<a href="#" role="button" class="control switch-mode">')
-                    .attr({ 'data-mode': 'year', 'data-value': m.year() })
+                $('<button role="button" class="switch-mode">')
+                    .attr({
+                        'data-mode': 'year',
+                        'data-value': m.year()
+                    })
                     .text(m.format('MMMM YYYY'))
             );
 
+            this.setNavigationLabels(gt('Go to previous month'), gt('Go to next month'));
+
             this.renderGrid(
                 $('<thead>').append(
-                    $('<tr class="weekdays">').append(function () {
-                        var w = m.clone().day(0);
-                        return _.range(0, 7).map(function () {
-                            w.add(1, 'day');
-                            return $('<td class="weekday">')
-                                .toggleClass('weekend', w.day() === 0 || w.day() === 6)
-                                .text(w.format('dd'));
-                        });
-                    })
+                    $('<tr role="row">')
+                        // add empty <th> due to calendar week column
+                        .append($('<th class="cw weekday">').text(gt('CW')))
+                        .append(function () {
+                            var w = m.clone().day(0);
+                            return _.range(0, 7).map(function () {
+                                w.add(1, 'day');
+                                return $('<th class="weekday">')
+                                    .toggleClass('weekend', w.day() === 0 || w.day() === 6)
+                                    .text(w.format('dd'));
+                            });
+                        })
                 ),
                 $('<tbody>').append(
                     function () {
                         var month = m.date(1).month(), start = m.clone(), end = m.clone().endOf('month');
                         if (m.day(1).isAfter(start)) m.subtract(1, 'week');
                         return _.range(0, Math.ceil(end.diff(m, 'days') / 7)).map(function () {
-                            return $('<tr class="week">').append(function () {
-                                return _.range(0, 7).map(function () {
-                                    try {
-                                        return $('<td role="gridcell" class="control date">')
-                                            .attr('aria-selected', isSame(m, current))
-                                            .data('date', m.valueOf())
-                                            .toggleClass('inside', m.month() === month)
-                                            .toggleClass('today', isToday(m))
-                                            .text(m.format('D'));
-                                    } finally {
-                                        m.add(1, 'day');
-                                    }
+                            return $('<tr role="row">')
+                                .append(
+                                    $('<th class="cw date">').text(m.week())
+                                )
+                                .append(function () {
+                                    return _.range(0, 7).map(function () {
+                                        try {
+                                            return $('<td role="gridcell" class="date">')
+                                                .attr({
+                                                    'id': 'date_' + m.format('l'),
+                                                    'aria-label': m.format('l, dddd'),
+                                                    'aria-selected': isSame(m, current)
+                                                })
+                                                .data('date', m.valueOf())
+                                                .toggleClass('inside', m.month() === month)
+                                                .toggleClass('today', isToday(m))
+                                                .text(m.format('D'));
+                                        } finally {
+                                            m.add(1, 'day');
+                                        }
+                                    });
                                 });
-                            });
                         });
                     }
                 )
             );
+
+            this.setActiveDescedant();
         },
 
         renderYear: function () {
 
-            var m = this.dates.display.clone().month(0);
+            var current = this.dates.current.month(),
+                m = this.dates.display.clone().month(0);
 
             this.renderHeader(
-                $('<a href="#" role="button" class="control switch-mode">')
-                    .attr({ 'data-mode': 'decade', 'data-value': m.year() })
+                $('<button role="button" class="switch-mode">')
+                    .attr({
+                        'data-mode': 'decade',
+                        'data-value': m.year()
+                    })
                     .text(m.format('YYYY'))
             );
 
+            this.setNavigationLabels(gt('Go to previous year'), gt('Go to next year'));
+
             this.renderGrid(function () {
                 return _.range(0, 4).map(function () {
-                    return $('<tr>').append(function () {
+                    return $('<tr role="row">').append(function () {
                         return _.range(0, 3).map(function () {
                             try {
-                                return $('<td role="gridcell" class="control month switch-mode">')
-                                    .attr({ 'aria-selected': false, 'data-mode': 'month', 'data-value': m.month() })
+                                return $('<td role="gridcell" class="month switch-mode">')
+                                    .attr({
+                                        'id': 'month_' + m.format('YYYY-MM'),
+                                        'aria-label': m.format('MMMM YYYY'),
+                                        'aria-selected': m.month() === current,
+                                        'data-mode': 'month',
+                                        'data-value': m.month()
+                                    })
                                     .text(m.format('MMM'));
                             } finally {
                                 m.add(1, 'month');
@@ -169,24 +214,35 @@ define('io.ox/backbone/views/datepicker', [
                     });
                 });
             });
+
+            this.setActiveDescedant();
         },
 
         renderDecade: function () {
 
-            var m = this.dates.display.clone(), year = m.year();
-            m.year(Math.floor(year / 10) * 10);
+            var current = this.dates.current.year(),
+                m = this.dates.display.clone();
+
+            m.year(Math.floor(m.year() / 10) * 10);
 
             this.renderHeader(
-                $('<span class="control">').text(m.format('YYYY') + ' - ' + m.clone().add(12, 'years').format('YYYY'))
+                $('<caption>').text(m.format('YYYY') + ' - ' + m.clone().add(12, 'years').format('YYYY'))
             );
+
+            this.setNavigationLabels(gt('Go to previous decade'), gt('Go to next decade'));
 
             this.renderGrid(function () {
                 return _.range(0, 4).map(function () {
-                    return $('<tr>').append(function () {
+                    return $('<tr role="row">').append(function () {
                         return _.range(0, 3).map(function () {
                             try {
-                                return $('<span href="#" role="button" class="control year switch-mode">')
-                                    .attr({ 'aria-selected': false, 'data-mode': 'year', 'data-value': m.year() })
+                                return $('<td role="gridcell" class="year switch-mode">')
+                                    .attr({
+                                        'id': 'year_' + m.year(),
+                                        'aria-selected': m.year() === current,
+                                        'data-mode': 'year',
+                                        'data-value': m.year()
+                                    })
                                     .text(m.format('YYYY'));
                             } finally {
                                 m.add(1, 'year');
@@ -195,92 +251,159 @@ define('io.ox/backbone/views/datepicker', [
                     });
                 });
             });
+
+            this.setActiveDescedant();
         },
 
         onNavigate: function (e) {
             e.preventDefault();
             var fn = $(e.currentTarget).hasClass('btn-prev') ? 'subtract' : 'add',
-                date = this.dates.display;
+                date = this.dates.current;
             switch (this.mode) {
                 case 'decade': date[fn](10, 'years'); break;
                 case 'year': date[fn](1, 'year'); break;
                 default: date[fn](1, 'month'); break;
             }
+            this.dates.display = this.dates.current.clone().date(1);
             this.render();
-        },
-
-        onChangeDate: function (e) {
-            e.preventDefault();
-            this.setCurrentDate($(e.currentTarget).data('date'));
         },
 
         onToday: function (e) {
             e.preventDefault();
-            this.setCurrentDate(moment());
             this.mode = 'month';
-            this.render();
+            this.setCurrentDate(moment());
+            this.$grid.focus();
         },
 
-        setCurrentDate: function (t) {
-            var m = moment(t);
-            this.dates.current = m.clone();
-            this.dates.display = m.clone().date(1);
-        },
-
-        onSwitchMode: function (e) {
-            e.preventDefault();
-            var node = $(e.currentTarget),
-                date = this.dates.display,
-                value = node.data('value');
-            this.mode = node.attr('data-mode');
+        switchMode: function (mode, value) {
+            var date = this.dates.display;
+            this.mode = mode;
             switch (this.mode) {
                 case 'year': date.year(value); break;
                 case 'month': date.month(value); break;
                 // no default
             }
-            this.render();
+            this.setCurrentDate(date);
+            this.$grid.focus();
         },
 
-        onKeydown: function (e) {
-            // escape
-            if (e.which === 27) this.close();
+        onSwitchMode: function (e) {
+            e.preventDefault();
+            var node = $(e.currentTarget), value = node.data('value');
+            this.switchMode(node.attr('data-mode'), value);
         },
 
-        onCursor: function (e) {
+        onKeydown: (function () {
 
-            // consider cursor movement only
-            if (e.which < 37 || e.which > 40) return;
-
-            var selected = this.$('[aria-selected="true"]'),
-                row = selected.parent(),
-                index = row.children().index(selected),
-                length = row.children().length;
-
-            function select(node) {
-                if (!node.length) return;
-                selected.attr('aria-selected', false);
-                node.attr('aria-selected', true);
+            function handleFocusChange($el, e) {
+                var tabbable = $el.find(':input, :button'),
+                    index = tabbable.index(e.target);
+                if ((e.shiftKey && index === 0) || (!e.shiftKey && index === tabbable.length - 1)) e.preventDefault();
             }
 
-            switch (e.which) {
-                // cursor left
-                case 37:
-                    select(index === 0 ? row.prev().children().last() : selected.prev());
-                    break;
-                // cursor right
-                case 39:
-                    select(index === length - 1 ? row.next().children().first() : selected.next());
-                    break;
-                // cursor up
-                case 38:
-                    select(row.prev().children().eq(index));
-                    break;
-                // cursor down
-                case 40:
-                    select(row.next().children().eq(index));
-                    break;
+            return function (e) {
+                // escape
+                if (e.which === 27) return this.close();
+                // focus trap
+                if (e.which === 9) return handleFocusChange(this.$el, e);
+            };
+        }()),
+
+        onGridKeydown: (function () {
+
+            var movement = {
+                horizontal: { month: { day: 1 }, year: { month: 1 }, decade: { year: 1 } },
+                vertical: { month: { week: 1 }, year: { months: 3 }, decade: { years: 3 } }
+            };
+
+            return function (e) {
+
+                switch (e.which) {
+                    case 13:
+                        handleEnter.call(this, e);
+                        break;
+                    case 33:
+                    case 34:
+                        handlePageUpDown.call(this, e);
+                        break;
+                    case 35:
+                    case 36:
+                        handleHomeEnd.call(this, e);
+                        break;
+                    case 37:
+                    case 38:
+                    case 39:
+                    case 40:
+                        handleCursor.call(this, e);
+                        break;
+                    // no default
+                }
+            };
+
+            function handleEnter() {
+                var selected = this.$('[aria-selected="true"]'),
+                    mode = selected.attr('data-mode');
+                if (mode) this.switchMode(mode, selected.data('value'));
+            }
+
+            function handlePageUpDown(e) {
+                var up = e.which === 33, step = e.shiftKey ? 'year' : 'month';
+                this.setCurrentDate(this.dates.current.add(up ? -1 : +1, step));
+            }
+
+            function handleHomeEnd(e) {
+                var isEnd = e.which === 35, fn = isEnd ? 'endOf' : 'startOf';
+                this.setCurrentDate(this.dates.current[fn]('month'));
+            }
+
+            function handleCursor(e) {
+
+                var selected = this.$('[aria-selected="true"]'),
+                    current = this.dates.current,
+                    type = 'add', direction = 'horizontal';
+
+                // take care of the unexpected
+                if (!selected.length) return this.setCurrentDate(current);
+
+                switch (e.which) {
+                    // cursor left (nothing to do for cursor right)
+                    case 37: type = 'subtract'; break;
+                    // cursor up
+                    case 38: type = 'subtract'; direction = 'vertical'; break;
+                    // cursor down
+                    case 40: direction = 'vertical'; break;
+                    // no default
+                }
+
+                this.setCurrentDate(current[type](movement[direction][this.mode]));
+            }
+
+        }()),
+
+        setCurrentDate: function (date) {
+
+            var current = this.dates.current = date.clone(), selector;
+
+            var select = function (node) {
+                if (!node.length) return;
+                this.$('[aria-selected="true"]').attr('aria-selected', false);
+                node.attr('aria-selected', true);
+                this.setActiveDescedant(node.attr('id'));
+            }.bind(this);
+
+            // currently visible?
+            switch (this.mode) {
+                case 'decade': selector = '#year_' + current.year(); break;
+                case 'year': selector = '#month_' + current.format('YYYY-MM'); break;
+                case 'month': selector = '#date_' + $.escape(current.format('l')); break;
                 // no default
             }
+            // found?
+            var node = this.$grid.find(selector);
+            if (node.length) return select(node);
+            // otherwise update view
+            this.dates.display = current.clone().date(1);
+            this.render();
         }
     });
 
