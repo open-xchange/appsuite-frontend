f7b63188d9d2bf1adf735f3d1ae24a87d3e66d55
Frank Paczynski <frank.paczynski@open-xchange.com>
2016-09-19 15:26:01 +0200

    Fixed: Bug 47184 - [L3] Forwarding mails with cc-recipients automatically opens cc field in mail compose
    Slightly modified version of the initial fix (80638bfcdbfc03e54db660b1467cc68314917ce5) that also supports forwarding of multiple mails
    
    (cherry picked from commit 8c0efe08057be8dba799350cff612bcd805ba8a0)

--- ui/apps/io.ox/mail/compose/main.js
+++ ui/apps/io.ox/mail/compose/main.js
@@ -17,6 +17,13 @@ define('io.ox/mail/compose/main', ['io.ox/mail/api', 'gettext!io.ox/mail'], func
 
     var blocked = {};
 
+    function keepData(obj) {
+        return /(compose|edit)/.test(obj.mode) ||
+               // forwarding muliple messages
+               /(forward)/.test(obj.mode) && !obj.id ||
+               obj.restored;
+    }
+
     // multi instance pattern
     function createInstance() {
 
@@ -50,7 +57,7 @@ define('io.ox/mail/compose/main', ['io.ox/mail/api', 'gettext!io.ox/mail'], func
             point.initial = false;
             // special flag/handling for 'replace' cause we want
             // to keep the attachments that will be removed otherwise
-            if (point.mode === 'reply') point.restored = true;
+            if (/(reply|replyall|forward)/.test(point.mode)) point.restored = true;
             return compose(point.mode)(point);
         };
 
@@ -75,8 +82,7 @@ define('io.ox/mail/compose/main', ['io.ox/mail/api', 'gettext!io.ox/mail'], func
                 win.busy().show(function () {
                     require(['io.ox/mail/compose/bundle'], function () {
                         require(['io.ox/mail/compose/view', 'io.ox/mail/compose/model'], function (MailComposeView, MailComposeModel) {
-                            var keepdata = /(compose|edit|forward)/.test(obj.mode) || obj.restored,
-                                data = keepdata ? obj : _.pick(obj, 'id', 'folder_id', 'mode', 'csid', 'content_type');
+                            var data = keepData(obj) ? obj : _.pick(obj, 'id', 'folder_id', 'mode', 'csid', 'content_type');
                             app.model = new MailComposeModel(data);
                             app.view = new MailComposeView({ app: app, model: app.model });
                             win.nodes.main.addClass('scrollable').append(app.view.render().$el);
