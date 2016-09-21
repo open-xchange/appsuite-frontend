905c7774725c0f4a77e0c387787d50d0788f273a
Francisco Laguna <francisco.laguna@open-xchange.com>
2016-09-21 10:29:20 +0200

    Revert "fix bug 48446"
    This reverts commit a217fbe69a1c048f6e7208996fbe0bde1e2f5f0e.

--- open-xchange-appsuite-saml/apps/io.ox.saml/register.js
+++ open-xchange-appsuite-saml/apps/io.ox.saml/register.js
@@ -7,21 +7,20 @@ define('io.ox.saml/register', ['io.ox/core/extensions', 'io.ox/core/session', 's
                 ext.point('io.ox.saml/relogin').invoke('handle', baton, baton);
             });        
         });
-
-        if (caps.has('saml-single-logout')) {
-            ext.point('io.ox/core/logout').extend({
-                id: 'saml_logout',
-                index: 'last',
-                logout: function () {
-                    var def = $.Deferred();
-                    $.get(ox.apiRoot + '/saml/init?flow=logout&session=' + ox.session).done(function (data) {
-                        var baton = new ext.Baton({data: data});
-                        ext.point('io.ox.saml/logout').invoke('handle', baton, baton);
-                    }).fail(def.reject);
-                    return def; // Hack to stop all further processing. This is never resolved.
-                }
-            });
-        }
     }
 
+    if (caps.has('saml-single-logout')) {
+        ext.point('io.ox/core/logout').extend({
+            id: 'saml_logout',
+            index: 'last',
+            logout: function () {
+                var def = $.Deferred();
+                $.get(ox.apiRoot + '/saml/init?flow=logout&session=' + ox.session).done(function (data) {
+                    var baton = new ext.Baton({data: data});
+                    ext.point('io.ox.saml/logout').invoke('handle', baton, baton);
+                }).fail(def.reject);
+                return def; // Hack to stop all further processing. This is never resolved.
+            }
+        });
+    }
 });
