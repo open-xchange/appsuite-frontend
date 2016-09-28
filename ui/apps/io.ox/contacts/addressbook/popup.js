7a3a1fbe7db0bafcb0ea0536dd28e3758aa5c673
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2016-09-28 12:18:18 +0200

    Revert "AB picker: Rename "All contacts" to "All address books""
    This reverts commit 6db57200a8bbd31cadd95f84a9b42fc3fa64a4ab.

--- ui/apps/io.ox/contacts/addressbook/popup.js
+++ ui/apps/io.ox/contacts/addressbook/popup.js
@@ -362,7 +362,7 @@ define('io.ox/contacts/addressbook/popup', [
                         ),
                         $('<div class="col-xs-6">').append(
                             $('<select class="form-control folder-dropdown invisible">').append(
-                                $('<option value="all">').text(gt('All address books')),
+                                $('<option value="all">').text(gt('All contacts')),
                                 $('<option value="all_lists">').text(gt('All distribution lists')),
                                 useLabels ? $('<option value="all_labels">').text(gt('All groups')) : $()
                             )
