f5b2c1e8134e37cc2ad456dcb4ed9c4bbb75a2f5
Julian Bäume <julian.baeume@open-xchange.com>
2016-09-30 11:09:56 +0200

    disable automatic grunt wipe task when changing config
    In many cases only minor things are changed and there is no need to cleanup
    everything. Developers are a little more responsible to handle such
    situations, but there will be less false positives about "major config
    changes, which require a complete rebuild". This change might be part of
    the next shared grunt config release.

---
+++ ui/grunt/tasks/watch.js
@@ -0,0 +1,19 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2016 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author David Bauer <david.bauer@open-xchange.com>
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+
+module.exports = function (grunt) {
+    'use strict';
+
+    // do not start any heavy operation when configs change. Developer is responsible, instead
+    grunt.config.set('watch.configs.files', []);
+};
