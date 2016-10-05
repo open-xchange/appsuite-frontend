open-xchange/appsuite-frontend

commit 004e298660bef704ce5c765bf7cd1345d56a7327

do not yell if message has already been handled
this allows alternative code paths to handle error messages and surpress
the dialog shown by yell.

ui/apps/io.ox/core/yell.js
@@ -130,7 +130,7 @@ define('io.ox/core/yell', ['gettext!io.ox/core'], function (gt) {
         }
 
         // add message
-        if (!validType.test(o.type)) return;
+        if (message.handled === true || !validType.test(o.type)) return;
 
         var alert = [];
         //screenreader only messages can be much simpler and don't need styling or formating (audio only)

commit 797227e77224613893a46c4337ef8b3bbe335c35

fix regression introduced by commit 004e298660be
"do not yell if message has already been handled"

----

FIXES Bug 49179 - Uncaught TypeError: Cannot read property 'handled' of
    undefined on calling showNotification

Root cause: insufficient testing when extending the API
Solution: use normalized message object instead of plain parameter
Resolution type: fix

ui/apps/io.ox/core/yell.js
@@ -130,7 +130,7 @@ define('io.ox/core/yell', ['gettext!io.ox/core'], function (gt) {
         }
 
         // add message
-        if (message.handled === true || !validType.test(o.type)) return;
+        if (o.message.handled === true || !validType.test(o.type)) return;
 
         var alert = [];
         //screenreader only messages can be much simpler and don't need styling or formating (audio only)