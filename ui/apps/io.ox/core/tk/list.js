28e4295d89d28bc7e79781fe3b498529f6280883
Frank Paczynski <frank.paczynski@open-xchange.com>
2016-10-25 12:59:14 +0200

    Fixed: Bug 48495 - [L3] New arriving mails are sorted somewhere into existing mails in list view
    Root cause: regression of fix for bug 47417; sort handler was called before models were drawn
    Solution: skip sort when queue contains items; call onSort manually once the queue has been processed
    Resolved state: Fix
    Version: 7.8.3
    Git repository: wd/frontend/web
    Git branch: release-7.8.3
    Package name: open-xchange-appsuite
    
    (cherry picked from commit adec63afddc4897e79a1d2b5c527d121b7034f31)

--- ui/apps/io.ox/core/tk/list.js
+++ ui/apps/io.ox/core/tk/list.js
@@ -268,6 +268,8 @@ define('io.ox/core/tk/list', [
             }
 
             return function () {
+                // needless cause added models not drawn yet (debounced renderListItems)
+                if (this.queue.list.length) return;
 
                 var dom, sorted, i, j, length, node, reference, index, done = {};
 
@@ -741,6 +743,9 @@ define('io.ox/core/tk/list', [
                 this.trigger('add', model, index);
 
             });
+
+            // needs to be called manually cause drawing is debounced
+            this.onSort();
         },
 
         getBaton: function (model) {
