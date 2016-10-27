6c2a49bd9181a855ca5254001503cc5cbe83b0c8
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2016-10-27 10:29:06 +0200

    Additional commit Bug 49669
    Value was not set correctly

--- ui/apps/io.ox/backbone/mini-views/common.js
+++ ui/apps/io.ox/backbone/mini-views/common.js
@@ -41,10 +41,11 @@ define('io.ox/backbone/mini-views/common', ['io.ox/backbone/mini-views/abstract'
         },
         update: function () {
             // trim left spaces if possible
-            this.$el.val(_.isString(this.model.get(this.name)) ? this.model.get(this.name).replace(/^\s+/, '') : this.model.get(this.name));
+            var val = _.isString(this.model.get(this.name)) ? this.model.get(this.name).replace(/^\s+/, '') : this.model.get(this.name);
+            this.$el.val(val);
             // update model too or the the left spaces are still in the model data. They would be saved when the model is saved, creating inconsistent data
             // infinite loops are not possible because the change event is only triggered if the new value is different
-            this.model.set(this.name, $.trim(this.model.get(this.name)));
+            this.model.set(val);
         },
         render: function () {
             this.$el.attr({ name: this.name });
