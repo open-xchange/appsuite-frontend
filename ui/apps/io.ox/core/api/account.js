5bb3f9ced90c38e114fd91900af07a3cbe5cb6ce
Alexander Quast <alexander.quast@open-xchange.com>
2016-12-01 13:37:28 +0100

    Fixed: Bug 50389 - DSC: External mail address is not selected by default
    Resolving the account ID by DSC root_folder is now possible

--- ui/apps/io.ox/core/api/account.js
+++ ui/apps/io.ox/core/api/account.js
@@ -152,6 +152,7 @@ define('io.ox/core/api/account', [
     api.hasDSCAccount = function () {
         return !_.isEmpty(dscHash);
     };
+
     /**
      * get unified mailbox name
      * @return { deferred} returns array or null
@@ -507,7 +508,11 @@ define('io.ox/core/api/account', [
                 idHash[account.id] = true;
                 // fill DSC hash if needed
                 if (isDSC) {
+<<<<<<< HEAD
                     if (account.id !== 0) dscHash[account.root_folder] = account.id;
+=======
+                    dscHash[account.root_folder] = account.id;
+>>>>>>> 5d9be53... Fixed: Bug 50389 - DSC: External mail address is not selected by default
                 }
                 // add inbox first
                 typeHash['default' + account.id + '/INBOX'] = 'inbox';
