92b71ea24484b5c75e51db21c2ea6a009ac6f3e1
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2016-12-02 15:47:24 +0100

    Fixed Bug 50041 - [L3] moving files with description to external storage not working

--- ui/apps/io.ox/files/api.js
+++ ui/apps/io.ox/files/api.js
@@ -826,7 +826,8 @@ define('io.ox/files/api', [
                 module: 'files',
                 params: {
                     action: 'move',
-                    folder: targetFolderId
+                    folder: targetFolderId,
+                    ignoreWarnings: ignoreWarnings
                 },
                 data: items,
                 appendColumns: false
