63969f8a651f5f516827b5e3d402564555b512d5
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2016-12-04 20:59:39 +0100

    Reuse code to show mail address as title

--- ui/apps/io.ox/mail/common-extensions.js
+++ ui/apps/io.ox/mail/common-extensions.js
@@ -164,15 +164,11 @@ define('io.ox/mail/common-extensions', [
                 // if bit 4096 is set, the server sorts by display name; if unset, it sorts by local part.
                 capabilities = folderAPI.pool.getModel(data.folder_id).get('capabilities') || 0,
                 isFromTo = baton.options.sort === 'from-to',
-                mailAdress = data[field][0] ? data[field][0][1] : '',
+                mailAddress = util.getFrom(data, { field: field, showDisplayName: false }).text(),
                 showDisplayName = !isFromTo || (capabilities & 4096);
 
-            if (mailAdress === '') {
-                mailAdress = (field === 'from' ? gt('Unknown sender') : gt('No recipients'));
-            }
-
             this.append(
-                $('<div class="from">').attr('title', mailAdress).append(
+                $('<div class="from">').attr('title', mailAddress).append(
                     util.getFrom(data, { field: field, reorderDisplayName: !isFromTo, showDisplayName: showDisplayName, unescapeDisplayName: !isFromTo })
                 )
             );
