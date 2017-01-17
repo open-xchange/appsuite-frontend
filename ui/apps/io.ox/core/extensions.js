283dbcfd4d10ab00320214229086754737cbe4fa
Frank Paczynski <frank.paczynski@open-xchange.com>
2017-01-17 12:26:02 +0100

    Fixed: Bug 51022 - Create or edit a document is broken
    Root cause: 56632e3c62539e6dfc532933e6afda0768672985
    Solution:
    - using existing extension mechanisms rather than custom enabled functions
    - introduced warning in case more extensions use enabled functions
    Resolved state: Fix
    Version: 7.8.4
    Git repository: wd/frontend/web
    Git branch: develop
    Package name: open-xchange-appsuite
    
    (cherry picked from commit e9a809cc4e0b3aa8f6b2994fe6629b214d196444)
    (cherry picked from commit b61df3510a49f9f2914f62015ccbe12c3d60541c)

--- ui/apps/io.ox/core/extensions.js
+++ ui/apps/io.ox/core/extensions.js
@@ -174,6 +174,14 @@ define('io.ox/core/extensions', ['io.ox/core/event'], function (Events) {
                 // skip duplicates (= same id)
                 if (!has(extension.id)) {
 
+                    if ('enabled' in extension) {
+                        if (_.isObject(extension.enabled)) {
+                            return console.error("Extending of '" + this.id + "' with '" + extension.id + "' failed. Ensure extensions 'enabled' property is a primitive.");
+                        }
+                        if (!extension.enabled) this.disable(extension.id);
+                        delete extension.enabled;
+                    }
+
                     extension.invoke = createInvoke(this, extension);
 
                     if (replacements[extension.id]) {
