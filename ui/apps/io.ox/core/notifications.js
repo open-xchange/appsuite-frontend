0298ea8ee3d9875b50da77b2cb7d3a3b3a729074
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2017-01-18 14:53:48 +0100

    a11y: improve keyboard support of notification area (open with space, navigation with arrow keys)
    AA / 15 F
    A / 19 F

--- ui/apps/io.ox/core/notifications.js
+++ ui/apps/io.ox/core/notifications.js
@@ -37,13 +37,21 @@ define('io.ox/core/notifications', [
         id: 'io-ox-notifications-display',
         events: {
             'click .clear-area-button': 'hide',
-            'click .hide-area-button': 'hideAll'
+            'click .hide-area-button': 'hideAll',
+            'keydown :tabbable': 'onKeydown',
+            'focus :tabbable': 'focusHover'
         },
         initialize: function () {
             var self = this;
             self.bannerHeight = 0;
             self.handledNotificationInfo = false;
             this.badgeview = new badgeview.view({ model: new badgeview.model() });
+            this.badgeview.$el.on('keydown', function (e) {
+                // toggle on space key, just like a dropdown
+                if (e.which === 32) {
+                    self.toggle();
+                }
+            });
             //close when clicked outside, since we don't have the overlay anymore
             //does not work with some dropdowns though (they prevent event bubbling), but the notification popup is in the background then
             $(document.body).on('click', function (e) {
@@ -110,7 +118,7 @@ define('io.ox/core/notifications', [
                     $('<div class=notification-area-header>').append(
                         $('<h1 class="notification-area-title">').text(gt('Notifications')),
                         $('<button type="button" class="btn btn-link clear-area-button fa fa-times">').attr('aria-label', gt('Close notification area')),
-                        $('<a role="button" class="btn btn-link hide-area-button">').text(gt('Notify me again later'))
+                        $('<button type="button" class="btn btn-link hide-area-button">').text(gt('Notify me again later'))
                     )
                 );
             }
@@ -225,7 +233,47 @@ define('io.ox/core/notifications', [
             } else {
                 cont();
             }
+        },
 
+        onKeydown: function (e) {
+            var items = [];
+            switch (e.which) {
+                // left or up arrow
+                case 37:
+                case 38:
+                    items = this.nodes.main.find(':tabbable');
+                    // add length once to avoid negative modulo operation, javascript has some issues with these
+                    var prevIndex = (_(items).indexOf(e.target) - 1 + items.length) % items.length;
+                    items[prevIndex].focus();
+                    break;
+                // right or down arrow
+                case 39:
+                case 40:items = this.nodes.main.find(':tabbable');
+                    var nextIndex = (_(items).indexOf(e.target) + 1) % items.length;
+                    items[nextIndex].focus();
+                    break;
+                // tab
+                case 9:
+                    // build a tabTrap so the menu behaves like a dropdown
+                    items = this.nodes.main.find(':tabbable');
+                    if (e.shiftKey && items[0] === e.target) {
+                        e.preventDefault();
+                        items[items.length - 1].focus();
+                    }
+                    if (!e.shiftKey && items.length && items[items.length - 1] === e.target) {
+                        e.preventDefault();
+                        items[0].focus();
+                    }
+                    break;
+                // no default
+            }
+            items = null;
+        },
+
+        // focus on an element inside an item should highlight the item as if the mouse hovers over it
+        focusHover: function (e) {
+            this.nodes.main.find('.item').removeClass('has-focus');
+            $(e.target).closest('.item', this.nodes.main).addClass('has-focus');
         },
 
         onCloseSidepopup: function () {
