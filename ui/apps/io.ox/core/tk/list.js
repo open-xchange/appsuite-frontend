051c731b8581c471c4627f378d12b9478469d7e8
Mario Schröder <mario.schroeder@open-xchange.com>
2017-02-17 13:03:49 +0100

    Fix for bug #51594# and #51596# [L3] Drive opens wrong files directly after upload - wrong link in UI.

--- ui/apps/io.ox/core/tk/list.js
+++ ui/apps/io.ox/core/tk/list.js
@@ -267,31 +267,22 @@ define('io.ox/core/tk/list', [
                 return node && parseInt(node.getAttribute('data-index'), 10);
             }
 
+            // fix for bugs #51594 and #51596
+            // [L3] Drive opens wrong files directly after upload - wrong link in UI
+            //
             return function () {
                 // needless cause added models not drawn yet (debounced renderListItems)
                 if (this.queue.list.length) return;
 
-                var dom, sorted, i, j, length, node, reference, index, done = {};
+                var items, detached, sorted;
 
                 // sort all nodes by index
-                dom = this.getItems().toArray();
-                sorted = _(dom).sortBy(getIndex);
+                items = this.getItems();
+                if (items.length > 1) {
+                    detached = items.detach();
+                    sorted = _.sortBy(detached, getIndex);
 
-                // apply sorting (step by step to keep focus)
-                // the arrays "dom" and "sorted" always have the same length
-                for (i = 0, j = 0, length = sorted.length; i < length; i++) {
-                    node = sorted[i];
-                    reference = dom[j];
-                    // mark as processed
-                    done[i] = true;
-                    // same element?
-                    if (node === reference) {
-                        // fast forward "j" if pointing at processed items
-                        do { index = getIndex(dom[++j]); } while (done[index]);
-                    } else if (reference) {
-                        // change position in dom
-                        this.el.insertBefore(node, reference);
-                    }
+                    this.$el.append(sorted);
                 }
             };
         }()),
