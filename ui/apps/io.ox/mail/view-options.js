84f0ca918fdbe64d7cdc074c3176f668bb1402a1
David Bauer <david.bauer@open-xchange.com>
2017-02-22 11:59:23 +0100

    Fixed whitespace in attributes

--- ui/apps/io.ox/mail/view-options.js
+++ ui/apps/io.ox/mail/view-options.js
@@ -118,7 +118,7 @@ define('io.ox/mail/view-options', [
         index: 100,
         draw: function (baton) {
             this.append(
-                $('<a href="#" class="toolbar-item select-all" role ="checkbox" aria-checked="false">').append(
+                $('<a href="#" class="toolbar-item select-all" role="checkbox" aria-checked="false">').append(
                     $('<i class="fa fa-square-o" aria-hidden="true">'),
                     $.txt(gt('Select all'))
                 )
