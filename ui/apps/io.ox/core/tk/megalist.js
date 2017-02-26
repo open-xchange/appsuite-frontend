6ae4bf49c3e8154f2137ec5fe9d521f577dd652a
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2017-02-26 21:12:50 +0100

    Megalist: Simplified code / fixed edge-cases

--- ui/apps/io.ox/core/tk/megalist.js
+++ ui/apps/io.ox/core/tk/megalist.js
@@ -15,78 +15,82 @@ define('io.ox/core/tk/megalist', [], function () {
 
     'use strict';
 
+    // draw example list
+
+    var li = $('<li tabindex="-1" role="option" aria-selected="false" aria-checked="false" data-cid="">');
+
     $('body').append(
         $('<ul class="megalist" role="listbox" aria-multiselectable="true">').append(
-            _.range(0, 20).map(function (i) {
-                return $('<li tabindex="-1" role="option" aria-selected="false" aria-checked="false">')
-                    .attr('data-cid', i)
-                    .text('List option #' + i);
+            _.range(0, 100).map(function (i) {
+                return li.clone().attr('data-cid', i).text('List option #' + i);
             })
         )
     );
 
-    var $list = $('.megalist'), $items = $list.children();
-
-    // initialize
-    $list.children().first().attr({ 'tabindex': 1, 'aria-selected': true });
-
-    // debug
-    $list.on('change', function (e, data) {
-        console.log('selection change', data);
-    });
+    var $el = $('.megalist'), el = $el[0];
 
     // the root element during range select
     var rangeStart = 0;
 
-    $list
+    $el
         .attr('tabindex', -1)
-        .find('li').attr('tabindex', -1).end()
-        .find('li:first').attr('tabindex', 0).end()
+        .find('li:first').attr({ 'tabindex': 1, 'aria-selected': true }).end()
         .focus();
 
     // DOM EVENTS
 
-    $list.on('focusin focusout', function (e) {
-        $(this).toggleClass('has-focus', e.type === 'focusin');
-    });
-
-    $list.on('keydown', function (e) {
-        switch (e.which) {
-            case 32: onSpace(e); break;
-            case 38:
-            case 40: onCursor(e); break;
-            /* no default */
-        }
-    });
-
-    $list.on('mousedown', 'li', function (e) {
-        trigger(e.currentTarget, e.metaKey ? 'toggle' : 'select', e);
-    });
-
-    $list.on('select', 'li', function (e) {
-        onSelect.call(e.currentTarget, e);
-    });
-
-    $list.on('toggle', 'li', function (e) {
-        onToggle.call(e.currentTarget, e);
-    });
+    $el
+        .on('focusin focusout', function (e) {
+            $(this).toggleClass('has-focus', e.type === 'focusin');
+        })
+        .on('keydown', function (e) {
+            switch (e.which) {
+                case 13: onEnter(e); break;
+                case 32: onSpace(e); break;
+                case 35:
+                case 36: onHomeEnd(e); break;
+                case 38:
+                case 40: onCursor(e); break;
+                /* no default */
+            }
+        })
+        .on('mousedown', 'li', function (e) {
+            trigger(e.currentTarget, e.metaKey ? 'toggle' : 'select', e);
+        })
+        .on('select', 'li', function (e) {
+            onSelect.call(e.currentTarget, e);
+        })
+        .on('toggle', 'li', function (e) {
+            onToggle.call(e.currentTarget, e);
+        })
+        .on('enter action', function (e, cid) {
+            console.log('selection:' + e.type, cid);
+        })
+        .on('change', function (e, data) {
+            console.log('selection:change', data);
+        });
 
     // HELPER
 
     var CHECKED = 'aria-checked', CHECKED_true = '[' + CHECKED + '="true"]',
+        CHECKED_false = '[' + CHECKED + '="false"]',
         SELECTED = 'aria-selected', SELECTED_true = '[' + SELECTED + '="true"]',
         TABINDEX = 'tabindex', TABINDEX_1 = '[' + TABINDEX + '="1"]';
 
     function get(arg) {
-        return _.isNumber(arg) ? $list.children().eq(arg) : $(arg);
+        return _.isNumber(arg) ? $el.children().eq(arg) : $(arg);
     }
 
     function getAllChecked() {
-        return $items.filter(CHECKED_true);
+        return $el.children().filter(CHECKED_true);
     }
 
     function getAllSelected() {
-        return $items.filter(SELECTED_true);
+        return $el.children().filter(SELECTED_true);
+    }
+
+    function hasChecked() {
+        return !!el.querySelector(CHECKED_true);
     }
 
     function trigger(arg, type, e) {
@@ -102,7 +106,7 @@ define('io.ox/core/tk/megalist', [], function () {
 
     function onSelect(e) {
 
-        var index = $items.index(this);
+        var index = $el.children().index(this);
 
         deselectAll();
 
@@ -120,22 +124,29 @@ define('io.ox/core/tk/megalist', [], function () {
 
         e.preventDefault();
 
-        var index = $items.index(document.activeElement), item;
+        var items = $el.children(),
+            index = items.index(document.activeElement), item;
 
         index += e.which === 38 ? -1 : +1;
-        if (index < 0 || index >= $items.length) return;
+        if (index < 0 || index >= items.length) return;
 
         item = get(index);
         focus(item);
 
-        // just focus movement if alt is pressed
-        if (e.metaKey) {
+        // just focus movement if cmd/ctrl key is pressed
+        if (e.ctrlKey || e.metaKey) {
             // clear selection if we're not in multi-selection
-            if (getAllChecked().length === 0) deselectAll();
+            if (!hasChecked()) deselectAll();
             rangeStart = index;
             return;
         }
 
+        trigger(item, 'select', e);
+    }
+
+    function onHomeEnd(e) {
+        var item = $el.children().eq(e.which === 35 ? -1 : 0);
+        focus(item);
         trigger(item, 'select', e);
     }
 
@@ -144,15 +155,20 @@ define('io.ox/core/tk/megalist', [], function () {
         trigger(document.activeElement, 'toggle', e);
     }
 
+    function onEnter() {
+        $el.trigger('enter', $(document.activeElement).attr('data-cid'));
+    }
+
     // CHANGE STATE
 
     function toggle(item) {
-        var checked = item.attr(CHECKED) === 'true';
-        item.attr(CHECKED, !checked).attr(SELECTED, !checked);
+        var newState = item.attr(CHECKED) !== 'true';
+        item.attr(CHECKED, newState).attr(SELECTED, newState);
+        if (newState) $el.children(CHECKED_false + SELECTED_true).attr(SELECTED, false);
     }
 
     function focus(item) {
-        $list.children(TABINDEX_1).attr(TABINDEX, -1);
+        $el.children(TABINDEX_1).attr(TABINDEX, -1);
         item.attr(TABINDEX, 1).focus();
     }
 
@@ -162,7 +178,7 @@ define('io.ox/core/tk/megalist', [], function () {
 
     function selectRange(index) {
         var start = Math.min(rangeStart, index), end = Math.max(rangeStart, index);
-        $items.slice(start, end + 1).attr(CHECKED, true).attr(SELECTED, true);
+        $el.children().slice(start, end + 1).attr(CHECKED, true).attr(SELECTED, true);
     }
 
     function deselectAll() {
@@ -173,10 +189,10 @@ define('io.ox/core/tk/megalist', [], function () {
     // PROGAPATE CHANGE
 
     function getCIDs(selector) {
-        return $items.filter(selector).map(function () { return $(this).attr('data-cid'); }).toArray();
+        return $el.children(selector).map(function () { return $(this).attr('data-cid'); }).toArray();
     }
 
     function propagateChange() {
-        $list.trigger('change', { checked: getCIDs(CHECKED_true), selected: getCIDs(SELECTED_true) });
+        $el.trigger('change', { checked: getCIDs(CHECKED_true), selected: getCIDs(SELECTED_true) });
     }
 });
