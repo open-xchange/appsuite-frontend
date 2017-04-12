ea9398fb8405552b2f97da0d481d29f3df2d379e
Julian Bäume <julian.baeume@open-xchange.com>
2017-04-12 15:02:44 +0200

    refactor some mail compose tests
    make it more easy to extend with new tests

--- ui/spec/io.ox/mail/compose/send_action_spec.js
+++ ui/spec/io.ox/mail/compose/send_action_spec.js
@@ -15,6 +15,7 @@ define([
     'io.ox/mail/compose/actions/send'
 ], function (ext) {
     describe('Mail send action extension point', function () {
+        //FIXME: move this function into a library
         function extensionCascade(point, baton) {
             return point.reduce(function (def, p) {
                 if (!def || !def.then) def = $.when(def);
@@ -37,42 +38,53 @@ define([
             }, $.when());
         }
         describe('send extension', function () {
-            var point = ext.point('io.ox/mail/compose/actions/send');
+            var point = ext.point('io.ox/mail/compose/actions/send'),
+                baton;
+
+            beforeEach(function () {
+                baton = new ext.Baton({
+                    mail: {
+                        subject: 'test',
+                        to: [['Test', 'test@example.com']],
+                        files: [],
+                        attachments: [{}]
+                    },
+                    view: {
+                        $el: $(),
+                        blockReuse: sinon.spy(),
+                        unblockReuse: sinon.spy()
+                    },
+                    stopPropagation: sinon.spy(),
+                    model: {
+                        dirty: sinon.spy(),
+                        set: sinon.spy()
+                    },
+                    app: {
+                        id: 'compose_test',
+                        getWindow: sinon.spy(),
+                        get: sinon.stub().returns($()),
+                        launch: sinon.spy(),
+                        quit: sinon.spy()
+                    }
+                });
+
+                // for 'disable-manual-close' extension
+                sinon.stub(ox.ui.apps, 'get').withArgs('compose_test').returns(baton.app);
+            });
+
+            afterEach(function () {
+                ox.ui.apps.get.restore();
+                baton = null;
+            });
 
             it('invoking all extensions and send fails should reject on error', function () {
                 this.server.respondWith('POST', /api\/mail\?action=new/, function (xhr) {
                     xhr.respond(200, { 'Content-Type': 'text/html;charset=UTF-8' }, '<!DOCTYPE html><html><head><META http-equiv="Content-Type" content="text/html; charset=UTF-8"><script type="text/javascript">(parent["callback_new"] || window.opener && window.opener["callback_new"])({"error":"A Guard server error occurred with HTTP status code 500. Error message: Bad password.","error_params":[500,"Bad password."],"categories":"ERROR","category":8,"code":"GUARD-0003","error_id":"-1678404933-142539","error_desc":"A Guard server error occurred. Status code: 500. Error message: Bad password."})</script></head></html>');
                 });
-                var result = $.Deferred(),
-                    baton = new ext.Baton({
-                        mail: {
-                            subject: 'test',
-                            to: [['Test', 'test@example.com']],
-                            files: [],
-                            attachments: [{}]
-                        },
-                        view: {
-                            $el: $(),
-                            blockReuse: sinon.spy(),
-                            unblockReuse: sinon.spy()
-                        },
-                        stopPropagation: sinon.spy(),
-                        app: {
-                            getWindow: sinon.spy(),
-                            get: sinon.spy(),
-                            launch: function () {
-                                return result.resolve('app launched again');
-                            }
-                        }
-                    });
 
-                point.disable('disable-manual-close');
-                extensionCascade(point, baton);
-
-                return result.then(function (result) {
-                    expect(result).to.be.equal('app launched again');
-                }).always(function () {
-                    point.enable('disable-manual-close');
+                return extensionCascade(point, baton).then(function () {
+                    expect(baton.app.launch.calledOnce, 'app has been restarted').to.be.true;
+                    expect(baton.model.dirty.called, 'model.dirty has been called').to.be.false;
                 });
             });
 
@@ -80,39 +92,10 @@ define([
                 this.server.respondWith('POST', /api\/mail\?action=new/, function (xhr) {
                     xhr.respond(200, { 'Content-Type': 'text/javascript;charset=UTF-8' }, JSON.stringify({ data: true }));
                 });
-                var result = $.Deferred(),
-                    baton = new ext.Baton({
-                        mail: {
-                            subject: 'test',
-                            to: [['Test', 'test@example.com']],
-                            files: [],
-                            attachments: [{}]
-                        },
-                        model: {
-                            dirty: sinon.spy()
-                        },
-                        view: {
-                            $el: $(),
-                            blockReuse: sinon.spy(),
-                            unblockReuse: sinon.spy()
-                        },
-                        stopPropagation: sinon.spy(),
-                        app: {
-                            getWindow: sinon.spy(),
-                            quit: function () {
-                                result.resolve('app quit');
-                            }
 
-                        }
-                    });
-
-                point.disable('disable-manual-close');
-                extensionCascade(point, baton);
-
-                return result.then(function (result) {
-                    expect(result).to.be.equal('app quit');
-                }).always(function () {
-                    point.enable('disable-manual-close');
+                return extensionCascade(point, baton).then(function () {
+                    expect(baton.app.quit.calledOnce, 'app has quit').to.be.true;
+                    expect(baton.model.dirty.calledWith(false), 'model.dirty(false) has been called').to.be.true;
                 });
             });
         });
