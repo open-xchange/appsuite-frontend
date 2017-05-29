d2c05e0bbaa08b2fbbcf9a0e3f67b49d96c5c95e
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2017-05-29 16:37:28 +0200

    Fixed Bug 53947 - calendar: monthly view scrolls

--- ui/apps/io.ox/calendar/month/perspective.js
+++ ui/apps/io.ox/calendar/month/perspective.js
@@ -354,7 +354,8 @@ define('io.ox/calendar/month/perspective', [
          *          number           duration: duration of the scroll animation
          */
         gotoMonth: function (target) {
-            var self = this;
+            var self = this,
+                isPrev;
 
             target = target || self.app.refDate || moment();
 
@@ -362,6 +363,7 @@ define('io.ox/calendar/month/perspective', [
                 if (target === 'today') {
                     target = moment();
                 } else if (target === 'prev') {
+                    isPrev = true;
                     target = moment(self.previous);
                 } else {
                     target = moment(self.current).add(1, 'month');
@@ -372,15 +374,17 @@ define('io.ox/calendar/month/perspective', [
             var nextMonth = moment(target).add(1, 'month'),
                 firstDay = $('#' + target.format('YYYY-MM'), self.pane),
                 nextFirstDay = $('#' + nextMonth.format('YYYY-MM'), self.pane),
+                // don't scroll to the first shown month (causes infinte scrolling because the scrollpos cannot be reached), draw a previous month first
+                isFirst = isPrev && $('.month-name', self.pane).first().attr('id') === target.format('YYYY-MM'),
                 scrollToDate = function () {
                     // scroll to position
                     if (firstDay.length === 0) return;
                     firstDay.get(0).scrollIntoView();
                 };
 
-            if (firstDay.length > 0 && nextFirstDay.length > 0) {
+            if (!isFirst && firstDay.length > 0 && nextFirstDay.length > 0) {
                 scrollToDate();
-            } else if (target.valueOf() < self.current.valueOf()) {
+            } else if (isFirst || target.valueOf() < self.current.valueOf()) {
                 this.drawWeeks({ up: true }).done(function () {
                     firstDay = $('#' + target.format('YYYY-MM'), self.pane);
                     scrollToDate();
@@ -474,8 +478,7 @@ define('io.ox/calendar/month/perspective', [
             this.app = app;
             this.current = moment(app.refDate || moment()).startOf('month');
             this.previous = moment(this.current).subtract(1, 'month');
-            // yes subtract another month. Otherwise the previous month has a scrolltop off 0 => scroll to previous month results in endless scrolling because scrolltop 0 cannot be reached ( endless scrolling )
-            this.firstWeek = moment(this.previous).subtract(1, 'month').startOf('week');
+            this.firstWeek = moment(this.previous).startOf('week');
             this.lastWeek = this.firstWeek.clone();
 
             this.main
