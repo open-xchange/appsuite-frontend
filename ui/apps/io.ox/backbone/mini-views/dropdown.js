open-xchange/appsuite-frontend

commit 90fb9e9a104acc4cab5c30927be2ffcb7d7b0106

Fixed: Bug 53797 - Wrong position of From/To drop-down of a contact token in mail search
Root cause: broken static top value
Solution: adjust static top value and add some generic handling
Resolved state: Fix
Version: 7.10.0
Git repository: wd/frontend/web
Git branch: develop
Package name: open-xchange-appsuite

ui/apps/io.ox/backbone/mini-views/dropdown.js
@@ -92,6 +92,9 @@ define('io.ox/backbone/mini-views/dropdown', ['io.ox/backbone/mini-views/abstrac
                 availableHeight = $(window).height(),
                 topbar = $('#io-ox-topbar');
 
+            // hides this.$el?
+            positions.top = Math.max(positions.top, this.$el.offset().top + this.$el.height());
+
             // hits bottom ?
             if (bounds.top + bounds.height > availableHeight + this.margin) {
                 // left or right?

ui/apps/themes/components/find.less
@@ -94,7 +94,7 @@
                         // left is set by js
                         .dropdown-menu {
                             position: fixed;
-                            top: 134px;
+                            top: 68px;
                         }
                     }
                 }

commit 373d1fd336b038d47ae5e57f13db157d71d5b693

temporary fix for regression in commit 90fb9e9a104acc4cab5c30927be2ffcb7d7b0106
- folder context menu dropdown was broken before this commit

ui/apps/io.ox/backbone/mini-views/dropdown.js
@@ -93,7 +93,9 @@ define('io.ox/backbone/mini-views/dropdown', ['io.ox/backbone/mini-views/abstrac
                 topbar = $('#io-ox-topbar');
 
             // hides this.$el?
-            positions.top = Math.max(positions.top, this.$el.offset().top + this.$el.height());
+            if (this.$el.css('position') !== 'fixed') {
+                positions.top = Math.max(positions.top, this.$el.offset().top + this.$el.height());
+            }
 
             // hits bottom ?
             if (bounds.top + bounds.height > availableHeight + this.margin) {