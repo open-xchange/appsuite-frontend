28d4f87925812e65321f62432dea8fc23a8f7186
David Bauer <david.bauer@open-xchange.com>
2017-06-09 11:15:54 +0200

    Updated TinyMCE language files
    (cherry picked from commit 300cf55efcb51ec1d85ece5a0626d6c51da35b7a)

--- ui/grunt/tasks/tinymce.js
+++ ui/grunt/tasks/tinymce.js
@@ -58,33 +58,15 @@ module.exports = function (grunt) {
     grunt.registerTask('tinymceLanguagePack', 'Fetches TinyMCE language files', function () {
         var fs = require('fs'),
             request = require('request'),
-            done = this.async();
-
-        request('http://archive.tinymce.com/i18n/index.php', function (error, response, body) {
+            done = this.async(),
+            dir = './tmp/';
+        if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+        var writeStream = fs.createWriteStream(dir + 'tinymce_language_pack.zip');
+        request('https://tinymce-services.azurewebsites.net/1/i18n/download?langs=af_ZA,ar,hy,az,be,bg_BG,ca,zh_CN,zh_CN.GB2312,zh_TW,hr,cs,cs_CZ,da,dv,nl,en_CA,en_GB,eo,et,fo,fi,fr_FR,fr_CH,gd,ka_GE,de,de_AT,el,he_IL,hi_IN,hu_HU,is_IS,id,ga,it,ja,kab,kk,km_KH,ko_KR,lv,lt,mk_MK,nb_NO,oc,fa_IR,pl,pt_BR,pt_PT,ro,ru,sr,sk,sl_SI,es,es_MX,sv_SE,ta,ta_IN,th_TH,tr,tr_TR,ug,uk,uk_UA,vi_VN,cy', function cb(error) {
             if (error) grunt.fail.warn(error);
-
-            if (!error && response.statusCode === 200) {
-                var re = /download\[\]" value="(.*)"/g,
-                    languages = body.match(re).map(function (str) {
-                        str = str.replace(re, '$1');
-                        // remove corrupt languages
-                        if (/zh_CN\.GB2312|ru@petr1708/.test(str)) return;
-                        return str;
-                    }),
-                    writeStream = fs.createWriteStream('tmp/tinymce_language_pack.zip');
-                request({
-                    url: 'http://archive.tinymce.com/i18n/download.php',
-                    method: 'POST',
-                    form: {
-                        'download': languages
-                    }
-                }, function cb(error) {
-                    if (error) grunt.fail.warn(error);
-                }).pipe(writeStream);
-                writeStream.on('close', done);
-                writeStream.on('error', done);
-            }
-        });
+        }).pipe(writeStream);
+        writeStream.on('close', done);
+        writeStream.on('error', done);
     });
 
     grunt.config.merge({
