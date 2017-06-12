549451cb882f9da6d868b49d249ab038b3a3c100
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2017-06-12 16:53:22 +0200

    Add phonetic extension (small kana )Bug 53688

--- ui/apps/l10n/ja_JP/io.ox/collation.js
+++ ui/apps/l10n/ja_JP/io.ox/collation.js
@@ -33,13 +33,14 @@ define('l10n/ja_JP/io.ox/collation', function () {
             'ゕ ゖ ' +              // Hiragana small
             'が ぎ ぐ げ ご ' +      // Hiragana with dakuten (ga gi gu ge go)
             'カ キ ク ケ コ ' +      // Katakana
-            'ヵ ヶ ' +              // Katakana small
+            'ヵ ㇰ ヶ ' +           // Katakana small
             'ｶ ｷ ｸ ｹ ｺ ' +          // Katakana half width
             'ガ ギ グ ゲ ゴ',       // Katakana with dakuten (ga gi gu ge go)
          // sa shi su se so
             'さ し す せ そ ' +      // Hiragana
-            'ざ じ ず ぜ ぞ ' +       // Hiragana with dakuten (za ji zu ze zo)
-            'サ シ ス セ ソ ' +       // Katakana
+            'ざ じ ず ぜ ぞ ' +      // Hiragana with dakuten (za ji zu ze zo)
+            'サ シ ス セ ソ ' +      // Katakana
+            'ㇱ ㇲ' +               // Katakana small
             'ｻ ｼ ｽ ｾ ｿ ' +          // Katakana half width
             'ザ ジ ズ ゼ ゾ',        // Katakana with dakuten (za ji zu ze zo)
         // ta chi tsu te to
@@ -47,24 +48,27 @@ define('l10n/ja_JP/io.ox/collation', function () {
             'っ ' +                 // Hiragana small
             'だ ぢ づ で ど ' +      // Hiragana with dakuten (da ji zu de do)
             'タ チ ツ テ ト ' +      // Katakana
-            'ｯ ' +                  // Katakana small
+            'ｯ ㇳ' +                 // Katakana small
             'ﾀ ﾁ ﾂ ﾃ ﾄ ' +           // Katakana half width
             'ダ ヂ ヅ デ ド っ ッ',   // Katakana with dakuten (da ji zu de do)
          // na ni nu ne no
             'な に ぬ ね の ' +      // Hiragana
             'ナ ニ ヌ ネ ノ ' +      // Katakana
+            'ㇴ ' +                 // Katakana small
             'ﾅ ﾆ ﾇ ﾈ ﾉ',            // Katakana half width
          // ha hi fu he ho
             'は ひ ふ へ ほ ' +      // Hiragana
             'ば び ぶ ぺ ぼ ' +      // Hiragana with dakuten (ba bi bu be bo)
             'ぱ ぴ ぷ ぺ ぽ ' +      // Hiragana with handakuten (pa pi pu pe po)
             'ハ ヒ フ ヘ ホ ' +      // Katakana
+            'ㇵ ㇶ ㇷ ㇸ ㇹ' +       // Katakana small
             'ﾊ ﾋ ﾌ ﾍ ﾎ ' +           // Katakana half width
             'バ ビ ブ ベ ボ ' +      // Katakana with dakuten (ba bi bu be bo)
             'パ ピ プ ペ ポ',        // Katakana with handakuten (pa pi pu pe po)
          // ma mi mu me mo
             'ま み む め も ' +      // Hiragana
             'マ ミ ム メ モ ' +      // Katakana
+            'ㇺ ' +                  // Katakana small
             'ﾏ ﾐ ﾑ ﾒ ﾓ',             // Katakana half width
          // ya yu yo                ('yi' and 'ye' do not exist)
             'や ゆ よ ' +            // Hiragana
@@ -75,6 +79,7 @@ define('l10n/ja_JP/io.ox/collation', function () {
          // ra ri ru re ro
             'ら り る れ ろ ' +      // Hiragana
             'ラ リ ル レ ロ ' +      // Katakana
+            'ㇻ ㇼ ㇽ ㇾ ㇿ' +       // Katakana small
             'ﾗ ﾘ ﾙ ﾚ ﾛ',            // Katakana half width
          // wa (wi)(we)wo n        ('wi' and 'we' are nearly obsolete. 'wu' does not exist. 'n' is an additional kana)
             'わ ゐ ゑ を ん ' +      // Hiragana
