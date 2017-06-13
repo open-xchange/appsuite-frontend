517099e9dc19d70cf9daf5f09f992429f9691825
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2017-06-13 10:45:07 +0200

    Fixed Bug 53688 - [L3] Contacts with Katakana "yomi" fields are sorted and categorized as "other"

--- ui/apps/l10n/ja_JP/io.ox/collation.js
+++ ui/apps/l10n/ja_JP/io.ox/collation.js
@@ -30,45 +30,45 @@ define('l10n/ja_JP/io.ox/collation', function () {
             'ヴ',                   // Katakana with dakuten uncommon (vu)
           // ka ki ku ke ko
             'か き く け こ ' +      // Hiragana
-            'ゕ ゖ ' +              // Hiragana small
+            'ゕ ゖ ' +              // Hiragana small (ka ke)
             'が ぎ ぐ げ ご ' +      // Hiragana with dakuten (ga gi gu ge go)
             'カ キ ク ケ コ ' +      // Katakana
-            'ヵ ㇰ ヶ ' +           // Katakana small
+            'ヵ ㇰ ヶ ' +           // Katakana small (ka ku ke)
             'ｶ ｷ ｸ ｹ ｺ ' +          // Katakana half width
             'ガ ギ グ ゲ ゴ',       // Katakana with dakuten (ga gi gu ge go)
          // sa shi su se so
             'さ し す せ そ ' +      // Hiragana
             'ざ じ ず ぜ ぞ ' +      // Hiragana with dakuten (za ji zu ze zo)
             'サ シ ス セ ソ ' +      // Katakana
-            'ㇱ ㇲ' +               // Katakana small
+            'ㇱ ㇲ ' +              // Katakana small (shi su)
             'ｻ ｼ ｽ ｾ ｿ ' +          // Katakana half width
             'ザ ジ ズ ゼ ゾ',        // Katakana with dakuten (za ji zu ze zo)
         // ta chi tsu te to
             'た ち つ て と ' +      // Hiragana
-            'っ ' +                 // Hiragana small
+            'っ ' +                 // Hiragana small (tsu)
             'だ ぢ づ で ど ' +      // Hiragana with dakuten (da ji zu de do)
             'タ チ ツ テ ト ' +      // Katakana
-            'ｯ ㇳ' +                 // Katakana small
-            'ﾀ ﾁ ﾂ ﾃ ﾄ ' +           // Katakana half width
-            'ダ ヂ ヅ デ ド っ ッ',   // Katakana with dakuten (da ji zu de do)
+            'ｯ ㇳ ' +               // Katakana small (tsu to)
+            'ﾀ ﾁ ﾂ ﾃ ﾄ ' +          // Katakana half width
+            'ダ ヂ ヅ デ ド ッ',     // Katakana with dakuten (da ji zu de do)
          // na ni nu ne no
             'な に ぬ ね の ' +      // Hiragana
             'ナ ニ ヌ ネ ノ ' +      // Katakana
-            'ㇴ ' +                 // Katakana small
+            'ㇴ ' +                 // Katakana small (nu)
             'ﾅ ﾆ ﾇ ﾈ ﾉ',            // Katakana half width
          // ha hi fu he ho
             'は ひ ふ へ ほ ' +      // Hiragana
             'ば び ぶ ぺ ぼ ' +      // Hiragana with dakuten (ba bi bu be bo)
             'ぱ ぴ ぷ ぺ ぽ ' +      // Hiragana with handakuten (pa pi pu pe po)
             'ハ ヒ フ ヘ ホ ' +      // Katakana
-            'ㇵ ㇶ ㇷ ㇸ ㇹ' +       // Katakana small
+            'ㇵ ㇶ ㇷ ㇸ ㇹ ' +      // Katakana small
             'ﾊ ﾋ ﾌ ﾍ ﾎ ' +           // Katakana half width
             'バ ビ ブ ベ ボ ' +      // Katakana with dakuten (ba bi bu be bo)
             'パ ピ プ ペ ポ',        // Katakana with handakuten (pa pi pu pe po)
          // ma mi mu me mo
             'ま み む め も ' +      // Hiragana
             'マ ミ ム メ モ ' +      // Katakana
-            'ㇺ ' +                  // Katakana small
+            'ㇺ ' +                  // Katakana small (mu)
             'ﾏ ﾐ ﾑ ﾒ ﾓ',             // Katakana half width
          // ya yu yo                ('yi' and 'ye' do not exist)
             'や ゆ よ ' +            // Hiragana
@@ -79,14 +79,14 @@ define('l10n/ja_JP/io.ox/collation', function () {
          // ra ri ru re ro
             'ら り る れ ろ ' +      // Hiragana
             'ラ リ ル レ ロ ' +      // Katakana
-            'ㇻ ㇼ ㇽ ㇾ ㇿ' +       // Katakana small
+            'ㇻ ㇼ ㇽ ㇾ ㇿ ' +      // Katakana small
             'ﾗ ﾘ ﾙ ﾚ ﾛ',            // Katakana half width
          // wa (wi)(we)wo n        ('wi' and 'we' are nearly obsolete. 'wu' does not exist. 'n' is an additional kana)
             'わ ゐ ゑ を ん ' +      // Hiragana
-            'ゎ ' +                 // Hiragana small
-            'ワ ヰ ヱ ヲ ン ' +     // Katakana
-            'ﾜ ｦ ﾝ ' +             // Katakana half width
-            'ヮ ' +                // Katakana small
+            'ゎ ' +                 // Hiragana small (wa)
+            'ワ ヰ ヱ ヲ ン ' +      // Katakana
+            'ﾜ ｦ ﾝ ' +              // Katakana half width (wa wo n)
+            'ヮ ' +                // Katakana small (wa)
             'ヷ ヸ ヹ ヺ'           // Katakana with dakuten uncommon (va vi ve vo ). vu is written as ヴ uncommon as well
         ],
         hash = {},
