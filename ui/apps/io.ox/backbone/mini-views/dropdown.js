a8b2bc8295cb760c885b558408d30e24fe42a9a5
Mario Schröder <mario.schroeder@open-xchange.com>
2017-06-14 14:50:51 +0200

    Fix for bug #54320# XSS for remote presentations through user name.

--- ui/apps/io.ox/backbone/mini-views/dropdown.js
+++ ui/apps/io.ox/backbone/mini-views/dropdown.js
@@ -269,7 +269,8 @@ define('io.ox/backbone/mini-views/dropdown', ['io.ox/backbone/mini-views/abstrac
                 .on('dragstart', false)
                 .append(
                     options.icon ? $('<i class="fa fa-fw" aria-hidden="true">') : $(),
-                    text
+                    // bug #54320
+                    $('<span>').text(text)
                 );
             if (options.data) link.data(options.data);
             if (callback) link.on('click', {}, callback);
