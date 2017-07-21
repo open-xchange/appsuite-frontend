b05446682d55d7887053ac3f94090bbc2c136571
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2017-07-22 00:17:45 +0200

    New search: Added limit

--- ui/apps/io.ox/mail/view-options.js
+++ ui/apps/io.ox/mail/view-options.js
@@ -40,6 +40,7 @@ define('io.ox/mail/view-options', [
 
             var collectionLoader = new CollectionLoader({
                 module: 'mail',
+                mode: 'search',
                 getQueryParams: function (params) {
 
                     var criteria = params.criteria, filters = [];
@@ -47,7 +48,7 @@ define('io.ox/mail/view-options', [
                     // The available field names are
                     // from, to, cc, bcc, subject, received_date, sent_date, size,flags, content, content_type, disp and priority
                     if (criteria.from) filters.push(['=', { field: 'from' }, criteria.from]);
-                    if (criteria.to) filters.push(['=', { field: 'to' }, criteria.to]);
+                    if (criteria.to) filters.push(['or', ['=', { field: 'to' }, criteria.to], ['=', { field: 'cc' }, criteria.to], ['=', { field: 'bcc' }, criteria.to]]);
                     if (criteria.subject) filters.push(['=', { field: 'subject' }, criteria.subject]);
                     if (criteria.words) filters.push(['=', { field: 'content' }, criteria.words]);
                     if (criteria.attachment === 'true') filters.push(['=', { field: 'content_type' }, 'multipart/mixed']);
@@ -57,23 +58,24 @@ define('io.ox/mail/view-options', [
                     return {
                         action: 'search',
                         folder: params.folder,
+                        columns: '102,600,601,602,603,604,605,606,607,608,610,611,614,652,656,X-Open-Xchange-Share-URL',
+                        sort: params.sort || '610',
+                        order: params.order || 'desc',
+                        timezone: 'utc',
                         data: { filter: ['and'].concat(filters) }
                     };
                 },
                 fetch: function (params) {
-                    return http.PUT({
-                        module: this.module,
-                        params: {
-                            action: params.action,
-                            folder: params.folder,
-                            columns: '102,600,601,602,603,604,605,606,607,608,610,611,614,652,656,X-Open-Xchange-Share-URL',
-                            sort: params.sort || '610',
-                            order: params.order || 'desc',
-                            timezone: 'utc'
-                        },
-                        data: params.data
+                    return http.wait().then(function () {
+                        return http.PUT({
+                            module: 'mail',
+                            params: _(params).omit('data'),
+                            data: params.data
+                        });
                     });
-                }
+                },
+                PRIMARY_PAGE_SIZE: 100,
+                SECONDARY_PAGE_SIZE: 100
             });
 
             this.append(
@@ -81,10 +83,12 @@ define('io.ox/mail/view-options', [
                 .on('search', function (criteria) {
                     listView.connect(collectionLoader);
                     listView.model.set({ criteria: criteria, thread: false, sort: 610, order: 'desc' });
+                    listView.$el.parent().find('.grid-options [data-name="thread"]').addClass('disabled');
                 })
                 .on('cancel', function () {
                     listView.connect(mailAPI.collectionLoader);
                     listView.model.unset('criteria');
+                    listView.$el.parent().find('.grid-options [data-name="thread"]').removeClass('disabled');
                 })
                 .render().$el
             );
