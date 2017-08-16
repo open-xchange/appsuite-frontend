3623dfcf27fb1b5531507897a0234a03283cc121
Julian Bäume <julian.baeume@open-xchange.com>
2017-08-16 19:04:17 +0200

    fix tests
    seems some naming has changed a little.

--- ui/spec/io.ox/settings/sessions/settings/pane_spec.js
+++ ui/spec/io.ox/settings/sessions/settings/pane_spec.js
@@ -25,7 +25,7 @@ define([
                     client: 'open-xchange-appsuite',
                     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
                 });
-                model.get('displayName').should.equal(gt('Chrome %1$s on %2$s', 58, 'MacOS'));
+                model.get('displayName').should.equal(gt('Chrome %1$s on %2$s', 58, 'macOS'));
                 model.get('platform').should.equal('desktop');
             });
 
@@ -43,7 +43,7 @@ define([
                     client: 'open-xchange-mobile-api-facade',
                     userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
                 });
-                model.get('displayName').should.equal(gt('%1$s on %2$s', settings.get('productname/mailapp'), 'iOS'));
+                model.get('displayName').should.equal(gt('%1$s for %2$s', settings.get('productname/mailapp'), 'iOS'));
                 model.get('platform').should.equal('smartphone');
             });
 
