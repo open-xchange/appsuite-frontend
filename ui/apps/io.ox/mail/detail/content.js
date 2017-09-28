ede45012de7f3805fed7488dd397bf7671d681d6
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2017-09-28 14:22:44 +0200

    Fixed: Bug 55398 - Text in angle brackets gets lost when forwarding emails
    (cherry picked from commit 67d37b3e31f5d2267aa133ee1f8330efc2612ca7)

--- ui/apps/io.ox/mail/detail/content.js
+++ ui/apps/io.ox/mail/detail/content.js
@@ -726,30 +726,6 @@ define('io.ox/mail/detail/content', [
             return { content: content, isLarge: baton.isLarge, type: baton.type, processedEmoji: baton.processedEmoji };
         },
 
-        // convert pseudo-plain-text to real plain text
-        adjustPlainText: (function () {
-
-            var div;
-
-            return function (str) {
-
-                return String(str || '')
-                    // convert <br> into newline
-                    .replace(/<br\s?\/?>/gi, '\n')
-                    // simply remove tags
-                    .replace(/<[a-z\/].*?>/gi, '')
-                    // replace entities
-                    .replace(/&.*?;/g, decodeEntity);
-            };
-
-            function decodeEntity(str) {
-                div = div || document.createElement('div');
-                div.innerHTML = str;
-                return div.textContent;
-            }
-
-        }()),
-
         beautifyPlainText: function (str) {
             var baton = ext.Baton({ data: str });
             ext.point('io.ox/mail/detail/beautify').invoke('process', this, baton);
@@ -757,8 +733,7 @@ define('io.ox/mail/detail/content', [
         },
 
         transformForHTMLEditor: function (str) {
-            var plain = this.adjustPlainText(str);
-            return this.text2html(plain, { blockquotes: true, links: true, lists: true, rulers: true });
+            return this.text2html(str, { blockquotes: true, links: true, lists: true, rulers: true });
         },
 
         // convert plain text to html
