8325098ce1f5c99f3bdc0a468cc6aea85d48a964
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2017-10-05 16:50:38 +0200

    Additional commit for Bug 55409

--- ui/apps/l10n/ja_JP/io.ox/collation.js
+++ ui/apps/l10n/ja_JP/io.ox/collation.js
@@ -20,73 +20,75 @@ define('l10n/ja_JP/io.ox/collation', function () {
     var dakuten = 'ﾞ ゙ ゛'.split(' '),
         handakuten = 'ﾟ ﾟ ゜'.split(' '),
         // only katakana that can have dakuten or handakuten
-        halfWidthKana = 'ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿ ﾀ ﾁ ﾂ ｯ ﾃ ﾄ ﾊ ﾋ ﾌ ﾍ ﾎ ﾜ'.split(' ');
+        halfWidthKana = 'ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿ ﾀ ﾁ ﾂ ｯ ﾃ ﾄ ﾊ ﾋ ﾌ ﾍ ﾎ ﾜ ｦ'.split(' ');
 
-    // raw alphabet data
+    // raw alphabet data ( super fragile stuff, don't change order etc )
     // see http://en.wikipedia.org/wiki/Goj%C5%ABon
     // or http://en.wikipedia.org/wiki/Japanese_writing_system#Collation
     // and for half width kana https://en.wikipedia.org/wiki/Half-width_kana
     var tableau = [
             // Hiragana, Hiragana small, Katakana, Katakana half-width, , Katakana small, Hiragana with dakuten, Katakana with dakuten, Hiragana with handakuten, Katakana with handakuten
-            'あ ぁ ア ｱ ァ ' + // a
-            'い ぃ イ ｲ ィ ' + // i
-            'う ぅ ウ ｳ ゥ ゔ ヴ ｳﾞ ' + // u, vu
-            'え ぇ エ ｴ ェ ' +  // e
-            'お ぉ オ ｵ ォ',  // o
-
-            'か ゕ カ ｶ ヵ が ガ ｶﾞ ' + // ka, ga
-            'き キ ｷ ぎ ギ ｷﾞ ' + // ki, gi
-            'く ク ｸ ㇰ ぐ グ ㇰﾞ ' + // ku, gu
-            'け ゖ ケ ｹ ヶ げ ゲ ｹﾞ ' + // ke, ge
-            'こ コ ｺ ご ゴ ｺﾞ',      // ko, go
-
-            'さ サ ｻ ざ ザ ｻﾞ ' + // sa, za
-            'し シ ｼ ㇱ じ ジ ｼﾞ ' + // shi, ji
-            'す ス ｽ ㇲ ず ズ ｽﾞ ' +// su ,zu
-            'せ セ ｾ ぜ ゼ ｾﾞ ' + // se, ze
-            'そ ソ ｿ ぞ ゾ ｿﾞ',  // so, zo
-
-            'た タ ﾀ だ ダ ﾀﾞ ' + // ta, da
-            'ち チ ﾀ ぢ ヂ ﾀﾞ ' + // chi, ji
-            'つ っ ツ ﾂ ッ ｯ づ ヅ ﾂﾞ ｯﾞ ' + // tsu, zu ( note: there is a small half-width katakana character for tsu)
-            'て テ ﾃ で デ ﾃﾞ ' + // te, de
-            'と ト ﾄ ㇳ ど ド ﾄﾞ', // to, do
-
-            'な ナ ﾅ ' + // na
-            'に ニ ﾆ ' + // ni
-            'ぬ ヌ ﾇ ㇴ ' + // nu
-            'ね ネ ﾈ ' + // ne
-            'の ノ ﾉ', // no
-
-            'は ハ ﾊ ㇵ ば バ ﾊﾞ ぱ パ ﾊﾟ ' + // ha, ba, pa
-            'ひ ヒ ﾋ ㇶ び ビ ﾋﾞ ぴ ピ ﾋﾟ ' + // hi, bi, pi
-            'ふ フ ﾌ ㇷ ぶ ブ ﾌﾞ ぷ プ ﾌﾟ ' + // fu , bu, pi
-            'へ ヘ ﾍ ㇸ べ ベ ﾍﾞ ぺ ペ ﾍﾟ ' + // he, be, pe
-            'ほ ホ ﾎ ㇹ ぼ ボ ﾎﾞ ぽ ポ ﾎﾟ',  // ho, bo, po
-
-            'ま マ ﾏ ' + // ma
-            'み ミ ﾐ ' + // mi
-            'む ム ﾑ ㇺ ' + // mu
-            'め メ ﾒ ' + // me
-            'も モ ﾓ',   // mo
-
-            // ('yi' and 'ye' do not exist)
-            'や ゃ ヤ ﾔ ャ ' + // ya
-            'ゆ ゅ ユ ﾕ ュ ' + // yu
-            'よ ょ ヨ ﾖ ョ', // yo
-
-            'ら ラ ﾗ ㇻ ' + // ra
-            'り リ ﾘ ㇼ ' + // ri
-            'る ル ﾙ ㇽ ' + // ru
-            'れ レ ﾚ ㇾ ' + // re
-            'ろ ロ ﾛ ㇿ', // ro
-
-            // ('wi' and 'we' are nearly obsolete. 'wu' does not exist. 'n' is an additional kana)
-            'わ ゎ ワ ﾜ ヮ ヷ  ﾜﾞ' + // wa, va
-            'ゐ ヰ ヸ ' + // wi
-            'ゑ ヱ ヹ ' + // we
-            'を ヲ ｦ ヺ ' + // wo
-            'ん ン ﾝ' // n
+            [
+                'あ ぁ ア ｱ ァ', // a
+                'い ぃ イ ｲ ィ', // i
+                'う ぅ ウ ｳ ゥ', 'ゔ ヴ ｳﾞ', // u, vu
+                'え ぇ エ ｴ ェ',  // e
+                'お ぉ オ ｵ ォ' // o
+            ], [
+                'か ゕ カ ｶ ヵ', 'が ガ ｶﾞ', // ka, ga
+                'き キ ｷ', 'ぎ ギ ｷﾞ', // ki, gi
+                'く ク ｸ ㇰ', 'ぐ グ ㇰﾞ', // ku, gu
+                'け ゖ ケ ｹ ヶ', 'げ ゲ ｹﾞ', // ke, ge
+                'こ コ ｺ', 'ご ゴ ｺﾞ'      // ko, go
+            ], [
+                'さ サ ｻ', 'ざ ザ ｻﾞ', // sa, za
+                'し シ ｼ ㇱ', 'じ ジ ｼﾞ', // shi, ji
+                'す ス ｽ ㇲ', 'ず ズ ｽﾞ', // su ,zu
+                'せ セ ｾ', 'ぜ ゼ ｾﾞ', // se, ze
+                'そ ソ ｿ', 'ぞ ゾ ｿﾞ'  // so, zo
+            ], [
+                'た タ ﾀ', 'だ ダ ﾀﾞ', // ta, da
+                'ち チ ﾀ', 'ぢ ヂ ﾀﾞ', // chi, ji
+                'つ っ ツ ﾂ ッ ｯ', 'づ ヅ ﾂﾞ ｯﾞ', // tsu, zu ( note: there is a small half-width katakana character for tsu)
+                'て テ ﾃ', 'で デ ﾃﾞ', // te, de
+                'と ト ﾄ ㇳ', 'ど ド ﾄﾞ' // to, do
+            ], [
+                'な ナ ﾅ', // na
+                'に ニ ﾆ', // ni
+                'ぬ ヌ ﾇ ㇴ', // nu
+                'ね ネ ﾈ', // ne
+                'の ノ ﾉ' // no
+            ], [
+                'は ハ ﾊ ㇵ', 'ば バ ﾊﾞ', 'ぱ パ ﾊﾟ', // ha, ba, pa
+                'ひ ヒ ﾋ ㇶ', 'び ビ ﾋﾞ', 'ぴ ピ ﾋﾟ', // hi, bi, pi
+                'ふ フ ﾌ ㇷ', 'ぶ ブ ﾌﾞ', 'ぷ プ ﾌﾟ', // fu , bu, pi
+                'へ ヘ ﾍ ㇸ', 'べ ベ ﾍﾞ', 'ぺ ペ ﾍﾟ', // he, be, pe
+                'ほ ホ ﾎ ㇹ', 'ぼ ボ ﾎﾞ', 'ぽ ポ ﾎﾟ'  // ho, bo, po
+            ], [
+                'ま マ ﾏ', // ma
+                'み ミ ﾐ', // mi
+                'む ム ﾑ ㇺ', // mu
+                'め メ ﾒ', // me
+                'も モ ﾓ'   // mo
+            ], [
+                // ('yi' and 'ye' do not exist)
+                'や ゃ ヤ ﾔ ャ', // ya
+                'ゆ ゅ ユ ﾕ ュ', // yu
+                'よ ょ ヨ ﾖ ョ' // yo
+            ], [
+                'ら ラ ﾗ ㇻ', // ra
+                'り リ ﾘ ㇼ', // ri
+                'る ル ﾙ ㇽ', // ru
+                'れ レ ﾚ ㇾ', // re
+                'ろ ロ ﾛ ㇿ' // ro
+            ], [
+                // ('wi' and 'we' are nearly obsolete. 'wu' does not exist. 'n' is an additional kana)
+                'わ ゎ ワ ﾜ ヮ', 'ヷ ﾜﾞ', // wa, va
+                'ゐ ヰ', 'ヸ', // wi
+                'ゑ ヱ', 'ヹ', // we
+                'を ヲ ｦ', 'ヺ', // wo
+                'ん ン ﾝ' // n
+            ]
         ],
         hash = {},
         label = {}, // first column of all rows
@@ -97,10 +99,14 @@ define('l10n/ja_JP/io.ox/collation', function () {
 
     // construct some meta data we need for sorting
     _(tableau).each(function (row) {
-        index.push(row[0]);
-        _(row.split(' ')).each(function (char) {
-            hash[char] = position++;
-            label[char] = row[0];
+        index.push(row[0][0]);
+        // kana with same sound have the same sort position
+        _(row).each(function (sound) {
+            _(sound.split(' ')).each(function (char) {
+                hash[char] = position;
+                label[char] = row[0][0];
+            });
+            position++;
         });
     });
 
@@ -140,8 +146,8 @@ define('l10n/ja_JP/io.ox/collation', function () {
             a = isHalfWidthWithDakutenOrHandakuten(a, aSortName);
             b = isHalfWidthWithDakutenOrHandakuten(b, bSortName);
 
-            // same kana? check the next
-            if (a === b) {
+            // same kana sound? check the next
+            if (hash[a] === hash[b]) {
                 a = { sort_name: aSortName.slice(tempA === a ? 1 : 2) };
                 b = { sort_name: bSortName.slice(tempB === b ? 1 : 2) };
                 return sorter(a, b);
