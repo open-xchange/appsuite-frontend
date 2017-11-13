712cfd308cd17c3d1b32012bbc739904f1477163
Richard Petersen <richard.petersen@open-xchange.com>
2017-11-13 10:43:48 +0100

    OXUI-484: Calender only folder columns
    Add caldav url column.

--- ui/apps/io.ox/core/http.js
+++ ui/apps/io.ox/core/http.js
@@ -251,7 +251,8 @@ define('io.ox/core/http', ['io.ox/core/event'], function (Events) {
                 // 3040 exists; around EAS; no need for it
                 '3050': 'com.openexchange.imap.extAccount',
                 '3060': 'com.openexchange.share.extendedPermissions',
-                '3201': 'com.openexchange.calendar.extendedProperties'
+                '3201': 'com.openexchange.calendar.extendedProperties',
+                '3220': 'com.openexchange.caldav.url'
             },
             'user': {
                 '610': 'aliases',
