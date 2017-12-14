open-xchange/appsuite-frontend

commit 10be2627ba0af03f67a2ea75fcd878e1b668de30

#docs-950# adding performance logging for the viewer

ui/apps/io.ox/core/viewer/util.js
@@ -10,10 +10,19 @@
  * @author Mario Schroeder <mario.schroeder@open-xchange.com>
  */
 define('io.ox/core/viewer/util', [
+    'io.ox/core/http',
+    'settings!io.ox/office',
     'gettext!io.ox/core/viewer'
-], function (gt) {
+], function (http, settings, gt) {
 
     'use strict';
+
+    var // whether the loading performance of the viewer shall be measured
+        logPerformance = settings.get('module/logPerformanceData', false),
+        // a global timer that can be used for performance debugging
+        performanceTimer = null,
+        // object for logging several time stamps
+        performanceLogger = logPerformance ? {} : null;
 
     var Util = {};
 
@@ -215,6 +224,80 @@ define('io.ox/core/viewer/util', [
     Util.minMax = function (value, min, max) {
         return Math.min(Math.max(value, min), max);
     };
+
+    // debugging --------------------------------------------------------------
+
+    /**
+     * Starting the performance timer.
+     */
+    Util.startPerformanceTimer = logPerformance ? function () {
+        performanceTimer = _.now();
+    } : $.noop;
+
+    /**
+     * Logging info with the performance timer.
+     *
+     * @param {String} key
+     *  A message that is saved at the performance timer.
+     */
+    Util.logPerformanceTimer = logPerformance ? function (key) {
+        if (performanceTimer) { Util.addToPerformanceLogger(key, (_.now() - performanceTimer)); }
+    } : $.noop;
+
+    /**
+     * Adding one key-value pair to the performance logger object.
+     *
+     * @param {String} key
+     *  The key for the performance logger object.
+     *
+     * @param {any} value
+     *  The value for the performance logger object.
+     */
+    Util.addToPerformanceLogger = logPerformance ? function (key, value) {
+        if (performanceTimer) { performanceLogger[key] = value; }
+    } : $.noop;
+
+    /**
+     * Adding some default information about browser, appsuite version, ...
+     * to the performance data.
+     *
+     * @param {Object} [data]
+     *  The data object given to the launcher of the OX Viewer.
+     */
+    Util.addDefaultPerformanceInfo = logPerformance ? function (data) {
+        Util.addToPerformanceLogger('user-agent', navigator.userAgent);
+        Util.addToPerformanceLogger('platform', navigator.platform);
+        Util.addToPerformanceLogger('user', ox.user);
+        Util.addToPerformanceLogger('version', ox.version);
+        Util.addToPerformanceLogger('server', ox.abs);
+        Util.addToPerformanceLogger('application', 'viewer');
+        Util.addToPerformanceLogger('filename', (data && data.selection && data.selection.filename) ? data.selection.filename : '');
+    } : $.noop;
+
+    /**
+     * Saving the collected performance data and sending them to the server.
+     *
+     * @param {Object} [data]
+     *  The data object given to the launcher of the OX Viewer.
+     *
+     * @returns {jQuery.Promise}
+     *  The promise from the http request. If no request is made, the promise
+     *  is already resolved.
+     */
+    Util.savePerformanceTimer = logPerformance ? function (data) {
+
+        var httpPromise = null;
+
+        if (performanceTimer && performanceLogger) {
+            // adding default information to the performance logger object
+            Util.addDefaultPerformanceInfo(data);
+            // sending the http post request to save data
+            httpPromise = http.POST({ module: 'oxodocumentfilter', params: { action: 'logperformancedata', performanceData: JSON.stringify(performanceLogger) } });
+        }
+
+        return httpPromise ? httpPromise : $.when();
+
+    } : $.noop;
 
     return Util;
 });

ui/apps/io.ox/core/tk/doc-utils/pageloader.js
@@ -295,8 +295,10 @@ define('io.ox/core/tk/doc-utils/pageloader', [
          *  The new zoom factor, as floating point number (the value 1
          *  represents the original page size).
          *
-         * @returns {PageLoader}
-         *  A reference to this instance.
+         * @returns {jquery.Promise}
+         *  The promise of the rendering function, that is resolved, when
+         *  rendering is finshed. If no rendering is required, the promise
+         *  is resolved immediately.
          */
         this.setPageZoom = function (pageNode, pageZoom) {
             var // the page number
@@ -310,18 +312,20 @@ define('io.ox/core/tk/doc-utils/pageloader', [
                 // the resulting width/height
                 width = Math.ceil(pageSize.width * pageZoom),
                 height = Math.ceil(pageSize.height * pageZoom),
-                newPageSize = { width: width, height: height };
+                newPageSize = { width: width, height: height },
+                // pdf render proimse
+                renderPromise = null;
 
             jqPageNode.data('page-zoom', pageZoom).attr(newPageSize).css(newPageSize);
 
             if (renderType === 'pdf') {
                 // <canvas> element: render page into canvas and create text overlay
-                pdfView.renderPDFPage(jqPageNode, pageNumber, pageZoom).then(function () {
+                renderPromise = pdfView.renderPDFPage(jqPageNode, pageNumber, pageZoom).then(function () {
                     jqPageNode.data('page-zoom', pageZoom).attr(newPageSize).css(ACTIVE_STYLE);
                 });
             }
 
-            return this;
+            return renderPromise ? renderPromise : $.when();
         };
 
         // initialization -----------------------------------------------------

ui/apps/io.ox/core/viewer/main.js
@@ -12,7 +12,10 @@
  * @author Edy Haryono <edy.haryono@open-xchange.com>
  */
 
-define('io.ox/core/viewer/main', ['io.ox/core/extensions'], function (ext) {
+define('io.ox/core/viewer/main', [
+    'io.ox/core/extensions',
+    'io.ox/core/viewer/util'
+], function (ext, Util) {
 
     'use strict';
 
@@ -45,6 +48,8 @@ define('io.ox/core/viewer/main', ['io.ox/core/extensions'], function (ext) {
          */
         this.launch = function (data) {
 
+            Util.startPerformanceTimer();
+
             if (!data) return console.error('Core.Viewer.main.launch(): no data supplied');
             if (!_.isArray(data.files) || data.files.length === 0) return console.error('Core.Viewer.main.launch(): no files to preview.');
             var self = this,
@@ -60,6 +65,9 @@ define('io.ox/core/viewer/main', ['io.ox/core/extensions'], function (ext) {
             }).attr('aria-hidden', true);
 
             function cont() {
+
+                Util.logPerformanceTimer('launchContStart');
+
                 // resolve dependencies now for an instant response
                 require(['io.ox/core/viewer/backbone', 'io.ox/core/viewer/views/mainview'], function (backbone, MainView) {
                     // create file collection and populate it with file models
@@ -79,6 +87,9 @@ define('io.ox/core/viewer/main', ['io.ox/core/extensions'], function (ext) {
 
             // Cleanup, remove hidden attributes
             function close() {
+
+                Util.savePerformanceTimer(data);
+
                 siblings.removeAttr('aria-hidden').each(function () {
                     var el = $(this);
                     if (el.data('ox-restore-aria-hidden')) el.attr('aria-hidden', el.data('ox-restore-aria-hidden'));

ui/apps/io.ox/core/viewer/views/mainview.js
@@ -42,6 +42,9 @@ define('io.ox/core/viewer/views/mainview', [
         },
 
         initialize: function (options) {
+
+            Util.logPerformanceTimer('MainView:initialize');
+
             _.extend(this, options);
             // apps with 'simple window' class (Portal) scrolls behind the viewer.
             // Hide it for the moment as a workaround.

ui/apps/io.ox/core/viewer/views/types/documentview.js
@@ -47,6 +47,9 @@ define('io.ox/core/viewer/views/types/documentview', [
     var DocumentView =  BaseView.extend({
 
         initialize: function (options) {
+
+            Util.logPerformanceTimer('DocumentView:initialize');
+
             _.extend(this, options);
             // amount of page side margins in pixels
             this.PAGE_SIDE_MARGIN = _.device('desktop') ? 20 : 10;
@@ -297,6 +300,8 @@ define('io.ox/core/viewer/views/types/documentview', [
         loadVisiblePages: function () {
             this.documentLoad.done(function () {
 
+                Util.logPerformanceTimer('DocumentView:loadVisiblePages');
+
                 var pagesToRender = this.getPagesToRender();
                 var beginPageNumber = _.first(pagesToRender);
                 var endPageNumber = _.last(pagesToRender);
@@ -391,15 +396,20 @@ define('io.ox/core/viewer/views/types/documentview', [
                 if (_.isNumber(pageNumberToRefresh)) {
                     // Process the page node
                     var pageNode = this.getPageNode(pageNumberToRefresh);
-                    this.pageLoader.setPageZoom(pageNode, this.currentZoomFactor / 100);
+                    Util.logPerformanceTimer('DocumentView:refresh_before_pdf_render_' + pageNumberToRefresh);
+                    this.pageLoader.setPageZoom(pageNode, this.currentZoomFactor / 100).done(function () {
+                        Util.logPerformanceTimer('DocumentView:refresh_' + pageNumberToRefresh);
+                    });
 
                 } else {
                     // empty all pages in case of a complete refresh request
                     this.pages.detach().each(function () {
                         self.emptyPageNode($(this));
-                        self.pageLoader.setPageZoom($(this), self.currentZoomFactor / 100);
-
-                    }).appendTo(self.documentContainer);
+                        Util.logPerformanceTimer('DocumentView:refresh_before_pdf_render_' + pageNumberToRefresh);
+                        self.pageLoader.setPageZoom($(this), self.currentZoomFactor / 100).done(function () {
+                            Util.logPerformanceTimer('DocumentView:refresh_' + pageNumberToRefresh);
+                        }).appendTo(self.documentContainer);
+                    });
                 }
 
                 if (_.isNumber(pageNumberToSelect)) {
@@ -575,6 +585,9 @@ define('io.ox/core/viewer/views/types/documentview', [
          *  the DocumentView instance.
          */
         show: function (options) {
+
+            Util.logPerformanceTimer('DocumentView:show');
+
             // ignore already loaded documents
             if (this.$el.find('.document-page').length > 0) {
                 // document pages are drawn, but there is no visible page rendered yet
@@ -599,6 +612,9 @@ define('io.ox/core/viewer/views/types/documentview', [
              *  page count of the pdf document delivered by the PDF.js library.
              */
             function pdfDocumentLoadSuccess(pageCount) {
+
+                Util.logPerformanceTimer('DocumentView:pdfDocumentLoadSuccess');
+
                 // do nothing and quit if a document is already disposed.
                 if (this.disposed || !this.pdfDocument) {
                     return;

commit d5bcba28b0a53849dbcd6b263256fa7b5591af0f

temporary fix of regression in 10be2627ba0af03f67a2ea75fcd878e1b668de30
- remove hard dependency

ui/apps/io.ox/core/viewer/util.js
@@ -11,14 +11,13 @@
  */
 define('io.ox/core/viewer/util', [
     'io.ox/core/http',
-    'settings!io.ox/office',
     'gettext!io.ox/core/viewer'
-], function (http, settings, gt) {
+], function (http, gt) {
 
     'use strict';
 
     var // whether the loading performance of the viewer shall be measured
-        logPerformance = settings.get('module/logPerformanceData', false),
+        logPerformance = false,
         // a global timer that can be used for performance debugging
         performanceTimer = null,
         // object for logging several time stamps