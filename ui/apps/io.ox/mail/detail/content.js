dbf3a6eaf75b68bdb7844ff256bf3d4bfb4c69eb
Alexander Quast <alexander.quast@open-xchange.com>
2017-12-15 13:40:33 +0100

    Fixed Bug 56524 - UI reloads within mail view

--- ui/apps/io.ox/mail/detail/content.js
+++ ui/apps/io.ox/mail/detail/content.js
@@ -75,6 +75,7 @@ define('io.ox/mail/detail/content', [
         },
 
         linkTarget: function (baton) {
+
             baton.source = baton.source
                 // add missing http protocol prefix
                 .replace(/<a[^>]*href=(?:"|')((?!https?:\/\/)[^'">]+)(?:"|')[^>]*>/g, addMissingProtocol)
@@ -206,7 +207,11 @@ define('io.ox/mail/detail/content', [
                     }
                 } else if (link.attr('href')) {
                     // other links
-                    link.attr('rel', 'noopener');
+                    link.attr({ 'rel': 'noopener', 'target': '_blank' });
+                } else if (!href) {
+                    // missing or broken href attribute
+                    // remove href as it points to nowhere
+                    link.removeAttr('href');
                 }
             });
         },
