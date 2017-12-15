b38f3b4a6805728d2029b020664dd614e1c32b43
Jonas Regier <jonas.regier@open-xchange.com>
2017-12-15 11:49:53 +0100

    revert 56490 temporary until the complete fix is applied to fix drive context menus in the meantime

--- ui/apps/io.ox/files/main.js
+++ ui/apps/io.ox/files/main.js
@@ -1501,8 +1501,6 @@ define('io.ox/files/main', [
         // go!
         return commons.addFolderSupport(app, null, 'infostore', options.folder)
             .always(function () {
-                app.mediate();
-
                 // contextmenu for all listviews
                 var def = $.Deferred();
                 require(['io.ox/files/contextmenu'], function (Contextmenu) {
@@ -1512,6 +1510,7 @@ define('io.ox/files/main', [
                     def.resolve();
                 });
                 def.done(function () {
+                    app.mediate();
                     win.show(function () {
                         // trigger grid resize
                         $(window).trigger('resize');
