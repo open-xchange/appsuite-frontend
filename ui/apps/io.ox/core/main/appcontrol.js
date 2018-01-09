3ee330e384c300935b680c7bdbd0ab10e0d97e13
Alexander Quast <alexander.quast@open-xchange.com>
2018-01-09 11:15:00 +0100

    Overhauled icons new styles for topbar minor changes

--- ui/apps/io.ox/core/main/appcontrol.js
+++ ui/apps/io.ox/core/main/appcontrol.js
@@ -24,7 +24,7 @@ define('io.ox/core/main/appcontrol', [
         svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-mail"><g class="stroke-w1 stroke-c1" stroke-width="3" fill="none" fill-rule="evenodd"><path d="M8 16h48v32H8z"/><path d="M8 20l24 16 24-16"/></g></svg>'
     }, {
         name: 'Calendar',
-        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-calendar"><g fill="none" fill-rule="evenodd"><g font-family="HelveticaNeue, Helvetica Neue" transform="translate(5 0)"><text class="fill-c1" font-size="18"><tspan x="21.992" y="45">31</tspan></text><text class="fill-c1" font-size="11" text-anchor="middle"><tspan x="27.27" y="24">Thu</tspan></text></g><path class="stroke-c1 stroke-w1" stroke-width="3" d="M8 11v42h48V11z"/></g></svg>'
+        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-calendar"><g fill="none" fill-rule="evenodd"><g font-family="HelveticaNeue, Helvetica Neue" transform="translate(5 0)"><text class="fill-c1" font-size="20" font-weight="bold"><tspan x="21.992" y="45">31</tspan></text><text class="fill-c1" font-size="11" text-anchor="middle"><tspan x="27.27" y="24">Thu</tspan></text></g><path class="stroke-c1 stroke-w1" stroke-width="3" d="M8 11v42h48V11z"/></g></svg>'
     }, {
         name: 'Addressbook',
         svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-addressbook"><g class="stroke-c1 stroke-w1" transform="translate(16 13)" stroke-width="3" fill="none" fill-rule="evenodd"><circle cx="16" cy="8" r="8"/><path d="M0 34c.488-6.369 4.627-11.705 10.308-13.88A8.864 8.864 0 0 0 16 22.156c2.18 0 4.172-.768 5.692-2.036C27.373 22.295 31.512 27.63 32 34H0z"/></g></svg>'
@@ -33,19 +33,19 @@ define('io.ox/core/main/appcontrol', [
         svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-drive"><path class="stroke-c1 stroke-w1" d="M17.041 46h30.973c1.556 0 8.537-2.642 7.951-10.25-.39-5.074-3.56-7.928-9.507-8.563-.906-4.427-3.472-5.89-7.696-4.388-.628.223-4.246-5.982-12.065-4.597-8.508 1.507-9.937 8.638-10.395 8.985-.437.332-9.099.91-8.242 10.558C8.646 44.348 14.538 46 17.04 46z" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>'
     }, {
         name: 'Portal',
-        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-portal"><g class="stroke-c1 stroke-w1" stroke-width="3" fill="none" fill-rule="evenodd"><path d="M9 17h16v12H9zM31 17h24v12H31zM55 47H39V35h16zM33 47H9V35h24z"/></g></svg>'
+        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-portal"><g class="stroke-c1 stroke-w1" stroke-width="3" fill="none" fill-rule="evenodd"><path d="M13 17h16v12H13zM51 47H35V35h16zM51 29H35V17h16zM29 47H13V35h16z"/></g></svg>'
     }, {
         name: 'Tasks',
         svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-tasks"><g class="stroke-c1 stroke-w1" stroke-width="3" fill="none" fill-rule="evenodd"><path d="M47 30v18H16V16h28"/><g stroke-linecap="square"><path d="M23.166 28.397l8.248 8.248M31.385 36.615l18.85-18.85"/></g></g></svg>'
     }, {
         name: 'Text',
-        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-text"><g fill="none" fill-rule="evenodd"><g class="fill-c1"><path d="M21 38h22v3H21zM21 31h18v3H21zM21 24h22v3H21z"/></g><g class="stroke-c1 stroke-w1" stroke-width="3"><path d="M38.93 11.5H17.5v41h29V19.07l-7.57-7.57z"/><path d="M37 11.833v7.334M45 19.167h-8" stroke-linecap="square"/></g></g></svg>'
+        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-text"><g fill="none" fill-rule="evenodd"><g class="stroke-c1 stroke-w1" stroke-width="3"><path d="M40.929 10L48 17.071V54H16V10h24.929z"/><path d="M40 11v7M48 18h-8" stroke-linecap="square"/></g><path d="M22 25h18M22 30h15M22 35h18M22 40h18M22 45h12" class="stroke-c1 stroke-w1" stroke-width="2" stroke-linecap="square"/></g></svg>'
     }, {
         name: 'Spreadsheet',
-        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-spreadsheet"><g class="stroke-c1 stroke-w1" fill="none" fill-rule="evenodd"><path class="stroke-c1 stroke-w1" stroke-width="3" d="M13 15h38v34H13z"/><path d="M13.5 24h37M13.5 32h37M13.5 40h37M22 15v34M32 15v34M42 15v34" stroke-linecap="square"/></g></svg>'
+        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-spreadsheet"><g fill="none" fill-rule="evenodd"><path fill-opacity=".3" class="fill-c1" d="M13 15h38v9H13z"/><path class="stroke-c1 stroke-w1" stroke-width="3" d="M13 15h38v34H13z"/><path d="M13.5 24h37M13.5 32h37M13.5 40h37M22 15v34M32 15v34M42 15v34" class="stroke-c1 stroke-w1" stroke-linecap="square"/></g></svg>'
     }, {
         name: 'Presentation',
-        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-presentation"><g fill="none" fill-rule="evenodd"><path d="M10 15h44v34H10z" class="stroke-c1 stroke-w1" stroke-width="3"/><g class="fill-c1"><path d="M32 23v9.004h9a9.001 9.001 0 1 1-18-.002A9.001 9.001 0 0 1 32 23z"/><path d="M34 21.014c.165-.01.332-.014.5-.014 4.694 0 8.5 3.582 8.5 8 0 .339-.022.672-.066 1H34v-8.986z"/></g></g></svg>'
+        svg: '<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" class="app-icon-presentation"><g fill="none" fill-rule="evenodd"><path d="M10 15h44v34H10z" class="stroke-c1 stroke-w1" stroke-width="3"/><g class="fill-c1"><path d="M32 23v9.004h9a9.001 9.001 0 1 1-18-.002A9.001 9.001 0 0 1 32 23z"/><path d="M34 21.014c.165-.01.332-.014.5-.014 4.694 0 8.5 3.582 8.5 8 0 .339-.022.672-.066 1H34v-8.986z" fill-opacity=".6"/></g></g></svg>'
     }];
 
     var quicklaunchSettings = 'Mail,Calendar';
@@ -66,9 +66,7 @@ define('io.ox/core/main/appcontrol', [
             $svg.find('tspan:last').text(moment().format('ddd'));
         }
         if (o.name === 'Mail') {
-            badge = $('<span class="badge" aria-hidden="true">').text(
-                '99+'
-            );
+            badge = $('<div class="indicator" aria-hidden="true">');
         }
         return $('<div class="lcell">').append(
             badge,
