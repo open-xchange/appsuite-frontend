d6f96bb65eefa82b55f1d8115ed8d1f58830601f
Richard Petersen <richard.petersen@open-xchange.com>
2018-01-25 13:53:22 +0100

    Check for series by flag

--- ui/apps/io.ox/calendar/util.js
+++ ui/apps/io.ox/calendar/util.js
@@ -1058,7 +1058,7 @@ define('io.ox/calendar/util', [
             };
             if (data.attributes['class'] === 'PRIVATE') icons.type.push($('<span class="private-flag">').append($('<i class="fa fa-user-circle" aria-hidden="true">'), $('<span class="sr-only">').text(gt('Private'))));
             if (data.attributes['class'] === 'CONFIDENTIAL') icons.type.push($('<span class="confidential-flag">').append($('<i class="fa fa-lock" aria-hidden="true">'), $('<span class="sr-only">').text(gt('Confidential'))));
-            if (data.attributes.recurrenceId) icons.property.push($('<span class="recurrence-flag">').append($('<i class="fa fa-refresh" aria-hidden="true">'), $('<span class="sr-only">').text(gt('Recurrence'))));
+            if (this.hasFlag(data, 'series')) icons.property.push($('<span class="recurrence-flag">').append($('<i class="fa fa-refresh" aria-hidden="true">'), $('<span class="sr-only">').text(gt('Recurrence'))));
             if (this.hasFlag(data, 'scheduled')) icons.property.push($('<span class="participants-flag">').append($('<i class="fa fa-users" aria-hidden="true">'), $('<span class="sr-only">').text(gt('Participants'))));
             return icons;
         },
