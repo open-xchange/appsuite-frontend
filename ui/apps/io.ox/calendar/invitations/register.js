eba8c9a19fd6620fd8f259171fad79b701e6d386
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2018-02-23 11:53:05 +0100

    Additional commit for Bug 56692

--- ui/apps/io.ox/calendar/invitations/register.js
+++ ui/apps/io.ox/calendar/invitations/register.js
@@ -123,7 +123,7 @@ define('io.ox/calendar/invitations/register', [
                 new dialogs.SidePopup({ tabTrap: true }).show(e, function (popup) {
                     popup.busy();
                     self.getFullModel().done(function (fullModel) {
-                        popup.idle().append(viewDetail.draw(fullModel));
+                        popup.idle().append(viewDetail.draw(fullModel, { noFolderCheck: true }));
                     });
                 });
             });
