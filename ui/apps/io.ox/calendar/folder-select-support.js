4daa6b1f9ebc1251f790f449c5cabb996ba5316a
Richard Petersen <richard.petersen@open-xchange.com>
2018-02-27 13:46:45 +0100

    Improved calendar folder selection

---
+++ ui/apps/io.ox/calendar/folder-select-support.js
@@ -0,0 +1,134 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ *
+ * © 2016 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
+ */
+
+define('io.ox/calendar/folder-select-support', [
+    'io.ox/core/folder/api',
+    'settings!io.ox/calendar'
+], function (folderAPI, settings) {
+
+    'use strict';
+
+    function setFolders(list, opt) {
+        var self = this;
+        opt = opt || {};
+        this.folders = _(list).unique();
+        sort.call(this);
+        _.defer(function () {
+            if (opt.silent !== true) self.app.trigger('folders:change', self.folders);
+            settings.set('selectedFolders', self.folders).save();
+        });
+    }
+
+    function sort() {
+        var base = _(folderAPI.pool.models).keys().length,
+            failed = [],
+            sorted = _(this.folders).sortBy(function (folderId) {
+                var folder = folderAPI.pool.models[folderId];
+                if (!folder) return failed.push(folderId);
+                if (folderAPI.is('private', folder.attributes)) return folder.get('index/flat/event/private');
+                if (folderAPI.is('public', folder.attributes)) return folder.get('index/flat/event/public') + base;
+                if (folderAPI.is('shared', folder.attributes)) return folder.get('index/flat/event/shared') + base * base;
+            });
+        if (failed.length === 0) this.folders = sorted;
+        // Keep this for debugging purposes
+        // else console.error('Sort was impossible due to missing folders in cache. ', failed);
+    }
+
+    function FolderSelection(app) {
+        _.extend(this, {
+            folders: [],
+            prevFolders: undefined,
+            singleSelection: false,
+            app: app
+        });
+
+        var initialList = settings.get('selectedFolders');
+        if (!initialList || initialList.length === 0) initialList = [folderAPI.getDefaultFolder('calendar')];
+        setFolders.call(this, initialList);
+    }
+
+    FolderSelection.prototype.isSingleSelection = function () {
+        return this.singleSelection;
+    };
+
+    FolderSelection.prototype.getData = function () {
+        var self = this;
+        return $.when.apply($, this.folders.map(function (folder) {
+            return folderAPI.get(folder).then(function (folder) {
+                if (!folder.subscribed) return;
+                return folder;
+            }, function () {
+                self.remove(folder);
+            });
+        })).then(function () {
+            var data = _(arguments).chain().toArray().compact().value();
+            return _.indexBy(data, 'id');
+        });
+    };
+
+    FolderSelection.prototype.isSelected = function (id) {
+        var list = this.prevFolders ? this.prevFolders : this.folders;
+        if (_.isObject(id)) id = id.id;
+        return list.indexOf(id) >= 0;
+    };
+
+    FolderSelection.prototype.list = function () {
+        return this.folders;
+    };
+
+    FolderSelection.prototype.add = function (folder, opt) {
+        if (this.singleSelection) this.reset();
+        var list = [].concat(this.folders);
+        list.push(folder);
+        this.repaintNode(folder);
+        setFolders.call(this, list, opt);
+    };
+
+    FolderSelection.prototype.remove = function (folder, opt) {
+        if (this.singleSelection) this.reset();
+        var list = _(this.folders).without(folder);
+        this.repaintNode(folder);
+        setFolders.call(this, list, opt);
+    };
+
+    FolderSelection.prototype.setOnly = function (folder) {
+        this.singleSelection = true;
+        if (!this.prevFolders) this.prevFolders = this.folders;
+        this.folders = [].concat([folder]);
+        this.app.folderView.tree.$el.addClass('single-selection');
+        _.defer(this.app.trigger.bind(this.app, 'folders:change', this.folders));
+    };
+
+    FolderSelection.prototype.reset = function () {
+        this.singleSelection = false;
+        if (!this.prevFolders) return;
+        this.folders = this.prevFolders;
+        this.prevFolders = undefined;
+        this.app.folderView.tree.$el.removeClass('single-selection');
+        _.defer(this.app.trigger.bind(this.app, 'folders:change', this.folders));
+    };
+
+    FolderSelection.prototype.repaintNode = _.debounce(function (id) {
+        var nodes = this.app.treeView.$('[data-id="' + id + '"]');
+        nodes.each(function () {
+            var node = $(this).data('view');
+            if (!node) return;
+            node.repaint();
+        });
+    }, 10);
+
+    return function (app) {
+        app.folders = new FolderSelection(app);
+    };
+
+});
--- ui/apps/io.ox/calendar/main.js
+++ ui/apps/io.ox/calendar/main.js
@@ -26,13 +26,14 @@ define('io.ox/calendar/main', [
     'io.ox/core/toolbars-mobile',
     'io.ox/core/page-controller',
     'io.ox/calendar/api',
+    'io.ox/calendar/folder-select-support',
     'io.ox/calendar/mobile-navbar-extensions',
     'io.ox/calendar/mobile-toolbar-actions',
     'io.ox/calendar/toolbar',
     'io.ox/calendar/actions',
     'less!io.ox/calendar/style',
     'io.ox/calendar/week/view'
-], function (commons, ext, capabilities, folderAPI, TreeView, FolderView, DatePicker, settings, gt, ListViewControl, CalendarListView, Bars, PageController, api) {
+], function (commons, ext, capabilities, folderAPI, TreeView, FolderView, DatePicker, settings, gt, ListViewControl, CalendarListView, Bars, PageController, api, addFolderSelectSupport) {
 
     'use strict';
 
@@ -314,9 +315,6 @@ define('io.ox/calendar/main', [
                 if (folder.module !== 'calendar') return;
                 app.folders.add(folder.id);
                 app.folder.set(folder.id);
-                // repaint node to ensure the checkmark
-                var node = app.treeView.getNodeView(folder.id);
-                if (node) node.repaint();
             });
         },
 
@@ -326,92 +324,14 @@ define('io.ox/calendar/main', [
             });
             api.on('all:fail', function (id) {
                 app.folders.remove(id, { silent: true });
-                var node = app.treeView.getNodeView(id);
-                if (node) node.repaint();
             });
         },
 
         'multi-folder-selection': function (app) {
-            var folders, prevFolders, singleSelection = false;
-            function setFolders(list, opt) {
-                opt = opt || {};
-                folders = _(list).unique();
-                sort();
-                _.defer(function () {
-                    if (opt.silent !== true) app.trigger('folders:change', folders);
-                    settings.set('selectedFolders', folders).save();
-                });
-            }
-            function sort() {
-                var base = _(folderAPI.pool.models).keys().length,
-                    failed = [],
-                    sorted = _(folders).sortBy(function (folderId) {
-                        var folder = folderAPI.pool.models[folderId];
-                        if (!folder) return failed.push(folderId);
-                        if (folderAPI.is('private', folder.attributes)) return folder.get('index/flat/event/private');
-                        if (folderAPI.is('public', folder.attributes)) return folder.get('index/flat/event/public') + base;
-                        if (folderAPI.is('shared', folder.attributes)) return folder.get('index/flat/event/shared') + base * base;
-                    });
-                if (failed.length === 0) folders = sorted;
-                // Keep this for debugging purposes
-                // else console.error('Sort was impossible due to missing folders in cache. ', failed);
-            }
-            var initialList = settings.get('selectedFolders');
-            if (!initialList || initialList.length === 0) initialList = [folderAPI.getDefaultFolder('calendar')];
-            setFolders(initialList);
-            app.folders = {
-                isSingleSelection: function () {
-                    return singleSelection;
-                },
-                getData: function () {
-                    return $.when.apply($, folders.map(function (folder) {
-                        return folderAPI.get(folder).then(function (folder) {
-                            if (!folder.subscribed) return;
-                            return folder;
-                        }, function () {
-                            app.folders.remove(folder);
-                        });
-                    })).then(function () {
-                        var data = _(arguments).chain().toArray().compact().value();
-                        return _.indexBy(data, 'id');
-                    });
-                },
-                isSelected: function (id) {
-                    var list = prevFolders ? prevFolders : folders;
-                    if (_.isObject(id)) id = id.id;
-                    return list.indexOf(id) >= 0;
-                },
-                list: function () {
-                    return folders;
-                },
-                add: function (folder, opt) {
-                    if (singleSelection) this.reset();
-                    var list = [].concat(folders);
-                    list.push(folder);
-                    setFolders(list, opt);
-                },
-                remove: function (folder, opt) {
-                    if (singleSelection) this.reset();
-                    var list = _(folders).without(folder);
-                    setFolders(list, opt);
-                },
-                setOnly: function (folder) {
-                    singleSelection = true;
-                    if (!prevFolders) prevFolders = folders;
-                    folders = [].concat([folder]);
-                    app.folderView.tree.$el.addClass('single-selection');
-                    _.defer(app.trigger.bind(app, 'folders:change', folders));
-                },
-                reset: function () {
-                    singleSelection = false;
-                    if (!prevFolders) return;
-                    folders = prevFolders;
-                    prevFolders = undefined;
-                    app.folderView.tree.$el.removeClass('single-selection');
-                    _.defer(app.trigger.bind(app, 'folders:change', folders));
-                }
-            };
-            app.on('folder:change', app.folders.reset);
+            addFolderSelectSupport(app);
+            app.on('folder:change', function () {
+                app.folders.reset();
+            });
             app.folderView.tree.on('dblclick', function (e, folder) {
                 if (!folder) return;
                 if ($(e.target).hasClass('color-label')) return;
