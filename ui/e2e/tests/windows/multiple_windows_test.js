918a857f1dd6b133f5b98447240bf8ba1287e91c
Julian Bäume <julian.baeume@open-xchange.com>
2018-03-01 23:00:44 +0100

    Add e2e test for Bug 57440
    bug has been fixed before, but this test should make sure, it stays this
    way

---
+++ ui/e2e/tests/windows/multiple_windows_test.js
@@ -0,0 +1,59 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2017 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Julian Bäume <julian.baeume@open-xchange.com>
+ */
+/// <reference path="../../steps.d.ts" />
+
+Feature('Floating windows');
+
+const { expect } = require('chai');
+
+Scenario('Opening multiple windows', async function (I) {
+    I.login();
+    I.click('#io-ox-launcher button.launcher-btn');
+    I.click('Calendar', '#io-ox-launcher');
+    I.waitForVisible('*[data-app-name="io.ox/calendar"]');
+    I.waitForDetached('#io-ox-refresh-icon .fa-spin');
+
+    await I.removeAllAppointments();
+
+    I.clickToolbar('New');
+    I.waitForVisible('*[data-app-name="io.ox/calendar/edit"]');
+
+    I.fillField('Subject', 'Participants test');
+    I.click('.fa.fa-address-book');
+    I.click('Ejaz', '.modal-content .list-item-content');
+    I.click('Select');
+    I.wait(0.5);
+
+    I.click('Create');
+    I.waitForDetached('*[data-app-name="io.ox/calendar/edit"]');
+
+    I.waitForVisible('.appointment');
+    I.click('Participants test', '.appointment');
+    I.click('Ejaz', '.participants-view');
+
+    I.waitForVisible({ css: '[data-block="messaging"]' });
+    I.click('eahmed@', { css: '[data-block="messaging"]' });
+
+    const composeIndex = await I.grabCssPropertyFrom('.io-ox-mail-compose-window', 'z-index');
+    const sidePopupIndizes = await I.grabCssPropertyFrom('.io-ox-sidepopup', 'z-index');
+    sidePopupIndizes.map(s => Number.parseInt(s, 10)).forEach(function (sidePopupIndex) {
+        expect(Number.parseInt(composeIndex, 10)).to.be.above(sidePopupIndex);
+    });
+
+    I.waitForDetached('[data-app-name="io.ox/mail/compose"] .busy');
+
+    I.click('Discard');
+
+    await I.removeAllAppointments('Participants test');
+
+    I.logout();
+});
