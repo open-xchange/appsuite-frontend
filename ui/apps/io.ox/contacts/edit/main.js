80273d400616067498d26561649a883cee95e3fa
Frank Paczynski <frank.paczynski@open-xchange.com>
2018-03-19 11:54:01 +0100

    introduce global cleanup of dropZone on app quite
    - works for apps and floating windows now

--- ui/apps/io.ox/contacts/edit/main.js
+++ ui/apps/io.ox/contacts/edit/main.js
@@ -179,12 +179,11 @@ define('io.ox/contacts/edit/main', [
                             }
                             considerSaved = true;
                             win.idle();
-                            if (app.dropZone) app.dropZone.remove();
                             app.quit();
                         });
 
                         if (settings.get('features/PIMAttachments', capabilities.has('filestore'))) {
-
+                            // use naming convention 'dropZone' to utilise global dropZone.remove on quit
                             app.dropZone = new dnd.UploadZone({
                                 ref: 'io.ox/contacts/edit/dnd/actions'
                             }, editView);
