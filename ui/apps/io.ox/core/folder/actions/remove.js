ee17de5a23d467fc1d0dca76097f48af59079d16
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2018-03-19 12:36:58 +0100

    Change wording for remove calendar action

--- ui/apps/io.ox/core/folder/actions/remove.js
+++ ui/apps/io.ox/core/folder/actions/remove.js
@@ -36,8 +36,8 @@ define('io.ox/core/folder/actions/remove', [
 
         var model = api.pool.getModel(id),
             dialog = new dialogs.ModalDialog(),
-            deleteNotice = gt('Do you really want to delete folder "%s"?', model.get('title')),
-            shareNotice = gt('This folder is shared with others. It won\'t be available for them any more.');
+            deleteNotice = model.get('module') === 'calendar' ? gt('Do you really want to delete calendar "%s"?', model.get('title')) : gt('Do you really want to delete folder "%s"?', model.get('title')),
+            shareNotice = model.get('module') === 'calendar' ? gt('This calendar is shared with others. It won\'t be available for them any more.') : gt('This folder is shared with others. It won\'t be available for them any more.');
 
         function isShared() {
             if (model.get('permissions').length > 1) return true;
