dfe806e06182d400df2869380963a54ff410e94f
Jonas Regier <jonas.regier@open-xchange.com>
2018-03-21 11:21:25 +0100

    just comments

--- ui/apps/io.ox/core/api/collection-loader.js
+++ ui/apps/io.ox/core/api/collection-loader.js
@@ -77,6 +77,8 @@ define('io.ox/core/api/collection-loader', ['io.ox/core/api/collection-pool', 'i
             if (type === 'load') {
                 complete = data.length < PAGE_SIZE;
             } else if (type === 'paginate') {
+                // the first data element is the last currently visible element
+                // in the list, therefore <=1 is already complete
                 complete = data.length <= 1;
             }
             if (complete !== collection.complete) {
