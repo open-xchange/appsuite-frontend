b36eeaa5dc24fb326820788fe484de13f6bac393
Julian Bäume <julian.baeume@open-xchange.com>
2018-04-06 14:28:41 +0200

    COVERITY#18285: make code more clear about falsy values
    when optionally handling promises, such variables should never be falsy
    but promises only.

--- ui/apps/io.ox/core/wizard/registry.js
+++ ui/apps/io.ox/core/wizard/registry.js
@@ -308,10 +308,7 @@ define('io.ox/core/wizard/registry', [
             }
             var def = null;
             if (this.currentPage) {
-                def = callMethod(this.currentPage, 'finish', this.index);
-                if (!def) {
-                    def = $.when();
-                }
+                def = callMethod(this.currentPage, 'finish', this.index) || $.when();
             }
             busy();
             def.then(function () {
@@ -327,12 +324,9 @@ define('io.ox/core/wizard/registry', [
             if (!isNextEnabled()) {
                 return;
             }
-            var def = null;
+            var def = $.when();
             if (this.currentPage) {
-                def = callMethod(this.currentPage, 'finish', this.index);
-                if (!def) {
-                    def = $.when();
-                }
+                def = callMethod(this.currentPage, 'finish', this.index) || $.when();
             }
             busy();
             def.done(function () {
