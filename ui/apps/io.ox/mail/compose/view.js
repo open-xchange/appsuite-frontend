99d7869cb6062a3374a4c105ed88715e9607b361
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2018-04-11 15:40:46 +0200

    Fixed Bug 58096 - XSS using "mailto:" links and forms at TinyMCE
    (cherry picked from commit d7a26966bcf2e120445133b0de3bf68098f80ad3)
    (cherry picked from commit fe50a4bd30c672acc992ac6addc50b93d2c8f1a5)

--- ui/apps/io.ox/mail/compose/view.js
+++ ui/apps/io.ox/mail/compose/view.js
@@ -28,10 +28,11 @@ define('io.ox/mail/compose/view', [
     'io.ox/mail/actions/attachmentQuota',
     'io.ox/core/tk/dialogs',
     'io.ox/mail/compose/signatures',
+    'io.ox/mail/sanitizer',
     'less!io.ox/mail/style',
     'less!io.ox/mail/compose/style',
     'io.ox/mail/compose/actions/send'
-], function (extensions, Dropdown, ext, mailAPI, mailUtil, textproc, settings, coreSettings, notifications, snippetAPI, accountAPI, gt, attachmentEmpty, attachmentQuota, dialogs, signatureUtil) {
+], function (extensions, Dropdown, ext, mailAPI, mailUtil, textproc, settings, coreSettings, notifications, snippetAPI, accountAPI, gt, attachmentEmpty, attachmentQuota, dialogs, signatureUtil, sanitizer) {
 
     'use strict';
 
@@ -361,6 +362,7 @@ define('io.ox/mail/compose/view', [
                 if (params.cc) { this.model.set('cc', parseRecipients(params.cc), { silent: true }); }
                 if (params.bcc) { this.model.set('bcc', parseRecipients(params.bcc), { silent: true }); }
 
+                params.body = sanitizer.sanitize({ content: params.body, content_type: 'text/html' }, { WHOLE_DOCUMENT: false }).content;
                 this.setSubject(params.subject || '');
                 this.model.setContent(params.body || '');
                 // clear hash
