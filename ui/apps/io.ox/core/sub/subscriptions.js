6d03dc2ee1be48f22f6224a6b791f557cd0cb25b
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2018-04-17 16:27:15 +0200

    Additional commit for Bug 58179

--- ui/apps/io.ox/core/sub/subscriptions.js
+++ ui/apps/io.ox/core/sub/subscriptions.js
@@ -156,6 +156,7 @@ define('io.ox/core/sub/subscriptions', [
                                 this.find('[data-action="add"]').hide();
                                 a11y.getTabbable(popup.getContentNode()).first().focus();
                             });
+                        popup.getBody().css('padding', '15px 11px');
                     } else {
                         popup.getBody().append($('<p>').text(gt('No subscription services available for this module')));
                         popup.addPrimaryButton('cancel', gt('Cancel')).show();
