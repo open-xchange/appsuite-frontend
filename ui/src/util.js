36a146d7c88e389ca1e6482ca7b4706082906841
Frank Paczynski <frank.paczynski@open-xchange.com>
2018-04-23 14:57:20 +0200

    fixes calander url parameter 'perspective'
    - regression of 8636e320562f5b3d5cb6e3a9a90571a90f62f495

--- ui/src/util.js
+++ ui/src/util.js
@@ -957,7 +957,7 @@
 
     _.sanitize = {
         option: function (value) {
-            return (value || '').replace(/[^a-z0-9._-]/ig, '').trim();
+            return (value || '').replace(/[^a-z0-9:._-]/ig, '').trim();
         }
     };
 
