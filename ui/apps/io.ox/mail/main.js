08aa69518edc0d6efabd567551fd36028eab85a1
Frank Paczynski <frank.paczynski@open-xchange.com>
2018-04-23 12:57:09 +0200

    Fixed: Bug 58226 - XSS through "theme" property injection
    - sanitization covers 'theme' and 'perspective' now
    
    (cherry picked from commit 8636e320562f5b3d5cb6e3a9a90571a90f62f495)

--- ui/apps/io.ox/mail/main.js
+++ ui/apps/io.ox/mail/main.js
@@ -1993,7 +1993,7 @@ define('io.ox/mail/main', [
             find: capabilities.has('search')
         });
 
-        if (_.url.hash().mailto) ox.registry.call('mail-compose', 'compose');
+        if (_.url.hash('mailto')) ox.registry.call('mail-compose', 'compose');
 
         app.setWindow(win);
         app.settings = settings;
