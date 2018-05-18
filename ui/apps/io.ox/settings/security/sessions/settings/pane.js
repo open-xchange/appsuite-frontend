ecf76e2e78b3475cb6dd0f1affd2ddaaf1c64af3
Richard Petersen <richard.petersen@open-xchange.com>
2018-05-18 13:37:07 +0200

    Fixed: Bug 58610 - Wrong order of active clients
    Make sure clients without active identifier are also correctly sorted

--- ui/apps/io.ox/settings/security/sessions/settings/pane.js
+++ ui/apps/io.ox/settings/security/sessions/settings/pane.js
@@ -189,8 +189,9 @@ define('io.ox/settings/security/sessions/settings/pane', [
         comparator: function (model) {
             // sort ascending
             // current session should always be topmost
-            if (model.get('sessionId') === ox.session) return -10000000000000;
-            return -model.get('lastActive');
+            if (model.get('sessionId') === ox.session) return -Number.MAX_VALUE;
+            // sessions without lastActive timestamp should be last
+            return model.has('lastActive') ? -model.get('lastActive') : Number.MAX_VALUE;
         },
 
         initialize: function () {
