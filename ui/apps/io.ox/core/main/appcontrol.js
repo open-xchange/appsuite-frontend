c8226f38599e97a38c2da96636a1932307acbde0
David Bauer <david.bauer@open-xchange.com>
2018-06-21 15:30:53 +0200

    A11y: aXe issues / contrast / wrong attributes in quicklaunchers

--- ui/apps/io.ox/core/main/appcontrol.js
+++ ui/apps/io.ox/core/main/appcontrol.js
@@ -208,7 +208,9 @@ define('io.ox/core/main/appcontrol', [
                 this.collection.map(function (model) {
                     return new LauncherView({
                         tagName: 'button',
-                        model: model, quicklaunch: true
+                        attributes: { tabindex: -1 },
+                        model: model,
+                        quicklaunch: true
                     }).render().$el.attr('tabindex', -1);
                 })
             );
