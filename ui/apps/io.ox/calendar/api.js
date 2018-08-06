2e05655a4bb9db3560f857740276ba3866e4d4f0
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2018-08-06 11:28:21 +0200

    simplify code

--- ui/apps/io.ox/calendar/api.js
+++ ui/apps/io.ox/calendar/api.js
@@ -259,10 +259,7 @@ define('io.ox/calendar/api', [
                             }
 
                             if (isRecurrenceMaster(obj)) {
-                                var res = api.pool.get('detail').add(data);
-                                // return an object, no array
-                                if (_.isArray(res)) res = res[0];
-                                return res;
+                                return api.pool.get('detail').add(obj);
                             }
                             var cid = util.cid(obj);
                             return api.pool.getModel(cid);
