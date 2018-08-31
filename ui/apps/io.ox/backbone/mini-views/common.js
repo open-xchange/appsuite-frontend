9b4d29b12bcec37caa7d68e889c1f466927bd275
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2018-08-31 15:42:53 +0200

    Revert "Fixed: bug 59228 - Date picker in vacation notice dialog often selects wrong date - refactored"
    This reverts commit d295c6cb7996a7dc8d464a20e2b8a07c90b49dd3.

--- ui/apps/io.ox/backbone/mini-views/common.js
+++ ui/apps/io.ox/backbone/mini-views/common.js
@@ -396,8 +396,12 @@ define('io.ox/backbone/mini-views/common', [
         }) :
         InputView.extend({
             format: 'l',
+            initialize: function (opt) {
+                opt.timezone = opt.timezone || moment().tz();
+                InputView.prototype.initialize.call(this, opt);
+            },
             onChange: function () {
-                var t = this.options.utcMode ? moment(this.$el.val(), this.format).valueOf() + moment(this.$el.val(), this.format).utcOffset() * 60 * 1000 : moment(this.$el.val(), this.format).valueOf();
+                var t = +moment(this.$el.val(), this.format).utc(true);
                 this.model.set(this.name, t);
             },
             update: function () {
@@ -405,7 +409,7 @@ define('io.ox/backbone/mini-views/common', [
                 this.$el.val(date || this.options.mandatory ? this.getFormattedDate(date) : '');
             },
             getFormattedDate: function (date) {
-                return this.options.utcMode ? moment(date).utc().format(this.format) : moment(date).format(this.format);
+                return moment(date).utc().format(this.format);
             },
             render: function () {
                 InputView.prototype.render.call(this);
@@ -413,10 +417,10 @@ define('io.ox/backbone/mini-views/common', [
                 require(['io.ox/backbone/views/datepicker'], function (DatePicker) {
                     // need to be async here otherwise parent is undefined
                     setTimeout(function () {
-                        new DatePicker({ parent: view.$el.closest('.modal, #io-ox-core'), mandatory: view.options.mandatory })
+                        new DatePicker({ parent: view.$el.closest('.modal, #io-ox-core'), mandatory: view.options.mandatory, timezone: view.options.timezone })
                             .attachTo(view.$el)
                             .on('select', function (date) {
-                                view.model.set(view.name, view.options.utcMode ? date.valueOf() + date.utcOffset() * 60 * 1000 : date.valueOf());
+                                view.model.set(view.name, date.valueOf());
                             });
                     });
                 });
