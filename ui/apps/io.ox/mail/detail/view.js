9e4c195e2f58b23cdd0485a982c11413c393c928
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2018-09-11 11:53:23 +0200

    Additional commit 2 Fixed Bug 60140 - [L3] mail content not displayed, Bug 60184 - HTML Mail not displayed correctly

--- ui/apps/io.ox/mail/detail/view.js
+++ ui/apps/io.ox/mail/detail/view.js
@@ -398,7 +398,8 @@ define('io.ox/mail/detail/view', [
 
             baton.content = content.get(baton.data, {}, baton.flow).content;
             var $content = $(baton.content), resizing = 0,
-                self = this;
+                self = this,
+                isHtml = !$content.hasClass('plain-text');
 
             // inject content and listen to resize event
             this.on('load', function () {
@@ -416,8 +417,8 @@ define('io.ox/mail/detail/view', [
 
                     $(this.contentDocument).find('head').append('<style>' + contentStyle + '</style>');
 
-                    if ($content.filter('body').length) {
-                        $(this.contentDocument).find('body').replaceWith($content.filter('body'));
+                    if (isHtml) {
+                        $(this.contentDocument).find('body').replaceWith($content);
                     } else {
                         $(this.contentDocument).find('body').append($content);
                     }
