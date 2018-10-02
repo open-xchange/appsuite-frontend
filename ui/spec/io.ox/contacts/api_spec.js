8f90041c24072502163f898796b8e273fb852f88
Julian Bäume <julian.baeume@open-xchange.com>
2018-10-02 11:13:55 +0200

    remove .only from spec description
    we want to run all the specs

--- ui/spec/io.ox/contacts/api_spec.js
+++ ui/spec/io.ox/contacts/api_spec.js
@@ -14,7 +14,7 @@
 define(['io.ox/contacts/api', 'io.ox/contacts/util'], function (api, util) {
     'use strict';
 
-    describe.only('Contact API', function () {
+    describe('Contact API', function () {
 
         beforeEach(function () {
             this.server.respondWith('GET', /api\/contacts\?action=get/, function (xhr) {
