open-xchange/appsuite-frontend

commit df6e1d06ed34664434d2c51ffb04d20dc4a2a78b

A11y: Bootstrap-tokenfield update and some adjustments.

ui/apps/io.ox/core/tk/tokenfield.js
@@ -378,13 +378,13 @@ define('io.ox/core/tk/tokenfield', [
                             label.css('max-width', label.width() - 16 + 'px');
                         }
                         // a11y: set title
-                        node.attr('title', function () {
+                        node.attr('aria-label', function () {
                             var token = model.get('token'),
                                 title = token.label;
                             if (token.label !== token.value) {
-                                title = token.label ? token.label + ' <' + token.value + '>' : token.value;
+                                title = token.label ? token.label + ' ' + token.value : token.value;
                             }
-                            return title;
+                            return gt('%1$s Press backspace to delete.', title);
                         });
                         // customize token
                         ext.point(self.options.extPoint + '/token').invoke('draw', e.relatedTarget, model, e);
@@ -587,7 +587,7 @@ define('io.ox/core/tk/tokenfield', [
                 });
             }
 
-            this.$el.closest('div.tokenfield').on('copy', function (e) {
+            this.$el.closest('div.tokenfield').attr('role', 'application').on('copy', function (e) {
                 // value might contain more than one id so split
                 var values = e.target.value.split(', ');
 
@@ -603,14 +603,6 @@ define('io.ox/core/tk/tokenfield', [
                 if (result !== '') {
                     e.originalEvent.clipboardData.setData('text/plain', result);
                     e.preventDefault();
-                }
-            }).on('keydown', function (e) {
-                //Remove on cut
-                if ((e.ctrlKey || e.metaKey) && e.which === 88) {
-                    $(this).find('.token.active').each(function () {
-                        self.collection.remove($(this).data().attrs.model);
-                    });
-                    self.redrawTokens();
                 }
             });
 

commit d299b234d8bf588942222851096fda4db97fd769

Fixed:
- caused by df6e1d06ed34664434d2c51ffb04d20dc4a2a78b
- partial revert of df6e1d06ed34664434d2c51ffb04d20dc4a2a78b
- splitting up aria-label and title for both uses cases (mouse hover and screenreader)
- remove role application as discussed with david

ui/apps/io.ox/core/tk/tokenfield.js
@@ -406,20 +406,19 @@ define('io.ox/core/tk/tokenfield', [
                     if (e.attrs) {
                         var model = e.attrs.model || self.getModelByCID(e.attrs.value),
                             node = $(e.relatedTarget),
-                            label = node.find('.token-label');
+                            label = node.find('.token-label'),
+                            token = model.get('token'),
+                            hasLabel = token.label && token.label !== token.value,
+                            title = hasLabel ? token.label + ' ' + token.value : token.value;
+
                         // remove wrongly calculated max-width
                         if (label.css('max-width') === '0px') label.css('max-width', 'none');
 
-                        // a11y: set title
-                        node.attr('aria-label', function () {
-                            var token = model.get('token'),
-                                title = token.label;
-                            if (token.label !== token.value) {
-                                title = token.label ? token.label + ' ' + token.value : token.value;
-                            }
-                            //.# Variable will be an contact or email address in a tokenfield. Text is used for screenreaders to provide a hint how to delete the token
-                            return gt('%1$s. Press backspace to delete.', title);
-                        });
+                        // mouse hover tooltip / a11y title
+                        label.attr({ 'aria-hidden': true, 'title': title });
+                        //.# Variable will be an contact or email address in a tokenfield. Text is used for screenreaders to provide a hint how to delete the token
+                        node.attr('aria-label', gt('%1$s. Press backspace to delete.', title));
+
                         // customize token
                         ext.point(self.options.extPoint + '/token').invoke('draw', e.relatedTarget, model, e, self.getInput());
                     }
@@ -669,7 +668,7 @@ define('io.ox/core/tk/tokenfield', [
                 });
             }
 
-            this.$el.closest('div.tokenfield').attr('role', 'application').on('copy', function (e) {
+            this.$el.closest('div.tokenfield').on('copy', function (e) {
                 // value might contain more than one id so split
                 var values = e.target.value.split(', ');
 