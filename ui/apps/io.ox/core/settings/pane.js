e736fc7d532e6de789c4121c18e0e75a58fcf56f
Frank Paczynski <frank.paczynski@open-xchange.com>
2018-11-21 12:16:21 +0100

    Fixed: Bug 61427 - [L3] Wrong hint in the Settings page for reload or relogin (2)
    - followup of 1e022264bca8d84e46e5b898a4f8fb17cd885822
    - consider also secretCookie
    
    (cherry picked from commit 8922269bc12afc409ffaba8faecbb08bb04e6b26)

--- ui/apps/io.ox/core/settings/pane.js
+++ ui/apps/io.ox/core/settings/pane.js
@@ -33,6 +33,7 @@ define('io.ox/core/settings/pane', [
 
     var INDEX = 0,
         MINUTES = 60000,
+        AUTOLOGIN = capabilities.has('autologin') && ox.secretCookie === true,
         availableApps = apps.forLauncher().map(function (o) {
             return {
                 label: o.getTitle(),
@@ -62,7 +63,7 @@ define('io.ox/core/settings/pane', [
                         });
                     },
 
-                    reloadHint: capabilities.has('autologin') ?
+                    reloadHint: AUTOLOGIN ?
                         gt('Some settings (language, timezone, theme) require a page reload or relogin to take effect.') :
                         gt('Some settings (language, timezone, theme) require a relogin to take effect.'),
 
@@ -162,7 +163,7 @@ define('io.ox/core/settings/pane', [
                     settings.saveAndYell(undefined, { force: !!showNotice }).then(
                         function success() {
                             if (!showNotice) return;
-                            var message = capabilities.has('autologin') ?
+                            var message = AUTOLOGIN ?
                                 gt('The setting requires a reload or relogin to take effect.') :
                                 gt('The setting requires a relogin to take effect.');
                             notifications.yell('success', message);
@@ -183,7 +184,7 @@ define('io.ox/core/settings/pane', [
                     $('<div class="help-block">').text(this.reloadHint + ' ').css('margin-bottom', '24px')
                     .append(
                         $('<a href="#" role="button" data-action="reload">').text(
-                            capabilities.has('autologin') ?
+                            AUTOLOGIN ?
                                 gt('Reload page') :
                                 gt('Relogin')
                         ).on('click', reload)
