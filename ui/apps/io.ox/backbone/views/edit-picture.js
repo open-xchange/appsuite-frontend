open-xchange/appsuite-frontend

commit 71bb89a6521f552cf129dc45edb00bdcc66a5c5e

Bug 61201 - Desktop, Firefox - Contact: Changed avatar is reset after re-opening edit screen added unique ids to image urls to prevent browser caching

ui/apps/io.ox/backbone/views/edit-picture.js
@@ -79,7 +79,8 @@ define('io.ox/backbone/views/edit-picture', [
                             // a) dataUrl (webcam photo)
                             if (_.isString(file)) return file;
                             // b) server image
-                            if (!file || !file.lastModified) return imageurl;
+                            // add unique identifier to prevent caching bugs
+                            if (!file || !file.lastModified) return imageurl + '&' + $.param({ uniq: _.now() });
                             // c) local file
                             return getContent(file);
                         }).then(self.setImage.bind(self));

ui/apps/io.ox/contacts/widgets/pictureUpload.js
@@ -171,7 +171,8 @@ define('io.ox/contacts/widgets/pictureUpload', [
                     dataUrl;
 
                 if (imageUrl) {
-                    imageUrl = util.getShardingRoot(util.replacePrefix(imageUrl));
+                    // add unique identifier to prevent caching bugs
+                    imageUrl = util.getShardingRoot(util.replacePrefix(imageUrl + '&' + $.param({ uniq: _.now() })));
                 } else if (this.model.get('image1') && this.model.get('image1_content_type')) {
                     // temporary support for data-url images
                     dataUrl = 'data:' + this.model.get('image1_content_type') + ';base64,' + this.model.get('image1');

commit c85aec57dbf1f1637a37eb1e4594e566210d8898

Fixed: Bug 61201 - Desktop, Firefox - Contact: Changed avatar is reset after re-opening edit screen (2)
- caused by 71bb89a6521f552cf129dc45edb00bdcc66a5c5e
- empty state was broken cause server image was always casted to string by uniq param

ui/apps/io.ox/backbone/views/edit-picture.js
@@ -76,11 +76,12 @@ define('io.ox/backbone/views/edit-picture', [
                         .then(function () {
                             var file = self.model.get('pictureFile'),
                                 imageurl = self.model.get('image1_url');
+                            // add unique identifier to prevent caching bugs
+                            if (imageurl) imageurl = imageurl + '&' + $.param({ uniq: _.now() });
                             // a) dataUrl (webcam photo)
                             if (_.isString(file)) return file;
-                            // b) server image
-                            // add unique identifier to prevent caching bugs
-                            if (!file || !file.lastModified) return imageurl + '&' + $.param({ uniq: _.now() });
+                            // b) server image (or blank)
+                            if (!file || !file.lastModified) return imageurl;
                             // c) local file
                             return getContent(file);
                         }).then(self.setImage.bind(self));