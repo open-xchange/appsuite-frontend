67785b2187606af9730749627189e9c81d3a018f
Julian Bäume <julian.baeume@open-xchange.com>
2019-01-25 18:58:35 +0100

    OXUI-577: As a user I want to see a list of related accounts to my OAuth Identity
    port away from the heuristic implementation of looking for matching
    accounts in all known account APIs to the new attribute provided by the
    middleware. Once finished, this should hopefully simplify the UI code
    needed to show the list of related accounts.

--- ui/apps/io.ox/settings/accounts/views.js
+++ ui/apps/io.ox/settings/accounts/views.js
@@ -14,14 +14,11 @@
 
 define('io.ox/settings/accounts/views', [
     'io.ox/core/extensions',
-    'io.ox/core/api/account',
-    'io.ox/core/api/filestorage',
-    'io.ox/core/folder/api',
     'io.ox/settings/util',
     'io.ox/backbone/mini-views/listutils',
     'io.ox/backbone/views/disposable',
     'gettext!io.ox/settings/accounts'
-], function (ext, accounts, filestorageApi, folderAPI, settingsUtil, listUtils, DisposableView, gt) {
+], function (ext, settingsUtil, listUtils, DisposableView, gt) {
     'use strict';
 
     var createExtpointForSelectedAccount = function (args) {
@@ -31,7 +28,7 @@ define('io.ox/settings/accounts/views', [
         },
         drawIcon = (function () {
             return function (model) {
-                var type = model.get('accountType'),
+                var type = model.get('accountType') || model.get('module'),
                     shortId = String(model.get('serviceId') || model.id).match(/\.?([a-zA-Z]*)(\d*)$/)[1] || 'fallback';
                 return $('<i class="account-icon fa" aria-hidden="true">')
                     .addClass(type.toLowerCase())
@@ -114,12 +111,8 @@ define('io.ox/settings/accounts/views', [
 
                 e.preventDefault();
 
-                var account = { id: this.model.get('id'), accountType: this.model.get('accountType') },
+                var account = this.model.pick('id', 'accountType', 'folder'),
                     self = this;
-
-                if (account.accountType === 'fileStorage') {
-                    account.filestorageService = this.model.get('filestorageService');
-                }
 
                 require(['io.ox/backbone/views/modal'], function (ModalDialog) {
                     new ModalDialog({
@@ -134,13 +127,12 @@ define('io.ox/settings/accounts/views', [
                     .on('delete', function () {
                         var popup = this,
                             // require correct api
-                            req = account.accountType === 'fileStorage' ? 'io.ox/core/api/filestorage' : 'io.ox/keychain/api';
+                            req = account.accountType === 'mail' ? 'io.ox/keychain/api' : 'io.ox/core/folder/api';
                         settingsUtil.yellOnReject(
                             require([req]).then(function (api) {
-                                return api.remove(account);
+                                return api.remove(account.accountType === 'mail' ? account : account.folder);
                             }).then(
                                 function success() {
-                                    folderAPI.list('1', { cache: false });
                                     if (self.disposed) {
                                         popup.close();
                                         return;
