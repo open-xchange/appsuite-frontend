77e06a92de2316352d771e42446c091951819582
Alexander Quast <alexander.quast@open-xchange.com>
2019-01-28 10:17:29 +0100

    Fixed settings paths for quickLauncher default settings

--- ui/apps/io.ox/core/settings/defaults.js
+++ ui/apps/io.ox/core/settings/defaults.js
@@ -34,12 +34,14 @@ define('io.ox/core/settings/defaults', function () {
         design: 'primary',
         autoStart: 'io.ox/mail/main',
         coloredIcons: false,
-        quicklaunch: ['io.ox/mail/main', 'io.ox/contacts/main'].join(','),
         autoOpenNotification: 'noEmail',
         autoLogout: 0,
         showDesktopNotifications: true,
         settings: {
             downloadsDisabled: false
+        },
+        apps: {
+            quickLaunch: 'io.ox/mail/main,io.ox/contacts/main,io.ox/portal/main'
         }
     };
 
