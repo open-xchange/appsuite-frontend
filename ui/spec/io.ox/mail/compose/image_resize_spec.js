90fdfb05fd3506fbb7ff1f0fbe74d951c707a3c7
Frank Paczynski <frank.paczynski@open-xchange.com>
2019-02-11 15:46:39 +0100

    fixes unit tests for image resize
    - caused by 42e0b92284ef4e60e73bf6b7e90d05cfa707af46

--- ui/spec/io.ox/mail/compose/image_resize_spec.js
+++ ui/spec/io.ox/mail/compose/image_resize_spec.js
@@ -14,15 +14,29 @@ define(['io.ox/mail/compose/resize', 'settings!io.ox/mail'], function (imageResi
 
     'use strict';
 
+    function getFilePlaceholder(obj) {
+        return {
+            type: obj.type || 'image/jpg',
+            size: obj.size || 1024,
+            _dimensions: {
+                width: obj.width || 1200,
+                height: obj.height || 800
+            }
+        };
+    }
+
     describe('Mail Compose', function () {
         describe('image resize', function () {
             describe('getTargetDimensions', function () {
+
                 it('should get the correct target sizes for horizontal images', function () {
                     var dimensions = { width: 2048, height: 1024 },
                         targetsize = 1024,
                         expectedWidth = 1024,
                         expectedHeight = 512;
-                    var resultDimensions = imageResize.getTargetDimensions(dimensions, targetsize);
+
+                    var file = getFilePlaceholder(dimensions);
+                    var resultDimensions = imageResize.getTargetDimensions(file, targetsize);
                     expect(resultDimensions.width).to.equal(expectedWidth);
                     expect(resultDimensions.height).to.equal(expectedHeight);
                 });
@@ -32,7 +46,8 @@ define(['io.ox/mail/compose/resize', 'settings!io.ox/mail'], function (imageResi
                         targetsize = 1024,
                         expectedWidth = 512,
                         expectedHeight = 1024;
-                    var resultDimensions = imageResize.getTargetDimensions(dimensions, targetsize);
+                    var file = getFilePlaceholder(dimensions);
+                    var resultDimensions = imageResize.getTargetDimensions(file, targetsize);
                     expect(resultDimensions.width).to.equal(expectedWidth);
                     expect(resultDimensions.height).to.equal(expectedHeight);
                 });
@@ -42,7 +57,8 @@ define(['io.ox/mail/compose/resize', 'settings!io.ox/mail'], function (imageResi
                         targetsize = 1024,
                         expectedWidth = 1024,
                         expectedHeight = 1024;
-                    var resultDimensions = imageResize.getTargetDimensions(dimensions, targetsize);
+                    var file = getFilePlaceholder(dimensions);
+                    var resultDimensions = imageResize.getTargetDimensions(file, targetsize);
                     expect(resultDimensions.width).to.equal(expectedWidth);
                     expect(resultDimensions.height).to.equal(expectedHeight);
                 });
@@ -54,58 +70,71 @@ define(['io.ox/mail/compose/resize', 'settings!io.ox/mail'], function (imageResi
                 it('should be true for jpgs', function () {
                     var jpgFile = { type: 'image/jpg', size: fileSizeMax - 1 },
                         jpegFile = { type: 'image/jpeg', size: fileSizeMax - 1 };
-                    expect(imageResize.isResizableImage(jpgFile)).to.be.true;
-                    expect(imageResize.isResizableImage(jpegFile)).to.be.true;
+                    expect(imageResize.matches('type', jpgFile)).to.be.true;
+                    expect(imageResize.matches('type', jpegFile)).to.be.true;
                 });
                 it('should be true for pngs', function () {
                     var pngFile = { type: 'image/png', size: fileSizeMax - 1 };
-                    expect(imageResize.isResizableImage(pngFile)).to.be.true;
+                    expect(imageResize.matches('type', pngFile)).to.be.true;
                 });
                 it('should be false for gifs', function () {
                     var gifFile = { type: 'image/gif', size: fileSizeMax - 1 };
-                    expect(imageResize.isResizableImage(gifFile)).to.be.false;
+                    expect(imageResize.matches('type', gifFile)).to.be.false;
                 });
                 it('should be false for other types', function () {
                     var tiffFile = { type: 'image/tiff', size: fileSizeMax - 1 },
                         applicationFile = { type: 'application/someApp', size: fileSizeMax - 1 };
-                    expect(imageResize.isResizableImage(tiffFile)).to.be.false;
-                    expect(imageResize.isResizableImage(applicationFile)).to.be.false;
+                    expect(imageResize.matches('type', tiffFile)).to.be.false;
+                    expect(imageResize.matches('type', applicationFile)).to.be.false;
                 });
                 it('should be false for files bigger than filesize maximum', function () {
                     var jpgFile = { type: 'image/jpg', size: fileSizeMax + 1 },
                         jpegFile = { type: 'image/jpeg', size: fileSizeMax + 1 },
                         pngFile = { type: 'image/png', size: fileSizeMax + 1 },
                         gifFile = { type: 'image/gif', size: fileSizeMax + 1 };
-                    expect(imageResize.isResizableImage(jpgFile)).to.be.false;
-                    expect(imageResize.isResizableImage(jpegFile)).to.be.false;
-                    expect(imageResize.isResizableImage(pngFile)).to.be.false;
-                    expect(imageResize.isResizableImage(gifFile)).to.be.false;
+                    expect(imageResize.matches('size', jpgFile)).to.be.false;
+                    expect(imageResize.matches('size', jpegFile)).to.be.false;
+                    expect(imageResize.matches('size', pngFile)).to.be.false;
+                    expect(imageResize.matches('size', gifFile)).to.be.false;
                 });
             });
             describe('resizeRecommended', function () {
 
                 var fileSizeThreshold = settings.get('features/imageResize/fileSizeThreshold', 1024 * 1024),
-                    imageSizeThreshold = settings.get('features/imageResize/imageSizeThreshold', 1024);
+                    imageSizeThreshold = settings.get('features/imageResize/imageSizeThreshold', 1024),
+                    maxSize = settings.get('features/imageResize/fileSizeMax', 10 * 1024 * 1024);
 
                 it('should be false for images within the thresholds', function () {
-                    var dimensions = { width: imageSizeThreshold, height: imageSizeThreshold },
-                        size = fileSizeThreshold;
-                    expect(imageResize.resizeRecommended(dimensions, size)).to.be.false;
+                    var file = getFilePlaceholder({
+                        size: fileSizeThreshold,
+                        width: imageSizeThreshold,
+                        height: imageSizeThreshold
+                    });
+                    expect(imageResize.resizeRecommended(file)).to.be.false;
                 });
                 it('should be true for images with too big width', function () {
-                    var dimensions = { width: imageSizeThreshold + 1, height: imageSizeThreshold },
-                        size = fileSizeThreshold;
-                    expect(imageResize.resizeRecommended(dimensions, size)).to.be.true;
+                    var file = getFilePlaceholder({
+                        size: fileSizeThreshold,
+                        width: imageSizeThreshold + 1,
+                        height: imageSizeThreshold
+                    });
+                    expect(imageResize.resizeRecommended(file)).to.be.true;
                 });
                 it('should be true for images with too big height', function () {
-                    var dimensions = { width: imageSizeThreshold, height: imageSizeThreshold + 1 },
-                        size = fileSizeThreshold;
-                    expect(imageResize.resizeRecommended(dimensions, size)).to.be.true;
+                    var file = getFilePlaceholder({
+                        size: fileSizeThreshold,
+                        width: imageSizeThreshold,
+                        height: imageSizeThreshold + 1
+                    });
+                    expect(imageResize.resizeRecommended(file)).to.be.true;
                 });
                 it('should be true for images with too big filesize', function () {
-                    var dimensions = { width: imageSizeThreshold, height: imageSizeThreshold },
-                        size = fileSizeThreshold + 1;
-                    expect(imageResize.resizeRecommended(dimensions, size)).to.be.true;
+                    var file = getFilePlaceholder({
+                        size: maxSize + 1,
+                        width: imageSizeThreshold + 1,
+                        height: imageSizeThreshold
+                    });
+                    expect(imageResize.resizeRecommended(file)).to.be.false;
                 });
             });
         });
