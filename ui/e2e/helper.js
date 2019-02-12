de08e6f58ef72309ba3fe83980584f2d594af93d
David Bauer <david.bauer@open-xchange.com>
2019-02-12 14:41:15 +0100

    E2E / A11y: Changed axe-core integration from actor to helper

--- ui/e2e/helper.js
+++ ui/e2e/helper.js
@@ -1,4 +1,5 @@
-const Helper = require('@open-xchange/codecept-helper').helper;
+const Helper = require('@open-xchange/codecept-helper').helper,
+    axe = require('axe-core');
 
 function assertElementExists(res, locator, prefixMessage = 'Element', postfixMessage = 'was not found by text|CSS|XPath') {
     if (!res || res.length === 0) {
@@ -60,6 +61,46 @@ class MyHelper extends Helper {
         this.debugSection('Grab', html);
         return html;
 
+    }
+
+    // This needs to be a helper, as actors are too verbose in this case
+    async grabAxeReport(options, context) {
+        let wdio = this.helpers['WebDriver'],
+            browser = wdio.browser;
+        if (typeof options === 'undefined') options = {};
+        if (typeof context === 'undefined') context = '';
+        const report = await browser.executeAsync(function (axeSource, context, options, done) {
+            if (typeof axe === 'undefined') {
+                // eslint-disable-next-line no-eval
+                window.eval(axeSource);
+            }
+            // Arity needs to be correct here so we need to compact arguments
+            window.axe.run.apply(this, _.compact([context || $('html'), options])).then(function (report) {
+                try {
+                    var nodes = [];
+                    for (const violation of report.violations) {
+                        for (const node of violation.nodes) {
+                            nodes.push(node.target);
+                            for (const combinedNodes of [node.all, node.any, node.none]) {
+                                if (!_.isEmpty(combinedNodes)) {
+                                    for (const any of combinedNodes) {
+                                        for (const relatedNode of any.relatedNodes) {
+                                            nodes.push(relatedNode.target);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    $(nodes.join(',')).css('border', '2px solid red');
+                } catch (err) {
+                    done(err.message);
+                }
+                done(report);
+            });
+        }, axe.source, context, options);
+        if (typeof report === 'string') throw report;
+        return report;
     }
 }
 
