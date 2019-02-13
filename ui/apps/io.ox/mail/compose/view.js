9406348d35b41ce455917c15f17e8d2fec37489c
David Bauer <david.bauer@open-xchange.com>
2019-02-13 14:34:28 +0100

    Fixup for 23224076afa74f15869711292afcb663f185bc85

--- ui/apps/io.ox/mail/compose/view.js
+++ ui/apps/io.ox/mail/compose/view.js
@@ -886,15 +886,15 @@ define('io.ox/mail/compose/view', [
             if (input.hasClass('hidden') || isString) {
                 input.removeClass('hidden');
                 button.addClass('active');
-                if (type === 'cc') button.attr('title', gt('Hide blind carbon copy input field'));
-                if (type === 'bcc') button.attr('title', gt('Hide carbon copy input field'));
+                if (type === 'cc') button.attr('title', gt('Hide carbon copy input field'));
+                if (type === 'bcc') button.attr('title', gt('Hide blind carbon copy input field'));
             } else if (!this.model.has(type) || _.isEmpty(this.model.get(type))) {
                 //We don't want to close it automatically! Bug: 35730
                 this.model.set(type, []);
                 input.addClass('hidden');
                 button.removeClass('active');
-                if (type === 'cc') button.attr('title', gt('Show blind carbon copy input field'));
-                if (type === 'bcc') button.attr('title', gt('Show carbon copy input field'));
+                if (type === 'cc') button.attr('title', gt('Show carbon copy input field'));
+                if (type === 'bcc') button.attr('title', gt('Show blind carbon copy input field'));
             }
             $(window).trigger('resize');
             return input;
