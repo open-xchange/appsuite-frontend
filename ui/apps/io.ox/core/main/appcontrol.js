8aa8138714eea1f4b567a9465e636e18e0b3cd3b
Frank Paczynski <frank.paczynski@open-xchange.com>
2019-03-12 13:59:43 +0100

    Fixed: Bug 63507 - Broken mail app icon upon login
    Root cause: Safari "bug"
    Solution: switch to svg > circle
    Resolved state: Fix
    Version: 7.10.2
    Git repository: wd/frontend/web
    Git branch: develop
    Package name: open-xchange-appsuite

--- ui/apps/io.ox/core/main/appcontrol.js
+++ ui/apps/io.ox/core/main/appcontrol.js
@@ -115,7 +115,9 @@ define('io.ox/core/main/appcontrol', [
             if (id === 'io.ox/calendar' || /calendar/.test(this.model.getName())) this.drawDate();
 
             var cell = $('<div class="lcell" aria-hidden="true">').append(
-                this.badge = $('<div class="indicator">').toggle(this.model.get('hasBadge')),
+                // important: do not add circle element via append (https://stackoverflow.com/a/3642265)
+                this.badge = $('<svg height="8" width="8" class="indicator"><circle cx="4" cy="4" r="4"></svg>')
+                    .toggle(this.model.get('hasBadge')),
                 $('<div class="icon">').append(this.$icon),
                 $('<div class="title">').text(this.model.get('title'))
             );
