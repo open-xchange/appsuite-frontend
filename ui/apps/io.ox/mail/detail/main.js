fb80dbf0d2e7b8b48e20d05d12fa28615ca64b2b
Frank Paczynski <frank.paczynski@open-xchange.com>
2019-03-25 13:48:02 +0100

    metrics: adjust tracking if mail toolbar

--- ui/apps/io.ox/mail/detail/main.js
+++ ui/apps/io.ox/mail/detail/main.js
@@ -47,34 +47,6 @@ define('io.ox/mail/detail/main', [
                     });
                 }
             };
-        },
-
-        'metrics': function (app) {
-            require(['io.ox/metrics/main'], function (metrics) {
-                if (!metrics.isEnabled()) return;
-                var body = app.getWindow().nodes.body;
-                // toolbar actions
-                body.on('mousedown', '.io-ox-action-link:not(.dropdown, [data-toggle="dropdown"])', function (e) {
-                    metrics.trackEvent({
-                        app: 'mail',
-                        target: 'detail/toolbar',
-                        type: 'click',
-                        action: $(e.currentTarget).attr('data-action')
-                    });
-                });
-                // toolbar options dropdown
-                body.on('mousedown', '.io-ox-inline-links .dropdown a:not([data-toggle])', function (e) {
-                    var action = $(e.target).closest('.dropdown').find('> a');
-                    metrics.trackEvent({
-                        app: 'mail',
-                        target: 'detail/toolbar',
-                        type: 'click',
-                        action: action.attr('data-action'),
-                        detail: $(e.target).val()
-                    });
-                });
-
-            });
         }
     });
 
