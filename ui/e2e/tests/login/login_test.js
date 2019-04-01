3480291e4611d9b9c8ccfef2d58f45e7c251a1b2
Julian Bäume <julian.baeume@open-xchange.com>
2019-04-01 10:37:56 +0200

    create test stub for login tests
    just to get going

---
+++ ui/e2e/tests/login/login_test.js
@@ -0,0 +1,25 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2019 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Maik Schäfer <maik.schaefer@open-xchange.com>
+ *
+ */
+
+/// <reference path="../../steps.d.ts" />
+
+Feature('Login');
+
+Before(async (users) => {
+    await users.create();
+});
+
+After(async (users) => {
+    await users.removeAll();
+});
+
