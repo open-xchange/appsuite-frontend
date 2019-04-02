89ef81f6eb675baabb5a6d02000f7bda69553ebf
Frank Paczynski <frank.paczynski@open-xchange.com>
2019-04-02 13:17:31 +0200

    Create folder within move dialog
    Same ID, two scenarios (...copy/move...)

---
+++ ui/e2e/tests/mail/move_copy_test.js
@@ -0,0 +1,108 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2017 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Frank Paczynski <frank.paczynski@open-xchange.com>
+ *
+ */
+
+/// <reference path="../../steps.d.ts" />
+
+Feature('Mail move and copy');
+
+Before(async function (I, users) {
+    await users.create();
+});
+
+After(async function (I, users) {
+    await users.removeAll();
+});
+
+// TODO: introduce global actors?
+const A = {
+    check: function (I, subject, folder) {
+        I.selectFolder(folder);
+        I.seeTextEquals(subject, '.leftside .list-view .subject .drag-title');
+    },
+    clickMoreAction: function (I, toolbar, action) {
+        I.click('~More actions', toolbar);
+        I.waitForVisible(`[data-action="${action}"]`, 'body > .dropdown');
+        I.click(`[data-action="${action}"]`, 'body > .dropdown');
+    },
+    createFolderInDialog: function (I, folder) {
+        I.waitForVisible('.folder-picker-dialog');
+        I.click('Create folder', '.folder-picker-dialog');
+
+        I.waitForElement('[data-point="io.ox/core/folder/add-popup"]');
+        I.fillField('Folder name', folder);
+        I.click('Add');
+
+        I.waitForDetached('[data-point="io.ox/core/folder/add-popup"]');
+        I.seeTextEquals(folder, '.folder-picker-dialog .selected .folder-label');
+    },
+    select: function (I, mode) {
+        I.click('.io-ox-mail-window .leftside ul li.list-item');
+        I.waitForVisible('.io-ox-mail-window .mail-detail-pane .subject');
+    }
+};
+
+// TODO: introduce global helpers?
+const H = {
+    fillInbox: async function create(I, user, subjects) {
+        [].concat(subjects).forEach(function (subject) {
+            I.haveMail({
+                attachments: [{
+                    content: `<p>${subject}</p>`,
+                    content_type: 'text/html',
+                    disp: 'inline'
+                }],
+                from: [[user.get('displayname'), user.get('primaryEmail')]],
+                sendtype: 0,
+                subject: subject,
+                to: [[user.get('displayname'), user.get('primaryEmail')]]
+            });
+        });
+    }
+};
+
+Scenario('[C114349] Create folder within move dialog', async function (I, users) {
+    let [user] = users,
+        folder = 'C114349-move';
+
+    await H.fillInbox(I, user, [folder]);
+    I.login('app=io.ox/mail');
+    I.waitForVisible('.io-ox-mail-window');
+
+    A.select(I, 'first');
+    A.clickMoreAction(I, '.detail-view-header', 'io.ox/mail/actions/move');
+    A.createFolderInDialog(I, folder);
+    I.click('Move', '.folder-picker-dialog');
+
+    A.check(I, folder, folder);
+
+    I.logout();
+});
+
+Scenario('[C114349] Create folder within copy dialog', async function (I, users) {
+    let [user] = users,
+        folder = 'C114349-copy';
+
+    await H.fillInbox(I, user, [folder]);
+    I.login('app=io.ox/mail');
+    I.waitForVisible('.io-ox-mail-window');
+
+    A.select(I, 'first');
+    A.clickMoreAction(I, '.detail-view-header', 'io.ox/mail/actions/copy');
+    A.createFolderInDialog(I, folder);
+    I.click('Copy', '.folder-picker-dialog');
+
+    A.check(I, folder, 'Inbox');
+    A.check(I, folder, folder);
+
+    I.logout();
+});
