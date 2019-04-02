78f51bc433a78b41be017c9ee0d9560d3eea0c37
olena.stute <olena.stute@open-xchange.com>
2019-04-02 13:53:12 +0200

    Switch to portal customization

---
+++ ui/e2e/tests/portal/switchToPortalCustomization_test.js
@@ -0,0 +1,29 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2019 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Olena Stute <olena.stute@open-xchange.com>
+ */
+
+/// <reference path="../../steps.d.ts" />
+
+Feature('Portal');
+
+Before(async (users) => {
+    await users.create();
+});
+After(async (users) => {
+    await users.removeAll();
+});
+
+Scenario('[C7474] Switch to portal customization', (I) => {
+    I.login('app=io.ox/portal');
+    I.waitForText('Customize this page');
+    I.click('Customize this page');
+    I.waitForText('Portal settings');
+});
