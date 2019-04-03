83158b5ecaa9337aa6e1bcd528d98f51de05330a
Francisco Laguna <francisco.laguna@open-xchange.com>
2019-04-03 10:35:29 +0200

    add new feature stub: send_attachment_from_drive_test.js

---
+++ ui/e2e/tests/mail/compose/send_attachment_from_drive_test.js
@@ -0,0 +1,27 @@
+/**
+* This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+* LICENSE. This work is protected by copyright and/or other applicable
+* law. Any use of the work other than as authorized under this license
+* or copyright law is prohibited.
+*
+* http://creativecommons.org/licenses/by-nc-sa/2.5/
+* © 2019 OX Software GmbH, Germany. info@open-xchange.com
+*
+* @author Francisco Laguna <francisco.laguna@open-xchange.com>
+*/
+
+/// <reference path="../../../steps.d.ts" />
+
+Feature('Mail > Compose');
+
+Before(async (users) => {
+    await users.create();
+});
+After(async (users) => {
+    await users.removeAll();
+});
+
+Scenario('[C7391] Send mail with attachment from Drive', (I) => {
+    I.login();
+    I.logout();
+});
