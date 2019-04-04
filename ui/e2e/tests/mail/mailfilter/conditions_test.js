65011a4a7876dbdcebcf71dd5e176c06e3539633
Christoph Kopp <christoph.kopp@open-xchange.com>
2019-04-04 10:10:03 +0200

    Filter mail on sender

---
+++ ui/e2e/tests/mail/mailfilter/conditions_test.js
@@ -0,0 +1,119 @@
+/**
+* This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+* LICENSE. This work is protected by copyright and/or other applicable
+* law. Any use of the work other than as authorized under this license
+* or copyright law is prohibited.
+*
+* http://creativecommons.org/licenses/by-nc-sa/2.5/
+* © 2019 OX Software GmbH, Germany. info@open-xchange.com
+*
+* @author Christoph Kopp <christoph.kopp@open-xchange.com>
+*/
+
+/// <reference path="../../../steps.d.ts" />
+
+Feature('Mail > Mailfilter');
+
+Before(async function (users) {
+    await users.create();
+    await users.create();
+});
+
+After(async function (users) {
+    await users.removeAll();
+});
+
+function createFilterRule(I, name, condition, value, flag) {
+    I.login('app=io.ox/settings');
+    I.waitForVisible('.io-ox-settings-main');
+    I.selectFolder('Mail');
+    I.waitForVisible('.rightside h1');
+
+    // open mailfilter settings
+    I.selectFolder('Filter Rules');
+
+    // checks the h1 and the empty message
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane .io-ox-mailfilter-settings h1');
+    I.see('Mail Filter Rules');
+
+    I.see('There is no rule defined');
+
+    // create a test rule and check the inintial display
+    I.click('Add new rule');
+    I.see('Create new rule');
+    I.see('This rule applies to all messages. Please add a condition to restrict this rule to specific messages.');
+    I.see('Please define at least one action.');
+
+    I.fillField('rulename', name);
+
+    // add condition
+    I.click('Add condition');
+    I.click(condition);
+    I.fillField('values', value);
+
+    // add action
+    I.click('Add action');
+    I.click('Set color flag');
+    I.click('.actions .dropdown-toggle');
+    I.waitForVisible('.flag-dropdown');
+    I.click(flag, '.flag-dropdown');
+
+}
+
+Scenario('[C7792] Filter mail on sender', async function (I, users) {
+    let [user] = users;
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' }
+    });
+
+    createFilterRule(I, 'TestCase0368', 'From', user.get('primaryEmail'), 'Blue');
+    // save the form
+    I.click('Save');
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane li.settings-list-item[data-id="0"]');
+
+    I.openApp('Mail');
+
+    // compose mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', user.get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'TestCase0368');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+    I.waitForElement('~Sent objects, 1 total');
+    I.waitForElement('~Inbox, 1 unread, 1 total');
+    I.seeElement('.vsplit .flag_2');
+
+});
+
+Scenario('[C7793] Filter mail on any recipient', async function (I, users) {
+    let [user] = users;
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' }
+    });
+
+    createFilterRule(I, 'TestCase0369', 'Any recipient', user.get('primaryEmail'), 'Red');
+    // save the form
+    I.click('Save');
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane li.settings-list-item[data-id="0"]');
+
+    I.openApp('Mail');
+
+    // compose mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', user.get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'TestCase0369');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+    I.waitForElement('~Sent objects, 1 total');
+    I.waitForElement('~Inbox, 1 unread, 1 total');
+    I.seeElement('.vsplit .flag_1');
+
+});
