6ce497e400b5b64c62d9853bd588fc08338ae191
Richard Petersen <richard.petersen@open-xchange.com>
2019-04-04 17:15:32 +0200

    Create a monthly recurring appointment on day 10 ends 31/12/2020

--- ui/e2e/tests/calendar/create_test.js
+++ ui/e2e/tests/calendar/create_test.js
@@ -233,6 +233,73 @@ Scenario('[C7418] Create a Yearly recurring appointment last day of week in dece
     });
 });
 
+Scenario('[C7419] Create a monthly recurring appointment on day 10 ends 31/12/2020', async function (I) {
+
+    await I.haveSetting({
+        'io.ox/core': { autoOpenNotification: false, showDesktopNotifications: false },
+        'io.ox/calendar': { showCheckboxes: true }
+    });
+    const date = moment('10', 'DD');
+
+    I.login('app=io.ox/calendar');
+
+    // and select the correct date
+    I.retry(5).click(`~${date.format('l, dddd')}, CW ${date.week()}`, '.window-sidepanel');
+
+    I.clickToolbar('New');
+    I.waitForText('Subject');
+    I.fillField('Subject', 'Testappointment');
+
+    I.click('~Date (M/D/YYYY)');
+    I.pressKey(['Control', 'a']);
+    I.pressKey(date.format('l'));
+    I.pressKey('Enter');
+
+    I.click('Repeat', '.io-ox-calendar-edit-window');
+    I.click(`Every ${date.format('dddd')}.`);
+
+    I.waitForElement('.modal-dialog');
+
+    I.selectOption('.modal-dialog [name="recurrence_type"]', 'Monthly');
+    I.selectOption('.modal-dialog [name="until change:occurrences"]', 'On specific date');
+    I.waitForElement('~Date (M/D/YYYY)', undefined, '.modal-dialog');
+    I.click('~Date (M/D/YYYY)', '.modal-dialog');
+    I.pressKey(['Control', 'a']);
+    I.pressKey(moment(date).add(6, 'years').format('l'));
+    I.pressKey('Enter');
+    I.see('Every month on day 10.');
+
+    I.click('Apply', '.modal-dialog');
+
+    I.waitForDetached('.modal-dialog');
+    I.see('Every month on day 10.');
+
+    // create
+    I.click('Create', '.io-ox-calendar-edit-window');
+    I.waitForDetached('.io-ox-calendar-edit-window', 5);
+
+    // open all views and load the appointments there
+    ['Week', 'Day', 'Month', 'List'].forEach((view) => {
+        I.clickToolbar('View');
+        I.click(view);
+        I.waitForVisible('.appointment', undefined, '.page.current');
+        I.see('Testappointment');
+    });
+
+    I.click('~Go to next month', '.window-sidepanel');
+    date.add(1, 'month');
+    I.click(`~${date.format('l, dddd')}, CW ${date.week()}`, '.window-sidepanel');
+
+    // open all views and load the appointments there
+    ['Week', 'Day', 'Month'].forEach((view) => {
+        I.clickToolbar('View');
+        I.click(view);
+        I.waitForVisible('.appointment', undefined, '.page.current');
+        I.see('Testappointment');
+    });
+
+});
+
 Scenario('[C274537] Support use-count calculation on Appointment create with Groups', async function (I, users) {
     let testrailID = 'C274537';
     var timestamp = Math.round(+new Date() / 1000);
