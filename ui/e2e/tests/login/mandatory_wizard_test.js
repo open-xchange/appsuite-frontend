07b887ddeac0f763259e26b118d4bdf0295c7ab6
Maik Schäfer <maik.schaefer@open-xchange.com>
2019-04-04 11:54:30 +0200

    Use first run mandatory wizard

---
+++ ui/e2e/tests/login/mandatory_wizard_test.js
@@ -0,0 +1,53 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2019 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Maik Schäfer <maik.schaefer@open-xchange.com>
+ *
+ */
+
+/// <reference path="../../steps.d.ts" />
+
+Feature('Login > Mandatory Wizard');
+
+Before(async (users) => {
+    await users.create();
+});
+
+After(async (users) => {
+    await users.removeAll();
+});
+
+Scenario('[C7341] Use first run mandatory wizard', async function (I, users) {
+    const [user] = users;
+    const first_name = 'John';
+    const last_name = 'Wayne';
+    await user.hasCapability('mandatory_wizard');
+
+    I.amOnPage('/');
+    I.wait(1);
+    I.fillField('User name', `${users[0].get('name')}@${users[0].context.id}`);
+    I.fillField('Password', users[0].get('password'));
+    I.click('Sign in');
+    I.waitForText('Welcome to OX App Suite');
+    I.click('Start tour');
+    I.waitForFocus('.form-control');
+    I.fillField('first_name', first_name);
+    I.fillField('last_name', last_name);
+    I.pressKey('Enter');
+    I.click('Next');
+    I.click('Finish');
+    I.waitForVisible('#io-ox-launcher');
+    I.click('.contact-picture');
+    I.waitForText('My contact data');
+    I.click('My contact data');
+    I.waitForText('My contact data');
+    I.seeInField('first_name', first_name);
+    I.seeInField('last_name', last_name);
+    I.logout();
+});
