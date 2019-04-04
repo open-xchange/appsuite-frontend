7fbd5dc0f8720b85b67b86fc45ca6b5ac66fea9c
Christoph Kopp <christoph.kopp@open-xchange.com>
2019-04-04 13:53:18 +0200

    Filter mail on header

--- ui/e2e/tests/mail/mailfilter/conditions_test.js
+++ ui/e2e/tests/mail/mailfilter/conditions_test.js
@@ -205,3 +205,36 @@ Scenario('[C7796] Filter mail on cc-field', async function (I, users) {
     I.waitForElement('~Inbox, 1 unread, 1 total');
     I.seeElement('.vsplit .flag_1');
 });
+
+Scenario('[C7797] Filter mail on header', async function (I, users) {
+    let [user] = users;
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' }
+    });
+
+    createFilterRule(I, 'TestCase0381', 'Header', user.get('primaryEmail'), 'Red');
+    I.click('Matches');
+    I.waitForVisible('.open.dropdownlink');
+    I.click('Contains');
+    I.fillField('headers', 'Reply-To');
+    // save the form
+    I.click('Save');
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane li.settings-list-item[data-id="0"]');
+
+    I.openApp('Mail');
+
+    // compose mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', user.get('primaryEmail'));
+
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'TestCase0381');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+    I.waitForElement('~Sent, 1 total');
+    I.waitForElement('~Inbox, 1 unread, 1 total');
+    I.seeElement('.vsplit .flag_1');
+});
