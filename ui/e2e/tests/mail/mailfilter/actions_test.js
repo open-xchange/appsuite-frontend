999eb6cd68ea5585f8918eee4ea52d1c4bb71337
Christoph Kopp <christoph.kopp@open-xchange.com>
2019-04-05 09:50:35 +0200

    Redirect filtered mail

--- ui/e2e/tests/mail/mailfilter/actions_test.js
+++ ui/e2e/tests/mail/mailfilter/actions_test.js
@@ -113,3 +113,91 @@ Scenario('[C7802] Discard filtered mail', async function (I, users) {
     I.seeElement('~Inbox');
 
 });
+
+Scenario('[C7803] Redirect filtered mail', async function (I, users) {
+
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' }
+    });
+
+    createFilterRule(I, 'TestCase0388', 'Redirect to');
+    I.fillField('to', users[1].get('primaryEmail'));
+    // save the form
+    I.click('Save');
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane li.settings-list-item[data-id="0"]');
+
+    I.openApp('Mail');
+
+    // compose mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', users[0].get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'TestCase0388');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+
+    I.waitForElement('~Sent, 1 total');
+    I.wait(1);
+    I.seeElement('~Inbox');
+    I.logout();
+
+    I.login('app=io.ox/mail', { user: users[1] });
+    I.waitForElement('~Inbox, 1 unread, 1 total');
+    I.see('TestCase0388', '.subject');
+
+});
+
+Scenario('[C7804] Move to Folder filtered mail', async function (I, users) {
+
+    let folder = 'TestCase0389';
+
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' }
+    });
+    await I.haveFolder(folder, 'mail', 'default0/INBOX');
+
+    createFilterRule(I, 'TestCase0389', 'File into');
+    I.click('Select folder');
+    I.waitForElement('.folder-picker-dialog');
+
+    I.waitForElement(locate('[role="presentation"]').withText('ox-e2e-backend.novalocal'));
+
+    I.click('[data-id="virtual/myfolders"] .folder-arrow', '.folder-picker-dialog');
+    I.waitForElement(`[data-id="default0/INBOX/${folder}"]`, '.folder-picker-dialog');
+    I.click(`[data-id="default0/INBOX/${folder}"]`, '.folder-picker-dialog');
+    I.waitForElement(`[data-id="default0/INBOX/${folder}"].selected`, '.folder-picker-dialog');
+    I.wait(1);
+    I.click('Ok');
+
+    // save the form
+    I.click('Save');
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane li.settings-list-item[data-id="0"]');
+
+    I.openApp('Mail');
+
+    // compose mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', users[0].get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'TestCase0389');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+
+    I.waitForElement('~Sent objects, 1 total');
+    I.wait(1);
+    I.waitForElement('~Inbox');
+    I.click('[data-id="virtual/myfolders"] .folder-arrow', '.io-ox-mail-window .window-sidepanel');
+    I.waitForElement(`[data-id="default0/INBOX/${folder}"]`, '.io-ox-mail-window .window-sidepanel');
+    I.click(`[data-id="default0/INBOX/${folder}"]`, '.io-ox-mail-window .window-sidepanel');
+    I.waitForElement(`[data-id="default0/INBOX/${folder}"].selected`, '.io-ox-mail-window .window-sidepanel');
+    I.wait(1);
+    I.waitForElement('~TestCase0389, 1 unread');
+    I.see('TestCase0389', '.subject');
+
+});
