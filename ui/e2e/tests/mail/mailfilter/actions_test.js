56282330e5459e9ea1863741467e2202c245fc56
Christoph Kopp <christoph.kopp@open-xchange.com>
2019-04-05 08:31:50 +0200

    Keep filtered mail

---
+++ ui/e2e/tests/mail/mailfilter/actions_test.js
@@ -0,0 +1,86 @@
+/**
+* This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+* LICENSE. This work is protected by copyright and/or other applicable
+* law. Any use of the work other than as authorized under this license
+* or copyright law is prohibited.
+*
+* http://creativecommons.org/licenses/by-nc-sa/2.5/
+* © 2019 OX Software GmbH, Germany. info@open-xchange.com
+*
+* @author Christoph Kopp <christoph.kopp@open-xchange.com>
+*/
+
+/// <reference path="../../../steps.d.ts" />
+
+Feature('Mail > Mailfilter');
+
+Before(async function (users) {
+    await users.create();
+    await users.create();
+});
+
+After(async function (users) {
+    await users.removeAll();
+});
+
+function createFilterRule(I, name, action) {
+    I.login('app=io.ox/settings');
+    I.waitForVisible('.io-ox-settings-main');
+    I.selectFolder('Mail');
+    I.waitForVisible('.rightside h1');
+
+    // open mailfilter settings
+    I.selectFolder('Filter Rules');
+
+    // checks the h1 and the empty message
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane .io-ox-mailfilter-settings h1');
+    I.see('Mail Filter Rules');
+
+    I.see('There is no rule defined');
+
+    // create a test rule and check the inintial display
+    I.click('Add new rule');
+    I.see('Create new rule');
+    I.see('This rule applies to all messages. Please add a condition to restrict this rule to specific messages.');
+    I.see('Please define at least one action.');
+
+    I.fillField('rulename', name);
+
+    // add condition
+    I.click('Add condition');
+    I.click('Subject');
+    I.fillField('values', name);
+
+    // add action
+    I.click('Add action');
+    I.click(action);
+
+}
+
+Scenario('[C7801] Keep filtered mail', async function (I, users) {
+    let [user] = users;
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' }
+    });
+
+    createFilterRule(I, 'C7801', 'Keep');
+    // save the form
+    I.click('Save');
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane li.settings-list-item[data-id="0"]');
+
+    I.openApp('Mail');
+
+    // compose mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', user.get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'C7801');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+    I.waitForElement('~Sent, 1 total');
+    I.waitForElement('~Inbox, 1 unread, 1 total');
+    I.see('C7801', '.subject');
+});
