7b321f2e011596c5f1b87179579a0fb12a0bda52
Christoph Kopp <christoph.kopp@open-xchange.com>
2019-04-08 13:56:00 +0200

    Filter mail using is exactly

--- ui/e2e/tests/mail/mailfilter/create_test.js
+++ ui/e2e/tests/mail/mailfilter/create_test.js
@@ -194,6 +194,48 @@ Scenario('[C7810] Filter mail using contains', async function (I, users) {
     I.waitForElement(locate('.list-item-row').withChild(':not(.flag_1)').withText('xxxTestCase0395xxx'));
 });
 
+Scenario('[C7811] Filter mail using is exactly', async function (I, users) {
+    let [user] = users;
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' }
+    });
+
+    createFilterRule(I, 'TestCase0396', 'Subject', 'Is exactly', 'TestCase0396', 'Red');
+    // save the form
+    I.click('Save');
+    I.waitForVisible('.io-ox-settings-window .settings-detail-pane li.settings-list-item[data-id="0"]');
+    I.openApp('Mail');
+
+    // compose mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', user.get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'TestCase0396');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+
+    // second mail
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', user.get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'xxxTestCase0396xxx');
+    I.fillField({ css: 'textarea.plain-text' }, 'This is a test');
+    I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
+
+    I.click('Send');
+
+    I.waitForElement('~Sent, 2 total');
+    I.waitForElement('~Inbox, 2 unread, 2 total');
+
+    I.waitForElement(locate('.list-item-row').withChild('.flag_1').withText('TestCase0396'));
+    I.waitForElement(locate('.list-item-row').withChild(':not(.flag_1)').withText('xxxTestCase0396xxx'));
+
+});
+
 Scenario('[C83386] Create mail filter based on mail', async function (I, users) {
     await I.haveSetting({
         'io.ox/mail': { messageFormat: 'text' }
