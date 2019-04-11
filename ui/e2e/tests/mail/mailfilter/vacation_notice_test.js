441253c29a1e65b48995b6fcda4eda81c49c6b2e
Christoph Kopp <christoph.kopp@open-xchange.com>
2019-04-11 08:16:41 +0200

    Vacation notice is time zone capable

--- ui/e2e/tests/mail/mailfilter/vacation_notice_test.js
+++ ui/e2e/tests/mail/mailfilter/vacation_notice_test.js
@@ -130,3 +130,67 @@ Scenario('[C163027] User gets notified if a vacation notice is avtive (banner in
     I.waitForElement('[data-action="edit-vacation-notice"]');
     I.see('Your vacation notice is active');
 });
+
+Scenario('[C110281] Vacation notice is time zone capable', async function (I, users) {
+
+    await I.haveSetting({
+        'io.ox/mail': { messageFormat: 'text' },
+        'io.ox/core': { timezone: 'Europe/London' }
+    }, { user: users[0] });
+    await I.haveSetting({
+        'io.ox/core': { timezone: 'Pacific/Kiritimati' }
+    }, { user: users[1] });
+
+    I.login('app=io.ox/settings', { user: users[1] });
+    I.waitForVisible('.rightside h1');
+    I.see('Basic settings', '.rightside h1');
+
+    I.waitForElement('li[data-id="virtual/settings/io.ox/mail"]');
+    I.selectFolder('Mail');
+    I.waitForVisible('.rightside .io-ox-mail-settings');
+    I.see('Mail', '.rightside h1');
+
+    I.waitForVisible('[data-action="edit-vacation-notice"]');
+    I.see('Vacation notice ...', '[data-action="edit-vacation-notice"]');
+    I.click('Vacation notice ...', '.form-group.buttons [data-action="edit-vacation-notice"]');
+    I.waitForElement('[data-point="io.ox/mail/vacation-notice/edit"]');
+
+    // enable
+    I.click('.modal-header .checkbox.switch.large');
+
+    I.fillField('input[name="subject"]', 'Vacation subject');
+    I.fillField('textarea[name="text"]', 'Vacation text');
+
+    I.click('Send vacation notice during this time only');
+    I.click('Apply changes');
+
+    I.see('Vacation notice ...', '[data-action="edit-vacation-notice"]');
+
+    I.waitForElement('[data-action="edit-vacation-notice"] .fa-toggle-on');
+    I.waitForInvisible('[data-point="io.ox/mail/vacation-notice/edit"]');
+
+    I.logout();
+
+    I.login('app=io.ox/mail', { user: users[0] });
+
+    // compose mail for user 1
+    I.clickToolbar('Compose');
+    I.waitForVisible('.io-ox-mail-compose textarea.plain-text,.io-ox-mail-compose .contenteditable-editor');
+    I.wait(1);
+    I.fillField('.io-ox-mail-compose div[data-extension-id="to"] input.tt-input', users[1].get('primaryEmail'));
+    I.fillField('.io-ox-mail-compose [name="subject"]', 'Test subject');
+    I.fillField({ css: 'textarea.plain-text' }, 'Test text');
+    I.seeInField({ css: 'textarea.plain-text' }, 'Test text');
+
+    I.click('Send');
+
+    // check for mail
+    I.waitForElement('~Sent, 1 total');
+    I.waitForElement('~Inbox, 1 unread, 1 total');
+
+    I.waitForVisible('.io-ox-mail-window .leftside ul li.unread');
+    I.click('.io-ox-mail-window .leftside ul li.unread');
+    I.waitForVisible('.io-ox-mail-window .mail-detail-pane .subject');
+    I.see('Vacation subject', '.mail-detail-pane');
+
+});
