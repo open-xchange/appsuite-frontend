7164679a2030762bb9672374faf1ce16c5efd186
Maik Schäfer <maik.schaefer@open-xchange.com>
2019-04-12 14:27:30 +0200

    Change the Signature

--- ui/e2e/tests/mail/compose/signature_test.js
+++ ui/e2e/tests/mail/compose/signature_test.js
@@ -13,7 +13,7 @@
 
 /// <reference path="../../../steps.d.ts" />
 
-Feature('Mail Compose Signatures');
+Feature('Mail > Compose > Signatures');
 
 Before(async (users) => {
     await users.create();
@@ -22,6 +22,7 @@ Before(async (users) => {
 After(async (users) => {
     await users.removeAll();
 });
+
 
 Scenario('[C8825] Add and replace signatures', async function (I) {
 
@@ -55,5 +56,63 @@ Scenario('[C8825] Add and replace signatures', async function (I) {
     within({ frame: '#mce_0_ifr' }, () => {
         I.waitForText('Super original and fabulous signature');
         I.dontSee('Very original and clever signature');
+    });
+});
+
+Scenario('[C265555] Change the Signature', async function (I) {
+
+    const firstSignatureContent = 'Very original and clever signature',
+        secondSignatureContent = 'Super original and fabulous signature',
+        firstSignature = await I.haveSnippet({
+            content: '<p>' + firstSignatureContent + '</p>',
+            displayname: 'My signature',
+            misc: { insertion: 'above', 'content-type': 'text/html' },
+            module: 'io.ox/mail',
+            type: 'signature'
+        });
+    await I.haveSnippet({
+        content: '<p>' + secondSignatureContent + '</p>',
+        displayname: 'Super signature',
+        misc: { insertion: 'above', 'content-type': 'text/html' },
+        module: 'io.ox/mail',
+        type: 'signature'
+    });
+
+    await I.haveSetting({ 'io.ox/mail': { defaultSignature: firstSignature.data } });
+
+    I.login('app=io.ox/mail');
+    I.clickToolbar('Compose');
+    I.waitForFocus('input[type="email"].token-input.tt-input');
+    within({ frame: '#mce_0_ifr' }, () => {
+        I.waitForText(firstSignatureContent);
+    });
+    I.fillField('input[type="email"].token-input.tt-input', 'foo@bar');
+    I.wait(1);
+    I.fillField('input[name="subject"]', 'test subject');
+    I.click('Discard');
+    I.click('Save as draft');
+    I.wait(1);
+    I.selectFolder('Drafts');
+    I.waitForText('test subject');
+    I.click('li[data-index="0"]');
+    I.clickToolbar({ css: '.io-ox-mail-window .classic-toolbar [data-action="more"]' });
+    I.click('Move', '.dropdown.open .dropdown-menu');
+    I.waitForVisible('.modal-footer');
+    I.click('Move', '.modal-footer');
+    I.dontSee('test subject');
+    I.selectFolder('Inbox');
+    I.waitForText('test subject');
+    I.click('li[data-index="0"]');
+    I.clickToolbar('~Reply to sender');
+    I.waitForVisible('.io-ox-mail-compose-window');
+    within({ frame: '#mce_1_ifr' }, () => {
+        I.waitForVisible(locate('blockquote').withText(firstSignatureContent));
+        I.waitForVisible(locate('div.io-ox-signature').withText(firstSignatureContent));
+    });
+    I.click('Signatures');
+    I.click('Super signature');
+    within({ frame: '#mce_1_ifr' }, () => {
+        I.waitForVisible(locate('div.io-ox-signature').withText(secondSignatureContent));
+        I.waitForVisible(locate('blockquote').withText(firstSignatureContent));
     });
 });
