88ab387f33b2b52a767ad97720f98a21e9a156ad
Markus Wagner <markus.wagner@open-xchange.com>
2019-04-24 09:57:47 +0200

    Add calendar widget

--- ui/e2e/tests/settings/portal_test.js
+++ ui/e2e/tests/settings/portal_test.js
@@ -64,3 +64,51 @@ Scenario('[C7822] Add Birthday widget', async function (I) {
     I.waitForText(contactDisplayName, 10, '.io-ox-sidepopup');
     I.waitForText('Buy a gift', 10, '.io-ox-sidepopup');
 });
+
+Scenario('[C7823] Add calendar widget', async function (I) {
+
+    const moment = require('moment');
+
+    const appointmentSummary = 'Summary7823';
+    const appointmentLocation = 'Location7823';
+    const appointmentDescription = 'Description7823';
+
+    const time = moment().add(6, 'hours');
+    const format = 'YYYYMMDD[T]HHmm00'; //YYYYMMDD[T]HHmmss
+    const timezone = 'Europe/Berlin';
+
+    // create an appointment which is shown in the portal widget
+    await I.haveAppointment({
+        folder: `cal://0/${await I.grabDefaultFolder('calendar')}`,
+        summary: appointmentSummary,
+        location: appointmentLocation,
+        description: appointmentDescription,
+        startDate: { value: time.format(format), tzid: timezone },
+        endDate: { value: time.add(1, 'hour').format(format), tzid: timezone }
+    });
+
+    // clear the portal settings
+    await I.haveSetting('io.ox/portal//widgets/user', '{}');
+
+    I.login(['app=io.ox/settings', 'folder=virtual/settings/io.ox/portal']);
+    I.waitForText('Portal settings');
+
+    // Add the portal widget
+    I.click('Add widget');
+    I.click('Appointments', '.io-ox-portal-settings-dropdown');
+
+    // Verify that the widget is shown in the list
+    I.waitForElement('~Color Appointments');
+
+    // Switch to portal an check the widget
+    I.openApp('Portal');
+    I.waitForText('Appointments');
+    I.waitForText(appointmentSummary);
+
+    // open the side popup
+    I.click('.item', '~Appointments');
+    I.waitForElement('.io-ox-sidepopup');
+    I.waitForText(appointmentSummary, '.io-ox-sidepopup');
+    I.waitForText(appointmentLocation, '.io-ox-sidepopup');
+
+});
