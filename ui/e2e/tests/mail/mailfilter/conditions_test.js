19183dd68b5d7fe96826629ad3fc01aaf786ba7d
Julian Bäume <julian.baeume@open-xchange.com>
2019-04-29 13:29:01 +0200

    Filter mail on any recipient

--- ui/e2e/tests/mail/mailfilter/conditions_test.js
+++ ui/e2e/tests/mail/mailfilter/conditions_test.js
@@ -112,7 +112,7 @@ Scenario('[C7793] Filter mail on any recipient', async function (I, users) {
     I.seeInField({ css: 'textarea.plain-text' }, 'This is a test');
 
     I.click('Send');
-    I.waitForElement('~Sent objects, 1 total');
+    I.waitForElement('~Sent, 1 total');
     I.waitForElement('~Inbox, 1 unread, 1 total');
     I.seeElement('.vsplit .flag_1');
 
