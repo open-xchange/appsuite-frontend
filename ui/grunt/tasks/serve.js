84aa42189c91ac5b67221023f793e5a7364ccacf
Frank Paczynski <frank.paczynski@open-xchange.com>
2019-05-03 10:35:00 +0200

    adjust 'grunt serve' to reflect recent changes
    - related to c509eb3f64cc90da31f97175b9addbe7be549d9b
    - warn in case certificates are missing

--- ui/grunt/tasks/serve.js
+++ ui/grunt/tasks/serve.js
@@ -1,12 +1,20 @@
 module.exports = function (grunt) {
-    var key = grunt.config('local.appserver.key') || 'ssl/host.key';
-    var cert = grunt.config('local.appserver.cert') || 'ssl/host.crt';
-    var ca = grunt.config('local.appserver.ca') || 'ssl/rootCA.crt';
+    var conf = {
+        key: grunt.config('local.appserver.key') || 'ssl/host.key',
+        cert: grunt.config('local.appserver.cert') || 'ssl/host.crt',
+        ca: grunt.config('local.appserver.ca') || 'ssl/rootCA.crt'
+    };
 
-    if (grunt.file.exists(key)) key = grunt.file.read(key);
-    if (grunt.file.exists(cert)) cert = grunt.file.read(cert);
-    if (grunt.file.exists(ca)) ca = grunt.file.read(ca);
-    grunt.config.set('connect.server.options.key', key);
-    grunt.config.set('connect.server.options.cert', cert);
-    grunt.config.set('connect.server.options.ca', ca);
+    (Object.keys(conf)).forEach(function (key) {
+        var value = conf[key];
+        // contains certificate content already
+        if (value.indexOf('-----') === 0) return;
+        // read file content
+        if (!grunt.file.exists(value)) grunt.fail.warn('Missing certificate file: "' + value + '"');
+        conf[key] = grunt.file.read(value);
+    });
+
+    grunt.config.set('connect.server.options.key', conf.key);
+    grunt.config.set('connect.server.options.cert', conf.cert);
+    grunt.config.set('connect.server.options.ca', conf.ca);
 };
