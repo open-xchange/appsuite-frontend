ae54edd1a9630fbb9346c641a52a3f49f96f9eb6
Julian Bäume <julian.baeume@open-xchange.com>
2019-05-09 18:54:39 +0200

    fix checkout problems on windows systems
    move problematic files into single json file and extract them from there.
    Helpers don't support uploading strings, yet, so this code has been forked for now
    until helpers support it.

--- ui/e2e/tests/mail/bad_mails/xss_examples_test.js
+++ ui/e2e/tests/mail/bad_mails/xss_examples_test.js
@@ -17,7 +17,10 @@ Feature('Mail > Listview @codeReview');
 
 const fs = require('fs'),
     util = require('util'),
-    readdir = util.promisify(fs.readdir);
+    readFile = util.promisify(fs.readFile),
+    assert = require('assert'),
+    FormData = require('form-data'),
+    helperUtil = require('@open-xchange/codecept-helper').util;
 
 Before(async function (users) {
     await users.create();
@@ -28,21 +31,40 @@ After(async function (users) {
 });
 
 async function importMails(I, path, start, end) {
-    const files = await readdir(path);
+    const files = JSON.parse(await readFile(path));
     let size = 0;
     // forEach does not handle await correctly
     I.say(start + ':' + end + ':' + files.length, 'blue');
     for (let index = 0; index < files.length; index++) {
         if (index >= start && index < end) {
             size++;
-            await I.haveMail({ folder: 'default0/INBOX', path: `${path}/${files[index]}` });
+            const { httpClient, session } = await helperUtil.getSessionForUser();
+            let form, response;
+
+            // import the mail
+            form = new FormData();
+            form.append('file', files[index].content, { filename: files[index].filename, 'Content-Type': files[index]['Content-Type'] });
+            response = await httpClient.post('/appsuite/api/mail', form, {
+                params: {
+                    action: 'import',
+                    session: session,
+                    folder: 'default0/INBOX',
+                    force: true
+                },
+                headers: form.getHeaders()
+            });
+
+            // magic hack to just get the json out of the html response
+            const matches = /\((\{.*?\})\)/.exec(response.data);
+            const resData = matches && matches[1] ? JSON.parse(matches[1]) : response.data;
+            assert.strictEqual(resData.error, undefined, JSON.stringify(resData));
         }
     }
     return size;
 }
 
 async function run(I, start, end) {
-    var size = await importMails(I, 'e2e/media/mails/badmails-xss', start, end);
+    var size = await importMails(I, 'e2e/media/mails/badmails-xss.json', start, end);
     I.login('app=io.ox/mail');
     I.waitForVisible('.io-ox-mail-window .list-view');
     for (let i = (size - 1); i >= 0; i--) {
@@ -52,20 +74,20 @@ async function run(I, start, end) {
 }
 
 Scenario('[C204747] Listing mailbox content (chunk #1)', async function (I) {
-    await run(I, 0, 20, 'e2e/media/mails/badmails-xss');
+    await run(I, 0, 20, 'e2e/media/mails/badmails-xss.json');
 });
 Scenario('[C204747] Listing mailbox content (chunk #2)', async function (I) {
-    await run(I, 20, 40, 'e2e/media/mails/badmails-xss');
+    await run(I, 20, 40, 'e2e/media/mails/badmails-xss.json');
 });
 Scenario('[C204747] Listing mailbox content (chunk #3)', async function (I) {
-    await run(I, 40, 60, 'e2e/media/mails/badmails-xss');
+    await run(I, 40, 60, 'e2e/media/mails/badmails-xss.json');
 });
 Scenario('[C204747] Listing mailbox content (chunk #4)', async function (I) {
-    await run(I, 60, 80, 'e2e/media/mails/badmails-xss');
+    await run(I, 60, 80, 'e2e/media/mails/badmails-xss.json');
 });
 Scenario('[C204747] Listing mailbox content (chunk #5)', async function (I) {
-    await run(I, 80, 100, 'e2e/media/mails/badmails-xss');
+    await run(I, 80, 100, 'e2e/media/mails/badmails-xss.json');
 });
 Scenario('[C204747] Listing mailbox content (chunk #6)', async function (I) {
-    await run(I, 100, 120, 'e2e/media/mails/badmails-xss');
+    await run(I, 100, 120, 'e2e/media/mails/badmails-xss.json');
 });
