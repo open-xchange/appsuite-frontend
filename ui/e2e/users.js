b2c8145be90b688332475e32c4c2d4776f767ddb
Julian Bäume <julian.baeume@open-xchange.com>
2019-05-16 17:19:35 +0200

    set access combination by name for new users
    not sure this is the way to go, but it seems, this can't be done for new contexts
    created via SOAP API. Works fine using cli, though. Needs investigation.

--- ui/e2e/users.js
+++ ui/e2e/users.js
@@ -1,9 +1,21 @@
 const users = require('@open-xchange/codecept-helper').users,
     contexts = require('@open-xchange/codecept-helper/src/contexts'),
+    helper = new (require('@open-xchange/codecept-helper').helper)(),
     wrappedCreate = users.create;
 
 users.create = function createUsers(user, ctxt) {
-    return wrappedCreate.apply(this, [user, ctxt || contexts[0]]);
+    return wrappedCreate.apply(this, [user, ctxt || { id: contexts[0].id }])
+        .then(user => {
+            user.setAccessCombinationByName = function (access_combination_name) {
+                return helper.executeSoapRequest('OXUserService', 'changeByModuleAccessName', {
+                    ctx: { id: user.context.id },
+                    access_combination_name,
+                    user: { id: user.get('id') },
+                    auth: user.auth
+                });
+            };
+            return user.setAccessCombinationByName('all').then(()=> user);
+        });
 };
 
 module.exports = users;
