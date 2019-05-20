open-xchange/appsuite-frontend

commit b1bcd5ff555dec16c3686577862f1ff0f1eec8fd

OXUI-415: Remove old calendar api and old calendar model

ui/apps/io.ox/calendar/list/listview.js
@@ -13,15 +13,14 @@
 
 define('io.ox/calendar/list/listview', [
     'io.ox/calendar/util',
-    'io.ox/calendar/chronos-util',
-    'io.ox/calendar/chronos-model',
-    'io.ox/calendar/chronos-api',
+    'io.ox/calendar/model',
+    'io.ox/calendar/api',
     'io.ox/core/extensions',
     'io.ox/core/folder/api',
     'io.ox/core/tk/list',
     'gettext!io.ox/calendar',
     'io.ox/calendar/list/view-options'
-], function (util, chronosUtil, models, api, ext, folderAPI, ListView, gt) {
+], function (util, models, api, ext, folderAPI, ListView, gt) {
 
     'use strict';
 
@@ -87,7 +86,7 @@ define('io.ox/calendar/list/listview', [
             this.append(
                 $('<div class="date">')
                     .text(util.getDateInterval(model.attributes))
-                    .toggle(!chronosUtil.isAllday(model) && (util.getDurationInDays(model.attributes) > 0))
+                    .toggle(!util.isAllday(model) && (util.getDurationInDays(model.attributes) > 0))
             );
         }
     });

ui/apps/io.ox/calendar/actions.js
@@ -14,9 +14,8 @@
 define('io.ox/calendar/actions', [
     'io.ox/core/extensions',
     'io.ox/core/extPatterns/links',
-    'io.ox/calendar/chronos-api',
+    'io.ox/calendar/api',
     'io.ox/calendar/util',
-    'io.ox/calendar/chronos-util',
     'io.ox/core/extPatterns/actions',
     'io.ox/core/print',
     'gettext!io.ox/calendar',
@@ -24,7 +23,7 @@ define('io.ox/calendar/actions', [
     'io.ox/calendar/actions/change-confirmation',
     'io.ox/core/folder/api',
     'settings!io.ox/calendar'
-], function (ext, links, api, util, chronosUtil, actions, print, gt, capabilities, changeStatus, folderAPI, settings) {
+], function (ext, links, api, util, actions, print, gt, capabilities, changeStatus, folderAPI, settings) {
 
     'use strict';
 
@@ -354,7 +353,7 @@ define('io.ox/calendar/actions', [
                         }
                     }
 
-                    freetime.getApp().launch({ startDate: startDate, attendees: [chronosUtil.createAttendee(user, { partStat: 'ACCEPTED' })] });
+                    freetime.getApp().launch({ startDate: startDate, attendees: [util.createAttendee(user, { partStat: 'ACCEPTED' })] });
                 });
             });
         }

ui/apps/io.ox/calendar/actions/acceptdeny.js
@@ -12,16 +12,15 @@
  */
 
 define('io.ox/calendar/actions/acceptdeny', [
-    'io.ox/calendar/chronos-api',
+    'io.ox/calendar/api',
     'io.ox/backbone/mini-views/alarms',
     'io.ox/core/tk/dialogs',
     'io.ox/core/folder/api',
     'io.ox/calendar/util',
-    'io.ox/calendar/chronos-util',
     'io.ox/core/notifications',
     'settings!io.ox/calendar',
     'gettext!io.ox/calendar'
-], function (calApi, AlarmsView, dialogs, folderAPI, util, chronosUtil, notifications, settings, gt) {
+], function (calApi, AlarmsView, dialogs, folderAPI, util, notifications, settings, gt) {
 
     'use strict';
 
@@ -79,7 +78,7 @@ define('io.ox/calendar/actions/acceptdeny', [
                         // convenience function to convert old alarms into new chronos alarms
                         // TODO remove once migration process is implemented
                         if (options.taskmode) {
-                            appointmentData.alarms = chronosUtil.convertAlarms(appointmentData.alarms);
+                            appointmentData.alarms = util.convertAlarms(appointmentData.alarms);
                         }
                     }
                     // backbone model is fine. No need to require chronos model

ui/apps/io.ox/calendar/actions/change-confirmation.js
@@ -12,7 +12,7 @@
  */
 
 define('io.ox/calendar/actions/change-confirmation', [
-    'io.ox/calendar/chronos-api'
+    'io.ox/calendar/api'
 ], function (calApi) {
 
     'use strict';

ui/apps/io.ox/calendar/actions/create.js
@@ -23,9 +23,9 @@ define('io.ox/calendar/actions/create', [
     'use strict';
 
     function openEditDialog(params) {
-        ox.load(['io.ox/calendar/edit/main', 'io.ox/calendar/chronos-model']).done(function (edit, chronosModel) {
+        ox.load(['io.ox/calendar/edit/main', 'io.ox/calendar/model']).done(function (edit, models) {
             edit.getApp().launch().done(function () {
-                this.create(new chronosModel.Model(params));
+                this.create(new models.Model(params));
             });
         });
     }

ui/apps/io.ox/calendar/actions/delete.js
@@ -12,7 +12,7 @@
  */
 
 define('io.ox/calendar/actions/delete', [
-    'io.ox/calendar/chronos-api',
+    'io.ox/calendar/api',
     'io.ox/core/notifications',
     'gettext!io.ox/calendar'
 ], function (api, notifications, gt) {

ui/apps/io.ox/calendar/actions/edit.js
@@ -13,7 +13,7 @@
 
 define('io.ox/calendar/actions/edit', [
     'io.ox/calendar/edit/main',
-    'io.ox/calendar/chronos-api',
+    'io.ox/calendar/api',
     'io.ox/core/notifications',
     'io.ox/calendar/util'
 ], function (m, api, notifications, util) {

commit 21a1c6967780e291c6d1d8d04f61c46e1890771d

Fixed: Bug 65208 - Search result in calender get's reordered
- caused by b1bcd5ff555dec16c3686577862f1ff0f1eec8fd
- disable client side sorting for search results

ui/apps/io.ox/calendar/list/listview.js
@@ -241,6 +241,8 @@ define('io.ox/calendar/list/listview', [
         },
 
         setCollection: function (collection) {
+            // disable sorting for search results
+            var options = this.loader.mode === 'search' ? { comparator: false } : {};
             function filter(model) {
                 if (util.getConfirmationStatus(model) !== 'DECLINED') return true;
                 return settings.get('showDeclinedAppointments', false);
@@ -249,7 +251,7 @@ define('io.ox/calendar/list/listview', [
             // use intermediate collection to filter declined appointments if necessary
             if (this.originalCollection) this.stopListening(this.originalCollection);
             this.originalCollection = collection;
-            collection = new models.Collection(collection.filter(filter));
+            collection = new models.Collection(collection.filter(filter), options);
             collection.cid = this.originalCollection.cid;
 
             // apply intermediate collection to ListView