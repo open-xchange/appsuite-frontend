dce6fa38f6f381ba373ce052f18a70a54be89276
Maik Schäfer <maik.schaefer@open-xchange.com>
2019-05-24 14:26:15 +0200

    [C45032] Edit Permissions at My shares

--- ui/e2e/tests/sharing/permissions_test.js
+++ ui/e2e/tests/sharing/permissions_test.js
@@ -44,7 +44,8 @@ Scenario('[C45032] Edit Permissions at "My shares" @shaky', async function (I, u
         I.waitForElement('.modal .list-view.address-picker li.list-item');
         I.fillField('Search', users[1].get('name'));
         I.waitForText(users[1].get('name'), 5, '.address-picker');
-        I.click('.address-picker .list-item');
+        I.waitForText(users[1].get('primaryEmail'));
+        I.click(users[1].get('primaryEmail'), '.address-picker .list-item');
         I.click({ css: 'button[data-action="select"]' });
         I.waitForElement(locate('.permissions-view .row').at(2));
         I.dontSee('Guest', '.permissions-view');
