cf6fa09f71bf208becf30158823a05ba83cd7c86
daniel.pondruff <daniel.pondruff@open-xchange.com>
2019-06-24 13:04:11 +0200

    e2e: add testrail plugin for QA testing

--- ui/codecept.conf.js
+++ ui/codecept.conf.js
@@ -113,7 +113,16 @@ module.exports.config = {
         }, 500);
     },
     plugins: {
-        allure: { enabled: true }
+        allure: { enabled: true },
+        testrail: {
+            require: './e2e/plugins/testrail',
+            host: process.env.TESTRAIL_HOST || 'https://testrail.local',
+            user: process.env.TESTRAIL_USERNAME || 'testuser',
+            password: process.env.TESTRAIL_API_KEY || 'testkey',
+            project_id: process.env.TESTRAIL_PROJECTID || '1',
+            runName: process.env.TESTRAIL_RUNNAME || 'test',
+            enabled: process.env.TESTRAIL_ENABLED || false
+        }
     },
     name: 'App Suite Core UI'
 };
