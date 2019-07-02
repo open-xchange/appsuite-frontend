e7fdc3852bc148b9e8de38b79d85aadae45a1826
Frank Paczynski <frank.paczynski@open-xchange.com>
2019-07-02 18:14:01 +0200

    tasks: move widget style to portal plugin

--- ui/apps/plugins/portal/tasks/register.js
+++ ui/apps/plugins/portal/tasks/register.js
@@ -15,8 +15,7 @@ define('plugins/portal/tasks/register', [
     'io.ox/core/extensions',
     'io.ox/tasks/api',
     'gettext!plugins/portal',
-    'io.ox/tasks/util',
-    'less!io.ox/tasks/style'
+    'io.ox/tasks/util'
 ], function (ext, taskAPI, gt, util) {
 
     'use strict';
