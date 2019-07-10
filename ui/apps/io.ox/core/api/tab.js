ad95db6ed7effad77d7ea6f8f3a578864aac67c2
kristof.kamin <kristof.kamin@open-xchange.com>
2019-07-10 11:55:25 +0200

    fixed Bug 65769 - UserID is null and ends in the "ERROR_UNKNOWN_REALTIME_FAILURE"

--- ui/apps/io.ox/core/api/tab.js
+++ ui/apps/io.ox/core/api/tab.js
@@ -608,10 +608,14 @@ define('io.ox/core/api/tab', [
      * Send a session over localStorage to other tabs
      */
     TabSession.propagateSession = function (parameters) {
-        if (!ox.session) {
+
+        // the login process is finished when session, user and user_id are set in the ox object
+        if (!ox.session || !ox.user || !ox.user_id) {
             TabSession.propagate('propagateNoSession');
             return;
         }
+
+        // the requested session is differently to ox.session. e.g. guest user vs normal login
         if (parameters.session && parameters.session !== ox.session) {
             return;
         }
