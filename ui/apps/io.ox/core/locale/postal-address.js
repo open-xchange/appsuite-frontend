768a16514c763c12604552d89e21e3674a0a6403
Frank Paczynski <frank.paczynski@open-xchange.com>
2019-07-15 10:16:55 +0200

    improve locale/format to allow country specifc address formating
    - format address based on COUNTRY of postal address
    - omit STATE for specific lands where this part is unused
    - add unittest
    
    (cherry picked from commit 65007a35dbad9ddb83abf998d3b23b8b3a50e169)
    (cherry picked from commit 4446fafbde7010ea1b4c504280e82a56218c90b4)
    (cherry picked from commit 8c141233292208c0899da9028701921c64e2405b)

--- ui/apps/io.ox/core/locale/postal-address.js
+++ ui/apps/io.ox/core/locale/postal-address.js
@@ -8,6 +8,7 @@
  *
  * © 2019 OX Software GmbH, Germany. info@open-xchange.com
  *
+ * @author Frank Paczynski <frank.paczynski@open-xchange.com>
  * @author Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
  *
  */
@@ -16,34 +17,33 @@ define('io.ox/core/locale/postal-address', [], function () {
 
     'use strict';
 
-    function format(data, type, locale) {
+    var _ = ' ', LF = '\n';
+
+    function format(data, type, countrycode) {
         type = type || 'home';
-        locale = locale || ox.locale || ox.language;
-
-        var address = {};
-        _(['street', 'postal_code', 'city', 'state', 'country']).each(function (field) {
-            address[field] = data[field + '_' + type] || '';
-        });
-
-        return getAddress(address, locale)
+        return getAddress(data, type, countrycode)
                 // remove inline spaces caused by undefined variables
                 .replace(/( )+/g, ' ')
                 .replace(/(\n |\n)+/g, '\n').trim();
     }
 
-    function getAddress(data, locale) {
-        locale = locale || ox.locale || ox.language;
+    function getAddress(data, type, countrycode) {
 
-        var location = window.test || locale.slice(3, 5) || 'US';
+        var street = $.trim(data['street_' + type]),
+            postal_code = $.trim(data['postal_code_' + type]),
+            city = $.trim(data['city_' + type]),
+            state = $.trim(data['state_' + type]),
+            country = $.trim(data['country_' + type]).toUpperCase();
 
-        switch (location) {
+        countrycode = countrycode || getCountryCode(country) || (ox.locale || ox.language || '').slice(3, 5) || 'US';
+
+        switch (countrycode) {
             case 'AR':
             case 'AT':
             case 'BE':
             case 'BO':
             case 'CH':
             case 'CZ':
-            case 'DE':
             case 'DK':
             case 'EE':
             case 'ES':
@@ -52,80 +52,107 @@ define('io.ox/core/locale/postal-address', [], function () {
             case 'IT':
             case 'NO':
             case 'SE':
-                return data.street + '\n' +
-                       data.postal_code + ' ' + data.city + ' ' + data.state + '\n' +
-                       data.country;
+                return street + LF +
+                       postal_code + _ + city + _ + state + LF +
+                       country;
+
+            // omits 'state'
+            case 'DE':
+                return street + LF +
+                       postal_code + _ + city + LF +
+                       country;
 
             case 'AU':
             case 'CA':
             case 'PR':
             case 'US':
-                return data.street + '\n' +
-                       data.city + ' ' + data.state + ' ' + data.postal_code + '\n' +
-                       data.country;
+                return street + LF +
+                       city + _ + state + _ + postal_code + LF +
+                       country;
 
             case 'IE':
             case 'GB':
             case 'RU':
-                return data.street + '\n' +
-                       data.city + '\n' +
-                       data.state + ' ' + data.postal_code + '\n' +
-                       data.country;
+                return street + LF +
+                       city + LF +
+                       state + _ + postal_code + LF +
+                       country;
 
             case 'NZ':
             case 'SG':
-                return data.street + '\n' +
-                       data.city + ' ' + data.postal_code + ' ' + data.state + '\n' +
-                       data.country;
+                return street + LF +
+                       city + _ + postal_code + _ + state + LF +
+                       country;
 
             case 'CN':
-                return data.street + ' ' + data.city + '\n' +
-                       data.postal_code + ' ' + data.state + '\n' +
-                       data.country;
+                return street + _ + city + LF +
+                       postal_code + _ + state + LF +
+                       country;
 
             case 'CR':
-                return data.street + '\n' +
-                       data.city + ' ' + data.state + '\n' +
-                       data.postal_code + ' ' + data.country;
+                return street + LF +
+                       city + _ + state + LF +
+                       postal_code + _ + country;
 
             case 'JP':
-                return data.street + '\n' +
-                       data.state + ' ' + data.city + ' ' + data.postal_code + '\n' +
-                       data.country;
+                return street + LF +
+                       state + _ + city + _ + postal_code + LF +
+                       country;
 
             case 'MX':
-                return data.street + '\n' +
-                       data.city + '\n' +
-                       data.postal_code + ' ' + data.state + '\n' +
-                       data.country;
+                return street + LF +
+                       city + LF +
+                       postal_code + _ + state + LF +
+                       country;
 
             case 'NL':
-                return data.street + '\n' +
-                       data.postal_code + ' ' + data.state + ' ' + data.city + '\n' +
-                       data.country;
+                return street + LF +
+                       postal_code + _ + state + _ + city + LF +
+                       country;
 
             case 'TW':
-                return data.street + '\n' +
-                       data.city + ' ' + data.state + ' ' + data.postal_code + '\n' +
-                       data.country;
+                return street + LF +
+                       city + _ + state + _ + postal_code + LF +
+                       country;
 
             case 'ZA':
-                return data.street + '\n' +
-                       data.city + '\n' +
-                       data.state + '\n' +
-                       data.postal_code + ' ' + data.country;
+                return street + LF +
+                       city + LF +
+                       state + LF +
+                       postal_code + _ + country;
 
             case 'RO':
             default:
-                return data.street + '\n' +
-                       data.postal_code + ' ' + data.city + '\n' +
-                       data.state + '\n' +
-                       data.country;
+                return street + LF +
+                       postal_code + _ + city + LF +
+                       state + LF +
+                       country;
         }
     }
 
+    function getCountryCode(country) {
+        country = $.trim(country).toLowerCase();
+
+        // order: en, de, fr, es, it
+        if (/^(germany|deutschland|allemange|alemania|germania|)$/.test(country)) return 'DE';
+        if (/^(united states|usa?|vereinigte staaten|(é|e)tats(-| )unis|estados unidos|stati uniti d.america)$/.test(country)) return 'US';
+        if (/^(uk|united kingdom|great britain|großbritannien|royaume-?uni|reino unido|regno Unito)$/.test(country)) return 'GB';
+        if (/^(france|frankreich|francia)$/.test(country)) return 'FR';
+        if (/^(spain|spanien|espagne|españa|spagna)$/.test(country)) return 'ES';
+        if (/^(italy|italien|itelie|italia)$/.test(country)) return 'IT';
+        if (/^((the )?netherlands|niederlande|(les )?pays-bas|(los )?pa(í|i)ses bajos|paesi bassi|nederland)$/.test(country)) return 'NL';
+        if (/^(finland|finnland|finlande|finlandia|suomi)$/.test(country)) return 'FI';
+        if (/^(japan|japon|japona|jap(ó|o)n|giappone|日本)$/.test(country)) return 'JP';
+        // just en, de and own
+        if (/^(austria|österreich)$/.test(country)) return 'AT';
+        if (/^(switzerland|schweiz|suisse|svizzera)$/.test(country)) return 'CH';
+
+        return '';
+    }
+
     return {
-        format: format
+        format: format,
+        getCountryCode: getCountryCode
     };
 
 });
