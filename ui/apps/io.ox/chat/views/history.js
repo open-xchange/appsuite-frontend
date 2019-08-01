a7334630e57f74d81ca81aa22bb2e2d852637087
Richard Petersen <richard.petersen@open-xchange.com>
2019-08-01 14:19:40 +0200

    Add toolbar to history

--- ui/apps/io.ox/chat/views/history.js
+++ ui/apps/io.ox/chat/views/history.js
@@ -12,12 +12,49 @@
  */
 
 define('io.ox/chat/views/history', [
+    'io.ox/core/extensions',
     'io.ox/backbone/views/disposable',
     'io.ox/chat/views/chatAvatar',
-    'io.ox/chat/data'
-], function (DisposableView, ChatAvatar, data) {
+    'io.ox/chat/data',
+    'io.ox/backbone/views/toolbar'
+], function (ext, DisposableView, ChatAvatar, data, ToolbarView) {
 
     'use strict';
+
+    ext.point('io.ox/chat/history/toolbar').extend({
+        id: 'back',
+        index: 100,
+        custom: true,
+        draw: function () {
+            this.attr('data-prio', 'hi').append(
+                $('<a href="#" role="menuitem" draggable="false" tabindex="-1" data-cmd="close-chat">').append(
+                    $('<i class="fa fa-chevron-left" aria-hidden="true">').css({ 'margin-right': '4px' }), 'Back'
+                )
+            );
+        }
+    });
+
+    ext.point('io.ox/chat/history/toolbar').extend({
+        id: 'title',
+        index: 200,
+        custom: true,
+        draw: function () {
+            this.addClass('toolbar-title').attr('data-prio', 'hi').text('Recent conversations');
+        }
+    });
+
+    ext.point('io.ox/chat/history/toolbar').extend({
+        id: 'switch-to-floating',
+        index: 300,
+        custom: true,
+        draw: function () {
+            this.attr('data-prio', 'hi').append(
+                $('<a href="#" role="menuitem" draggable="false" tabindex="-1" data-cmd="switch-to-floating">').append(
+                    $('<i class="fa fa-external-link" aria-hidden="true">')
+                )
+            );
+        }
+    });
 
     var History = DisposableView.extend({
 
@@ -37,10 +74,11 @@ define('io.ox/chat/views/history', [
 
         render: function () {
             this.$el.append(
-                $('<div class="header abs">').append(
+                $('<div class="header">').append(
                     $('<h2>').append('Recent conversations')
                 ),
-                $('<div class="scrollpane abs">').append(
+                new ToolbarView({ point: 'io.ox/chat/history/toolbar', title: 'History actions' }).render(new ext.Baton()).$el,
+                $('<div class="scrollpane">').append(
                     $('<ul>').append(
                         this.getItems().map(this.renderItem, this)
                     )
