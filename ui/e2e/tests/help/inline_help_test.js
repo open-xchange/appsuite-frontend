444204c709d631127b77f8a94fc8dc1f884390db
Luca Stein <luca.stein@open-xchange.com>
2019-09-30 03:41:25 -0700

    help window language check

--- ui/e2e/tests/help/inline_help_test.js
+++ ui/e2e/tests/help/inline_help_test.js
@@ -64,3 +64,54 @@ Scenario('Open the help app in a modal', async function (I) {
 
     I.logout();
 });
+
+Scenario('Check help window for supposed language', async function (I) {
+
+    // check major languages
+    var languages = {
+        'de_DE': ['help/l10n/de_DE/ox.appsuite.user.sect.settings.globalsettings.html', 'Deutsch (Deutschland)', 'Grundeinstellungen anpassen', '~Schließen'],
+        'en_US': ['help/l10n/en_US/ox.appsuite.user.sect.settings.globalsettings.html', 'English (United States)', 'Customizing the basic settings', '~Close'],
+        'en_GB': ['help/l10n/en_GB/ox.appsuite.user.sect.settings.globalsettings.html', 'English (United Kingdom)', 'Customising the Basic Settings', '~Close'],
+        'es_ES': ['help/l10n/es_ES/ox.appsuite.user.sect.settings.globalsettings.html', 'Español (Espana)', 'Personalización de la configuración básica', '~Cerrar'],
+        'es_MX': ['help/l10n/es_MX/ox.appsuite.user.sect.settings.globalsettings.html', 'Español (México)', 'Personalización de la configuración básica', '~Cerrar'],
+        'fr_FR': ['help/l10n/fr_FR/ox.appsuite.user.sect.settings.globalsettings.html', 'Français (France)', 'Personnaliser les réglages de base', '~Fermer'],
+        'it_IT': ['help/l10n/it_IT/ox.appsuite.user.sect.settings.globalsettings.html', 'Italiano (Italia)', 'Personalizzare le impostazioni di base','~Chiudi'],
+        'ja_JP': ['help/l10n/ja_JP/ox.appsuite.user.sect.settings.globalsettings.html', '日本語 (日本)', '基本設定のカスタマイズ','~閉じる'],
+        'nl_NL': ['help/l10n/nl_NL/ox.appsuite.user.sect.settings.globalsettings.html', 'Nederlands (Nederland)', 'Aanpassen van de Basisinstellingen','~Sluiten'],
+        'pl_PL': ['help/l10n/pl_PL/ox.appsuite.user.sect.settings.globalsettings.html', 'Polski (Polska)', 'Dostosowywanie ustawień podstawowych','~Zamknij'],
+        'tr_TR': ['help/l10n/tr_TR/ox.appsuite.user.sect.settings.globalsettings.html', 'Türkçe (Türkiye)', 'Temel Ayarları Özelleştirme','~Kapat'],
+        'zh_CN': ['help/l10n/zh_CN/ox.appsuite.user.sect.settings.globalsettings.html', '中文 (简体)', '自定义基本设置','~关闭'],
+        'zh_TW': ['help/l10n/zh_TW/ox.appsuite.user.sect.settings.globalsettings.html', '中文 (繁體)', '自訂基礎設定','~關閉']
+    };
+    
+    I.login(['app=io.ox/settings', 'folder=virtual/settings/io.ox/core']);
+
+    for (var id in languages) {
+        //Select language
+        I.waitForElement('select[name="language"]');
+        I.selectOption('select[name="language"]', languages[id][1]);
+        
+        // wait for "yell" so that the change arrived at server
+        I.waitForVisible('.io-ox-alert', 30);
+        I.refreshPage();
+
+        //open help window
+        I.waitForElement('#io-ox-context-help-icon', 20);
+        I.click('#io-ox-context-help-icon');
+        I.waitForElement('.inline-help-iframe', 20);
+
+        //check language in help window
+        I.waitForElement(locate('.inline-help-iframe').withAttr({src: languages[id][0]}));
+        await within({frame: '.inline-help-iframe'},()=>{
+            I.see(languages[id][2]);
+        });
+
+        //close help window
+        I.click(languages[id][3], '.io-ox-help-window');        
+    }
+
+    I.logout();
+
+});
+
+
