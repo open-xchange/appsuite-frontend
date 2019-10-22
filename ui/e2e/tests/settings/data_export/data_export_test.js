1c48f5a94edc0d84bfc8b63e046eef55ad224a9e
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2019-10-22 14:02:54 +0200

    e2e add gdpr test

---
+++ ui/e2e/tests/settings/data_export/data_export_test.js
@@ -0,0 +1,82 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2019 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
+ */
+
+Feature('Settings > Data export (GDPR)');
+
+Before(async function (users) {
+    await users.create();
+});
+
+After(async function (users) {
+    await users.removeAll();
+});
+
+Scenario('request a new download and cancel it', async function (I) {
+    I.login('app=io.ox/settings');
+    I.waitForVisible('*[data-app-name="io.ox/settings"]');
+
+    I.waitForText('Download personal data', 5);
+
+    I.click('Download personal data');
+    I.waitForText('Download your personal data');
+
+    within('.io-ox-personal-data-settings', () => {
+
+        // click some options
+        I.uncheckOption('Drive');
+        I.uncheckOption('Tasks');
+        I.uncheckOption('Calendar');
+    });
+
+    // click suboption
+    var optionsLocator = locate({ css: '.io-ox-personal-data-settings [data-toggle="dropdown"]' }).first();
+    I.click(optionsLocator);
+    I.click('.smart-dropdown-container.open a[data-name="includeTrash"]');
+
+    // check if suboption is enabled
+    I.click(optionsLocator);
+    I.seeElement('.smart-dropdown-container.open a[data-name="includeTrash"] .fa-check');
+    I.pressKey('Escape');
+
+    within('.io-ox-personal-data-settings', () => {
+        // select a filesize
+        I.selectOption('select', '512 MB');
+
+        I.click('Request download');
+
+        I.wait(1);
+
+        // check if view switches correctly and buttons are disabled
+        I.see('Your requested archive is currently being created. Depending on the size of the requested data this may take hours or days. You will be informed via email when your download is ready.');
+
+        I.seeElement('input:disabled');
+        I.seeElement('input:disabled');
+
+        // cancel again, we don't want to clutter the filesystem
+        I.click('Cancel download request');
+    });
+
+    I.waitForText('Do you really want to cancel the current download request?', 5);
+    I.click('Cancel download request', '.modal-dialog');
+
+    I.wait(1);
+
+    within('.io-ox-personal-data-settings', () => {
+        // check if view switches correctly and buttons are enabled again
+        I.dontSee('Your requested archive is currently being created. Depending on the size of the requested data this may take hours or days. You will be informed via email when your download is ready.');
+
+        I.dontSeeElement('input:disabled');
+        I.dontSeeElement('input:disabled');
+    });
+
+    I.logout();
+});
