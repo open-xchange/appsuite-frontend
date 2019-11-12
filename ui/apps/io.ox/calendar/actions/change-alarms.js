c3dafffd14982ce64d664a84d6ba8fcf7259adca
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2019-11-12 14:00:37 +0100

    Fixed Bug 67655 - Changing calendar notifications doesn't detect broken series

--- ui/apps/io.ox/calendar/actions/change-alarms.js
+++ ui/apps/io.ox/calendar/actions/change-alarms.js
@@ -22,7 +22,8 @@ define('io.ox/calendar/actions/change-alarms', [
 
     'use strict';
 
-    return function (appointmentData) {
+    var showDialog = function (appointmentData, options) {
+        options = options || {};
         var alarmsview = new AlarmsView.linkView({ model: new Backbone.Model(appointmentData) });
         new ModalDialog({
             title: gt('Change reminders')
@@ -52,8 +53,32 @@ define('io.ox/calendar/actions/change-alarms', [
         .addAlternativeButton({ action: 'cancel', label: gt('Cancel') })
         .addButton({ action: 'ok', label: gt('Ok'), className: 'btn-primary' })
         .on('ok', function () {
-            calApi.updateAlarms({ id: appointmentData.id, folder: appointmentData.folder, alarms: alarmsview.model.get('alarms') }, util.getCurrentRangeOptions());
+            var params = { id: appointmentData.id, folder: appointmentData.folder, alarms: alarmsview.model.get('alarms') };
+            if (options.single && appointmentData.recurrenceId) {
+                params.recurrenceId = appointmentData.recurrenceId;
+            }
+            calApi.updateAlarms(params, util.getCurrentRangeOptions());
         })
         .open();
     };
+
+    return function (appointmentData) {
+        if (appointmentData.recurrenceId && appointmentData.seriesId) {
+            new ModalDialog({ title: gt('Change appointment status'), width: 600 })
+                .build(function () {
+                    this.$body.append(gt('This appointment is part of a series. Do you want to change your confirmation for the whole series or just for this appointment within the series?'));
+                })
+                .addCancelButton({ left: true })
+                .addButton({ className: 'btn-default', label: gt('Change appointment'), action: 'appointment' })
+                .addButton({ action: 'series',
+                    //#. Use singular in this context
+                    label: gt('Change series') })
+                .on('series', function () { showDialog(appointmentData, { single: false }); })
+                .on('appointment', function () { showDialog(appointmentData, { single: true }); })
+                .open();
+            return;
+        }
+        showDialog(appointmentData, { single: true });
+    };
+
 });
