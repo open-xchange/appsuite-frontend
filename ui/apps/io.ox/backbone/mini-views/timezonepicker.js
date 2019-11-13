cf05eea12d38fde76af9d1cddf9b2069c09091a7
Matthias Biggeleben <matthias.biggeleben@open-xchange.com>
2019-11-13 13:58:04 +0100

    Timezone Picker: Different groups, different order for better readability

--- ui/apps/io.ox/backbone/mini-views/timezonepicker.js
+++ ui/apps/io.ox/backbone/mini-views/timezonepicker.js
@@ -21,78 +21,188 @@ define('io.ox/backbone/mini-views/timezonepicker', [
 
     'use strict';
 
-    function getDisplayName(timezone) {
-        var tz = moment.tz(timezone),
-            displayName = tz.format('Z ') + tz.zoneAbbr() + ' ' + timezone;
-
-        return displayName.replace(/_/g, ' ');
-    }
-
-    var getTimezones = _.memoize(function () {
-        var available = settingOptions.get('availableTimeZones', {}),
-            now = moment();
-
-        return _(moment.tz.names())
-            .chain()
-            .map(moment.tz.zone)
-            .filter(function (tz) {
-                tz.displayName = getDisplayName(tz.name);
-                return !!available[tz.name];
-            })
-            .sortBy(function (tz) {
-                return tz.utcOffset(now) * -1;
-            })
-            .map(function (tz) {
-                return { label: tz.displayName, value: tz.name };
-            })
-            .value();
-    });
-
     var TimezonePicker = miniViews.SelectView.extend({
 
         initialize: function (options) {
             options = options || {};
             if (options.showFavorites && calendarSettings.get('favoriteTimezones')) {
                 options.favorites = _(calendarSettings.get('favoriteTimezones')).map(function (favorite) {
-                    return { label: getDisplayName(favorite), value: favorite };
+                    return { label: getTimezoneData(favorite, true).label, value: favorite };
                 });
             }
             options.list = getTimezones();
             miniViews.SelectView.prototype.initialize.call(this, options);
         },
 
-        renderOptions: function (list, parent) {
-            parent.append(_(list).map(function (option) {
-                return $('<option>').attr({ value: option.value }).text(option.label);
-            }));
-        },
-
         render: function () {
-            var self = this;
             this.$el.attr({ name: this.name });
             if (this.id) this.$el.attr({ id: this.id });
             _.defer(function () {
-                if (self.options.favorites) {
-                    var standard = $('<optgroup>').attr('label', gt('Standard timezone')),
-                        favorites = $('<optgroup>').attr('label', gt('Favorites')),
-                        all = $('<optgroup>').attr('label', gt('All timezones'));
-
-                    self.renderOptions([{
-                        label: getDisplayName(coreSettings.get('timezone')),
-                        value: coreSettings.get('timezone')
-                    }], standard);
-                    self.renderOptions(self.options.favorites, favorites);
-                    self.renderOptions(self.options.list, all);
-
-                    self.$el.append(standard, favorites, all);
-                } else {
-                    self.renderOptions(self.options.list, self.$el);
+                // current timezone
+                this.$el.append(
+                    this.renderOptions(
+                        [getTimezoneData(coreSettings.get('timezone'), true)],
+                        $('<optgroup>').attr('label', gt('Standard timezone'))
+                    )
+                );
+                // favorites
+                if (this.options.favorites) {
+                    this.$el.append(
+                        this.renderOptions(this.options.favorites, $('<optgroup>').attr('label', gt('Favorites')))
+                    );
                 }
-                self.update();
-            });
+                // all timezones
+                this.renderGroups();
+                this.update();
+            }.bind(this));
             return this;
+        },
+
+        renderGroups: function () {
+            this.$el.append(
+                _(regions)
+                .chain()
+                .keys()
+                .map(function (region) {
+                    var list = _(this.options.list)
+                        .filter(function (zone) { return zone.region === region; });
+                    if (!list.length) return $();
+                    return this.renderOptions(list, $('<optgroup>').attr('label', regions[region]));
+                    //     sections = _(list).chain().pluck('offset').uniq().value();
+                    // return _(sections).map(function (offset) {
+                    //     var subset = _(list).where({ offset: offset });
+                    //     return this.renderOptions(subset, $('<optgroup>').attr('label', regions[region] + ' ' + offset));
+                    // }, this);
+                }, this)
+                .flatten()
+                .value()
+            );
+        },
+
+        renderOptions: function (list, parent) {
+            return parent.append(
+                _(list).map(function (option) {
+                    return $('<option>').attr({ value: option.value }).text(option.label);
+                }, this)
+            );
         }
     });
+
+    //
+    // Utility functions
+    //
+
+    var getTimezones = _.memoize(function () {
+        return _(getAvailableTimezones())
+            .chain()
+            .map(getTimezoneData)
+            .sortBy('label')
+            .sortBy('sort')
+            .value();
+    });
+
+    function getAvailableTimezones() {
+        var serverSide = settingOptions.get('availableTimeZones', {}),
+            current = coreSettings.get('timezone');
+        return _(moment.tz.names()).filter(function (name) {
+            // we can only offer timezones that are defined both server-side and client-side
+            if (!serverSide[name]) return false;
+            // always include current timezone
+            if (name === current) return true;
+            // Drop DEPRECATED zones
+            // cp. https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+            if (/^Australia\/(ACT|LHI|North|NSW|Queensland|South|Tasmania|Victoria|West)$/.test(name)) return false;
+            if (/^(Brazil|Canada|Chile|Mexico|US)\//.test(name)) return false;
+            if (/^(CET|CST6CDT|Cuba|EET|Egypt|Eire|EST|EST5EDT|GB|GB-Eire|GMT+0|GMT0|GMT-0|Greenwich|Hongkong|HST)$/.test(name)) return false;
+            if (/^(Iceland|Iran|Israel|Jamaica|Japan|Kwajalein|Libya|MET|MST|MST7MDT|Navajo|NZ|NZ-CHAT)$/.test(name)) return false;
+            if (/^(Poland|Portugal|PRC|PST8PDT|ROC|ROK|Singapore|Turkey|UCT|Universal|WET|W-SU|Zulu)$/.test(name)) return false;
+            if (/^Etc\/(Greenwich|UCT|Universal|Zulu)$/.test(name)) return false;
+            // Drop ALIAS zones
+            // Africa
+            if (/^Africa\/(Addis_Ababa|Asmara|Bamako|Bangui|Banjul|Blantyre|Brazzaville|Bujumbura|Conakry|Dakar)$/.test(name)) return false;
+            if (/^Africa\/(Dar_es_Salaam|Djibouti|Douala|Freetown|Gaborone|Harare|Kampala|Kigali|Kinshasa|Libreville)$/.test(name)) return false;
+            if (/^Africa\/(Lome|Luanda|Lubumbashi|Lusaka|Malabo|Maseru|Mbabane|Mogadishu|Niamey|Nouakchott|Ouagadougou|Porto-Novo|Sao_Tome|Timbuktu)$/.test(name)) return false;
+            // America
+            if (/^America\/(Anguilla|Antigua|Argentina\/ComodRivadavia|Aruba|Atka|Buenos_Aires|Cayman|Cordoba|Dominica)$/.test(name)) return false;
+            if (/^America\/(Fort_Wayne|Grenada|Guadeloupe|Indianapolis|Jujuy|Knox_IN|Kralendijk|Louisville|Lower_Princes)$/.test(name)) return false;
+            if (/^America\/(Marigot|Mendoza|Montreal|Montserrat|Porto_Acre|Rosario|Santa_Isabel|Shiprock|St_Barthelemy|St_Kitts|St_Lucia|St_Thomas|St_Vincent|Tortola|Virgin)$/.test(name)) return false;
+            // Antarctica & Arctic
+            if (/^Antarctica\/(McMurdo|South_Pole)$/.test(name)) return false;
+            if (/^Arctic\/Longyearbyen$/.test(name)) return false;
+            // Asia
+            if (/^Asia\/(Aden|Ashkhabad|Bahrain|Calcutta|Chongqing|Chungking|Dacca|Istanbul|Kashgar|Kathmandu)$/.test(name)) return false;
+            if (/^Asia\/(Kuwait|Macao|Muscat|Phnom_Penh|Rangoon|Saigon|Tel_Aviv|Thimbu|Ujung_Pandang|Ulan_Bator|Vientiane)$/.test(name)) return false;
+            // Atlantic Ocean
+            if (/^Asia\/(Faeroe|St_Helena)$/.test(name)) return false;
+            // Australia
+            if (/^Australia\/(Canberra|Yancowinna)$/.test(name)) return false;
+            // Europe
+            if (/^Australia\/(Belfast|Bratislava|Busingen|Guernsey|Isle_of_Man|Jersey|Ljubljana|Mariehamn|Podgorica|San_Marino|Sarajevo|Skopje|Tiraspol|Vaduz|Vatican|Zagreb)$/.test(name)) return false;
+            // Indian Ocean
+            if (/^Indian\/(Antananarivo|Comoro|Mayotte)$/.test(name)) return false;
+            // Pacific Ocean
+            if (/^Pacific\/(Johnston|Midway|Ponape|Saipan|Samoa|Truk|Yap)$/.test(name)) return false;
+            // otherwise
+            return true;
+        });
+    }
+
+    function getTimezoneData(name, longFormat) {
+        var tz = moment.tz(name),
+            offset = tz.format('Z'),
+            abbr = tz.zoneAbbr(),
+            label = name.replace(/_/g, ' ');
+        // label / display name
+        label = label.replace(/^(\w+)\//, longFormat === true ? replaceRegion : '');
+        label = '(' + offset + ') ' + (/^\w+$/.test(abbr) && abbr !== label ? abbr + ' ' : '') + label;
+        // return data
+        return { label: label, value: name, region: getRegion(name), offset: offset, sort: tz.utcOffset() };
+    }
+
+    function getRegion(name) {
+        var region = name.split('/')[0];
+        if (/^(America|Atlantic|Europe|Africa|Indian|Asia|Australia|Pacific|Arctic|Antarctica)$/.test(region)) return region;
+        return 'Other';
+    }
+
+    function replaceRegion(all, region) {
+        return (regions[region] || region) + '/';
+    }
+
+    //
+    // Regions and their translations
+    //
+
+    var regions = {
+        //#. Timezon region
+        America: gt('America'),
+        //#. Timezon region / Atlantic Ocean
+        Atlantic: gt('Atlantic'),
+        //#. Timezon region
+        Europe: gt('Europe'),
+        //#. Timezon region
+        Africa: gt('Africa'),
+        //#. Timezon region / Indian Ocean
+        Indian: gt('Indian Ocean'),
+        //#. Timezon region
+        Asia: gt('Asia'),
+        //#. Timezon region
+        Australia: gt('Australia'),
+        //#. Timezon region / Pacific Ocean
+        Pacific: gt('Pacific'),
+        //#. Timezon region
+        Arctic: gt('Arctic'),
+        //#. Timezon region
+        Antarctica: gt('Antarctica'),
+        //#. Timezon region
+        Other: gt('Other')
+    };
+
+    // expose to test
+    TimezonePicker.util = {
+        getTimezones: getTimezones,
+        getAvailableTimezones: getAvailableTimezones
+    };
 
     return TimezonePicker;
 });
