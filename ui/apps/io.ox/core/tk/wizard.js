38eb40cedb31653512b4f5f0251f4662f1479d43
Jöran Kuschel <joeran.kuschel@open-xchange.com>
2019-11-25 10:34:22 +0100

    Fixes Bug 68399 - Mandatory Wizard: Cannot disable steps on mobile anymore Root cause description: Smartphones seem to not work with prop('disabled') anymore Solution description: Switched to attr('disabled') instead Resolved state: fixed
    (cherry picked from commit 66bef06a54f55196a994fd79009d0ebe1cb926d2)

--- ui/apps/io.ox/core/tk/wizard.js
+++ ui/apps/io.ox/core/tk/wizard.js
@@ -507,7 +507,10 @@ define('io.ox/core/tk/wizard', [
         // enable/disable 'next' button
         toggleNext: function (state) {
             if (_.device('smartphone')) {
-                return this.parent.container.find('[data-action="next"]').prop('disabled', !state).attr('aria-disabled', !state);
+                return this.parent.container.find('[data-action="next"]').attr({
+                    'disabled': !state,
+                    'aria-disabled': !state
+                });
             }
             this.$('.btn[data-action="next"]').prop('disabled', !state);
             return this;
