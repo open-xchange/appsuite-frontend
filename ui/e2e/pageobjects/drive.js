c444867813bcec9da92b3f98a9da89b8e91f3347
Richard Petersen <richard.petersen@open-xchange.com>
2019-12-05 14:36:26 +0100

    E2E: Fix for puppeteer, because waitFor does not wait for any but the first found element. But this will not alway become visible.
    In this case, when you visited the "My shares" folder, a second toolbar is injected into the dom which will destroy the selector

--- ui/e2e/pageobjects/drive.js
+++ ui/e2e/pageobjects/drive.js
@@ -5,8 +5,9 @@ module.exports = {
         // wait untill all importand nodes are drawn
         I.waitForElement('.file-list-view.complete');
         I.waitForVisible({ css: '.io-ox-files-window .folder-tree' }, 5);
-        I.waitForVisible({ css: '.io-ox-files-window .classic-toolbar-container .classic-toolbar' }, 5);
-        I.waitForVisible({ css: '.secondary-toolbar .breadcrumb-view' }, 5);
+        // TODO this does not always work. When the user is in the "Myshares" folder, this will not be visible
+        I.waitForVisible({ css: '.io-ox-files-window .window-body > .classic-toolbar-container .classic-toolbar' }, 5);
+        I.waitForVisible({ css: '.window-content .secondary-toolbar .breadcrumb-view' }, 5);
         // wait a bit because breadcrumb has some redraw issues atm (redraws 7 times)
         // TODO Fix the redraw issue
         I.wait(0.5);
