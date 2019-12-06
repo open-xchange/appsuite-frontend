c51f98044d3ebc2df055ebb41eea4bac54ac7905
Richard Petersen <richard.petersen@open-xchange.com>
2019-12-06 10:25:29 +0100

    E2E: Improve waitforapp selectors to match common and "my shares" folder

--- ui/e2e/pageobjects/drive.js
+++ ui/e2e/pageobjects/drive.js
@@ -5,9 +5,8 @@ module.exports = {
         // wait untill all importand nodes are drawn
         I.waitForElement('.file-list-view.complete');
         I.waitForVisible({ css: '.io-ox-files-window .folder-tree' }, 5);
-        // TODO this does not always work. When the user is in the "Myshares" folder, this will not be visible
-        I.waitForVisible({ css: '.io-ox-files-window .window-body > .classic-toolbar-container .classic-toolbar' }, 5);
-        I.waitForVisible({ css: '.window-content .secondary-toolbar .breadcrumb-view' }, 5);
+        I.waitForVisible({ xpath: './/*[contains(@class, "io-ox-files-window")]//*[@class="classic-toolbar-container"]//*[@class="classic-toolbar" and not(ancestor::*[contains(@style, "display: none;")])]' });
+        I.waitForVisible({ xpath: './/*[contains(@class, "secondary-toolbar")]//*[contains(@class, "breadcrumb-view") and not(ancestor::*[contains(@style, "display: none")])]' }, 5);
         // wait a bit because breadcrumb has some redraw issues atm (redraws 7 times)
         // TODO Fix the redraw issue
         I.wait(0.5);
