33e3f77b5227a80e8d7aaf30eb9ba3e506cf3e62
Julian Bäume <julian.baeume@open-xchange.com>
2020-01-15 10:28:19 +0100

    switch to build date as package version string for code loading
    the current implementation relied on the version string in package.json
    being updated for each update. This currently should be true but relies
    on a semi-manual process to bump the version number for each release
    (i.e. revision). According to the release team, in the future there
    should be less updates of package.json, one for each stable release.
    By just using the build date as an indicator for the version of a
    package, we still workaround the problem of the middleware distributing
    wrong sources for older ui versions and this even holds true for
    snapshot builds.

--- ui/grunt/tasks/versions.js
+++ ui/grunt/tasks/versions.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
 
     grunt.registerTask('version_txt', 'Create text files containing the version string', function () {
-        grunt.file.write('build/version.txt', grunt.config('pkg').version);
+        grunt.file.write('build/version.txt', grunt.config('pkg').buildDate);
     });
 };
