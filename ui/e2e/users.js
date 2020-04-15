6bb7e41a0e97e53a54e86c099470376d0554baa3
Julian Bäume <julian.baeume@open-xchange.com>
2020-04-15 16:05:08 +0200

    [e2e] remove provisioning workaround
    this is already handled by codecept helper lib since at least one version
    and it will break with the next release.

--- ui/e2e/users.js
+++ ui/e2e/users.js
@@ -1,21 +1,3 @@
-const users = require('@open-xchange/codecept-helper').users,
-    contexts = require('@open-xchange/codecept-helper/src/contexts'),
-    helper = new (require('@open-xchange/codecept-helper').helper)(),
-    wrappedCreate = users.create;
-
-users.create = function createUsers(user, ctxt) {
-    return wrappedCreate.apply(this, [user, ctxt || { id: contexts[0].id }])
-        .then(user => {
-            user.setAccessCombinationByName = function (access_combination_name) {
-                return helper.executeSoapRequest('OXUserService', 'changeByModuleAccessName', {
-                    ctx: { id: user.context.id },
-                    access_combination_name,
-                    user: { id: user.get('id') },
-                    auth: user.auth
-                });
-            };
-            return user.setAccessCombinationByName('all').then(()=> user);
-        });
-};
+const users = require('@open-xchange/codecept-helper').users;
 
 module.exports = users;
