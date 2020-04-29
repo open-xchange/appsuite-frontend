76473ea58d9c56def9f0ecb570c86eec0af5310d
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2020-04-29 16:52:01 +0200

    Add EmulateDevice Helper

--- ui/e2e/helper.js
+++ ui/e2e/helper.js
@@ -126,6 +126,83 @@ class MyHelper extends Helper {
         await devTools.send('Network.emulateNetworkConditions', presets[networkConfig.toUpperCase()] || networkConfig);
     }
 
+    // emulate other devices
+    // use this before login, otherwise your session is lost (user agent change)
+    async emulateDevice(device) {
+
+        // add some presets
+        // Todo: list is just some examples at this point, we need to check which devices are needed here
+        const presets = {
+            'APPLE IPHONE 4': {
+                width: 320,
+                height: 480,
+                deviceScaleFactor: 2,
+                touch: { enabled: true },
+                mobile: true,
+                userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5'
+            },
+            'APPLE IPHONE 5': {
+                width: 320,
+                height: 568,
+                deviceScaleFactor: 2,
+                touch: { enabled: true },
+                mobile: true,
+                userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+            },
+            'APPLE IPHONE X': {
+                width: 375,
+                height: 812,
+                deviceScaleFactor: 3,
+                touch: { enabled: true },
+                mobile: true,
+                userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+            },
+            'GOOGLE NEXUS 4': {
+                width: 384,
+                height: 640,
+                deviceScaleFactor: 2,
+                touch: { enabled: true },
+                mobile: true,
+                userAgent: 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19'
+            },
+            'GOOGLE NEXUS 5': {
+                width: 360,
+                height: 640,
+                deviceScaleFactor: 3,
+                touch: { enabled: true },
+                mobile: true,
+                userAgent: 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19'
+            },
+            'GOOGLE NEXUS S': {
+                width: 320,
+                height: 533,
+                deviceScaleFactor: 1.5,
+                touch: { enabled: true },
+                mobile: true,
+                userAgent: 'Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; Nexus S Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
+            },
+            'SAMSUNG GALAXY S10': {
+                width:  360,
+                height: 740,
+                deviceScaleFactor: 4,
+                touch: { enabled: true },
+                mobile: true,
+                userAgent: 'Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G973F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.0 Chrome/67.0.3396.87 Mobile Safari/537.36'
+            }
+        };
+
+        const { page } = this.helpers['Puppeteer'];
+
+        // get Chrome DevTools session
+        const devTools = await page.target().createCDPSession();
+        // Override UserAgent
+        await devTools.send('Emulation.setUserAgentOverride', (presets[device.toUpperCase()] || device));
+        // Enable Touch events
+        await devTools.send('Emulation.setTouchEmulationEnabled', (presets[device.toUpperCase()] || device).touch);
+        // Override DeviceMetrics
+        await devTools.send('Emulation.setDeviceMetricsOverride', (presets[device.toUpperCase()] || device));
+    }
+
     /*
      * Overwrite native puppeteer d&d, because it does not work for every case
      * Maybe this is going to be fixed in the future by puppeteer, then this can be removed.
