9a9bfb1f5724fbbede07923eb717647019869c7f
Tran Dong Tran <tran-dong.tran@open-xchange.com>
2020-05-18 14:47:36 +0200

    e2e: Add test 'Re-order multiple recipients'
    - selects multiple recipients and checks if proper focus and classes are upheld

--- ui/e2e/tests/mail/compose/reorder_recipients_test.js
+++ ui/e2e/tests/mail/compose/reorder_recipients_test.js
@@ -131,3 +131,88 @@ Scenario('[C8832] Re-order recipients', async function (I, users, mail) {
     I.see(tokenLabelFor(recipient2), '.person-cc');
     I.see(tokenLabelFor(recipient3), '.person-bcc');
 });
+
+// also see [OXUIB-226]
+Scenario('Re-order multiple recipients', async function (I, users, mail) {
+
+    let [sender, recipient1, recipient2, recipient3, recipient4] = users;
+
+    const mailSubject = 'C8832 Move recipients between between field';
+
+    const tokenLabelFor = user => `${user.get('given_name')} ${user.get('sur_name')}`;
+    const dragTarget = user => `~${user.get('given_name')} ${user.get('sur_name')} ${user.get('primaryEmail')}`;
+    const dropTarget = fieldClass => {
+        return { css: `[data-extension-id="${fieldClass}"] .tokenfield` };
+    };
+    const tokenSelector = token => {
+        return { css: `[data-extension-id="${token}"] .token` };
+    };
+
+    await I.haveSetting('io.ox/mail//features/registerProtocolHandler', false);
+
+    I.login('app=io.ox/mail', { user: sender });
+    mail.newMail();
+    I.click('~Maximize');
+    I.click('CC');
+    I.click('BCC');
+
+    I.fillField('Subject', mailSubject);
+
+    // add a recipient for all fields
+    I.fillField('To', recipient1.get('primaryEmail'));
+    I.waitForVisible('.focus .tt-dropdown-menu');
+    I.pressKey('Enter');
+    I.see(tokenLabelFor(recipient1), tokenSelector('to'));
+    I.fillField('CC', recipient2.get('primaryEmail'));
+    I.waitForVisible('.focus .tt-dropdown-menu');
+    I.pressKey('Enter');
+    I.see(tokenLabelFor(recipient2), tokenSelector('cc'));
+    I.fillField('BCC', recipient3.get('primaryEmail'));
+    I.waitForVisible('.focus .tt-dropdown-menu');
+    I.pressKey('Enter');
+    I.see(tokenLabelFor(recipient3), tokenSelector('bcc'));
+    I.fillField('BCC', recipient4.get('primaryEmail'));
+    I.waitForVisible('.focus .tt-dropdown-menu');
+    I.pressKey('Enter');
+    I.see(tokenLabelFor(recipient4), tokenSelector('bcc'));
+
+    // select both recipients in bcc
+    I.pressKeyDown('Shift');
+    I.click(dragTarget(recipient3));
+    I.click(dragTarget(recipient4));
+    I.pressKeyUp('Shift');
+    I.seeNumberOfVisibleElements('.token.active', 2);
+    I.waitForFocus(dragTarget(recipient4));
+
+    // drag both recipients from bcc to cc with focus on recipient4
+    I.dragAndDrop(dragTarget(recipient4), dropTarget('cc'));
+    I.seeNumberOfVisibleElements('.token.active', 2);
+    I.waitForFocus(dragTarget(recipient4));
+
+    // drag both recipients from cc to to with focus on recipient3
+    I.dragAndDrop(dragTarget(recipient3), dropTarget('to'));
+    I.seeNumberOfVisibleElements('.token.active', 2);
+    I.waitForFocus(dragTarget(recipient3));
+
+    // drag both recipients to first position and check positions
+    I.dragAndDrop(dragTarget(recipient3), dragTarget(recipient1));
+    I.seeNumberOfVisibleElements('.token.active', 2);
+    I.waitForFocus(dragTarget(recipient3));
+    I.see(tokenLabelFor(recipient3), { css: '[data-extension-id="to"] .token:nth-of-type(3)' });
+    I.see(tokenLabelFor(recipient4), { css: '[data-extension-id="to"] .token:nth-of-type(4)' });
+    I.see(tokenLabelFor(recipient1), { css: '[data-extension-id="to"] .token:nth-of-type(5)' });
+
+    // send the mail
+    mail.send();
+
+    // open detail view of sent mail
+    I.selectFolder('Sent');
+    mail.selectMail(mailSubject);
+
+    // check recipients
+    I.click('.show-all-recipients');
+    I.see(tokenLabelFor(recipient1), '.person-to');
+    I.see(tokenLabelFor(recipient2), '.person-cc');
+    I.see(tokenLabelFor(recipient3), '.person-to');
+    I.see(tokenLabelFor(recipient4), '.person-to');
+});
