0e494f0891cdb1d242e6d6c8f39203847fefad71
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2020-06-09 16:43:31 +0200

    Fixed OXUIB-302 URL scrambled in Resource, when it contains numerical string
    (cherry picked from commit 968226704056bf6eff3d398d885af51ecb288f52)
    (cherry picked from commit 0cea0873c20a07d26f87a503a943e77f07422550)

--- ui/apps/io.ox/contacts/view-detail.js
+++ ui/apps/io.ox/contacts/view-detail.js
@@ -781,7 +781,13 @@ define('io.ox/contacts/view-detail', [
     // only applies to resource because they have a "description" field.
     // contacts just have a "note"
 
-    var regPhone = /(\+?[\d\x20/()]{4,})/g,
+    // this matches phone numbers in 4 cases:
+    // string has phone number with empty space in front of it and after it
+    // string has starts with phone number and has an empty space after it
+    // string ends with phone number and has an empty space in front of it
+    // string starts and ends with phone number
+    // what we don't want is match numbers inside links for example: http://some.service.com/begin?123456?pwd=7890
+    var regPhone = /(^|\s+)(\+?[\d\x20/()]{4,})($|\s+)/g,
         regClean = /[^+0-9]/g;
 
     ext.point('io.ox/contacts/detail/content').extend({
@@ -795,6 +801,9 @@ define('io.ox/contacts/view-detail', [
             // find phone numbers & links
             str = str.replace(regPhone, function (match) {
                 var number = match.replace(regClean, '');
+                // check if we have an empty match after cleaning. We want to avoid phone numbers like (((( etc
+                if (number.length === 0) return match;
+
                 return '<a href="callto:' + number + '">' + match + '</a>';
             });
 
