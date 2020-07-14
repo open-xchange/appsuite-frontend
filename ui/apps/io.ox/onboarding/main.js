3284af6f7851faf44d6c0d127d7722cd9041586c
Maik Schäfer <maik.schaefer@open-xchange.com>
2020-07-14 16:58:51 +0200

    refactor code to use views and collections

--- ui/apps/io.ox/onboarding/main.js
+++ ui/apps/io.ox/onboarding/main.js
@@ -14,13 +14,14 @@
 define('io.ox/onboarding/main', [
     'io.ox/core/extensions',
     'io.ox/core/tk/wizard',
+    'io.ox/backbone/views/disposable',
     'gettext!io.ox/core/onboarding',
     'less!io.ox/onboarding/style'
-], function (ext, Wizard, gt) {
+], function (ext, Wizard, DisposableView, gt) {
     'use strict';
 
     var title = gt('Connect your device'),
-        platforms =
+        /* platforms =
         [
             {
                 'title': 'Windows PC',
@@ -102,33 +103,214 @@ define('io.ox/onboarding/main', [
                 'icon': 'fa-apple',
                 'data': 'iphone'
             }
-        ],
-        descriptions =
-        [
-            [
-                {
-                    'class': 'info',
-                    'text': gt('This wizard helps you to use App Suite on other devices.')
-                },
-                {
-                    'class': 'prompt',
-                    'text': gt('Which device do you want to configure?')
-                }
-            ],
-            [
-                {
-                    'class': 'prompt',
-                    'text': gt('Which application do you want to use?')
-                }
-            ]
-        ],
-
+        ], */
         wizard;
+
+    var platformList = new Backbone.Collection([
+        {
+            'title': 'Windows PC',
+            'icon': 'fa-windows',
+            'platform': 'desktop'
+        },
+        {
+            'title': 'Android phone or tablet',
+            'icon': 'fa-android',
+            'platform': 'android'
+        },
+        {
+            'title': 'MacOS',
+            'icon': 'fa-apple',
+            'platform': 'macos'
+        },
+        {
+            'title': 'iPhone or iPad',
+            'icon': 'fa-apple',
+            'platform': 'iphone'
+        }
+    ]);
+
+    var appList = new Backbone.Collection([
+        {
+            'title': 'Drive App',
+            'icon': 'fa-cloud',
+            'app': 'drive',
+            'platform': 'desktop'
+        },
+        {
+            'title': 'Mail',
+            'icon': 'fa-envelope-o',
+            'app': 'mail',
+            'platform': 'desktop'
+        },
+        {
+            'title': 'Mail + Calendar + Address Book',
+            'icon': 'fa-users',
+            'app': 'emclient',
+            'platform': 'desktop'
+        },
+        {
+            'title': 'Email über Android Mail',
+            'icon': 'fa-cloud',
+            'app': 'mailsync',
+            'platform': 'android'
+        },
+        {
+            'title': 'Email über die OX Mail App',
+            'icon': 'fa-envelope-o',
+            'app': 'mailapp',
+            'platform': 'android'
+        },
+        {
+            'title': 'Contacts',
+            'icon': 'fa-users',
+            'app': 'addressbook',
+            'platform': 'android'
+        },
+        {
+            'title': 'Calendar',
+            'icon': 'fa-users',
+            'app': 'calendar',
+            'platform': 'android'
+        },
+        {
+            'title': 'Drive',
+            'icon': 'fa-users',
+            'app': 'driveapp',
+            'platform': 'android'
+        }
+
+    ]);
+
+    var PlatformView = Backbone.View.extend({
+
+        tagName: 'div',
+        className: 'description',
+
+        initialize: function () {
+            this.listView = new ListView({ collection: platformList, model: this.model });
+        },
+
+        render: function () {
+            this.$el.empty()
+            .append(
+                $('<p class="info">').text(gt('This wizard helps you to use App Suite on other devices.')),
+                $('<p class="prompt">').text(gt('Which device do you want to configure?'))
+            );
+            this.$el.append(this.listView.render().$el);
+            return this;
+        }
+    });
+
+    var AppView = Backbone.View.extend({
+
+        tagName: 'div',
+        className: 'description',
+
+        initialize: function () {
+            this.listView = new ListView({ collection: appList, model: this.model });
+        },
+
+        render: function () {
+            this.$el.empty()
+            .append(
+                $('<p class="prompt">').text(gt('Which application do you want to use?'))
+            );
+            this.$el.append(this.listView.render().$el);
+            return this;
+        }
+    });
+
+    var ListView = Backbone.View.extend({
+
+        tagName: 'ul',
+        className: 'content-list',
+
+        initialize: function () {
+
+            this.listenTo(this.model, 'change', this.renderListItems);
+        },
+        events: {
+            'click .list-btn': 'selectItem'
+        },
+        selectItem: function (e) {
+            var target = $(e.currentTarget);
+            this.model.set('app', target.attr('data-app'));
+            this.model.set('platform', target.attr('data-platform'));
+        },
+
+        render: function () {
+            console.log('%c events', 'background: #222; color: #bada55', { event: this.events, el: this.$el });
+            this.$el.empty();
+            this.renderListItems();
+            return this;
+        },
+
+        renderListItems: function () {
+            var list = this.collection,
+                self = this;
+
+            self.$el.append(
+                list
+                .filter(function (model) {
+                    return self.model.get('platform') === undefined || self.model.get('platform') === model.get('platform');
+                })
+                .map(function (model) {
+                    var view = new ListItemView({ model: model });
+                    return view.render().$el;
+                })
+            );
+
+        }
+    });
+
+    var ListItemView = DisposableView.extend({
+        tagName: 'li',
+        className: 'list-item',
+
+        render: function () {
+            this.$el
+                .append(
+                    $('<button type="button" class="list-btn" data-action="next">')
+                    .addClass(this.model.get('data'))
+                    .attr('data-platform', this.model.get('platform'))
+                    .attr('data-app', this.model.get('app'))
+                    .append(
+                        $('<i class="icon-btn fa">').addClass(this.model.get('icon')),
+                        $('<div class="list-description">').text(this.model.get('title')),
+                        $('<i class="icon-next fa fa-chevron-right">')
+                    ));
+            return this;
+        }
+    });
+
+    var ProgressionView = Backbone.View.extend({
+
+        tagName: 'div',
+        className: 'progress-container',
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+        },
+        render: function () {
+            var platform = !!this.model.get('platform'),
+                app = !!this.model.get('app');
+            this.$el.empty()
+                .append($('<ul class="progress-steps">')
+                .append(
+                    $('<li class="progress-step-one">').text('1').append($('<p class="progress-description">').text(gt('Platform')))
+                    .addClass(!platform && !app ? 'active' : ''),
+                    $('<li class="progress-step-two">').text('2').append($('<p class="progress-description">').text(gt('App')))
+                    .addClass(platform && !app ? 'active' : ''),
+                    $('<li class="progress-step-three">').text('3').append($('<p class="progress-description">').text(gt('Setup')))
+                ));
+            return this;
+        }
+
+    });
 
     function drawScaffold() {
         this.$('div[role="document"]').addClass('connect-wizard');
         this.$('.wizard-title').text(title);
-        this.$('.wizard-content').append($('<div class="progress-container">'));
         this.$('.wizard-footer').empty().append(
             this.parent.currentStep === 0 ? '' : $('<button type="button" class="btn btn-default" data-action="back">').text(gt('Back')),
             $('<button type="button" class="btn btn-default" data-action="close">').text(gt('Close'))
@@ -148,56 +330,7 @@ define('io.ox/onboarding/main', [
                 ));
     }
 
-    function drawContent() {
-        var wizard = this.parent,
-            currentStep = wizard.currentStep,
-            model = currentStep === 0 ? wizard.model : wizard.currentModel.choices;
-        console.log('%c currentStep', 'background: #222; color: #bada55', { currentStep: currentStep, currentModel: model });
-        this.$('.wizard-content').empty();
-        drawScaffold.call(this);
-        drawProgress.call(this, currentStep);
-
-        this.$('.wizard-content')
-                .append(
-                    $('<div class="description">')
-                        .append(function () {
-                            return descriptions[currentStep].map(function (desc) {
-                                return $('<p>').addClass(desc.class).text(desc.text);
-                            });
-                        }
-                        ),
-                    $('<ul class="content-list">').append(
-                        function () {
-                            return model.map(
-                                function (content) {
-                                    return $('<li class="apps">')
-                                        .attr('data-value', content.data)
-                                        .append(
-                                            $('<button type="button" class="list-btn" data-action="next">')
-                                            .addClass(content.data)
-                                            .attr('data-value', content.data)
-                                            .append(
-                                                $('<i class="icon-btn fa">').addClass(content.icon),
-                                                $('<div class="list-description">').text(content.title),
-                                                $('<i class="icon-next fa fa-chevron-right">')
-                                            )
-                                            .click(function () {
-                                                var data = $(this).attr('data-value'),
-                                                    choiceModel = model.find(function (item) {
-                                                        return item.data === data;
-                                                    });
-                                                wizard.currentModel = choiceModel;
-                                            })
-                                        );
-                                }
-                            );
-                        }
-                    )
-                );
-    }
-
     function drawSetup() {
-        console.log('%c currentStep', 'background: #222; color: #bada55', { currentStep: currentStep, currentModel: model });
         var wizard = this.parent,
             currentStep = wizard.currentStep,
             model = wizard.currentModel.setupdata;
@@ -217,7 +350,7 @@ define('io.ox/onboarding/main', [
                         ));
     }
 
-    var ConnectDeviceView = Backbone.View.extend({
+    var ConnectDeviceView = DisposableView.extend({
 
         initialize: function () {
 
@@ -225,16 +358,29 @@ define('io.ox/onboarding/main', [
                 id: 'connect-wizard',
                 title: title
             };
-            this.model = this.currentModel = platforms;
+            this.model = new Backbone.Model({ app: undefined, platform: undefined });
             Wizard.registry.add(options, this.render.bind(this));
             Wizard.registry.run(options.id);
         },
 
         render: function () {
-            var connectTour = new Wizard();
+            var connectTour = new Wizard(),
+                wizardModel = this.model;
 
-            connectTour.model = this.model;
-            connectTour.currentModel = this.model;
+            var platformsView = new PlatformView({ model: wizardModel });
+            var appsView = new AppView({ model: wizardModel });
+            var progressView = new ProgressionView({ model: wizardModel });
+
+            connectTour.on('step:close', function () {
+                console.log('close');
+                platformsView.listView.remove();
+                platformsView.remove();
+                appsView.listView.remove();
+                appsView.remove();
+                progressView.remove();
+            });
+
+            progressView.render();
 
             connectTour.step({
                 id: 'platform',
@@ -242,7 +388,14 @@ define('io.ox/onboarding/main', [
                 next: false,
                 minWidth: '600px'
             })
-            .on('before:show', drawContent)
+            .on('before:show', function () {
+                var el = platformsView.render();
+                drawScaffold.call(this);
+                this.$('.wizard-content').empty().append(
+                    progressView.$el,
+                    el.$el);
+
+            })
             .end()
             .step({
                 id: 'apps',
@@ -250,7 +403,17 @@ define('io.ox/onboarding/main', [
                 next: false,
                 minWidth: '600px'
             })
-            .on('before:show', drawContent)
+            .on('before:show', function () {
+                var el = appsView.render();
+                drawScaffold.call(this);
+                this.$('.wizard-content').empty().append(
+                    progressView.$el,
+                    el.$el);
+
+            })
+            .on('back', function () {
+                wizardModel.set('platform', undefined);
+            })
             .end()
             .step({
                 id: 'setup',
