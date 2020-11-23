5d7647da91dd0b25c59e4e980aa147c2b355ead5
Tran Dong Tran <tran-dong.tran@open-xchange.com>
2020-11-23 11:21:28 +0100

    e2e: Fix test '[OXUIB-442] Calling contact via jitsi'
    - updated selector for call button

--- ui/e2e/tests/switchboard/jitsi_integration_test.js
+++ ui/e2e/tests/switchboard/jitsi_integration_test.js
@@ -56,8 +56,8 @@ Scenario('[OXUIB-442] Calling contact via jitsi', async (I, users, contacts, dia
         I.waitForVisible('.io-ox-contacts-window .classic-toolbar');
         I.waitForVisible('.io-ox-contacts-window .tree-container');
         contacts.selectContact(`${user2.get('sur_name')}, ${user2.get('given_name')}`);
-        I.waitForText('Call', 5, '.action-button-rounded');
-        I.waitForEnabled(locate('.action-button-rounded .btn').withText('Call'));
+        I.waitForText('Call', 5, '.switchboard-actions');
+        I.waitForEnabled(locate('.switchboard-actions .btn').withText('Call'));
         I.click('Call');
         I.waitForText('Call via Jitsi', 5, '.dropdown.open');
         I.waitForEnabled('.dropdown.open .dropdown-menu a');
