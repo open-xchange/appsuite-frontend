e6e27bbfb8a35e50ffbf4733b59544d8464dbbc3
Julian Bäume <julian.baeume@open-xchange.com>
2020-11-24 16:13:49 +0100

    add lint-staged and husky
    this should prevent commiting unlinted stuff. This means it also can be
    removed from the grunt build stage.

--- ui/grunt/tasks/eslint.js
+++ ui/grunt/tasks/eslint.js
@@ -1,6 +1,4 @@
 module.exports = function (grunt) {
-    var patterns = grunt.config.get('eslint.all.files.0.src');
-    patterns.push('e2e/**/*.{js,json}');
-    grunt.config.set('eslint.all.files.0.src', patterns);
+    grunt.config.set('eslint.all.files', []);
 };
 
