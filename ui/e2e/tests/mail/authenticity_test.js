4405eaa08a585983f3ef6c3ada4447a86848086c
Julian Bäume <julian.baeume@open-xchange.com>
2021-01-06 18:19:13 +0100

    [e2e] refactor authenticity tests
    speed up testrun by running 9 chunks of 9 mails each to distribute accross runners.
    
    also some minor code cleanup and async fixes

--- ui/e2e/tests/mail/authenticity_test.js
+++ ui/e2e/tests/mail/authenticity_test.js
@@ -11,136 +11,69 @@
  */
 ///  <reference path="../../steps.d.ts" />
 
-const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
 
-Scenario('[C244757] SPF, DKIM, DMARC, DMARC Policy matrix', async function (I, users, mail) {
+Feature('Middleware > Mail authenticity');
+
+Before(async (users) => {
+    await users.create();
+});
+
+After(async (users) => {
+    await users.removeAll();
+});
+
+const testMails = new DataTable(['testMails']);
+/* global codecept_dir: true */
+const files = fs.readdirSync(path.join(codecept_dir, 'e2e/media/mails/authenticity'));
+/*
+  files should contain 81 files which are split into chunks of 9 mails.
+  this reduces the overhead of running one test per mail while keeping the
+  possibility to speed up testrun using parallelisation
+*/
+while (files.length) {
+    testMails.add([files.splice(0, 9)]);
+}
+
+Data(testMails).Scenario('[C244757] SPF, DKIM, DMARC, DMARC Policy matrix', async function (I, users, mail, current) {
     let [user] = users;
-    user.hasConfig('com.openexchange.mail.authenticity.enabled', true);
-    user.hasConfig('com.openexchange.mail.authenticity.authServId', 'mx.recipient.ox');
+    const mails = current.testMails.map(m => {
+        return I.haveMail({ folder: 'default0/INBOX', path: path.join('e2e/media/mails/authenticity', m) }, { user });
+    });
     await Promise.all([
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/1-A-SPFpass-DKIMpass-DMARCpass-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/1-A-SPFpass-DKIMpass-DMARCpass-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/1-A-SPFpass-DKIMpass-DMARCpass-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/10-B-SPFpass-DKIMmissing-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/11-C-SPFpass-DKIMfail-DMARCpass-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/11-C-SPFpass-DKIMfail-DMARCpass-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/11-C-SPFpass-DKIMfail-DMARCpass-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/12-C-SPFpass-DKIMfail-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/13-C-SPFpass-DKIMfail-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/14-C-SPFpass-DKIMfail-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/15-C-SPFpass-DKIMfail-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/16-D1-SPFmissing-DKIMpass-DMARCpass-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/16-D1-SPFmissing-DKIMpass-DMARCpass-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/16-D1-SPFmissing-DKIMpass-DMARCpass-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/16-D1-SPFneutral-DKIMpass-DMARCpass-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/16-D1-SPFneutral-DKIMpass-DMARCpass-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/16-D1-SPFneutral-DKIMpass-DMARCpass-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/17-D1-SPFmissing-DKIMpass-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/17-D1-SPFneutral-DKIMpass-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/18-D1-SPFmissing-DKIMpass-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/18-D1-SPFneutral-DKIMpass-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/19-D1-SPFmissing-DKIMpass-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/19-D1-SPFneutral-DKIMpass-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/2-A-SPFpass-DKIMpass-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/20-D1-SPFmissing-DKIMpass-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/20-D1-SPFneutral-DKIMpass-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/21-D2-SPFmissing-DKIMmissing-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/21-D2-SPFneutral-DKIMmissing-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/22-D2-SPFmissing-DKIMmissing-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/22-D2-SPFneutral-DKIMmissing-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/23-D2-SPFmissing-DKIMmissing-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/23-D2-SPFneutral-DKIMmissing-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/24-D2-SPFmissing-DKIMmissing-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/24-D2-SPFneutral-DKIMmissing-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/25-D3-SPFmissing-DKIMfail-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/25-D3-SPFneutral-DKIMfail-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/26-D3-SPFmissing-DKIMfail-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/26-D3-SPFneutral-DKIMfail-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/27-D3-SPFmissing-DKIMfail-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/27-D3-SPFneutral-DKIMfail-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/28-D3-SPFmissing-DKIMfail-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/28-D3-SPFneutral-DKIMfail-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/29-E1-SPFsoftfail-DKIMpass-DMARCpass-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/29-E1-SPFsoftfail-DKIMpass-DMARCpass-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/29-E1-SPFsoftfail-DKIMpass-DMARCpass-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/3-A-SPFpass-DKIMpass-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/30-E1-SPFsoftfail-DKIMpass-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/31-E1-SPFsoftfail-DKIMpass-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/32-E1-SPFsoftfail-DKIMpass-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/33-E1-SPFsoftfail-DKIMpass-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/34-E2-SPFsoftfail-DKIMmissing-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/35-E2-SPFsoftfail-DKIMmissing-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/36-E2-SPFsoftfail-DKIMmissing-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/37-E2-SPFsoftfail-DKIMmissing-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/38-E3-SPFsoftfail-DKIMfail-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/39-E3-SPFsoftfail-DKIMfail-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/4-A-SPFpass-DKIMpass-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/40-E3-SPFsoftfail-DKIMfail-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/41-E3-SPFsoftfail-DKIMfail-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/42-F1-SPFfail-DKIMpass-DMARCpass-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/42-F1-SPFfail-DKIMpass-DMARCpass-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/42-F1-SPFfail-DKIMpass-DMARCpass-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/43-F1-SPFfail-DKIMpass-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/44-F1-SPFfail-DKIMpass-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/45-F1-SPFfail-DKIMpass-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/46-F1-SPFfail-DKIMpass-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/47-F2-SPFfail-DKIMmissing-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/48-F2-SPFfail-DKIMmissing-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/49-F2-SPFfail-DKIMmissing-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/5-A-SPFpass-DKIMpass-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/50-F2-SPFfail-DKIMmissing-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/51-F3-SPFfail-DKIMfail-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/52-F3-SPFfail-DKIMfail-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/53-F3-SPFfail-DKIMfail-DMARCfail-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/54-F3-SPFfail-DKIMfail-DMARCmissing.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/6-B-SPFpass-DKIMmissing-DMARCpass-none.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/6-B-SPFpass-DKIMmissing-DMARCpass-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/6-B-SPFpass-DKIMmissing-DMARCpass-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/7-B-SPFpass-DKIMmissing-DMARCfail-reject.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/8-B-SPFpass-DKIMmissing-DMARCfail-quarantine.eml' }, { user }),
-        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/9-B-SPFpass-DKIMmissing-DMARCfail-none.eml' }, { user })
-    ]);
+        user.hasConfig('com.openexchange.mail.authenticity.enabled', true),
+        user.hasConfig('com.openexchange.mail.authenticity.authServId', 'mx.recipient.ox')
+    ].concat.apply(mails, mails));
     I.login('app=io.ox/mail');
     mail.waitForApp();
-    const numOfMails = await I.grabNumberOfVisibleElements('.list-view li.list-item');
-    for (let i = 0; i < numOfMails; i++) {
-        mail.selectMailByIndex(i);
+    for (let index = 0; index < mails.length; index++) {
+        mail.selectMailByIndex(index);
         I.waitForElement('.mail-detail-frame');
         I.switchTo('.mail-detail-frame');
         let mailContent = await I.grabTextFrom('.mail-detail-content');
         let result = mailContent.replace(/Result: /, '');
-        var notifications, addressColor, backgroundColor;
+        I.switchTo();
+        //I.waitForElement('.address.authenticity-sender');
+        I.waitForElement('.address');
         switch (result) {
             case 'pass':
             case 'neutral':
-                I.switchTo();
-                I.waitForElement('.address');
-                addressColor = await I.grabCssPropertyFrom('.address', 'color');
-                assert.equal(addressColor, 'rgb(118, 118, 118)');
-                notifications = await I.grabHTMLFrom('.mail-detail .notifications');
-                assert.equal(notifications, '<div></div>');
+                I.seeCssPropertiesOnElements('.address', { color: 'rgb(118, 118, 118)' });
                 I.dontSee('Warning: Be careful with this message. It might be spam or a phishing mail.');
                 I.dontSee('Warning: This is a dangerous email containing spam or malware.');
                 break;
             case 'suspicious':
-                I.switchTo();
-                I.waitForElement('.address.authenticity-sender.suspicious');
-                addressColor = await I.grabCssPropertyFrom('.address.authenticity-sender.suspicious', 'color');
-                assert.equal(addressColor, 'rgb(197, 0, 0)');
+                I.seeCssPropertiesOnElements('.address', { color: 'rgb(197, 0, 0)' });
                 I.waitForElement('.authenticity .suspicious');
-                backgroundColor = await I.grabCssPropertyFrom('.notifications .authenticity .suspicious', 'background-color');
-                assert.equal(backgroundColor, 'rgb(197, 0, 0)');
+                I.seeCssPropertiesOnElements('.notifications .authenticity .suspicious', { 'background-color': 'rgb(197, 0, 0)' });
                 I.waitForElement('.mail-detail .authenticity-icon-suspicious');
                 I.see('Warning: Be careful with this message. It might be spam or a phishing mail.');
                 break;
             case 'fail':
-                I.switchTo();
-                I.waitForElement('.address.authenticity-sender.fail');
-                addressColor = await I.grabCssPropertyFrom('.address.authenticity-sender.fail', 'color');
-                assert.equal(addressColor, 'rgb(197, 0, 0)');
+                I.seeCssPropertiesOnElements('.address', { color: 'rgb(197, 0, 0)' });
                 I.waitForElement('.authenticity .fail');
-                backgroundColor = await I.grabCssPropertyFrom('.authenticity .fail', 'background-color');
-                assert.equal(backgroundColor, 'rgb(197, 0, 0)');
+                I.seeCssPropertiesOnElements('.authenticity .fail', { 'background-color': 'rgb(197, 0, 0)' });
                 I.waitForElement('.authenticity-icon-fail');
                 I.see('Warning: This is a dangerous email containing spam or malware.');
                 break;
@@ -148,4 +81,5 @@ Scenario('[C244757] SPF, DKIM, DMARC, DMARC Policy matrix', async function (I, u
                 throw new Error(`Unknown result: ${result}`);
         }
     }
+    I.seeNumberOfVisibleElements('.list-view li.list-item', mails.length);
 });
