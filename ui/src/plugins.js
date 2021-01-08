f5e9fb1527077d9a783b7d7ba7301dad6878187e
Julian Bäume <julian.baeume@open-xchange.com>
2021-01-08 12:02:54 +0100

    Fixed: [OXUIB-645] XSS using script code as module at app loader
    Root cause: relative paths in JS module urls might lead browsers to request
        resources outside the actual code loading servlet
    Solution: do not allow relative paths as JS module names, those need to be resolved
        elsewhere
    
    (cherry picked from commit aa17734960cdf079d17af58e233b664ff34e906d)

--- ui/src/plugins.js
+++ ui/src/plugins.js
@@ -405,7 +405,11 @@
 
                 while (modules.length > 0) {
                     url = base;
-                    while (modules[0] && url.length + 1 + modules[0].length < limit) {
+                    while (modules[0] && ox.abs.length + url.length + 1 + modules[0].length < limit) {
+                        if (modules[0].indexOf('..') >= 0) {
+                            console.error('module names must not be relative');
+                            modules.shift();
+                        }
                         url += ',' + modules.shift();
                     }
                     requests.push($.ajax({ url: url, dataType: 'text' }));
