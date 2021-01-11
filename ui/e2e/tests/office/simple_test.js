51b5cd70b5d08afe619edbe5c489cf6b3a93740c
Daniel Rentz <daniel.rentz@open-xchange.com>
2021-01-11 09:29:26 +0100

    activate toolbars, use toolbar buttons

--- ui/e2e/tests/office/simple_test.js
+++ ui/e2e/tests/office/simple_test.js
@@ -18,6 +18,10 @@ After(async (users) => {
     await users.removeAll();
 });
 
+const TEXT1 = 'This is a test paragraph.';
+const TEXT2 = ' Bold text.';
+const ALLSAVED = 'All changes saved';
+
 Scenario('[C9999] simple test', async function (I, users) {
 
     // login and wait for the "Text Portal" app to appear
@@ -25,7 +29,7 @@ Scenario('[C9999] simple test', async function (I, users) {
     I.login('app=io.ox/office/portal/text', { user });
     I.waitForVisible('.io-ox-office-portal-text-window');
 
-    // lick the "Blank text document" button to create a new text document
+    // click the "Blank text document" button to create a new text document
     I.waitForText('Blank text document');
     I.click('Blank text document');
 
@@ -38,9 +42,18 @@ Scenario('[C9999] simple test', async function (I, users) {
     I.waitForVisible('.io-ox-office-text-main .page.edit-mode', 10);
 
     // type some text into the page, wait for the text to be saved to server
-    I.type('This is a test paragraph.', 25);
-    I.waitForText('This is a test paragraph.', 1, '.page.edit-mode');
-    I.waitForText('All changes saved');
+    I.type(TEXT1, 25);
+    I.waitForText(TEXT1, 1, '.page.edit-mode');
+    I.waitForText(ALLSAVED);
+
+    // activate the "Format" toolpane, press the "Bold" button
+    I.click('.group[data-key="view/toolbars/tab"] a[data-value="format"]');
+    I.click('.group[data-key="character/bold"] a');
+
+    // type more text into the page, wait for the text to be saved to server
+    I.type(TEXT2, 25);
+    I.waitForText(TEXT2, 1, '.page.edit-mode');
+    I.waitForText(ALLSAVED);
 
     // close the document, and wait for the "Text Portal" app in the browser tab
     I.click('.group[data-key="app/quit"] a');
