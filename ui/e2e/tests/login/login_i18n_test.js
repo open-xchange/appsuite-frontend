0ea598bf0d201282ae9d87406113be9bee21b3cd
Maik Schäfer <maik.schaefer@open-xchange.com>
2021-01-13 09:45:13 +0100

    e2e: fix test OXUI-700

--- ui/e2e/tests/login/login_i18n_test.js
+++ ui/e2e/tests/login/login_i18n_test.js
@@ -52,7 +52,7 @@ Scenario('[OXUI-700] for guest users with password', async (I, users, dialogs) =
     I.waitForFocus('#io-ox-login-password');
     I.click('#io-ox-languages .dropdown > a');
     I.click('Deutsch (Deutschland)');
-    I.waitForText('hat Ordner "Musik" für Sie freigegeben', 5);
+    I.waitForText('hat den Ordner "Musik" für Sie freigegeben', 5);
     I.click('#io-ox-languages .dropdown > a');
     I.click('English (United States)');
     I.waitForText('has shared the folder "Music" with you', 5);
