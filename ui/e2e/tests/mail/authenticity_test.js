0d2e77db1bbc3d0dc8233ba654696c27b2da25f5
anne.matthes <anne.matthes@open-xchange.com>
2021-01-18 09:32:18 +0100

    add test [C241140] Availability within folders

--- ui/e2e/tests/mail/authenticity_test.js
+++ ui/e2e/tests/mail/authenticity_test.js
@@ -83,3 +83,93 @@ Data(testMails).Scenario('[C244757] SPF, DKIM, DMARC, DMARC Policy matrix', asyn
     }
     I.seeNumberOfVisibleElements('.list-view li.list-item', mails.length);
 });
+
+const helper = {
+    selectAndVerifyAuthenticity: function (I, mail, isSuspicious) {
+        mail.selectMail('12-C-SPFpass-DKIMfail-DMARCfail-reject');
+        if (isSuspicious) {
+            I.waitForElement('.mail-detail-frame');
+            I.waitForElement('.authenticity .fail');
+            I.see('Warning: This is a dangerous email containing spam or malware.');
+        } else {
+            I.waitForElement('.mail-detail-frame');
+            I.waitForElement('.mail-detail .notifications');
+            I.dontSee('Warning: This is a dangerous email containing spam or malware.');
+        }
+        I.waitForNetworkTraffic();
+    },
+    openMoveDialog: function (I) {
+        I.waitForVisible('~More actions', '.classic-toolbar-container');
+        I.waitForClickable('.classic-toolbar-container [data-action="more"]');
+        I.waitForEnabled('.classic-toolbar-container [data-action="more"]');
+        I.wait(0.5);
+        I.clickToolbar('.classic-toolbar-container [data-action="more"]');
+        I.waitForVisible('.dropdown.open');
+        I.clickDropdown('Move');
+        I.waitForElement('.folder-picker-dialog');
+    },
+    selectFolderInMoveDialog: function (I, folderName) {
+        I.click(`.folder-picker-dialog .folder[data-id="default0/INBOX/${folderName}"]`);
+        I.waitForElement(`.folder-picker-dialog .folder[data-id="default0/INBOX/${folderName}"].selected`);
+    },
+    clickMoveAndChangeFolder: function (I, dialogs, folderName) {
+        dialogs.clickButton('Move');
+        I.waitForDetached('.modal-dialog');
+        I.waitForText(folderName, 5, '.folder-tree');
+        I.selectFolder(folderName, 'mail');
+        I.waitForElement(`.folder.selected[aria-label*="${folderName}"]`, 5, '.folder-tree');
+        I.waitForDetached('.busy-indicator.io-ox-busy', 5, '.window-content.vsplit .leftside');
+    }
+};
+
+Scenario('[C241140] Availability within folders', async function ({ I, users, mail, dialogs }) {
+    const [user] = users;
+    await Promise.all([
+        user.hasConfig('com.openexchange.mail.authenticity.enabled', true),
+        user.hasConfig('com.openexchange.mail.authenticity.authServId', 'mx.recipient.ox'),
+        // 1.) Receive a mail with authentication headers, stored to INBOX.
+        I.haveMail({ folder: 'default0/INBOX', path: 'e2e/media/mails/authenticity/12-C-SPFpass-DKIMfail-DMARCfail-reject.eml' }, { user })
+    ]);
+    I.login('app=io.ox/mail');
+    mail.waitForApp();
+    helper.selectAndVerifyAuthenticity(I, mail, true);
+    // 2.) Create a new mail folder and move that mail there.
+    helper.openMoveDialog(I);
+    I.click('Create folder');
+    I.waitForText('Add new folder', 5, dialogs.locators.header);
+    I.fillField('Folder name', 'Testfolder');
+    dialogs.clickButton('Add');
+    I.waitForText('Move', 5, dialogs.locators.header);
+    I.waitForElement('.folder-picker-dialog .folder[data-id="default0/INBOX/Testfolder"].selected');
+    helper.clickMoveAndChangeFolder(I, dialogs, 'Testfolder');
+    helper.selectAndVerifyAuthenticity(I, mail, true);
+    // 3.) Move the mail to "Spam"
+    helper.openMoveDialog(I);
+    helper.selectFolderInMoveDialog(I, 'Spam');
+    helper.clickMoveAndChangeFolder(I, dialogs, 'Spam');
+    helper.selectAndVerifyAuthenticity(I, mail, true);
+    // 4.) Move the mail to "Trash"
+    helper.openMoveDialog(I);
+    helper.selectFolderInMoveDialog(I, 'Trash');
+    helper.clickMoveAndChangeFolder(I, dialogs, 'Trash');
+    helper.selectAndVerifyAuthenticity(I, mail, true);
+    // 5.) Archive the mail
+    I.waitForVisible('~Archive', '.classic-toolbar-container');
+    I.click('~Archive', '.classic-toolbar-container');
+    I.waitForText('Archive', '.folder-tree');
+    I.waitForElement('.folder-tree [data-id="default0/INBOX/Archive"] .folder-arrow');
+    I.click('.folder-tree [data-id="default0/INBOX/Archive"] .folder-arrow');
+    I.waitForText('2018', '.folder-tree');
+    I.selectFolder('2018');
+    helper.selectAndVerifyAuthenticity(I, mail, true);
+    // 6.) Move the mail to "Sent objects"
+    helper.openMoveDialog(I);
+    helper.selectFolderInMoveDialog(I, 'Sent');
+    helper.clickMoveAndChangeFolder(I, dialogs, 'Sent');
+    helper.selectAndVerifyAuthenticity(I, mail, false);
+    // 7.) Move the mail to "Drafts"
+    helper.openMoveDialog(I);
+    helper.selectFolderInMoveDialog(I, 'Drafts');
+    helper.clickMoveAndChangeFolder(I, dialogs, 'Drafts');
+    helper.selectAndVerifyAuthenticity(I, mail, false);
+});
