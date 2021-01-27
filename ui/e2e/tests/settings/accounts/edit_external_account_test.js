8e3032222ab9148f1cb53c0ad3fb9ac6f19e14ba
Julian Bäume <julian.baeume@open-xchange.com>
2021-01-27 11:17:31 +0100

    simplify external account test

--- ui/e2e/tests/settings/accounts/edit_external_account_test.js
+++ ui/e2e/tests/settings/accounts/edit_external_account_test.js
@@ -15,71 +15,44 @@
 
 Feature('Custom mail account');
 
-Before(async function (users, contexts) {
-    const [JITTER_MIN, JITTER_MAX] = [1000, 2000];
-    const secondContextId = Math.trunc(process.env.CONTEXT_ID)
-        + Math.floor(Math.random()
-        * (JITTER_MAX - JITTER_MIN + 1) + JITTER_MIN);
+Before(async function ({ users }) {
     await users.create();
-    const secondContext = await contexts.create({ id: secondContextId });
-    await users.create(users.getRandom(), secondContext);
 });
 
-After(async function (users, contexts) {
+After(async function ({ users }) {
     await users.removeAll();
-    await contexts.removeAll();
 });
 
-Scenario('[C7839] Edit external mail account', async (I, users, mail, settings, dialogs) => {
+Scenario('[C7839] Edit external mail account', async ({ I, mail, settings, dialogs }) => {
     await I.haveSetting('io.ox/mail//messageFormat', 'text');
-    const [internalUser, externalUser] = users;
-    const {
-        primaryEmail,
-        imapSchema,
-        imapServer,
-        imapPort,
-        imapLogin,
-        password,
-        smtpSchema,
-        smtpServer,
-        smtpPort
-    } = externalUser.userdata;
 
-    const externalAccount = {
-        name: 'External',
-        primary_address: primaryEmail,
-        login: imapLogin,
-        password: password,
-        mail_url: `${imapSchema}${imapServer}:${imapPort}`,
-        transport_url: `${smtpSchema}${smtpServer}:${smtpPort}`,
-        transport_auth: 'none'
-    };
+    const { data: {
+        primary_address
+    } } = await I.haveMailAccount({ name: 'External', extension: 'ext' });
 
-    await I.haveMailAccount(externalAccount);
-
-    I.login({ user: internalUser });
+    I.login();
     mail.waitForApp();
-    I.click('~Settings');
+    I.openApp('Settings');
     settings.waitForApp();
     settings.select('Accounts');
     I.waitForVisible('~Edit External');
     I.retry(10).click('~Edit External');
     dialogs.waitForVisible();
-    I.fillField('Account name', externalUser.userdata.primaryEmail);
-    I.fillField('Email address', externalUser.userdata.primaryEmail);
-    I.seeAttributesOnElements('input[name="name"]', { disabled: false });
-    I.seeAttributesOnElements('input[name="personal"]', { disabled: false });
-    I.seeAttributesOnElements('input[name="primary_address"]', { disabled: false });
-    I.seeAttributesOnElements('select[name="mail_protocol"]', { disabled: true });
-    I.seeAttributesOnElements('input[name="mail_server"]', { disabled: false });
-    I.seeAttributesOnElements('select[name="mail_secure"]', { disabled: false });
-    I.seeAttributesOnElements('input[name="mail_port"]', { disabled: false });
-    I.seeAttributesOnElements('input[name="login"]', { disabled: false });
-    I.seeAttributesOnElements('input[id="password"]', { disabled: false });
-    I.seeAttributesOnElements('input[name="transport_server"]', { disabled: false });
-    I.seeAttributesOnElements('select[name="transport_secure"]', { disabled: false });
-    I.seeAttributesOnElements('input[name="transport_port"]', { disabled: false });
-    I.seeAttributesOnElements('select[name="transport_auth"]', { disabled: false });
-    I.seeAttributesOnElements('input[name="transport_login"]', { disabled: true });
-    I.seeAttributesOnElements('input[id="transport_password"]', { disabled: true });
+    I.fillField('Account name', primary_address);
+    I.fillField('Email address', primary_address);
+    I.dontSeeElement('input[name="name"][disabled]');
+    I.dontSeeElement('input[name="personal"][disabled]');
+    I.dontSeeElement('input[name="primary_address"][disabled]');
+    I.seeElement('select[name="mail_protocol"][disabled]');
+    I.dontSeeElement('input[name="mail_server"][disabled]');
+    I.dontSeeElement('select[name="mail_secure"][disabled]');
+    I.dontSeeElement('input[name="mail_port"][disabled]');
+    I.dontSeeElement('input[name="login"][disabled]');
+    I.dontSeeElement('input[id="password"][disabled]');
+    I.dontSeeElement('input[name="transport_server"][disabled]');
+    I.dontSeeElement('select[name="transport_secure"][disabled]');
+    I.dontSeeElement('input[name="transport_port"][disabled]');
+    I.dontSeeElement('select[name="transport_auth"][disabled]');
+    I.seeElement('input[name="transport_login"][disabled]');
+    I.seeElement('input[id="transport_password"][disabled]');
 });
