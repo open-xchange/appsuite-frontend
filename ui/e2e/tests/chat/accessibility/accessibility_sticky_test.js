c72304416ede07950898f27f8b407f65bc71c44f
Tran Dong Tran <tran-dong.tran@open-xchange.com>
2021-02-02 16:15:46 +0100

    e2e: Add helpers and two tests
    helpers:
    - pressTab: press tab x times
    - pressShiftTab: press shift+tab x times
    - checkDropdowns: opens and closes them with keyboard and checks correct focus on keyboard navigation
    - openChat: for initial login, navigate to chat by keyboard
    - getChatLocator: returns locator for chat entry by name
    
    tests:
    - Check Tab Navigation in sticky mode - Main
    - Check Tab Navigation in sticky mode - Modals and Private chats

--- ui/e2e/tests/chat/accessibility/accessibility_sticky_test.js
+++ ui/e2e/tests/chat/accessibility/accessibility_sticky_test.js
@@ -23,245 +23,167 @@ After(async ({ users }) => {
     await users.removeAll();
 });
 
-Scenario.only('Check Tab Navigation in sticky mode', async ({ I, mail, dialogs }) => {
-
-    function pressTab(times) {
-        for (let i = 0; i < times; i++) {
-            I.pressKey('Tab');
-        }
+async function pressTab(I, times) {
+    for (let i = 0; i < times; i++) {
+        I.pressKey('Tab');
     }
+}
+
+async function pressShiftTab(I, times) {
+    I.pressKeyDown('Shift');
+    for (let i = 0; i < times; i++) {
+        I.pressKey('Tab');
+    }
+    I.pressKeyUp('Shift');
+}
+
+async function checkDropdowns(I, toggleSelector) {
+    const toggleLocator = locate(toggleSelector);
+
+    I.waitForFocus(toggleLocator);
+
+    // dropdown 'New chat': open and close
+    ['Enter', 'ArrowDown', 'ArrowUp', 'Space'].forEach(function (key) {
+        I.pressKey(key);
+        I.waitForVisible('.dropdown.open ul');
+        I.pressKey('Escape');
+        I.waitForInvisible('.dropdown.open ul');
+        I.waitForFocus(toggleLocator);
+    });
+
+    I.pressKey('Enter');
+    const size = await I.grabNumberOfVisibleElements('.dropdown-menu > li > a');
+
+    for (let i = size; i > 0; i--) {
+        I.waitForFocus(locate(`.dropdown.open ul li:nth-of-type(${i % size + (size - 1)}) > a`).as(`Dropdown item #${i % size + 1}`));
+        I.pressKey('ArrowUp');
+    }
+    I.pressKey('Escape');
+    I.waitForFocus(toggleLocator);
+    I.pressKey('Enter');
+    for (let i = 0; i < size; i++) {
+        I.waitForFocus(locate(`.dropdown.open ul li:nth-of-type(${i % size + (size - 1)}) > a`).as(`Dropdown item #${i % size + 1}`));
+        I.pressKey('ArrowDown');
+    }
+    I.pressKey('Escape');
+    I.waitForFocus(toggleLocator);
+}
+
+// navigate to chat, open chat, initial focus: New Chat
+async function openChat(I) {
+    await pressShiftTab(I, 3);
+    I.pressKey('ArrowLeft');
+    I.pressKey('ArrowLeft');
+    I.pressKey('Enter');
+    I.waitForVisible('.ox-chat');
+    I.waitForFocus(locate('a.dropdown-toggle').withText('New Chat'));
+}
+
+function getChatLocator(name) {
+    return locate('.accessible-list .chat-list > li').withText(name);
+}
+
+// for tab navigation and dropdowns in main menu of sticky mode
+Scenario('Check Tab Navigation in sticky mode - Main', async ({ I, mail }) => {
+
+    const closeChatButton = locate('~Close chat'),
+        navButtonLocator = (text) => locate({ css: '.navigation-actions button.btn-nav' }).withText(text);
 
     I.login();
     mail.waitForApp();
+    await openChat(I);
 
-    // Open chat, initial focus: New Chat
-    I.pressKey(['Shift', 'Tab']);
-    I.wait(1);
-    I.pressKey(['Shift', 'Tab']);
-    I.pressKey('ArrowLeft');
-    I.pressKey('ArrowLeft');
+    await checkDropdowns(I, locate('a.dropdown-toggle').withText('New Chat'));
 
-    I.pressKey('Enter');
-    I.waitForVisible('.ox-chat');
-
-    // test dropdown: open and close
-    ['Enter', 'ArrowDown', 'ArrowUp', 'Space'].forEach(function (key) {
-        I.pressKey(key);
-        I.waitForVisible('.ox-chat .classic-toolbar-container .dropdown-menu');
-        I.pressKey('Escape');
-        I.waitForInvisible('.ox-chat .classic-toolbar-container .dropdown-menu');
-        I.waitForFocus('.ox-chat .classic-toolbar-container .dropdown-toggle');
-    });
-
-    // test dropdown: navigate with arrow keys
-    I.pressKey('Enter');
-    for (let i = 3; i > 0; i--) {
-        I.waitForFocus(`.ox-chat .classic-toolbar-container .dropdown-menu li:nth-of-type(${i % 3 + 2}) > a`);
-        I.pressKey('ArrowUp');
-    }
-    I.pressKey('Escape');
-    I.waitForFocus('.ox-chat .classic-toolbar-container .dropdown-toggle');
-    I.pressKey('Enter');
-    for (let i = 0; i < 3; i++) {
-        I.waitForFocus(`.ox-chat .classic-toolbar-container .dropdown-menu li:nth-of-type(${i % 3 + 2}) > a`);
-        I.pressKey('ArrowDown');
-    }
-
-    // check create private chat
-    I.pressKey('Enter');
-    dialogs.waitForVisible();
-    I.waitForText('Start new conversation', 5, dialogs.locators.header);
-    I.waitForFocus('.modal-dialog .search-field');
-    pressTab(2);
-    I.pressKey('Enter');
-    I.waitForEnabled('.modal-dialog .btn-primary', 5);
-    pressTab(2);
-    I.pressKey('Enter');
-    I.waitForDetached('.modal-dialog');
-    I.waitForElement('.ox-chat .controls');
-
-    // send message in newly created chat
-    I.waitForFocus('[aria-label="Message"]');
-    I.type('Test message');
+    // navigate to action
     I.pressKey('Tab');
-    I.pressKey('Enter');
+    I.waitForFocus(locate('~Search or start new chat').as('Search field'));
+    I.pressKey('Tab');
+    I.waitForFocus('.accessible-list');
+    I.pressKey('Tab');
 
-    // go back to list
-    for (let i = 0; i < 4; i++) I.pressKey(['Shift', 'Tab']);
+    // check tab order of navigation actions
+    // All channels
+    I.waitForFocus(navButtonLocator('All channels'));
+    I.pressKey('Enter');
+    I.waitForFocus(closeChatButton);
+    I.pressKey('Enter');
+    I.waitForFocus(navButtonLocator('All channels'));
+    I.pressKey('Tab');
+    // All files
+    I.waitForFocus(navButtonLocator('All files'));
+    I.pressKey('Enter');
+    I.waitForFocus(closeChatButton);
+    I.pressKey('Enter');
+    I.waitForFocus(navButtonLocator('All files'));
+    I.pressKey('Tab');
+    // History
+    I.waitForFocus(navButtonLocator('History'));
+    I.pressKey('Enter');
+    I.waitForFocus(closeChatButton);
+    I.pressKey('Enter');
+    I.waitForFocus(navButtonLocator('History'));
+
+    // navigate to mode-toggle and check focus
+    await pressShiftTab(I, 5);
     I.pressKey('RightArrow');
-    I.pressKey('RightArrow');
-
-    // test dropdown 'More actions': open and close
-    ['Enter', 'ArrowDown', 'ArrowUp', 'Space'].forEach(function (key) {
-        I.pressKey(key);
-        I.waitForVisible('.smart-dropdown-container .dropdown-menu');
-        I.pressKey('Escape');
-        I.waitForInvisible('.smart-dropdown-container .dropdown-menu');
-        I.waitForFocus('.ox-chat .chat-rightside .classic-toolbar-container .dropdown-toggle');
-    });
-
-    // test dropdown 'More actions: navigate with arrow keys
-    I.pressKey('Enter');
-    for (let i = 2; i > 0; i--) {
-        I.waitForFocus(`.smart-dropdown-container li:nth-of-type(${i % 2 + 1}) > a`);
-        I.pressKey('ArrowUp');
-    }
-    I.pressKey('Escape');
-    I.waitForFocus('.ox-chat .chat-rightside .classic-toolbar-container .dropdown-toggle');
-    I.pressKey('Enter');
-    for (let i = 0; i < 3; i++) {
-        I.waitForFocus(`.smart-dropdown-container .dropdown-menu li:nth-of-type(${i % 2 + 1}) > a`);
-        I.pressKey('ArrowDown');
-    }
-
-    // TBD: is this the proper fix? expect first tabbable to be focused
-    // switch to floating window and back
-    I.pressKey('Escape');
-    I.pressKey('ArrowLeft');
+    I.waitForFocus(locate('~Detach window'));
     I.pressKey('Enter');
     I.waitForVisible('.floating-window .ox-chat');
-    I.pressKey(['Shift', 'Tab']);
-    I.pressKey('Enter');
+    I.waitForFocus(locate('~New'));
+});
 
-    I.waitForVisible('.ox-chat.columns');
-    I.pressKey('Enter');
-    I.waitForVisible('.ox-chat .accessible-list');
-    I.pressKey(['Shift', 'Tab']);
-    I.pressKey(['Shift', 'Tab']);
-    I.pressKey('ArrowDown');
+Scenario('Check Tab Navigation in sticky mode - Modals and Private chats', async ({ I, users, mail, dialogs }) => {
+    const newChatLocator = locate('a.dropdown-toggle').withText('New Chat'),
+        messageLocator = locate('~Message');
 
-    // check create group chat
+    I.login();
+    mail.waitForApp();
+    await openChat(I);
+
+    // 1. close and re-open modal-dialog
+    I.pressKey('Enter');
+    I.waitForVisible('.ox-chat .open .dropdown-menu');
     I.pressKey('Enter');
     dialogs.waitForVisible();
-    I.waitForText('Create group chat', 5, dialogs.locators.header);
-    I.waitForFocus('.modal-dialog .form-group input.form-control:nth-of-type(1)');
-    I.pressKey(['Shift', 'Tab']);
-    I.waitForFocus('.modal-dialog div.contact-photo.empty');
-    I.pressKey('Tab');
-    I.type('Test group');
-    pressTab(2);
-    I.pressKey('Enter');
-    I.waitForText('Select contacts', 5, dialogs.locators.header);
     I.pressKey('Escape');
-    I.waitForText('Create group chat', 5, dialogs.locators.header);
-    pressTab(4);
+    I.waitForFocus(newChatLocator);
     I.pressKey('Enter');
-    I.waitForDetached('.modal-dialog');
-    I.waitForElement('.ox-chat .controls');
-
-    I.pressKey('Tab');
-    I.pressKey('Tab');
-    I.pressKey('Enter');
-    I.waitForEnabled('.modal-dialog .btn-primary', 5);
-    I.pressKey('Tab');
-    I.pressKey('Tab');
-    I.pressKey('Enter');
-    I.waitForDetached('.modal-dialog');
-    I.waitForElement('.ox-chat .controls');    // check private chat
+    I.waitForVisible('.ox-chat .open .dropdown-menu');
     I.pressKey('Enter');
     dialogs.waitForVisible();
+
+    // 2. create a private chat
     I.waitForText('Start new conversation', 5, dialogs.locators.header);
     I.waitForFocus('.modal-dialog .search-field');
-    I.pressKey('Tab');
-    I.pressKey('Tab');
+    await pressTab(I, 2);
     I.pressKey('Enter');
     I.waitForEnabled('.modal-dialog .btn-primary', 5);
-    I.pressKey('Tab');
-    I.pressKey('Tab');
+    await pressTab(I, 2);
     I.pressKey('Enter');
     I.waitForDetached('.modal-dialog');
     I.waitForElement('.ox-chat .controls');
+    I.waitForVisible(messageLocator);
+    I.haveFocus(messageLocator);
 
+    // 3. go back and check focus
+    // TODO: Remove check for dropdown
+    await pressShiftTab(I, 3);
+    I.pressKey('RightArrow');
+    I.pressKey('RightArrow');
+    await checkDropdowns(I, locate('~More actions'));
+    I.pressKey('LeftArrow');
+    I.pressKey('LeftArrow');
+    I.pressKey('Enter');
+    I.waitForFocus(getChatLocator(users[1].userdata.sur_name));
 
-    // // Close chat
-    // // TODO: Behavior for '~Close chat' or '~Chats' expected: something is selected from chat | actual: nothing is selected
-    // // TBD: selection on chat if existing (should be after Richards fix), otherwise first of list -> Ask David
-
-    // // TODO: Same for group and channels, expected: 3 in list
-    // // TBD: How to access editing a group profile picture?
-
-    // // TODO: All Channels, All files, History
-
-    // // check group chat
-    // I.pressKey('Enter');
-    // I.pressKey('ArrowDown');
-    // I.pressKey('Enter');
-    // dialogs.waitForVisible();
-    // I.waitForText('Create group chat', dialogs.locators.header);
-    // dialogs.clickButton('Cancel');
-    // I.waitForDetached('.modal-dialog');
-
-    // // check channel
-    // I.pressKey('Enter');
-    // I.pressKey('ArrowDown');
-    // I.pressKey('ArrowDown');
-    // I.pressKey('Enter');
-    // dialogs.waitForVisible();
-    // I.waitForText('Create group chat', dialogs.locators.header);
-    // dialogs.clickButton('Cancel');
-    // I.waitForDetached('.modal-dialog');
-
-    // pause();
-
-    // I.pressKey(['Shift', 'Tab']);
-    // I.waitForFocus('~New Chat');
-    // Check
-
-    // // toolbar: unselect-state
-    // I.say('> toolbar: unselect-state');
-    // I.pressKey('Tab');
-    // I.pressKey('Tab');
-    // I.pressKey('Tab');
-    // I.pressKey('Tab');
-    // await toolbar.testFocus(selector);
-    // await toolbar.testWrapAround(selector);
-    // //await toolbar.testDropdowns(selector);
-    // await toolbar.testTabindex(selector);
-
-    // // toolbar: sidepopup
-    // I.say('> toolbar: sidepopup');
-    // selector = '.io-ox-sidepopup-pane .inline-toolbar';
-    // I.click('.appointment');
-    // I.waitForElement(selector);
-    // I.pressKey('Tab');
-    // await toolbar.testFocus(selector);
-    // await toolbar.testWrapAround(selector);
-    // //await toolbar.testDropdowns(selector);
-    // await toolbar.testTabindex(selector);
-    // I.pressKey('Escape');
-    // I.waitForDetached(selector);
-
-    // // toolbar: unselect-state (list)
-    // I.say('> toolbar: unselect-state (list)');
-    // selector = '.classic-toolbar-container > ul[role="toolbar"]';
-    // I.clickToolbar(calendar.locators.view);
-    // I.click('List', calendar.locators.dropdown);
-    // I.waitForText('test appointment');
-    // I.pressKey('Tab');
-    // await toolbar.testFocus(selector);
-    // await toolbar.testWrapAround(selector);
-    // //await toolbar.testDropdowns(selector);
-    // await toolbar.testTabindex(selector);
-
-    // // toolbar: select-state
-    // I.say('> toolbar: select-state');
-    // I.click('.list-item.selectable');
-    // I.pressKey(['Shift', 'Tab']);
-    // await toolbar.testFocus(selector);
-    // await toolbar.testWrapAround(selector);
-    // //await toolbar.testDropdowns(selector);
-    // await toolbar.testTabindex(selector);
-
-    // // toolbar: detail view (floating window)
-    // I.say('> toolbar: detail view (floating window');
-    // selector = '.io-ox-calendar-detail-window .inline-toolbar';
-    // I.doubleClick(locate('.list-view .list-item.selectable').first());
-    // I.waitForElement(selector);
-    // I.pressKey('Tab');
-    // I.pressKey('Tab');
-    // I.pressKey('Tab');
-    // await toolbar.testFocus(selector);
-    // await toolbar.testWrapAround(selector);
-    // //await toolbar.testDropdowns(selector);
-    // await toolbar.testTabindex(selector);
+    // 4. When refocus is correct, this should re-open the chat
+    I.pressKey('Enter');
+    I.waitForText(users[1].userdata.sur_name, 5, '.ox-chat .toolbar-title');
 });
+
+// TODO SCENARIOS:
+// - Check focus on favorite toggle
+// - Check focus when there are multiple people in chat-list (and favorite?)
+// - Check modals for creating groups
+// -
