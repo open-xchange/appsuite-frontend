06f6e74f61f71b6e6936fbf88136ae17163aad5f
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2021-03-17 16:40:25 +0100

    Additional commit for OXUIB-770 Users can be lured to rogue OX Chat servers

--- ui/apps/io.ox/chat/api.js
+++ ui/apps/io.ox/chat/api.js
@@ -13,13 +13,14 @@
 
 
 define('io.ox/chat/api', [
-    'io.ox/switchboard/api'
-], function (switchboardApi) {
+    'io.ox/switchboard/api',
+    'settings!io.ox/chat'
+], function (switchboardApi, settings) {
 
     'use strict';
 
     // use '' as fallback so one missing chathost doesn't block the whole UI
-    var host = ox.serverConfig.chatHost || '';
+    var host = ox.serverConfig.chatHost || settings.get('host') || '';
     var url = (_.device('!IE') && host !== '') ? new URL('https://' + host.replace(/^https?:\/\//, '')) :
         // simple fallback, might not work for every host url
         {
