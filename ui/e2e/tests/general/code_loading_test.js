e1da78931ffafb8778351f78bcf98f31a4c57ae4
Maik Schäfer <maik.schaefer@open-xchange.com>
2021-04-14 17:11:44 +0200

    [e2e] fix code loading test

--- ui/e2e/tests/general/code_loading_test.js
+++ ui/e2e/tests/general/code_loading_test.js
@@ -26,11 +26,11 @@ Scenario('[XSS] [OXUIB-400] No malicious code execution when code loading fails'
     drive.waitForApp();
 });
 
-Scenario('[OXUIB-645] XSS using script code as module at app loader', async function ({ I, drive }) {
+Scenario('[OXUIB-645] XSS using script code as module at app loader', async function ({ I, drive, dialogs }) {
     I.login('app=io.ox/files');
     drive.waitForApp();
     I.clickToolbar('New');
-    I.clickDropdown('Note');
+    I.clickDropdown('Add note');
     I.waitForElement('.io-ox-editor .title');
     I.fillField('.io-ox-editor .title', 'OXUIB-645.js');
     I.fillField('.io-ox-editor .content', 'document.write("XSS");');
@@ -38,12 +38,12 @@ Scenario('[OXUIB-645] XSS using script code as module at app loader', async func
     I.wait(1);
     I.click('Close');
     I.click(locate('.list-item').withText('OXUIB-645.js'));
-    drive.shareItem();
-    I.selectOption('Who can access this folder?', 'Anyone with the link and invited people');
+    drive.shareItem('Create sharing link');
+    dialogs.waitForVisible();
     I.waitForNetworkTraffic();
-    let url = await I.grabValueFrom('.public-link-url-input');
+    let url = await I.grabValueFrom('.input-group.link-group .form-control');
     url = new URL(url);
-    I.click('Share', '.modal');
+    dialogs.clickButton('Close');
     I.waitToHide('.modal');
     const module = `${new Array(60).join('/..')}${url.pathname}?dl=1&cut=`;
     I.amOnPage('ui#!!&app=io.ox/files:foo,' + encodeURIComponent(module));
