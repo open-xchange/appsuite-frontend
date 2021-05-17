a2f0f0456cc1d71abf9c37f6757eafad1bbe3568
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2021-05-17 17:10:00 +0200

    Add token list
    improve style add tokenfield functionality
    
    Fix strange issue with zombie model

--- ui/apps/io.ox/contacts/enterprisepicker/dialog.js
+++ ui/apps/io.ox/contacts/enterprisepicker/dialog.js
@@ -25,6 +25,52 @@ define('io.ox/contacts/enterprisepicker/dialog', [
 
     var mockData = {};
 
+    var SelectedContactsView = DisposableView.extend({
+
+        tagName: 'ul',
+        className: 'selected-contacts-view list-unstyled',
+
+        events: {
+            'click button': 'removeContact'
+        },
+
+        initialize: function () {
+            this.model.get('selectedContacts').on('add reset remove', this.render.bind(this));
+        },
+
+        dispose: function () {
+            this.model.get('selectedContacts').off('add reset remove');
+        },
+
+        render: function () {
+            this.$el.empty();
+            var self = this,
+                length = this.model.get('selectedContacts').length;
+
+            this.$el.toggleClass('empty', length === 0);
+            if (length === 0) return this;
+
+            //#. %1$d is number of selected contacts
+            this.$el.append($('<div>').text(gt.ngettext('%1$d contact selected', '%1$d contacts selected', length, length)));
+            this.model.get('selectedContacts').each(function (contact) {
+                self.$el.append(
+                    $('<li>').append(
+                        $('<div class="name">').text(util.getFullName(contact.attributes, false)),
+                        $('<button class="btn">').attr('data-id', contact.get('id')).append($.icon('fa-times', gt('Remove contact from selection')))
+                    )
+                );
+            });
+
+            return this;
+        },
+
+        removeContact: function (e) {
+            e.stopPropagation();
+            this.model.get('selectedContacts').remove(e.currentTarget.getAttribute('data-id'));
+        }
+
+    });
+
     var ContactListView = DisposableView.extend({
 
         tagName: 'ul',
@@ -36,16 +82,19 @@ define('io.ox/contacts/enterprisepicker/dialog', [
 
         initialize: function () {
             this.model.get('contacts').on('add reset remove', this.render.bind(this));
+            this.model.get('selectedContacts').on('add reset remove', this.updateCheckboxes.bind(this));
         },
 
         dispose: function () {
             this.model.get('contacts').off('add reset remove');
+            this.model.get('selectedContacts').off('add reset remove');
         },
 
         renderContact: function (contact) {
             var node = $('<li>').attr('data-id', contact.get('id')).append(
-                $('<input type="checkbox">').attr('checked', this.model.get('selectedContacts').indexOf(contact.get('id')) > -1),
-                $('<div class="contact-picture" aria-hidden="true">').css('background-image', 'url(' + util.getImage(contact.attributes) + ')'),
+                $('<input type="checkbox">').attr({ 'data-id': contact.get('id'), checked: !!this.model.get('selectedContacts').get(contact.get('id')) }),
+                $('<div class="contact-picture" aria-hidden="true">')
+                    .css('background-image', 'url(' + (contact.get('image1_url') ? util.getImage(contact.attributes) : api.getFallbackImage()) + ')'),
                 $('<div class="contact-container">').append(
                     $('<div class="name" aria-hidden="true">').text(util.getFullName(contact.attributes, false)),
                     $('<div class="department" aria-hidden="true">').text(contact.get('department'))
@@ -88,22 +137,29 @@ define('io.ox/contacts/enterprisepicker/dialog', [
             e.stopPropagation();
             var target = e.currentTarget,
                 id = target.getAttribute('data-id'),
-                isSelected = this.model.get('selectedContacts').indexOf(id) > -1;
+                model = (this.model.get('contacts').length === 0 ? this.model.get('lastContacts') : this.model.get('contacts')).get(id),
+                isSelected = !!this.model.get('selectedContacts').get(id);
 
             if (isSelected) {
-                this.model.set('selectedContacts', _(this.model.get('selectedContacts')).without(id));
+                this.model.get('selectedContacts').remove(model);
             } else {
-                this.model.get('selectedContacts').push(id);
+                this.model.get('selectedContacts').add(model);
             }
+        },
 
-            $(e.currentTarget).find('input').attr('checked', !isSelected);
+        updateCheckboxes: function () {
+            var checkboxes = this.$el.find('input'),
+                ids = this.model.get('selectedContacts').pluck('id');
+
+            checkboxes.each(function (index, checkbox) {
+                checkbox.checked = ids.indexOf(checkbox.getAttribute('data-id')) > -1;
+            });
         }
     });
 
     var open = function (callback) {
         // use our gab to generate mock data for now
         return api.getAll({ columns: '20,1,101,500,501,502,505,519,520,521,522,524,543,555,556,557,569,602,606,607,616,617,5,2' }, false).then(function (data) {
-
             mockData = _(data).groupBy('department');
             var lists = _(_(mockData).keys().sort()).map(function (department) {
                 // later on this is folder name and id, but since this is mock data...
@@ -116,7 +172,7 @@ define('io.ox/contacts/enterprisepicker/dialog', [
                 searchQuery: '',
                 filterQuery: '',
                 selectedList: 'no-list-selected',
-                selectedContacts: [],
+                selectedContacts: new Backbone.Collection(),
                 contacts: new Backbone.Collection(),
                 lastContacts: new Backbone.Collection(_(data).sample(3)),
                 addressLists: lists
@@ -131,64 +187,62 @@ define('io.ox/contacts/enterprisepicker/dialog', [
                 help: 'ox.appsuite.user.sect.email.send.enterpriserpicker.html',
                 title: gt('Global address list')
             })
-            .extend({
-                addClass: function () {
-                    this.$el.addClass('enterprise-picker');
-                },
-                header: function () {
-                    var listSelectBox = new Mini.SelectView({ name: 'selectedList', model: model, list: model.get('addressLists') }).render().$el;
+            .build(function () {
+                this.$el.addClass('enterprise-picker');
+                var listSelectBox = new Mini.SelectView({ name: 'selectedList', model: model, list: model.get('addressLists') }).render().$el;
 
-                    model.on('change:filterQuery', function () {
-                        var query = model.get('filterQuery').trim().toLowerCase(),
-                            options = listSelectBox.find('option');
-                        if (!query) return options.show();
+                model.on('change:filterQuery', function () {
+                    var query = model.get('filterQuery').trim().toLowerCase(),
+                        options = listSelectBox.find('option');
+                    if (!query) return options.show();
 
-                        _(listSelectBox.find('option')).each(function (option) {
-                            // never hide the placeholder option
-                            if ($(option).val() === 'no-list-selected') return;
-                            $(option).toggle(option.text.toLowerCase().indexOf(query) !== -1);
-                        });
+                    _(listSelectBox.find('option')).each(function (option) {
+                        // never hide the placeholder option
+                        if ($(option).val() === 'no-list-selected') return;
+                        $(option).toggle(option.text.toLowerCase().indexOf(query) !== -1);
                     });
+                });
 
-                    this.$('.modal-header').append(
-                        $('<div class="top-bar">').append(
-                            $('<label>').text(gt('Search')).append(
-                                $('<div class="input-group">').append(
-                                    new Mini.InputView({ name: 'searchQuery', model: model }).render().$el
+                this.$('.modal-header').append(
+                    $('<div class="top-bar">').append(
+                        $('<label>').text(gt('Search')).append(
+                            $('<div class="input-group">').append(
+                                new Mini.InputView({ name: 'searchQuery', model: model }).render().$el
                                         .attr('placeholder', gt('Search for name, department, position')),
-                                    $('<span class="input-group-btn">').append(
-                                        $('<button type="button" class="search-button btn btn-default">')
+                                $('<span class="input-group-btn">').append(
+                                    $('<button type="button" class="search-button btn btn-default">')
                                             //#. used as a verb
                                             .attr({ title: gt('Search') })
                                             .append($.icon('fa-search'))
-                                    )
                                 )
-                            ),
-                            $('<label>').text(gt('Filter')).append(
-                                $('<div class="input-group">').append(
-                                    new Mini.InputView({ name: 'filterQuery', model: model }).render().$el
+                            )
+                        ),
+                        $('<label>').text(gt('Filter')).append(
+                            $('<div class="input-group">').append(
+                                new Mini.InputView({ name: 'filterQuery', model: model }).render().$el
                                         .attr('placeholder', gt('Filter address lists')),
-                                    $('<span class="input-group-btn">').append(
-                                        $('<button type="button" class="filter-button btn btn-default">')
+                                $('<span class="input-group-btn">').append(
+                                    $('<button type="button" class="filter-button btn btn-default">')
                                             .attr({ title: gt('Filter address lists') })
                                             .append($.icon('fa-filter'))
-                                    )
                                 )
-                            ),
-                            $('<label>').text(gt('Address list')).append(
-                                listSelectBox
                             )
+                        ),
+                        $('<label>').text(gt('Address list')).append(
+                            listSelectBox
                         )
-                    );
-                },
-                body: function () {
-                    this.$('.modal-body').append(new ContactListView({ model: model }).render().$el);
-                }
+                    )
+                );
+                this.$('.modal-body').append(new ContactListView({ model: model }).render().$el);
+                this.$('.modal-body').after(new SelectedContactsView({ model: model }).render().$el);
             })
             .on({
                 'select': function () {
-                    var selection = [];
-                    if (_.isFunction(callback)) callback(selection);
+                    if (_.isFunction(callback)) callback(model.get('selectedContacts').toJSON());
+                },
+                'close': function () {
+                    model = null;
+                    mockData = null;
                 }
             })
             .addCancelButton()
