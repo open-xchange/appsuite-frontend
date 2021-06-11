d621198df44ae91508018077980d20449039d51a
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2021-06-11 15:41:38 +0200

    Better use of busy spinner
    Fix empty sections
    
    Fix Distribution lists

--- ui/apps/io.ox/contacts/enterprisepicker/dialog.js
+++ ui/apps/io.ox/contacts/enterprisepicker/dialog.js
@@ -29,7 +29,7 @@ define('io.ox/contacts/enterprisepicker/dialog', [
     'use strict';
 
     // for convenience, so we only have to change one line
-    var columns = '20,1,101,500,501,502,505,519,520,521,522,524,542,543,547,548,549,551,552,553,555,556,557,569,602,606,607,616,617,5,2';
+    var columns = '20,1,101,500,501,502,505,519,520,521,522,524,542,543,547,548,549,551,552,553,555,556,557,569,592,602,606,607,616,617,5,2';
 
     var SelectedContactsView = DisposableView.extend({
 
@@ -200,154 +200,180 @@ define('io.ox/contacts/enterprisepicker/dialog', [
     });
 
     var open = function (callback) {
-        // use our gab to generate mock data for now
-        var defs = [],
-            lastSearchedContacts = settings.get('lastSearchedContacts', []);
+        var model;
 
-        defs.push(folderApi.list(folderApi.getDefaultFolder('contacts'), { all: true }));
-        _(lastSearchedContacts).each(function (contact) {
-            if (!contact) return;
-            var def = $.Deferred();
-            // use get request so we can sort out broken or missing contacts better, always resolve. we don't want a missing contact to break the picker
-            api.get({ folder_id: contact.folder_id, id: contact.id }).always(def.resolve);
-            defs.push(def);
-        });
-        return $.when.apply($, defs).then(function (folders) {
-            var lists = _(folders).map(function (folder) {
-                    return { label: folder.title, value: folder.id };
-                }),
-                folderList = _(folders).pluck('id');
-            lists.unshift({ label: gt('Search all address lists'), value: 'all' });
-
-            var lastSearchedContacts = Array.prototype.slice.call(arguments, 1);
-
-            // filter broken stuff and save to settings
-            lastSearchedContacts = lastSearchedContacts.filter(function (contact) {
-                return !contact.error;
-            });
-            settings.set('lastSearchedContacts', _(lastSearchedContacts).pluck('id', 'folder_id')).save();
-            var model = new Backbone.Model({
-                searchQuery: '',
-                filterQuery: '',
-                selectedList: 'all',
-                selectedContacts: new Backbone.Collection(),
-                contacts: new Backbone.Collection(),
-                lastContacts: new Backbone.Collection(lastSearchedContacts),
-                addressLists: lists
-            });
-
-            var updateContactsAfterSearch = function (contacts) {
-                model.get('contacts').reset(contacts);
-                // update the last searched contacts
-                var lastContacts = model.get('lastContacts');
-                // put at start of collection, since this search is newer
-                lastContacts.unshift(contacts);
-                // limit to 10 for now
-                lastContacts.reset(lastContacts.slice(0, 10));
-                settings.set('lastSearchedContacts', _(lastContacts.models).map(function (contact) {
-                    return { folder_id: contact.get('folder_id'), id: contact.get('id') };
-                })).save();
-            };
-
-            model.on('change:selectedList', function (model, selectedList) {
-                var isSearch = model.get('searchQuery') && model.get('searchQuery').length > 1;
-                if (selectedList === 'all' && !isSearch) return model.get('contacts').reset([]);
-
-                if (isSearch) {
-                    api.advancedsearch(model.get('searchQuery'), { omitFolder: true, folders: selectedList === 'all' ? folderList : selectedList, columns: columns, names: 'on', phones: 'on', job: 'on' })
-                        .then(updateContactsAfterSearch);
-                    return;
-                }
-                api.getAll({ folder: selectedList, columns: columns })
-                    .then(function (contacts) {
-                        model.get('contacts').reset(contacts);
-                    });
-            });
-
-            model.on('change:searchQuery', function (model, query) {
-                var selectedList = model.get('selectedList');
-                // no search query? show full selected list
-                if (query.length === 0) return model.trigger('change:selectedList', model, selectedList);
-                // less than 2 characters no change (MW request requires a minimum of 2 characters)
-                if (query.length < 2) return;
-                if (selectedList === 'all') selectedList = folderList;
-                api.advancedsearch(query, { omitFolder: true, folders: selectedList, columns: columns, names: 'on', phones: 'on', job: 'on' })
-                    .then(updateContactsAfterSearch);
-            });
-
-            return new ModalDialog({
-                point: 'io.ox/contacts/enterprisepicker-dialog',
-                help: 'ox.appsuite.user.sect.email.send.enterpriserpicker.html',
-                title: gt('Global address list')
-            })
+        return new ModalDialog({
+            point: 'io.ox/contacts/enterprisepicker-dialog',
+            help: 'ox.appsuite.user.sect.email.send.enterpriserpicker.html',
+            title: gt('Global address list')
+        })
             .build(function () {
                 this.$el.addClass('enterprise-picker');
-                var listSelectBox = new Mini.SelectView({ name: 'selectedList', model: model, list: model.get('addressLists') }).render().$el;
+                this.$('.modal-content').busy();
 
-                model.on('change:filterQuery', function () {
-                    var query = model.get('filterQuery').trim().toLowerCase(),
-                        options = listSelectBox.find('option');
-                    if (!query) return options.show();
+                var self = this,
+                    defs = [],
+                    lastSearchedContacts = settings.get('lastSearchedContacts', []);
 
-                    _(listSelectBox.find('option')).each(function (option) {
-                        // never hide the placeholder option
-                        if ($(option).val() === 'all') return;
-                        $(option).toggle(option.text.toLowerCase().indexOf(query) !== -1);
-                    });
+                defs.push(folderApi.flat({ module: 'contacts', all: true }));
+                _(lastSearchedContacts).each(function (contact) {
+                    if (!contact || !contact.folder_id || !contact.id) return;
+                    var def = $.Deferred();
+                    // use get request so we can sort out broken or missing contacts better, always resolve. we don't want a missing contact to break the picker
+                    api.get({ folder_id: contact.folder_id, id: contact.id }).always(def.resolve);
+                    defs.push(def);
                 });
 
-                this.$('.modal-header').append(
-                    $('<div class="top-bar">').append(
-                        $('<label>').text(gt('Search')).append(
-                            $('<div class="input-group">').append(
-                                new Mini.InputView({ name: 'searchQuery', model: model, autocomplete: false }).render().$el
+                $.when.apply($, defs).then(function (folders) {
+
+                    // flat request returns folders in sections, add them to a single array, leave out the hidden section
+                    folders = _([].concat(folders.private, folders.shared, folders.public)).compact();
+                    var lists = _(folders).map(function (folder) {
+                            return { label: folder.title, value: folder.id };
+                        }),
+                        folderList = _(folders).pluck('id');
+                    lists.unshift({ label: gt('Search all address lists'), value: 'all' });
+
+                    var lastSearchedContacts = Array.prototype.slice.call(arguments, 1);
+
+                    // filter broken stuff and save to settings
+                    lastSearchedContacts = lastSearchedContacts.filter(function (contact) {
+                        return !contact.error;
+                    });
+                    settings.set('lastSearchedContacts', _(lastSearchedContacts).map(function (contact) {
+                        return { folder_id: contact.folder_id, id: contact.id };
+                    })).save();
+                    model = new Backbone.Model({
+                        searchQuery: '',
+                        filterQuery: '',
+                        selectedList: 'all',
+                        selectedContacts: new Backbone.Collection(),
+                        contacts: new Backbone.Collection(),
+                        lastContacts: new Backbone.Collection(lastSearchedContacts),
+                        addressLists: lists
+                    });
+
+                    var updateContactsAfterSearch = function (contacts) {
+                        self.$('.modal-content').idle();
+                        model.get('contacts').reset(contacts);
+                        // update the last searched contacts
+                        var lastContacts = model.get('lastContacts');
+                        // put at start of collection, since this search is newer
+                        lastContacts.unshift(contacts);
+                        // limit to 10 for now
+                        lastContacts.reset(lastContacts.slice(0, 10));
+                        settings.set('lastSearchedContacts', _(lastContacts.models).map(function (contact) {
+                            return { folder_id: contact.get('folder_id'), id: contact.get('id') };
+                        })).save();
+                    };
+
+                    model.on('change:selectedList', function (model, selectedList) {
+                        var isSearch = model.get('searchQuery') && model.get('searchQuery').length > 1;
+                        if (selectedList === 'all' && !isSearch) return model.get('contacts').reset([]);
+
+                        if (isSearch) {
+                            self.$('.modal-content').busy();
+                            api.advancedsearch(model.get('searchQuery'), { omitFolder: true, folders: selectedList === 'all' ? folderList : selectedList, columns: columns, names: 'on', phones: 'on', job: 'on' })
+                            .then(updateContactsAfterSearch);
+                            return;
+                        }
+                        api.getAll({ folder: selectedList, columns: columns })
+                        .then(function (contacts) {
+                            model.get('contacts').reset(contacts);
+                        });
+                    });
+
+                    model.on('change:searchQuery', function (model, query) {
+                        var selectedList = model.get('selectedList');
+                        // no search query? show full selected list
+                        if (query.length === 0) return model.trigger('change:selectedList', model, selectedList);
+                        // less than 2 characters no change (MW request requires a minimum of 2 characters)
+                        if (query.length < 2) return;
+                        if (selectedList === 'all') selectedList = folderList;
+                        self.$('.modal-content').busy();
+                        api.advancedsearch(query, { omitFolder: true, folders: selectedList, columns: columns, names: 'on', phones: 'on', job: 'on' })
+                        .then(updateContactsAfterSearch);
+                    });
+
+                    self.$('.modal-content').idle();
+
+                    var listSelectBox = new Mini.SelectView({ name: 'selectedList', model: model, list: model.get('addressLists') }).render().$el;
+
+                    model.on('change:filterQuery', function () {
+                        var query = model.get('filterQuery').trim().toLowerCase(),
+                            options = listSelectBox.find('option');
+                        if (!query) return options.show();
+
+                        _(listSelectBox.find('option')).each(function (option) {
+                        // never hide the placeholder option
+                            if ($(option).val() === 'all') return;
+                            $(option).toggle(option.text.toLowerCase().indexOf(query) !== -1);
+                        });
+                    });
+
+                    self.$('.modal-header').append(
+                        $('<div class="top-bar">').append(
+                            $('<label>').text(gt('Search')).append(
+                                $('<div class="input-group">').append(
+                                    new Mini.InputView({ name: 'searchQuery', model: model, autocomplete: false }).render().$el
                                         .attr('placeholder', gt('Search for name, department, position'))
                                         .on('keyup', _.debounce(function () {
                                             model.set('searchQuery', this.value);
                                         }, 300)),
-                                $('<span class="input-group-addon">').append($.icon('fa-search', gt('Search for name, department, position')))
-                            )
-                        ),
-                        $('<label>').text(gt('Filter')).append(
-                            $('<div class="input-group">').append(
-                                new Mini.InputView({ name: 'filterQuery', model: model, autocomplete: false }).render().$el
+                                    $('<span class="input-group-addon">').append($.icon('fa-search', gt('Search for name, department, position')))
+                                )
+                            ),
+                            $('<label>').text(gt('Filter')).append(
+                                $('<div class="input-group">').append(
+                                    new Mini.InputView({ name: 'filterQuery', model: model, autocomplete: false }).render().$el
                                         .attr('placeholder', gt('Filter address lists'))
                                         .on('keyup', _.debounce(function () {
                                             model.set('filterQuery', this.value);
                                         }, 300)),
-                                $('<span class="input-group-addon">').append($.icon('fa-filter', gt('Filter address lists')))
+                                    $('<span class="input-group-addon">').append($.icon('fa-filter', gt('Filter address lists')))
+                                )
+                            ),
+                            $('<label>').text(gt('Address list')).append(
+                                listSelectBox
                             )
-                        ),
-                        $('<label>').text(gt('Address list')).append(
-                            listSelectBox
                         )
-                    )
-                );
-                this.$('.modal-body').append(new ContactListView({ model: model, modalBody: this.$('.modal-body') }).render().$el)
+                    );
+                    self.$('.modal-body').append(new ContactListView({ model: model, modalBody: self.$('.modal-body') }).render().$el)
                     .after(new SelectedContactsView({ model: model }).render().$el);
+
+                    // triggers focus and fixes "compact" class
+                    self.idle();
+                });
             })
             .on({
-                'select': function () {
-
-                    var list = _(model.get('selectedContacts').toJSON()).chain()
+                // this function is called recursively if a distribution list is processed
+                'select':  function processContacts(distributionListMembers) {
+                    var list = _(distributionListMembers || model.get('selectedContacts').toJSON()).chain()
                         .filter(function (item) {
                             item.mail_full_name = util.getMailFullName(item);
-                            item.email = $.trim(item.email1 || item.email2 || item.email3).toLowerCase();
-                            return item.email;
+                            item.email = $.trim(item.email1 || item.email2 || item.email3 || item.mail).toLowerCase();
+                            return item.email || item.mark_as_distributionlist;
                         })
                         .map(function (item) {
-                            var name = item.mail_full_name, mail = item.mail || item.email;
-                            return {
-                                array: [name || null, mail || null],
-                                display_name: name,
-                                id: item.id,
-                                folder_id: item.folder_id,
-                                email: mail,
-                                user_id: item.user_id
-                            };
+                            if (item.mark_as_distributionlist) return processContacts(item.distribution_list);
+                            var name = item.mail_full_name, mail = item.mail || item.email,
+                                result = {
+                                    array: [name || null, mail || null],
+                                    display_name: name,
+                                    id: item.id,
+                                    folder_id: item.folder_id,
+                                    email: mail,
+                                    user_id: item.user_id
+                                };
+                                // mail_field is used in distribution lists
+                            if (item.mail_field) result.field = item.mail_field;
+                            return result;
                         }, this)
+                        .flatten()
                         .uniq(function (item) { return item.email; })
                         .value();
+
+                    if (distributionListMembers) return list;
                     if (_.isFunction(callback)) callback(list);
                 },
                 'close': function () {
@@ -358,7 +384,6 @@ define('io.ox/contacts/enterprisepicker/dialog', [
             //#. Context: Add selected contacts; German "Auswählen", for example
             .addButton({ label: gt('Select'), action: 'select' })
             .open();
-        });
     };
 
     // use same names as default addressbook picker, to make it easier to switch between the two
