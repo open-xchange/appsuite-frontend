797501fb1961c65141a5d70938fbfe66de753443
Anne Matthes <anne.matthes@open-xchange.com>
2021-06-30 12:07:58 +0200

    OXUI-902: License change from CC-BY-NC-SA to AGPL v3 (4)
    - update license for core plugins
    
    (cherry picked from commit 3b054a31621904bee53429f2b47f63c295fe1823)

--- update-themes/lib/grunt/config.js
+++ update-themes/lib/grunt/config.js
@@ -1,3 +1,25 @@
+/*
+*
+* @copyright Copyright (c) OX Software GmbH, Germany <info@open-xchange.com>
+* @license AGPL-3.0
+*
+* This code is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+
+* You should have received a copy of the GNU Affero General Public License
+* along with OX App Suite. If not, see <https://www.gnu.org/licenses/agpl-3.0.txt>.
+*
+* Any use of the work other than as authorized under this license or copyright law is prohibited.
+*
+*/
+
 'use strict';
 
 module.exports = function (grunt) {
@@ -17,7 +39,7 @@ module.exports = function (grunt) {
     grunt.config.extend('clean', {
         default: {}
     });
-    grunt.file.expand({cwd: 'apps/themes/'}, '*/common.css').forEach(function (file) {
+    grunt.file.expand({ cwd: 'apps/themes/' }, '*/common.css').forEach(function (file) {
         //directories with a common.css file are themes that need to be cleaned
         //this way, css files of already uninstalled themes get removed by update-themes
         var themeName = file.replace(/\/common.css$/, '');
@@ -30,7 +52,7 @@ module.exports = function (grunt) {
     grunt.config.extend('less', {
         default: {}
     });
-    grunt.file.expand({cwd: 'apps/themes/'}, '*/definitions.less').forEach(function (file) {
+    grunt.file.expand({ cwd: 'apps/themes/' }, '*/definitions.less').forEach(function (file) {
         //directories with a definitions.less file are themes that need to be built
         var themeName = file.replace(/\/definitions.less$/, '');
         grunt.verbose.writeln('found theme', themeName);
