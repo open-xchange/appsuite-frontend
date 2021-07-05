open-xchange/appsuite-frontend

commit ff15cc2cbe0862ca103a05369223672ee54c2f00

Additional commit Fixed Bug 60140 - [L3] mail content not displayed, Bug 60184 - HTML Mail not displayed correctly

ui/apps/io.ox/mail/detail/view.js
@@ -414,9 +414,13 @@ define('io.ox/mail/detail/view', [
 
                     if (_.device('ios && smartphone')) html.attr('class', 'ios smartphone');
 
-                    $(this.contentDocument)
-                        .find('head').append('<style>' + contentStyle + '</style>').end()
-                        .find('body').append($content);
+                    $(this.contentDocument).find('head').append('<style>' + contentStyle + '</style>');
+
+                    if ($content.filter('body').length) {
+                        $(this.contentDocument).find('body').replaceWith($content.filter('body'));
+                    } else {
+                        $(this.contentDocument).find('body').append($content);
+                    }
                     $(this.contentWindow)
                         .on('complete toggle-blockquote', { iframe: $(this) }, onImmediateResize)
                         .on('resize', { iframe: $(this) }, onWindowResize)

ui/apps/io.ox/mail/detail/content.js
@@ -629,10 +629,11 @@ define('io.ox/mail/detail/content', [
 
                 if (baton.isHTML) {
                     // robust constructor for large HTML -- no jQuery here to avoid its caches
-                    content = document.createElement('DIV');
-                    content.className = 'mail-detail-content noI18n';
-                    // replace possible body nodes with div nodes, this way we don't loose possible inline styles of body nodes (body nodes are removed because they cannot be children of div nodes)
-                    content.innerHTML = baton.source.replace('<body', '<div').replace('</body', '</div');
+                    content = document.createElement('HTML');
+                    content.innerHTML = baton.source;
+                    // we only need the body
+                    content = content.getElementsByTagName('body')[0];
+                    content.className = content.className + ' mail-detail-content noI18n';
                     // last line of defense
                     each(content, 'script, base, meta', function (node) {
                         node.parentNode.removeChild(node);

commit 34d1e9a1188291febee8f9e899932833f491241d

OXUIB-895: missing "show entire message" button for truncated mails
- selector was broken for html mails  (.body .mail-detail-content)
- caused by ff15cc2cbe0862ca103a05369223672ee54c2f00

ui/apps/io.ox/mail/detail/view.js
@@ -559,7 +559,8 @@ define('io.ox/mail/detail/view', [
                     });
 
                     $(this.contentDocument)
-                        .find('body .mail-detail-content')
+                        // plaintext mail vs. html mail
+                        .find('.mail-detail-content')
                         .append(
                             $('<div class="max-size-warning">').append(
                                 $.txt(gt('This message has been truncated due to size limitations.')), $.txt(' '),