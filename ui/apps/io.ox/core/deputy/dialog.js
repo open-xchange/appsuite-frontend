e3bd785d81ad18e060cebb5935e4028929ade915
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2021-08-23 16:29:08 +0200

    Add edit deputy permisssion dialog
    add some mock data
    
    Use proper deputy data,

--- ui/apps/io.ox/core/deputy/dialog.js
+++ ui/apps/io.ox/core/deputy/dialog.js
@@ -23,18 +23,99 @@
 define('io.ox/core/deputy/dialog', [
     'io.ox/backbone/views/modal',
     'io.ox/backbone/views/disposable',
+    'io.ox/backbone/mini-views',
     'io.ox/participants/add',
     'io.ox/contacts/util',
+    'io.ox/core/api/user',
     'gettext!io.ox/core',
     'less!io.ox/core/deputy/style'
-], function (ModalDialog, DisposableView, AddParticipantView, util, gt) {
+], function (ModalDialog, DisposableView, mini, AddParticipantView, util, userApi, gt) {
 
     'use strict';
+
+    // permissions given to newly added deputies
+    var defaultPermissions = {
+            'sendOnBehalf': true,
+            'modulePermissions': {
+                'mail': {
+                    'permission': 7564567657,
+                    'folderIds': [
+                        'default0/INBOX'
+                    ]
+                },
+                'calendar': {
+                    'permission': 3452345345,
+                    'folderIds': [
+                        '32'
+                    ]
+                }
+            }
+        },
+        mockdata =  [{
+            'deputyId': 'dc5b3fbbee434035a94a7c949721cb77',
+            'user': 395,
+            'sendOnBehalf': true,
+            'modulePermissions': {
+                'mail': {
+                    'permission': 7564567657,
+                    'folderIds': [
+                        'default0/INBOX'
+                    ]
+                },
+                'calendar': {
+                    'permission': 3452345345,
+                    'folderIds': [
+                        '32'
+                    ]
+                }
+            }
+        }];
+
+    function openEditDialog(model) {
+        var prevValues = {
+            sendOnBehalf: model.get('sendOnBehalf'),
+            modulePermissions: model.get('modulePermissions')
+        };
+
+        new ModalDialog({
+            //#. %1$s name of the deputy
+            title: gt('Deputy: %1$s', util.getFullName(model.get('userData').attributes, false))
+        })
+        .build(function () {
+            this.$body.append(
+                $('<div>').text(gt('The deputy has the following permissions')),
+                $('<div class="select-container">').append(
+                    $('<label for="inbox-deputy-selector">').text(gt('Inbox')),
+                    new mini.SelectView({ id: 'inbox-deputy-selector', name: 'inbox-role', model: model, list: [] }).render().$el
+                ),
+                new mini.CustomCheckboxView({ id: 'send-on-behalf-checkbox', name: 'sendOnBehalf', label: gt('Deputy can send Emails on your behalf'), model: model }).render().$el,
+                $('<div class="select-container">').append(
+                    $('<label for="inbox-deputy-selector">').text(gt('Calendar')),
+                    new mini.SelectView({ id: 'calendar-deputy-selector', name: 'calendar-role', model: model, list: [] }).render().$el
+                )
+            ).addClass('deputy-permissions-dialog');
+        })
+        .addCancelButton()
+        .addButton({ className: 'btn-primary', label: gt('save'), action: 'save' })
+        .on('cancel', function () {
+            // cancel on a model that was not saved means we should remove it from the list
+            if (model.get('deputyId') === undefined) {
+                model.collection.remove(model);
+                return;
+            }
+            model.set(prevValues);
+        })
+        .on('save', function () {
+            console.log('do totaly funky api stuff');
+        })
+        .open();
+    }
 
     var deputyListView = DisposableView.extend({
 
         events: {
-            'click .remove': 'removeDeputy'
+            'click .remove': 'removeDeputy',
+            'click .edit': 'showPermissions'
         },
 
         tagName: 'ul',
@@ -55,33 +136,39 @@ define('io.ox/core/deputy/dialog', [
             return this;
         },
         renderDeputy: function (deputy) {
-            var contact = deputy,
-                name = util.getFullName(contact.attributes, false),
-                initials = util.getInitials(contact.attributes),
+            var user = deputy.get('userData'),
+                name = util.getFullName(user.attributes, false),
+                initials = util.getInitials(user.attributes),
                 initialsColor = util.getInitialsColor(initials),
-                contactPicture = contact.get('image1_url') ? $('<i class="contact-picture" aria-hidden="true">').css('background-image', 'url(' + util.getImage(contact.attributes) + ')')
-                    : $('<div class="contact-picture initials" aria-hidden="true">').text(initials).addClass(initialsColor);
+                userPicture = user.get('image1_url') ? $('<i class="user-picture" aria-hidden="true">').css('background-image', 'url(' + util.getImage(user.attributes) + ')')
+                    : $('<div class="user-picture initials" aria-hidden="true">').text(initials).addClass(initialsColor);
 
 
             this.$el.append(
-                $('<li>').attr('data-id', contact.get('id')).append(
+                $('<li>').attr('data-id', user.get('id')).append(
                     $('<div class="flex-item">').append(
-                        contactPicture,
+                        userPicture,
                         $('<div class="data-container">').append(
                             $('<div class="name">').text(name),
                             $('<div class="permissions">').text('darf alles')
                         )
                     ),
                     $('<div class="flex-item">').append(
-                        $('<button class="btn btn-link edit">').text(gt('Edit')),
-                        $('<button class="btn btn-link remove">').attr('data-id', deputy.get('id')).append($.icon('fa-times', gt('Remove')))
+                        $('<button class="btn btn-link edit">').attr('data-cid', deputy.cid).text(gt('Edit')),
+                        $('<button class="btn btn-link remove">').attr('data-cid', deputy.cid).append($.icon('fa-times', gt('Remove')))
                     )
                 )
             );
         },
         removeDeputy: function (e) {
             e.stopPropagation();
-            this.collection.remove(e.currentTarget.getAttribute('data-id'));
+            this.collection.remove(e.currentTarget.getAttribute('data-cid'));
+        },
+        showPermissions: function (e) {
+            var model = this.collection.get(e.currentTarget.getAttribute('data-cid'));
+            if (!model) return;
+
+            openEditDialog(model);
         }
     });
 
@@ -91,16 +178,51 @@ define('io.ox/core/deputy/dialog', [
             title: gt('Manage deputies')
         })
         .build(function () {
-            this.deputyListView = new deputyListView();
-            this.$body.append(new AddParticipantView({
-                apiOptions: {
-                    users: true
-                },
-                placeholder: gt('Add people'),
-                collection: this.deputyListView.collection,
-                scrollIntoView: true
-            }).render().$el,
-            this.deputyListView.render().$el);
+            var self = this,
+                // collection to store userdata
+                userCollection = new Backbone.Collection();
+
+            this.$body.busy();
+            $.when(mockdata).then(function (deputies) {
+                var defs = _(deputies).map(function (deputy) {
+                    return userApi.get({ id: deputy.user }).then(function (data) {
+                        userCollection.add(data);
+                        deputy.userData = userCollection.get(data);
+                        console.log('userdata added');
+                    });
+                });
+
+                $.when.apply($, defs).then(function () {
+                    console.log('draw');
+                    self.$body.idle();
+                    // since this is async modal dialog may think the body is empty and adds this class
+                    self.$el.removeClass('compact');
+                    self.deputyListView = new deputyListView({ deputies: deputies });
+                    self.$body.append(
+                        new AddParticipantView({
+                            apiOptions: {
+                                users: true
+                            },
+                            placeholder: gt('Add people'),
+                            collection: userCollection,
+                            scrollIntoView: true
+                        }).render().$el,
+                        self.deputyListView.render().$el
+                    );
+
+                    userCollection.on('add', function (user) {
+                        var deputy = _.extend({}, defaultPermissions, { user: user.get('id'), userData: user }),
+                            model = self.deputyListView.collection.add(deputy);
+
+                        openEditDialog(model);
+                    });
+
+                    // deputy removed? remove from user collection as well
+                    self.deputyListView.collection.on('remove', function (deputy) {
+                        userCollection.remove(deputy.get('user'));
+                    });
+                });
+            });
         })
         .addButton({ className: 'btn-primary', label: gt('close'), action: 'cancel' })
         .open();
