open-xchange/appsuite-frontend

commit 1190f829b376af97156bede19b14c0e8a08d9185

Fixed OXUIB-546: Fix and consolidate limit and quota checks for Mail Compose
Root cause: Checks were missing
Solution: Check quotas on adding inline images or activating/deactivating drive usage. Improve attachment quota check.

ui/apps/io.ox/mail/actions/attachmentQuota.js
@@ -20,51 +20,80 @@ define('io.ox/mail/actions/attachmentQuota', [
 
     'use strict';
 
-    function checkQuota(file, accumulatedSize) {
+    function accumulate(attachmentCollection, type) {
+        return attachmentCollection.filter(function (m) {
+            var size = m.get('size') || m.get('origin').file.size;
+            return typeof size !== 'undefined' && m.get('contentDisposition') === type;
+        })
+        .map(function (m) { return m.get('size') || m.get('origin').file.size; })
+        .reduce(function (m, n) { return m + n; }, 0);
+    }
+
+    function checkQuota(model, file, inlineImageSize) {
         var list = file,
             properties = coreSettings.get('properties'),
-            total = accumulatedSize || 0,
-            maxFileSize,
-            quota,
+            attachmentCollection = model.get('attachments'),
+            accumulatedSizeOfAttached = accumulate(attachmentCollection, 'ATTACHMENT'),
+            accumulatedSizeOfInline = accumulate(attachmentCollection, 'INLINE'),
+            total = (accumulatedSizeOfAttached + accumulatedSizeOfInline) || 0,
+            mailSize = model.get('content').replace(/src="data:image[^"]*"/g, '').length,
             autoPublish = require('io.ox/core/capabilities').has('publish_mail_attachments'),
+            useDrive = model.get('sharedAttachments').enabled,
+            maxFileSize,
+            maxMailSize,
+            quota,
             result = {};
-
-        if (!list.length) return;
 
         maxFileSize = autoPublish ? -1 : properties.attachmentQuotaPerFile;
         quota = autoPublish ? -1 : properties.attachmentQuota;
+        maxMailSize = autoPublish ? -1 : require('settings!io.ox/mail').get('compose/maxMailSize');
+        inlineImageSize = inlineImageSize || 0;
 
-        _.find(list, function (item) {
-            var fileTitle = item.filename || item.name || item.subject,
-                fileSize = item.file_size || item.size;
-            if (fileSize) {
-                total += fileSize;
+        if ((list && list.length) || inlineImageSize) {
+            _.find(list, function (item) {
+                var fileTitle = item.filename || item.name || item.subject,
+                    fileSize = item.file_size || item.size;
+                if (fileSize) {
+                    total += fileSize;
 
-                if (quota > 0 && total > quota) {
-                    result.error = gt('The file "%1$s" cannot be uploaded because it exceeds the total attachment size limit of %2$s', fileTitle, strings.fileSize(quota));
-                    result.reason = 'filesize';
-                    return true;
-                }
-
-                if (maxFileSize > 0 && fileSize > maxFileSize) {
-                    result.error = gt('The file "%1$s" cannot be uploaded because it exceeds the maximum file size of %2$s', fileTitle, strings.fileSize(maxFileSize));
-                    result.reason = 'filesize';
-                    return true;
-                }
-                if (autoPublish) {
-                    if (properties.infostoreMaxUploadSize > 0 && fileSize > properties.infostoreMaxUploadSize) {
-                        result.error = gt('The file "%1$s" cannot be uploaded because it exceeds the attachment publication maximum file size of %2$s', fileTitle, strings.fileSize(properties.infostoreMaxUploadSize));
-                        result.reason = 'filesizeAutoPublish';
+                    if (quota > 0 && total > quota) {
+                        result.error = gt('The file "%1$s" cannot be uploaded because it exceeds the total attachment size limit of %2$s', fileTitle, strings.fileSize(quota));
+                        result.reason = 'filesize';
                         return true;
                     }
-                    if (properties.infostoreQuota > 0 && (total > (properties.infostoreQuota - properties.infostoreUsage))) {
-                        result.error = gt('The file "%1$s" cannot be uploaded because it exceeds the infostore quota limit of %2$s', fileTitle, strings.fileSize(properties.infostoreQuota));
-                        result.reason = 'quotaAutoPublish';
+
+                    if (maxFileSize > 0 && fileSize > maxFileSize) {
+                        result.error = gt('The file "%1$s" cannot be uploaded because it exceeds the maximum file size of %2$s', fileTitle, strings.fileSize(maxFileSize));
+                        result.reason = 'filesize';
                         return true;
                     }
+
+                    if (autoPublish || useDrive) {
+                        if (properties.infostoreMaxUploadSize > 0 && fileSize > properties.infostoreMaxUploadSize) {
+                            result.error = gt('The file "%1$s" cannot be uploaded because it exceeds the attachment publication maximum file size of %2$s', fileTitle, strings.fileSize(properties.infostoreMaxUploadSize));
+                            result.reason = 'filesizeAutoPublish';
+                            return true;
+                        }
+                    }
                 }
+            });
+        }
+
+        if ((autoPublish || useDrive) && properties.infostoreQuota > 0 && (accumulatedSizeOfAttached > (properties.infostoreQuota - (properties.infostoreUsage || 0)))) {
+            result.error = gt('The attachment cannot be uploaded because it exceeds the infostore quota limit of %2$s', strings.fileSize(properties.infostoreQuota));
+            result.reason = 'quotaAutoPublish';
+            return true;
+        }
+
+        total += inlineImageSize + mailSize - (useDrive ? accumulatedSizeOfAttached : 0);
+        if (maxMailSize > 0 && total > maxMailSize) {
+            if (model.changed && model.changed.sharedAttachments && model.changed.sharedAttachments.enabled === false) {
+                result.error = gt('The uploaded attachment exceeds the maximum email size of %1$s', strings.fileSize(maxMailSize));
+            } else {
+                result.error = gt('The file cannot be uploaded because it exceeds the maximum email size of %1$s', strings.fileSize(maxMailSize));
             }
-        });
+            result.reason = 'mailsize';
+        }
 
         if (result.error) {
             notifications.yell('error', result.error);

ui/apps/io.ox/mail/compose/extensions.js
@@ -60,16 +60,9 @@ define('io.ox/mail/compose/extensions', [
     var attachmentUploadHelper = function (model, files) {
         // also support events
         files = files.target ? files.target.files : files;
-        var self = this,
-            attachmentCollection = model.get('attachments'),
-            accumulatedSize = attachmentCollection.filter(function (m) {
-                var size = m.get('size');
-                return typeof size !== 'undefined';
-            })
-            .map(function (m) { return m.get('size'); })
-            .reduce(function (m, n) { return m + n; }, 0);
+        var self = this;
 
-        if (attachmentQuota.checkQuota(files, accumulatedSize)) {
+        if (attachmentQuota.checkQuota(model, files)) {
             //#. %s is a list of filenames separeted by commas
             //#. it is used by screenreaders to indicate which files are currently added to the list of attachments
             self.trigger('aria-live-update', gt('Added %s to attachments.', _(files).map(function (file) { return file.name; }).join(', ')));

ui/apps/io.ox/mail/compose/sharing.js
@@ -229,6 +229,10 @@ define('io.ox/mail/compose/sharing', [
             var sharedAttachments = this.model.get('sharedAttachments');
             if (mailSettings.get('compose/shareAttachments/forceAutoDelete', false)) sharedAttachments.autodelete = true;
             this.sharingModel.set(sharedAttachments);
+
+            require(['io.ox/mail/actions/attachmentQuota'], function (attachmentQuota) {
+                if (!attachmentQuota.checkQuota(this.model)) this.model.set('sharedAttachments', { enabled: false });
+            }.bind(this));
         },
 
         syncToMailModel: function (options) {

ui/apps/io.ox/mail/compose/view.js
@@ -442,21 +442,35 @@ define('io.ox/mail/compose/view', [
         id: 'image-loader',
         index: 200,
         perform: function (baton) {
-            var self = this;
+            var self = this,
+                space = baton.model.get('id');
+
             if (this.config.get('editorMode') !== 'html') return;
             baton.options.imageLoader = {
                 upload: function (file) {
-                    var attachment = new Attachments.Model({ filename: file.name, uploaded: 0, contentDisposition: 'INLINE' }),
+                    var attachment = new Attachments.Model({ filename: file.name, size: file.size, uploaded: 0, contentDisposition: 'INLINE' }),
                         def = new $.Deferred();
-                    composeUtil.uploadAttachment({
-                        model: self.model,
-                        filename: file.name,
-                        origin: { file: file },
-                        attachment: attachment,
-                        contentDisposition: 'inline'
+
+                    require(['io.ox/mail/actions/attachmentQuota'], function (attachmentQuota) {
+                        if (!attachmentQuota.checkQuota(self.model, [], attachment.get('size'))) {
+                            var $editor = self.view.$el.find('iframe').contents().find('#tinymce');
+                            $editor.children().remove();
+                            $editor.append(self.model._previousAttributes.content);
+                            composeAPI.space.attachments.remove(space, attachment.get('id'));
+                            return def.reject();
+                        }
+
+                        composeUtil.uploadAttachment({
+                            model: self.model,
+                            filename: file.name,
+                            origin: { file: file },
+                            attachment: attachment,
+                            contentDisposition: 'inline'
+                        });
+                        attachment.once('upload:complete', def.resolve);
+                        self.model.attachFiles([attachment]);
                     });
-                    attachment.once('upload:complete', def.resolve);
-                    self.model.attachFiles([attachment]);
+
                     return def;
                 },
                 getUrl: function (response) {

ui/e2e/tests/mail/compose/attachment_quota_test.js
@@ -1,0 +1,177 @@
+/**
+ * This work is provided under the terms of the CREATIVE COMMONS PUBLIC
+ * LICENSE. This work is protected by copyright and/or other applicable
+ * law. Any use of the work other than as authorized under this license
+ * or copyright law is prohibited.
+ *
+ * http://creativecommons.org/licenses/by-nc-sa/2.5/
+ * © 2019 OX Software GmbH, Germany. info@open-xchange.com
+ *
+ * @author Anne Matthes <anne.matthes@open-xchange.com>
+ */
+
+/// <reference path="../../../steps.d.ts" />
+
+Feature('Mail Compose > Attachment quota');
+
+const expect = require('chai').expect;
+
+Before(async ({ users }) => {
+    await users.create();
+});
+
+After(async ({ users }) => {
+    await users.removeAll();
+});
+
+Scenario('I can not send too large files as mail attachments', async ({ I, users, mail }) => {
+    I.login('app=io.ox/mail');
+    mail.waitForApp();
+    await I.executeScript(function () {
+        require('settings!io.ox/core').set('properties', { attachmentQuotaPerFile: 2 * 1024 * 1024 });
+    });
+
+    // compose mail with receiver and subject
+    mail.newMail();
+    I.fillField('To', users[0].get('primaryEmail'));
+    I.fillField('Subject', 'Test');
+
+    // add a too large file as attachment
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/files/generic/16MB.dat');
+    I.waitForText('The file "16MB.dat" cannot be uploaded because it exceeds the maximum file size of 2 MB', 5, '.io-ox-alert.io-ox-alert-error');
+    I.waitForElement('.btn-link.close', 5, '.io-ox-alert.io-ox-alert-error');
+    I.click('.btn-link.close', '.io-ox-alert.io-ox-alert-error');
+    I.waitForDetached('.io-ox-alert.io-ox-alert-error', 5);
+
+    // change attachment and send successfully
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/files/generic/2MB.dat');
+    I.waitForText('DAT', 5, '.inline-items.preview');
+    I.waitForDetached('.progress-container', 15, '.share-attachments');
+
+    // send mail successfully
+    mail.send();
+    I.waitForElement('.list-item.selectable.unread', 30, '.list-view.mail-item');
+});
+
+Scenario('I can not send too large accumulated mail attachments', async ({ I, users, mail }) => {
+    I.login('app=io.ox/mail');
+    mail.waitForApp();
+    await I.executeScript(function () {
+        require('settings!io.ox/core').set('properties', { attachmentQuotaPerFile: 16 * 1024 * 1024 });
+        require('settings!io.ox/core').set('properties', { attachmentQuota: 5 * 1024 * 1024 });
+    });
+
+    // compose mail with receiver and subject
+    mail.newMail();
+    I.fillField('To', users[0].get('primaryEmail'));
+    I.fillField('Subject', 'Test');
+
+    // add attachments
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/files/generic/2MB.dat');
+    I.waitForText('DAT', 5, '.inline-items.preview');
+    I.waitForDetached('.progress-container', 15, '.share-attachments');
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/files/generic/16MB.dat');
+
+    I.waitForText('The file "16MB.dat" cannot be uploaded because it exceeds the total attachment size limit of 5 MB', 5, '.io-ox-alert.io-ox-alert-error');
+});
+
+Scenario('I can not send an email that exceeds the mail max size', async ({ I, users, mail }) => {
+    I.login('app=io.ox/mail');
+    mail.waitForApp();
+    await I.executeScript(function () {
+        require('settings!io.ox/mail').set('compose/maxMailSize', 3 * 1024);
+    });
+
+    // compose mail with receiver and subject
+    mail.newMail();
+    I.fillField('To', users[0].get('primaryEmail'));
+    I.fillField('Subject', 'Test');
+
+    // first attached, second inline
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/placeholder/800x600-limegreen.png');
+    I.waitForDetached('.progress-container', 15, '.share-attachments');
+    I.attachFile('.tinymce-toolbar input[type="file"]', 'e2e/media/placeholder/800x600.png');
+
+    I.waitForText('The file cannot be uploaded because it exceeds the maximum email size of 3 KB', 5, '.io-ox-alert.io-ox-alert-error');
+    I.click('.btn-link.close', '.io-ox-alert.io-ox-alert-error');
+    I.waitForDetached('.io-ox-alert.io-ox-alert-error', 5);
+    await within({ frame: '.mce-edit-area iframe' }, async () => {
+        I.dontSee('#tinymce img');
+        expect(await I.grabNumberOfVisibleElements('#tinymce img')).to.equal(0);
+    });
+
+    // remove attached
+    I.click('.mail-attachment-list a[title="Toggle preview"]');
+    I.waitForElement('.mail-attachment-list a[title="Remove attachment"]');
+    I.click('.mail-attachment-list a[title="Remove attachment"]');
+    I.waitForDetached('.mail-attachment-list span[title="2MB.dat"]');
+
+    // first inline, second attached
+    I.attachFile('.tinymce-toolbar input[type="file"]', 'e2e/media/placeholder/800x600-limegreen.png');
+    await within({ frame: '.mce-edit-area iframe' }, async () => {
+        I.waitForElement('#tinymce img');
+        expect(await I.grabNumberOfVisibleElements('#tinymce img')).to.equal(1);
+    });
+    I.attachFile('.tinymce-toolbar input[type="file"]', 'e2e/media/placeholder/800x600.png');
+
+    I.waitForText('The file cannot be uploaded because it exceeds the maximum email size of 3 KB', 5, '.io-ox-alert.io-ox-alert-error');
+    I.click('.btn-link.close', '.io-ox-alert.io-ox-alert-error');
+    I.waitForDetached('.io-ox-alert.io-ox-alert-error', 5);
+
+    // try multiple inline images
+    I.attachFile('.tinymce-toolbar input[type="file"]', 'e2e/media/placeholder/800x600-mango.png');
+    I.waitForText('The file cannot be uploaded because it exceeds the maximum email size of 3 KB', 5, '.io-ox-alert.io-ox-alert-error');
+    I.click('.btn-link.close', '.io-ox-alert.io-ox-alert-error');
+    I.waitForDetached('.io-ox-alert.io-ox-alert-error', 5);
+
+    await within({ frame: '.mce-edit-area iframe' }, async () => {
+        I.waitForElement('#tinymce img');
+        expect(await I.grabNumberOfVisibleElements('#tinymce img')).to.equal(1);
+    });
+});
+
+Scenario('I can not use drive if the infoStore limits get exceeded', async ({ I, users, mail }) => {
+    I.login('app=io.ox/mail');
+    mail.waitForApp();
+    await I.executeScript(async function () {
+        require('settings!io.ox/mail').set('compose/maxMailSize', 3 * 1024);
+        require('settings!io.ox/core').set('properties', { infostoreQuota: 7 * 1024 });
+    });
+
+    // compose mail with receiver and subject
+    mail.newMail();
+    I.fillField('To', users[0].get('primaryEmail'));
+    I.fillField('Subject', 'Test');
+
+    // attach
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/placeholder/800x600.png');
+    I.waitForDetached('.progress-container', 15, '.share-attachments');
+
+    // error for attachment and inline
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/placeholder/800x600-limegreen.png');
+    I.waitForText('The file cannot be uploaded because it exceeds the maximum email size of 3 KB', 5, '.io-ox-alert.io-ox-alert-error');
+    I.click('.btn-link.close', '.io-ox-alert.io-ox-alert-error');
+    I.waitForDetached('.io-ox-alert.io-ox-alert-error', 5);
+    I.attachFile('.tinymce-toolbar input[type="file"]', 'e2e/media/placeholder/800x600-limegreen.png');
+    I.waitForText('The file cannot be uploaded because it exceeds the maximum email size of 3 KB', 5, '.io-ox-alert.io-ox-alert-error');
+    I.click('.btn-link.close', '.io-ox-alert.io-ox-alert-error');
+    I.waitForDetached('.io-ox-alert.io-ox-alert-error', 5);
+
+    // use drive
+    I.checkOption('Use Drive Mail', '.share-attachments');
+
+    // attach another
+    I.attachFile('.composetoolbar input[type="file"]', 'e2e/media/placeholder/800x600-mango.png');
+    I.waitForDetached('.progress-container', 15, '.share-attachments');
+    I.attachFile('.tinymce-toolbar input[type="file"]', 'e2e/media/placeholder/800x600.png');
+    await within({ frame: '.mce-edit-area iframe' }, async () => {
+        I.waitForElement('#tinymce img');
+        expect(await I.grabNumberOfVisibleElements('#tinymce img')).to.equal(1);
+    });
+
+    I.uncheckOption('Use Drive Mail', '.share-attachments');
+    I.waitForText('The uploaded attachment exceeds the maximum email size of 3 KB', 5, '.io-ox-alert.io-ox-alert-error');
+});
+
+// Scenario.skip('I can remove and upload new inlineImages without exceeding the max mail size', async () => {
+// });

commit 4e97c789a4c90db362a19a20c5e3adc6d6e6942b

Fixed: OXUIB-1018 - Drive mail draft: ui error when adding another attachment
- handle size of 0 properly
- caused by 1190f829b376af97156bede19b14c0e8a08d9185

ui/apps/io.ox/mail/actions/attachmentQuota.js
@@ -30,11 +30,11 @@ define('io.ox/mail/actions/attachmentQuota', [
     'use strict';
 
     function accumulate(attachmentCollection, type) {
-        return attachmentCollection.filter(function (m) {
-            var size = m.get('size') || m.get('origin').file.size;
-            return typeof size !== 'undefined' && m.get('contentDisposition') === type;
+        return attachmentCollection.map(function (m) {
+            if (m.get('contentDisposition') !== type) return 0;
+            if (m.get('size') >= 0) return m.get('size');
+            if (m.get('origin')) return (m.get('origin').file || {}).size || 0;
         })
-        .map(function (m) { return m.get('size') || m.get('origin').file.size; })
         .reduce(function (m, n) { return m + n; }, 0);
     }
 