ecab41ddc641d257b97a320bb9fda91ab08200e1
Viktor Pracht <viktor.pracht@open-xchange.com>
2021-10-12 15:57:47 +0200

    Fixed: OXUIB-1045 - Reset password inputs are inconsistent and untranslated
    Root cause: Inconsistencies left over after last refactoring, typo in
    "Confirm new Password".
    Solution: Clean up behavior of labels and placeholders to be consistent
    with the username field, fixed "Password" -> "password"..
    
    (cherry picked from commit b821fe3ed2f72e131b5d17442b2bcc66ea1e931a)

--- ui/apps/io.ox/core/boot/i18n.js
+++ ui/apps/io.ox/core/boot/i18n.js
@@ -76,7 +76,7 @@ define('io.ox/core/boot/i18n', ['io.ox/core/boot/util', 'gettext!io.ox/core/boot
         gt('Reload');
         gt('Privacy Policy');
         gt('Imprint');
-        gt('Confirm new Password');
+        gt('Confirm new password');
         //#. While or after requesting a new password you can go back to the initial login page
         gt('Back to sign in');
         //#. While requesting a new password on the login page; %1$s will be replaced by the product name e.g. OX App Suite
