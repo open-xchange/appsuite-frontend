2ccc41f3bdccfa8bcd2407a9260a11a1d5fdad71
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2021-10-20 16:13:50 +0200

    OXUI-927 Calendar edit action should send the maskUid parameter for free busy

--- ui/apps/io.ox/calendar/freetime/timeView.js
+++ ui/apps/io.ox/calendar/freetime/timeView.js
@@ -609,7 +609,10 @@ define('io.ox/calendar/freetime/timeView', [
                 from = moment(this.model.get('startDate')).startOf('day').utc();
                 until = moment(from).add((this.model.get('dateRange') === 'week' ? 7 : this.model.get('startDate').daysInMonth()), 'days').utc();
             }
-            return api.freebusy(attendees, { from: from.format(util.ZULU_FORMAT), until: until.format(util.ZULU_FORMAT) }).done(function (items) {
+
+            var options = { from: from.format(util.ZULU_FORMAT), until: until.format(util.ZULU_FORMAT) };
+            if (this.parentView.parentModel && this.parentView.parentModel.get('uid')) options.maskUid = this.parentView.parentModel.get('uid');
+            return api.freebusy(attendees, options).done(function (items) {
 
                 if (items.length === 0 && attendees.length !== 0) {
                     // remove busy animation again
