open-xchange/appsuite-frontend

commit 4452afcfa4b503822b76057a6e83f2a980bdc90f

Bump chart.js from 2.9.4 to 3.6.0
- fix chart.js usage for mail and error statistics

ui/apps/io.ox/core/settings/errorlog/settings/pane.js
@@ -254,13 +254,17 @@ define('io.ox/core/settings/errorlog/settings/pane', [
                 type: 'line',
                 data: chart,
                 options: {
-                    legend: { display: false },
-                    tooltips: { enabled: false },
+                    plugins: {
+                        legend: { display: false },
+                        tooltips: { enabled: false }
+                    },
                     elements: {
                         line: {
                             backgroundColor: 'rgba(0, 136, 204, 0.15)',
                             borderColor: 'rgba(0, 136, 204, 0.80)',
-                            borderWidth: 2
+                            borderWidth: 2,
+                            fill: true,
+                            tension: 0.5
                         },
                         point: {
                             backgroundColor: 'rgba(0, 136, 204, 1)',

ui/apps/io.ox/mail/statistics.js
@@ -77,13 +77,18 @@ define('io.ox/mail/statistics', [
             type: 'line',
             data: data,
             options: {
-                legend: { display: false },
-                tooltips: { enabled: false },
+                plugins: {
+                    legend: { display: false },
+                    tooltips: { enabled: false }
+                },
                 elements: {
                     line: {
                         backgroundColor: 'rgba(0, 136, 204, 0.15)',
                         borderColor: 'rgba(0, 136, 204, 0.80)',
-                        borderWidth: 2
+
+                        borderWidth: 2,
+                        fill: true,
+                        tension: 0.5
                     },
                     point: {
                         backgroundColor: 'rgba(0, 136, 204, 1)',

commit 30f0448e136d316ab0fa88f1c86cfad0af191c10

OXUIB-1097: Opening the settings fails
- name of js file changed to lowercase
- caused by 4452afcfa4b503822b76057a6e83f2a980bdc90f

ui/apps/io.ox/core/settings/errorlog/settings/pane.js
@@ -28,7 +28,7 @@ define('io.ox/core/settings/errorlog/settings/pane', [
     'io.ox/core/capabilities',
     'settings!io.ox/core',
     'gettext!io.ox/core',
-    'static/3rd.party/Chart.min.js'
+    'static/3rd.party/chart.min.js'
 ], function (ext, ExtensibleView, util, http, capabilities, settings, gt, Chart) {
 
     'use strict';

ui/apps/io.ox/mail/statistics.js
@@ -26,7 +26,7 @@ define('io.ox/mail/statistics', [
     'io.ox/core/extensions',
     'io.ox/backbone/views/modal',
     'gettext!io.ox/mail',
-    'static/3rd.party/Chart.min.js'
+    'static/3rd.party/chart.min.js'
 ], function (api, accountAPI, ext, ModalDialog, gt, Chart) {
 
     'use strict';

ui/grunt/tasks/thirdparty.js
@@ -83,7 +83,7 @@ module.exports = function (grunt) {
                             '@open-xchange/bootstrap-tokenfield/js/bootstrap-tokenfield.js',
                             'socket.io-client/dist/socket.io.slim.js',
                             'bigscreen/bigscreen.min.js',
-                            'chart.js/dist/Chart.min.js',
+                            'chart.js/dist/chart.min.js',
                             'clipboard/dist/clipboard.min.js',
                             'croppie/croppie.min.js',
                             'resize-polyfill/lib/polyfill-resize.js',