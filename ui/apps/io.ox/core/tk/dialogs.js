7f61c74820d2b4e8e4205af644ffc5a9b7c40daa
Moritz Bach <moritz.bach@open-xchange.com>
2022-01-07 15:50:02 +0100

    not hiding contact sidepopup when changing appointment color
    see OXUIB-1143

--- ui/apps/io.ox/core/tk/dialogs.js
+++ ui/apps/io.ox/core/tk/dialogs.js
@@ -637,7 +637,7 @@ define('io.ox/core/tk/dialogs', [
                 // see bug 41822
                 '.io-ox-dialog-sidepopup-toggle'
             ].join(', ');
-            return target.closest(whiteList).length > 0 || target.is('html');
+            return target.closest(whiteList).length > 0 || target.is('html') || !document.contains(target.get(0));
         }
 
         if (target.hasClass('apptitle')) {
