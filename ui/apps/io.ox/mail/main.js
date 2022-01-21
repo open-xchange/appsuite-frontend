open-xchange/appsuite-frontend

commit 5956eb489879af85189229b7da32722724b3d5ab

Fixed OXUIB-853 Thread View gets lost when switching folders

ui/apps/io.ox/mail/main.js
@@ -30,7 +30,6 @@ define('io.ox/mail/main', [
     'io.ox/mail/threadview',
     'io.ox/core/extensions',
     'io.ox/backbone/views/actions/util',
-    'io.ox/core/api/account',
     'io.ox/core/notifications',
     'io.ox/core/toolbars-mobile',
     'io.ox/core/page-controller',
@@ -54,7 +53,7 @@ define('io.ox/mail/main', [
     'io.ox/mail/import',
     'less!io.ox/mail/style',
     'io.ox/mail/folderview-extensions'
-], function (util, api, composeAPI, commons, MailListView, ListViewControl, ThreadView, ext, actionsUtil, account, notifications, Bars, PageController, capabilities, TreeView, FolderView, folderAPI, QuotaView, ActionDropdownView, categories, accountAPI, gt, settings, coreSettings, certificateAPI, certUtils) {
+], function (util, api, composeAPI, commons, MailListView, ListViewControl, ThreadView, ext, actionsUtil, notifications, Bars, PageController, capabilities, TreeView, FolderView, folderAPI, QuotaView, ActionDropdownView, categories, accountAPI, gt, settings, coreSettings, certificateAPI, certUtils) {
 
     'use strict';
 
@@ -419,7 +418,7 @@ define('io.ox/mail/main', [
                 ox.on('http:error:OAUTH-0040 http:error:MSG-0114', function (err) {
                     var account = keychain.accounts.get(err.error_params[reauthHandler.columnForError(err.code)]);
                     if (!account) return;
-                    var mailAccount = _(account.get('associations')).filter({ module: 'mail' })[0];
+                    var mailAccount = _(accountAPI.get('associations')).filter({ module: 'mail' })[0];
                     if (!mailAccount) return;
                     app.addAccountErrorHandler(mailAccount.folder, 'OAuthReauthorize', { account: account, err: err }, true);
                 });
@@ -661,6 +660,8 @@ define('io.ox/mail/main', [
             app.getViewOptions = function (folder) {
                 var options = app.settings.get(['viewOptions', folder], {});
                 if (!app.settings.get('threadSupport', true)) options.thread = false;
+                // no thread support in drafts/sent folders. This breaks caching (Sent folders get incomplete threads). See OXUIB-853
+                if (accountAPI.is('sent|drafts', folder)) options.thread = false;
 
                 // ignore unavailable sort options
                 var isUnavailable =
@@ -688,7 +689,7 @@ define('io.ox/mail/main', [
             app.props.on('change:sort', function (model, value) {
                 model = app.listView.model;
                 // resolve from-to
-                if (value === 'from-to') value = account.is('sent|drafts', model.get('folder')) ? 604 : 603;
+                if (value === 'from-to') value = accountAPI.is('sent|drafts', model.get('folder')) ? 604 : 603;
                 // do not accidentally overwrite other attributes on folderchange
                 if (!app.changingFolders) {
                     // set proper order first
@@ -811,7 +812,7 @@ define('io.ox/mail/main', [
 
             app.supportsTextPreviewConfiguration = function () {
                 var id = app.folder.get();
-                return support && (account.isPrimary(id) || id === 'virtual/all-unseen');
+                return support && (accountAPI.isPrimary(id) || id === 'virtual/all-unseen');
             };
 
             app.useTextPreview = function () {
@@ -919,7 +920,7 @@ define('io.ox/mail/main', [
 
                 // explicitly update when set to from-to (see bug 44458)
                 if (options.sort === 'from-to') {
-                    app.listView.model.set('sort', account.is('sent|drafts', id) ? 604 : 603);
+                    app.listView.model.set('sort', accountAPI.is('sent|drafts', id) ? 604 : 603);
                 }
 
                 app.listView.model.set('folder', id);
@@ -1158,7 +1159,7 @@ define('io.ox/mail/main', [
                     if (app.props.get('checkboxes')) app.showMultiple(list);
                 },
                 'selection:action': function (list) {
-                    var isDraftFolder = _.contains(account.getFoldersByType('drafts'), this.model.get('folder'));
+                    var isDraftFolder = _.contains(accountAPI.getFoldersByType('drafts'), this.model.get('folder'));
 
                     if (app.listView.selection.get().length === 1 && !app.props.get('checkboxes')) {
                         // check for thread
@@ -1617,7 +1618,7 @@ define('io.ox/mail/main', [
                 if (app.isThreaded()) list = _(api.threads.get(list[0])).pluck('cid');
                 var cid = list[0],
                     obj = _.cid(cid),
-                    isDraft = account.is('drafts', obj.folder_id);
+                    isDraft = accountAPI.is('drafts', obj.folder_id);
                 if (isDraft) {
                     api.get(obj).then(function (data) {
                         actionsUtil.invoke('io.ox/mail/actions/edit', data);
@@ -1905,7 +1906,7 @@ define('io.ox/mail/main', [
                 if (!editFor || data.mailPath) return;
 
                 var cid = _.cid({ id: editFor.originalId, folder_id: editFor.originalFolderId }),
-                    draftsId = account.getFoldersByType('drafts');
+                    draftsId = accountAPI.getFoldersByType('drafts');
                 _(draftsId).each(function (id) {
                     _(api.pool.getByFolder(id)).each(function (collection) {
                         collection.remove(cid);
@@ -1916,14 +1917,14 @@ define('io.ox/mail/main', [
             composeAPI.on('after:save', function (data) {
                 if (data.mailPath) return;
                 var folder = app.folder.get();
-                if (account.is('drafts', folder)) app.listView.reload();
+                if (accountAPI.is('drafts', folder)) app.listView.reload();
             });
             // existing draft removed
             composeAPI.on('after:send', function (data) {
                 var editFor = data.meta.editFor;
                 if (!editFor || data.mailPath) return;
                 var folder = app.folder.get();
-                if (account.is('drafts', folder)) app.listView.reload();
+                if (accountAPI.is('drafts', folder)) app.listView.reload();
             });
         },
 
@@ -1944,7 +1945,7 @@ define('io.ox/mail/main', [
                 if (!mailPath) return;
                 // immediate reload when currently selected
                 var folder = app.folder.get();
-                if (account.is('drafts', folder)) return app.listView.reload();
+                if (accountAPI.is('drafts', folder)) return app.listView.reload();
                 // delayed reload on next select
 
                 _(api.pool.getByFolder(mailPath.folderId)).each(function (collection) {
@@ -2212,7 +2213,7 @@ define('io.ox/mail/main', [
 
         'unified-folder-support': function () {
             // only register if we have a unified mail account
-            account.getUnifiedMailboxName().done(function (unifiedMailboxName) {
+            accountAPI.getUnifiedMailboxName().done(function (unifiedMailboxName) {
                 if (!unifiedMailboxName) {
                     return;
                 }
@@ -2228,7 +2229,7 @@ define('io.ox/mail/main', [
                             12: 'Trash'
                         };
 
-                    return account.get(accountId).then(function (accountData) {
+                    return accountAPI.get(accountId).then(function (accountData) {
                         var folder, originalFolderId, unifiedFolderId, unifiedSubfolderId;
                         if (!accountData) {
                             folder = folderAPI.pool.models[model.get('folder_id')];

ui/apps/io.ox/mail/api.js
@@ -1749,7 +1749,8 @@ define('io.ox/mail/api', [
                 };
             }
             // use threads?
-            if (params.thread === true) {
+            // no thread support in drafts/sent folders. This breaks caching (Sent folders get incomplete threads). See OXUIB-853
+            if (params.thread === true && !accountAPI.is('sent|drafts', params.folder)) {
                 return {
                     action: 'threadedAll',
                     folder: params.folder,

ui/apps/io.ox/mail/view-options.js
@@ -134,7 +134,7 @@ define('io.ox/mail/view-options', [
                     if (!gridOptions.hasClass('disabled')) return;
                     listView.connect(mailAPI.collectionLoader);
                     listView.model.unset('criteria');
-                    gridOptions.removeClass('disabled');
+                    gridOptions.toggleClass('disabled', account.is('sent|drafts', baton.app.folder.get()));
                 })
                 .render();
             });
@@ -199,6 +199,9 @@ define('io.ox/mail/view-options', [
                 // toggle visibily of 'has attachments'
                 link = view.$('a[data-value="602"]');
                 link.toggleClass('hidden', !folderAPI.pool.getModel(folder).supports('ATTACHMENT_MARKER'));
+                // no thread support in drafts/sent folders. This breaks caching (Sent folders get incomplete threads). See OXUIB-853
+                link = view.$('a[data-name="thread"]');
+                link.toggleClass('disabled', account.is('sent|drafts', folder)).parent().toggleClass('disabled', account.is('sent|drafts', folder));
             }
         }
     });
@@ -220,9 +223,11 @@ define('io.ox/mail/view-options', [
         draw: function (baton) {
             // don't add if thread view is disabled server-side
             if (baton.app.settings.get('threadSupport', true) === false) return;
+            var disabled = account.is('sent|drafts', baton.app.folder.get());
             this.data('view')
                 .divider()
-                .option('thread', true, gt('Conversations'));
+                .option('thread', true, gt('Conversations'))
+                .$ul.find('a[data-name="thread"]').toggleClass('disabled', disabled).parent().toggleClass('disabled', disabled);
         }
     });
 

commit e8f8a43324d3b8123d847a85f26256c9a9ddaa7f

OXUIB-1265 - Mail: OAuthReauthorize error icon not drawn
- caused by typo in 5956eb489879af85189229b7da32722724b3d5ab

ui/apps/io.ox/mail/main.js
@@ -419,7 +419,7 @@ define('io.ox/mail/main', [
                 ox.on('http:error:OAUTH-0040 http:error:MSG-0114', function (err) {
                     var account = keychain.accounts.get(err.error_params[reauthHandler.columnForError(err.code)]);
                     if (!account) return;
-                    var mailAccount = _(accountAPI.get('associations')).filter({ module: 'mail' })[0];
+                    var mailAccount = _(account.get('associations')).filter({ module: 'mail' })[0];
                     if (!mailAccount) return;
                     app.addAccountErrorHandler(mailAccount.folder, 'OAuthReauthorize', { account: account, err: err }, true);
                 });