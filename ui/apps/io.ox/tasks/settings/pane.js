5cfb561a71ce82b37f66f5e9f35c9ef5231d098f
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2022-03-15 11:32:53 +0100

    Partial fix for OXUIB-1402 "Subscribe to shared calendar/address book" ignores permissions

--- ui/apps/io.ox/tasks/settings/pane.js
+++ ui/apps/io.ox/tasks/settings/pane.js
@@ -25,8 +25,9 @@ define('io.ox/tasks/settings/pane', [
     'settings!io.ox/tasks',
     'io.ox/core/extensions',
     'io.ox/core/settings/util',
+    'io.ox/core/capabilities',
     'gettext!io.ox/tasks'
-], function (ExtensibleView, settings, ext, util, gt) {
+], function (ExtensibleView, settings, ext, util, capabilities, gt) {
 
     'use strict';
 
@@ -92,6 +93,7 @@ define('io.ox/tasks/settings/pane', [
                             help: 'ox.appsuite.user.sect.tasks.folder.subscribeshared.html',
                             title: gt('Subscribe to shared task folders'),
                             point: 'io.ox/core/folder/subscribe-shared-tasks-folders',
+                            noSync: !capabilities.has('caldav'),
                             sections: {
                                 public: gt('Public tasks folders'),
                                 shared: gt('Shared tasks folders'),
