597e9ca14c65a7f9fbb30938b765bddbd73aa9c7
Anne Matthes <Anne.Matthes@open-xchange.com>
2022-07-29 08:45:09 +0200

    [e2e] Fix 'Reorder appointments with labels in list view'
    Root cause: OXUIB-1676 - toolbar got uptated too often and did break the focus
    
    (cherry picked from commit 2ea4886c8be7a8d60d6ddb2d0d67bf05a518c42d)

--- ui/apps/io.ox/calendar/toolbar.js
+++ ui/apps/io.ox/calendar/toolbar.js
@@ -288,8 +288,7 @@ define('io.ox/calendar/toolbar', [
                 _.defer(function () { app.forceUpdateToolbar(getSelection(app)); });
             });
             // models receive some fields (e.g. attendees) later which could affect the toolbar
-            var update = _.debounce(function () { app.updateToolbar(getSelection(app)); }, 0);
-            app.listView.on('collection:change', update);
+            app.listView.on('toolbar:update', function () { app.updateToolbar(getSelection(app)); });
         }
     });
 
