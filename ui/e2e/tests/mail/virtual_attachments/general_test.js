639622ec2dbf65805a674f1dead1b5a232d3ee72
Julian Bäume <julian.baeume@open-xchange.com>
2023-01-17 17:31:19 +0100

    [e2e] remove broken e2e test ("backport" from 8.x stream)
    this test was broken anyway

--- ui/e2e/tests/mail/virtual_attachments/general_test.js
+++ ui/e2e/tests/mail/virtual_attachments/general_test.js
@@ -129,38 +129,6 @@ Scenario('[C83399] View all SENT attachments', async ({ I, drive }) => {
     });
 });
 
-Scenario('[C83404] Attachments can be copied', async ({ I, drive }) => {
-    await prepare();
-    I.logout();
-
-    // Login Drive and select Attachment Folder
-    I.login('app=io.ox/files');
-    drive.waitForApp();
-    I.selectFolder('My attachments');
-    I.selectFolder('My attachments');
-    within('.list-view', () => {
-        I.waitForText('testdocument.rtf');
-        I.waitForText('testdocument.odt');
-        I.waitForText('testpresentation.ppsm');
-    });
-
-    // Open Dropdown and select 'Copy' to copy last file
-    I.rightClick(locate('.list-item').withText('testpresentation.ppsm').last());
-    I.clickDropdown('Copy');
-
-    // Wait for modal and copy file to default folder 'My files'
-    I.waitForElement('.modal-dialog');
-    I.see('Copy', '.modal-footer');
-    I.click('Copy', '.modal-footer');
-    I.waitForText('File has been copied');
-
-    // Select 'My files' folder and checkfor copied file
-    I.selectFolder('My files');
-    within('.list-view', () => {
-        I.waitForText('testpresentation.ppsm');
-    });
-});
-
 Scenario('[C125297] Attachments are linked to mails', async ({ I, drive }) => {
     await prepare();
     I.logout();
