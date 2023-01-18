e1d54b2925a62e46967282b99280638f0d409883
Julian Bäume <julian.baeume@open-xchange.com>
2023-01-18 15:16:34 +0100

    [e2e] Change: create dedicated user in haveMailAccount helper
    when creating an external mail account, this now makes sure the account being used as external
    account does exist and the system has the possiblity to create the imap account as well.
    So this works explicitly with different configurations.

--- ui/e2e/tests/mail/external_accounts_test.js
+++ ui/e2e/tests/mail/external_accounts_test.js
@@ -44,7 +44,7 @@ Scenario('[C125352] No mail oauth service available', function ({ I, mail }) {
 });
 
 Scenario('[OXUIB-225] Password recovery for account passwords after password change', async ({ I, dialogs, users }) => {
-    await I.haveMailAccount({ name: 'My External', extension: 'ext' });
+    await I.haveMailAccount({ name: 'My External' });
 
     I.login();
     // Check for the external account being registered
@@ -85,7 +85,7 @@ Scenario('[OXUIB-225] Password recovery for account passwords after password cha
 
 Scenario('[OXUIB-1966] Permissions dialog is disabled for external and secondary accounts', async ({ I, mail, users }) => {
     const additionalAccount = await users.create();
-    await I.haveMailAccount({ additionalAccount, name: 'My External', extension: 'ext' });
+    await I.haveMailAccount({ additionalAccount, name: 'My External' });
     I.login();
     mail.waitForApp();
     I.waitForText('My External');
