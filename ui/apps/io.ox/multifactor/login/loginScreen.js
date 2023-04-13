c6a97945eb89154d0b0e4956f16b195867109170
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2023-04-13 14:36:19 +0200

    Fix: OXUIB-2253: Calendar appointments set to 00:00 when 2FA enabled

--- ui/apps/io.ox/multifactor/login/loginScreen.js
+++ ui/apps/io.ox/multifactor/login/loginScreen.js
@@ -20,62 +20,39 @@
  *
  */
 
-define('io.ox/multifactor/login/loginScreen', ['io.ox/core/extensions', 'io.ox/core/main/addLauncher', 'gettext!io.ox/core', 'io.ox/backbone/mini-views/dropdown',
-    'io.ox/core/main/appcontrol', 'io.ox/core/main/icons', 'io.ox/core/api/apps', 'less!io.ox/multifactor/login/style'
-], function (ext, addLauncher, gt, Dropdown) {
+define('io.ox/multifactor/login/loginScreen', [
+    'settings!io.ox/core',
+    'less!io.ox/multifactor/login/style'
+], function (settings) {
 
     'use strict';
-
-    var styleModified = false;
-
-    // Function that adds icon placeholders.  These are non functional, and need to be redrawn once complete login done.
-    function addPlaceholderIcons(node) {
-        node.append(
-            addLauncher(
-                'right',
-                $('<i class="fa fa-refresh launcher-icon" aria-hidden="true">').attr('title', gt('Refresh')),
-                function () {
-                    return $.when();
-                },
-                gt('Refresh')
-            ).attr('id', 'io-ox-refresh-icon'));
-        var a = $('<a href="#" class="dropdown-toggle f6-target" data-toggle="dropdown" tabindex="-1">').attr('title', gt('Settings')),
-            dropdown = new Dropdown({
-                tagName: 'li',
-                id: 'io-ox-topbar-settings-dropdown-icon',
-                className: 'launcher dropdown',
-                $toggle: a
-            });
-        var tempPic = $('<div class="contact-picture">').append('...');
-        a.append(tempPic);
-        node.append(dropdown.render().$el);
-    }
 
     function loadBackground() {
         $('body').append($('<iframe class="multifactorBackground" src="' + ox.serverConfig.multifactorBackground + '">'));
         document.styleSheets[0].insertRule('.modal-backdrop.in { opacity: 0.1 !important; }', 0);
-        styleModified = true;
     }
 
-    var login = {
+    return {
         create: function () {
-            if (ox.serverConfig.multifactorBackground) {  // If configured background, use that instead of toolbar
-                loadBackground();
-                return;
-            }
-            var topbar = $('#io-ox-appcontrol');
-            ext.point('io.ox/core/appcontrol').invoke('draw', topbar);  // Draw the top bar
-            $('#io-ox-core').show();  // We need to show the core if hidden
-            addPlaceholderIcons(topbar.find('#io-ox-toprightbar .taskbar'));  // Add temporary icons
+            // If configured background, use that instead of toolbar
+            if (ox.serverConfig.multifactorBackground) return loadBackground();
+            var logoFileName = settings.get('logoFileName', 'logo.png');
+            // fake topbar logo (do not use the actual toolbar here -> leads to various errors because code is required to early (no settings set etc.))
+            $('#io-ox-appcontrol').append($('<div id="io-ox-top-logo">').append(
+                $('<img>').attr({
+                    alt: ox.serverConfig.productName,
+                    src: ox.base + '/apps/themes/' + ox.theme + '/' + logoFileName
+                })
+            ));
             require(['io.ox/core/main/designs']);  // Load the users selected colors for the top bar
+            $('#io-ox-core, #io-ox-appcontrol').show(); // We need to show the core if hidden
         },
         destroy: function () {
+            if (ox.serverConfig.multifactorBackground) {
+                $('.multifactorBackground').remove();
+                document.styleSheets[0].deleteRule(0);
+            }
             $('#io-ox-appcontrol').empty();   // Wipe the temporary bar
-            $('.multifactorBackground').remove();
-            if (styleModified) document.styleSheets[0].deleteRule(0);
         }
     };
-
-    return login;
-
 });
