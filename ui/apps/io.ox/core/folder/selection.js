bf0a8074d71c3d66427f0e9ae2f78bfb36ea86f4
Frank Paczynski <frank.paczynski@open-xchange.com>
2024-01-10 14:25:56 +0100

    Revert "Fix: OXUIB-2558: Selected folder label stays truncated after folder pane enlarged"
    This reverts commit 92b5c3e177a233b49dcc3f4ca54eee8b7bd4af79.

--- ui/apps/io.ox/core/folder/selection.js
+++ ui/apps/io.ox/core/folder/selection.js
@@ -251,13 +251,23 @@ define('io.ox/core/folder/selection', [], function () {
 
         check: function (nodes) {
             if (this.view.disposed) return $();
-            return nodes.addClass('selected').attr({ 'aria-selected': true, tabindex: 0 });
+            var width = this.view.$el.width();
+            return nodes.addClass('selected')
+            .attr({ 'aria-selected': true, tabindex: 0 })
+            .find('.folder-label:first').each(function () {
+                // special handling for settings for now
+                if (nodes.length === 1 && (nodes.first().attr('data-id') && nodes.first().attr('data-id').indexOf('virtual/settings') === 0)) return;
+                var left = $(this).position().left, maxWidth = width - left - 76;
+                $(this).css('max-width', Math.max(maxWidth, 80));
+            })
+            .end();
         },
 
         uncheck: function (items) {
             items = items || this.getItems();
             items.filter('.selected')
-                .removeClass('selected').attr({ 'aria-selected': false, tabindex: -1 });
+                .removeClass('selected').attr({ 'aria-selected': false, tabindex: -1 })
+                .find('.folder-label').css('max-width', 'initial');
             return this;
         },
 
