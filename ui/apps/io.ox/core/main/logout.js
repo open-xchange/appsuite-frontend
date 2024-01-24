2f9e35ff882909a622b942e57960548a7da32c0c
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2024-01-24 12:43:13 +0100

    Fix: OXUIB-2693: Context/User level logout location config.

--- ui/apps/io.ox/core/main/logout.js
+++ ui/apps/io.ox/core/main/logout.js
@@ -252,7 +252,7 @@ define('io.ox/core/main/logout', [
             ? !settings.isConfigurable('customLocations/guestLogout') && settings.get('customLocations/guestLogout') || ox.serverConfig.guestLogoutLocation
             : !settings.isConfigurable('customLocations/logout') && settings.get('customLocations/logout') || ox.serverConfig.logoutLocation;
 
-        if (location && location.match(/^https:\/\/.*$|^\/.*$/)) {
+        if (location && location.match(/^https?:\/\/.*$|^\/.*$/)) {
             return _.url.vars(location);
         }
         return _.url.vars(ox.logoutLocation || '');
