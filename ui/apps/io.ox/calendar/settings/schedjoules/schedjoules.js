bd49848e3c704f81e89e5122844378d4c911b42c
Frank Paczynski <frank.paczynski@open-xchange.com>
2024-01-24 11:07:02 +0100

    #88: Update corporate name in AGPL license header

--- ui/apps/io.ox/calendar/settings/schedjoules/schedjoules.js
+++ ui/apps/io.ox/calendar/settings/schedjoules/schedjoules.js
@@ -1,6 +1,6 @@
 /*
  *
- * @copyright Copyright (c) OX Software GmbH, Germany <info@open-xchange.com>
+ * @copyright Copyright (c) Open-Xchange GmbH, Germany <info@open-xchange.com>
  * @license AGPL-3.0
  *
  * This code is free software: you can redistribute it and/or modify
@@ -25,7 +25,7 @@
 
 /*
 *
-* @copyright Copyright (c) OX Software GmbH, Germany <info@open-xchange.com>
+* @copyright Copyright (c) Open-Xchange GmbH, Germany <info@open-xchange.com>
 * @license AGPL-3.0
 *
 * This code is free software: you can redistribute it and/or modify
