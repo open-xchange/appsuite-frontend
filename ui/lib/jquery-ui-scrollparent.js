6cbb1579c78b35bca54e8abfabca5c760f09db36
Frank Paczynski <frank.paczynski@open-xchange.com>
2024-02-16 13:04:48 +0100

    Remove usage of jquery-ui and add content of `scroll-parent.js` to `boot.js`/`boot-stripped.js`
    This is a breaking change.

---
+++ ui/lib/jquery-ui-scrollparent.js
@@ -0,0 +1,25 @@
+/*! jQuery UI - v1.13.2 - 2024-02-16
+* http://jqueryui.com
+* Includes: scroll-parent.js
+* Copyright jQuery Foundation and other contributors; Licensed MIT */
+
+
+(function ($) {
+    $.fn.scrollParent = function( includeHidden ) {
+        var position = this.css( "position" ),
+            excludeStaticParent = position === "absolute",
+            overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/,
+            scrollParent = this.parents().filter( function() {
+                var parent = $( this );
+                if ( excludeStaticParent && parent.css( "position" ) === "static" ) {
+                    return false;
+                }
+                return overflowRegex.test( parent.css( "overflow" ) + parent.css( "overflow-y" ) +
+                    parent.css( "overflow-x" ) );
+            } ).eq( 0 );
+
+        return position === "fixed" || !scrollParent.length ?
+            $( this[ 0 ].ownerDocument || document ) :
+            scrollParent;
+    };
+})(jQuery);
