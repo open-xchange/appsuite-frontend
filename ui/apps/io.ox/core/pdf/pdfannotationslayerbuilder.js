bfc8407911e8162d33fd30adb5f99aa59ea613ea
Jonas Regier <jonas.regier@open-xchange.com>
2025-01-29 21:15:48 +0100

    fix annotationLayer render function

--- ui/apps/io.ox/core/pdf/pdfannotationslayerbuilder.js
+++ ui/apps/io.ox/core/pdf/pdfannotationslayerbuilder.js
@@ -62,21 +62,12 @@ define('io.ox/core/pdf/pdfannotationslayerbuilder', [
                 };
 
                 this.pdfPage.getAnnotations(parameters).then(function (annotations) {
-                    viewport = viewport.clone({ dontFlip: true });
-                    parameters = {
-                        viewport: viewport,
-                        div: self.div,
-                        annotations: annotations,
-                        page: self.pdfPage,
-                        linkService: self.linkService,
-                        // Path for image resources, mainly for annotation icons. Include trailing slash.
-                        imageResourcesPath: ox.abs + ox.root + '/apps/pdfjs-dist/web/images/'
-                    };
-
+                    var alayer;
+                    // TODO update does not work like this, need to find a solution
                     if (self.div) {
                         // If an annotationLayer already exists, refresh its children's
                         // transformation matrices.
-                        PDFJSLib.AnnotationLayer.update(parameters);
+                        alayer.update(parameters);
                     } else {
                         // Create an annotation layer div and render the annotations
                         // if there is at least one annotation.
@@ -84,12 +75,33 @@ define('io.ox/core/pdf/pdfannotationslayerbuilder', [
                             return;
                         }
 
+                        viewport = viewport.clone({ dontFlip: true });
                         self.div = document.createElement('div');
                         self.div.className = 'annotationLayer';
                         self.pageDiv.appendChild(self.div);
                         parameters.div = self.div;
 
-                        PDFJSLib.AnnotationLayer.render(parameters);
+                        parameters = {
+                            viewport: viewport,
+                            div: self.div,
+                            annotations: annotations,
+                            page: self.pdfPage,
+                            linkService: self.linkService,
+                            // Path for image resources, mainly for annotation icons. Include trailing slash.
+                            imageResourcesPath: ox.abs + ox.root + '/apps/pdfjs-dist/web/images/'
+                        };
+                        alayer = new PDFJSLib.AnnotationLayer({
+                            div: self.div, //                         div: self.pageDiv,
+                            page: self.pdfPage,
+                            viewport: viewport
+                        });
+
+                        // self.div = document.createElement('div');
+                        // self.div.className = 'annotationLayer';
+                        // self.pageDiv.appendChild(self.div);
+                        // parameters.div = self.div;
+
+                        alayer.render(parameters);
                         if (typeof mozL10n !== 'undefined') {
                             mozL10n.translate(self.div);
                         }
