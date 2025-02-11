589ec98d78f49fb4b44d2625d3d5f20cf907e731
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2025-02-11 11:14:28 +0100

    Fix: Update dom purify 1.0.0 -> 2.5.8

--- ui/bower_components/dompurify/src/utils.js
+++ ui/bower_components/dompurify/src/utils.js
@@ -1,14 +1,66 @@
-const { hasOwnProperty, setPrototypeOf } = Object;
-let { apply } = typeof Reflect !== 'undefined' && Reflect;
+const {
+  hasOwnProperty,
+  setPrototypeOf,
+  isFrozen,
+  getPrototypeOf,
+  getOwnPropertyDescriptor,
+} = Object;
+
+let { freeze, seal, create } = Object; // eslint-disable-line import/no-mutable-exports
+let { apply, construct } = typeof Reflect !== 'undefined' && Reflect;
 
 if (!apply) {
-  apply = function(fun, thisValue, args) {
+  apply = function (fun, thisValue, args) {
     return fun.apply(thisValue, args);
   };
 }
 
+if (!freeze) {
+  freeze = function (x) {
+    return x;
+  };
+}
+
+if (!seal) {
+  seal = function (x) {
+    return x;
+  };
+}
+
+if (!construct) {
+  construct = function (Func, args) {
+    return new Func(...args);
+  };
+}
+
+const arrayForEach = unapply(Array.prototype.forEach);
+const arrayIndexOf = unapply(Array.prototype.indexOf);
+const arrayPop = unapply(Array.prototype.pop);
+const arrayPush = unapply(Array.prototype.push);
+const arraySlice = unapply(Array.prototype.slice);
+
+const stringToLowerCase = unapply(String.prototype.toLowerCase);
+const stringToString = unapply(String.prototype.toString);
+const stringMatch = unapply(String.prototype.match);
+const stringReplace = unapply(String.prototype.replace);
+const stringIndexOf = unapply(String.prototype.indexOf);
+const stringTrim = unapply(String.prototype.trim);
+
+const regExpTest = unapply(RegExp.prototype.test);
+
+const typeErrorCreate = unconstruct(TypeError);
+
+export function unapply(func) {
+  return (thisArg, ...args) => apply(func, thisArg, args);
+}
+
+export function unconstruct(func) {
+  return (...args) => construct(func, args);
+}
+
 /* Add properties to a lookup table */
-export function addToSet(set, array) {
+export function addToSet(set, array, transformCaseFunc) {
+  transformCaseFunc = transformCaseFunc ?? stringToLowerCase;
   if (setPrototypeOf) {
     // Make 'in' and truthy checks like Boolean(set.constructor)
     // independent of any properties defined on Object.prototype.
@@ -20,10 +72,10 @@ export function addToSet(set, array) {
   while (l--) {
     let element = array[l];
     if (typeof element === 'string') {
-      const lcElement = element.toLowerCase();
+      const lcElement = transformCaseFunc(element);
       if (lcElement !== element) {
         // Config presets (e.g. tags.js, attrs.js) are immutable.
-        if (!Object.isFrozen(array)) {
+        if (!isFrozen(array)) {
           array[l] = lcElement;
         }
 
@@ -39,14 +91,72 @@ export function addToSet(set, array) {
 
 /* Shallow clone an object */
 export function clone(object) {
-  const newObject = {};
+  const newObject = create(null);
 
   let property;
   for (property in object) {
-    if (apply(hasOwnProperty, object, [property])) {
+    if (apply(hasOwnProperty, object, [property]) === true) {
       newObject[property] = object[property];
     }
   }
 
   return newObject;
 }
+
+/* IE10 doesn't support __lookupGetter__ so lets'
+ * simulate it. It also automatically checks
+ * if the prop is function or getter and behaves
+ * accordingly. */
+function lookupGetter(object, prop) {
+  while (object !== null) {
+    const desc = getOwnPropertyDescriptor(object, prop);
+    if (desc) {
+      if (desc.get) {
+        return unapply(desc.get);
+      }
+
+      if (typeof desc.value === 'function') {
+        return unapply(desc.value);
+      }
+    }
+
+    object = getPrototypeOf(object);
+  }
+
+  function fallbackValue(element) {
+    console.warn('fallback value for', element);
+    return null;
+  }
+
+  return fallbackValue;
+}
+
+export {
+  // Array
+  arrayForEach,
+  arrayIndexOf,
+  arrayPop,
+  arrayPush,
+  arraySlice,
+  // Object
+  freeze,
+  getPrototypeOf,
+  getOwnPropertyDescriptor,
+  hasOwnProperty,
+  isFrozen,
+  setPrototypeOf,
+  seal,
+  // RegExp
+  regExpTest,
+  // String
+  stringIndexOf,
+  stringMatch,
+  stringReplace,
+  stringToLowerCase,
+  stringToString,
+  stringTrim,
+  // Errors
+  typeErrorCreate,
+  // Other
+  lookupGetter,
+};
