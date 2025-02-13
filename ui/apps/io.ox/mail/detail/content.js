b766573ab973149ad1db73acd1287c2241b73881
Daniel Dickhaus <daniel.dickhaus@open-xchange.com>
2025-02-13 12:16:49 +0100

    Fix: Vulnerability in HTML mails, XSS executing malicious JS through <img> tab
    uses code from  5dad3293913ac299ea7a8926e81212cd35a0eb23 and 67bfd61df7727bc4ed654eceee1ea8dfbdfb7ec0
    
    closes: #895

--- ui/apps/io.ox/mail/detail/content.js
+++ ui/apps/io.ox/mail/detail/content.js
@@ -488,6 +488,10 @@ define('io.ox/mail/detail/content',
                 ext.point('io.ox/mail/detail/source').invoke('process', $(), baton);
 
                 if (baton.isHTML) {
+                    baton.source = sanitizer.sanitize({
+                        content_type: 'text/html',
+                        content: baton.source
+                    }).content;
                     // robust constructor for large HTML -- no jQuery here to avoid its caches
                     content = document.createElement('DIV');
                     content.className = 'content noI18n';
